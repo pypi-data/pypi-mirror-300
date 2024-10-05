038fdcd088ca7e7f0f9c0eefa618fe7e9ea10d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:06:30 +0000

    Add package treinta-ms-utils/1.4.2/treinta_ms_utils-1.4.2.tar.gz

---
+++ packages/treinta-ms-utils/treinta_ms_utils-1.4.2.tar.gz/treinta_ms_utils-1.4.2/treinta_ms_utils/__init__.py
@@ -0,0 +1,11 @@
+
+from .core import (
+    create_log,
+    update_log,
+    print_update_log,
+    delete_log,
+    get_log,
+    publish_message_sns,
+    configure_datadog,
+    log_with_datadog
+)
