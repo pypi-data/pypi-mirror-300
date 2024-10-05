5f3a22d77360bab217be993a3760f41f5054546e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:49 +0000

    Add package nexaai/0.0.8.6.1/nexaai-0.0.8.6.1.tar.gz

---
+++ packages/nexaai/nexaai-0.0.8.6.1.tar.gz/nexaai-0.0.8.6.1/dependency/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 3644;
+char const *LLAMA_COMMIT = "3a355263";
+char const *LLAMA_COMPILER = "gcc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
