919ea259610b955f668930842214087d44fab1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 06:03:24 +0000

    Add package node-hermes-core/1.0.0.post1/node_hermes_core-1.0.0.post1.tar.gz

---
+++ packages/node-hermes-core/node_hermes_core-1.0.0.post1.tar.gz/node_hermes_core-1.0.0.post1/README.md
@@ -0,0 +1,23 @@
+# Hermes Core
+
+```bash
+hermes-core schema -o schema.json --config test/yaml/basic_config.hermes
+```
+
+```bash
+hermes-core run test/yaml/basic_config.hermes
+```
+
+
+# push to pypi (poetry)
+```bash
+poetry build
+poetry publish
+```
+
+pipy auth
+```bash
+c <token>
+```
+
+poetry config pypi-token.pypi pypi-AgEIcHlwaS5vcmcCJGY0ZGE2OWZjLTY1ZGItNDExNS04OTIzLTkyMDcwNzdmZTM2MwACKlszLCJkYWQ0MzczMC1iN2E5LTQxZDktYWNjOS01OWNlMjhmMDJmZGUiXQAABiC-pBBqXV7jAEN9vJwT8EEDNmCAXNPaDtEgJi70nPRZPg
---
+++ packages/node-hermes-core/node_hermes_core-1.0.0.post1.tar.gz/node_hermes_core-1.0.0.post1/src/node_hermes_core/depencency/node_dependency.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING, Type
+
+if TYPE_CHECKING:
+    from ..nodes.generic_node import GenericNode
+
+import logging
+from typing import List
+
+
+class NodeDependency:
+    nodes: "List[GenericNode]"
+
+    def __init__(self, name: str, config: "GenericNode.Config | str | None", reference: "Type|None"):
+        self.name = name
+        self.config = config
+        self.ref = reference
+        self.nodes = []
+
+    def add_node(self, node: "GenericNode"):
+        logging.info(f"Adding '{node}'as dependency for '{self.name}'")
+        if len(self.nodes) > 0:
+            raise ValueError("Node already added")
+
+        if node not in self.nodes:
+            if self.ref is not None and not isinstance(node, self.ref):
+                raise ValueError(f"Node {node} is not of type {self.ref}")
+            self.nodes.append(node)
+            # self.dependency_change.emit()
+
+    def get_dependency(self):
+        assert len(self.nodes) == 1, "More than one node found"
+        return self.nodes[0]
