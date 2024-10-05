a72f827377d6f6bef68b060b244ed9eb4489079e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:07:47 +0000

    Add package mtmai-client/0.3.1048/mtmai_client-0.3.1048-py3-none-any.whl

---
+++ packages/mtmai-client/mtmai_client-0.3.1048-py3-none-any.whl/mtmai_client/models/__init__.py
@@ -0,0 +1,141 @@
+"""Contains all the data models used in inputs/outputs"""
+
+from .agent_bootstrap import AgentBootstrap
+from .agent_chat_message_request import AgentChatMessageRequest
+from .agent_view_type import AgentViewType
+from .blog_post_create_req import BlogPostCreateReq
+from .blog_post_create_response import BlogPostCreateResponse
+from .blog_post_detail_response import BlogPostDetailResponse
+from .blog_post_item import BlogPostItem
+from .blog_post_list_response import BlogPostListResponse
+from .blog_post_update_request import BlogPostUpdateRequest
+from .blog_post_update_response import BlogPostUpdateResponse
+from .body_auth_login_access_token import BodyAuthLoginAccessToken
+from .chat_bot_ui_state_response import ChatBotUiStateResponse
+from .chat_messages_item import ChatMessagesItem
+from .chat_messages_item_artifacts_type_0_item import ChatMessagesItemArtifactsType0Item
+from .chat_messages_item_props_type_0 import ChatMessagesItemPropsType0
+from .chat_messages_response import ChatMessagesResponse
+from .chat_profile import ChatProfile
+from .chat_profile_starters_type_0_item import ChatProfileStartersType0Item
+from .common_form_data import CommonFormData
+from .common_form_field import CommonFormField
+from .copilot_screen import CopilotScreen
+from .dash_config import DashConfig
+from .dash_nav_item import DashNavItem
+from .dash_nav_item_variant_type_0 import DashNavItemVariantType0
+from .get_threads_request import GetThreadsRequest
+from .http_validation_error import HTTPValidationError
+from .input_widget_base import InputWidgetBase
+from .input_widget_base_items_item import InputWidgetBaseItemsItem
+from .input_widget_base_options_type_0 import InputWidgetBaseOptionsType0
+from .input_widget_base_type_type_0 import InputWidgetBaseTypeType0
+from .list_site_hosts_response import ListSiteHostsResponse
+from .message import Message
+from .new_password import NewPassword
+from .page_meta_author import PageMetaAuthor
+from .page_meta_response import PageMetaResponse
+from .pagination import Pagination
+from .read_file_req import ReadFileReq
+from .run_bash_req import RunBashReq
+from .search_index_public import SearchIndexPublic
+from .search_index_response import SearchIndexResponse
+from .search_request import SearchRequest
+from .site_host_create_request import SiteHostCreateRequest
+from .site_host_create_response import SiteHostCreateResponse
+from .site_host_delete_response import SiteHostDeleteResponse
+from .site_host_item_public import SiteHostItemPublic
+from .site_host_update_request import SiteHostUpdateRequest
+from .site_host_update_response import SiteHostUpdateResponse
+from .tag_item_public import TagItemPublic
+from .tag_list_response import TagListResponse
+from .task_form_request import TaskFormRequest
+from .task_form_response import TaskFormResponse
+from .text_2_image_request import Text2ImageRequest
+from .theme import Theme
+from .thread_filter import ThreadFilter
+from .thread_filter_feedback_type_0 import ThreadFilterFeedbackType0
+from .thread_form import ThreadForm
+from .thread_form_display import ThreadFormDisplay
+from .thread_form_variant import ThreadFormVariant
+from .thread_ui_state import ThreadUIState
+from .thread_ui_state_fab_display_position_type_0 import ThreadUIStateFabDisplayPositionType0
+from .thread_ui_state_input_position_type_0 import ThreadUIStateInputPositionType0
+from .token import Token
+from .types_response import TypesResponse
+from .update_password import UpdatePassword
+from .user_register import UserRegister
+from .user_update_me import UserUpdateMe
+from .validation_error import ValidationError
+from .workspace import Workspace
+
+__all__ = (
+    "AgentBootstrap",
+    "AgentChatMessageRequest",
+    "AgentViewType",
+    "BlogPostCreateReq",
+    "BlogPostCreateResponse",
+    "BlogPostDetailResponse",
+    "BlogPostItem",
+    "BlogPostListResponse",
+    "BlogPostUpdateRequest",
+    "BlogPostUpdateResponse",
+    "BodyAuthLoginAccessToken",
+    "ChatBotUiStateResponse",
+    "ChatMessagesItem",
+    "ChatMessagesItemArtifactsType0Item",
+    "ChatMessagesItemPropsType0",
+    "ChatMessagesResponse",
+    "ChatProfile",
+    "ChatProfileStartersType0Item",
+    "CommonFormData",
+    "CommonFormField",
+    "CopilotScreen",
+    "DashConfig",
+    "DashNavItem",
+    "DashNavItemVariantType0",
+    "GetThreadsRequest",
+    "HTTPValidationError",
+    "InputWidgetBase",
+    "InputWidgetBaseItemsItem",
+    "InputWidgetBaseOptionsType0",
+    "InputWidgetBaseTypeType0",
+    "ListSiteHostsResponse",
+    "Message",
+    "NewPassword",
+    "PageMetaAuthor",
+    "PageMetaResponse",
+    "Pagination",
+    "ReadFileReq",
+    "RunBashReq",
+    "SearchIndexPublic",
+    "SearchIndexResponse",
+    "SearchRequest",
+    "SiteHostCreateRequest",
+    "SiteHostCreateResponse",
+    "SiteHostDeleteResponse",
+    "SiteHostItemPublic",
+    "SiteHostUpdateRequest",
+    "SiteHostUpdateResponse",
+    "TagItemPublic",
+    "TagListResponse",
+    "TaskFormRequest",
+    "TaskFormResponse",
+    "Text2ImageRequest",
+    "Theme",
+    "ThreadFilter",
+    "ThreadFilterFeedbackType0",
+    "ThreadForm",
+    "ThreadFormDisplay",
+    "ThreadFormVariant",
+    "ThreadUIState",
+    "ThreadUIStateFabDisplayPositionType0",
+    "ThreadUIStateInputPositionType0",
+    "Token",
+    "TypesResponse",
+    "UpdatePassword",
+    "UserRegister",
+    "UserUpdateMe",
+    "ValidationError",
+    "Workspace",
+)
