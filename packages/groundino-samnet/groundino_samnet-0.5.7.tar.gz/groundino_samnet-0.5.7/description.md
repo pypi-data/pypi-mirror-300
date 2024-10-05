eba0722daf3c3a0be9d52e6be51c9086eb5cae29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:05:18 +0000

    Add package groundino-samnet/0.5.7/groundino_samnet-0.5.7.tar.gz

---
+++ packages/groundino-samnet/groundino_samnet-0.5.7.tar.gz/groundino_samnet-0.5.7/description.md
@@ -0,0 +1,42 @@
+# DiagAssistAI
+![GitHub License](https://img.shields.io/github/license/WilhelmBuitrago/DiagAssistAI)
+![Static Badge](https://img.shields.io/badge/Python-3.10.12-blue?logo=python)
+![PyPI - Version](https://img.shields.io/pypi/v/groundino-samnet?link=https%3A%2F%2Fpypi.org%2Fproject%2Fgroundino-samnet%2F)
+
+![modelo](https://raw.githubusercontent.com/WilhelmBuitrago/DiagAssistAI/main/.asset/model_p.png)
+
+# Installation
+
+GSamNetwork requires `python==3.10.12`, as well as `torch==2.4.0`.
+
+## Installing PyTorch
+
+Make sure to check the version of Python that is compatible with your CUDA version at the following link: [Installing torch locally](https://pytorch.org/get-started/locally/).
+
+In this project, **CUDA 12.1** was used. You can install PyTorch with support for CUDA 12.1 using the following command:
+
+``
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+``
+
+## Installing Package
+
+To install the package, use:
+
+``
+pip install groundino-samnet
+``
+
+## Version 0.5.7
+
+### Fixed
+
+Fixed: Error to load .pt file for MobileSAM model
+
+Fixed: Error implement MobileSAM for model.py
+
+### Added
+
+Added: Options for only points propmt for SAM
+
+
---
+++ packages/groundino-samnet/groundino_samnet-0.5.7.tar.gz/groundino_samnet-0.5.7/setup.py
@@ -0,0 +1,141 @@
+import os
+import glob
+import sys  
+os.environ['TORCH_CUDA_ARCH_LIST'] = '8.0'
+import subprocess
+try:
+    import torch
+except:
+    try:
+        subprocess.check_call([
+            sys.executable, '-m', 'pip', 'install', 
+            'torch', 'torchvision', 'torchaudio', 
+            '--index-url', 'https://download.pytorch.org/whl/cu121'
+        ])
+        print("Instalación completada con CUDA 12.1.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error durante la instalación: {e}")
+
+from torch.utils.cpp_extension import CUDA_HOME,CppExtension,CUDAExtension,BuildExtension
+from setuptools.command.install import install
+from setuptools import find_namespace_packages
+import pathlib
+
+from setuptools import setup, find_packages
+
+REQUIRED_PACKAGES = [
+    "numpy==1.26.4",
+    "transformers==4.42.4",
+    "huggingface_hub==0.23.5",
+    "addict==2.4.0",
+    "opencv-python==4.10.0.84",
+    "pycocotools",
+    "yapf",
+    "timm",
+    "supervision==0.22.0",
+    "tqdm>=4.66.1",
+    "scikit-learn",
+    "hydra-core>=1.3.2",
+    "iopath>=0.1.10",
+    "ninja",
+    "kaggle",
+    "pandas"
+]
+
+def get_extensions():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    extensions_dir = os.path.join("src", "groundingdino", "models", "GroundingDINO", "csrc")
+    extension_dir_sam = os.path.join("src","segment_anything2","csrc")
+    srcs_sam2 = glob.glob(os.path.join("src","segment_anything2","csrc","*.cu"),recursive=True)
+
+    #main_source = os.path.join(extensions_dir, "vision.cpp")
+
+    sources = glob.glob(os.path.join(extensions_dir, "**", "*.cpp"),recursive=True)
+    source_cuda = glob.glob(os.path.join(extensions_dir, "**", "*.cu"),recursive=True) 
+    extension = CppExtension
+
+    extra_compile_args = {"cxx": []}
+    define_macros = []
+
+    if CUDA_HOME is not None and (torch.cuda.is_available() or "TORCH_CUDA_ARCH_LIST" in os.environ):
+        print("Compiling with CUDA")
+        extension = CUDAExtension
+        sources += source_cuda
+
+        define_macros += [("WITH_CUDA", None)]
+        extra_compile_args["nvcc"] = [
+            "-DCUDA_HAS_FP16=1",
+            "-D__CUDA_NO_HALF_OPERATORS__",
+            "-D__CUDA_NO_HALF_CONVERSIONS__",
+            "-D__CUDA_NO_HALF2_OPERATORS__",
+            "-allow-unsupported-compiler"
+        ]
+
+    else:
+        print("Compiling without CUDA")
+        define_macros += [("WITH_HIP", None)]
+        extra_compile_args["nvcc"] = []
+        return None
+
+    sources = [s for s in sources]
+    include_dirs = [extensions_dir] 
+    ext_modules = [
+        extension(
+            "groundingdino._C",
+            sources,
+            include_dirs=include_dirs,
+            define_macros=define_macros,
+            extra_compile_args=extra_compile_args,
+        ),
+        extension(
+            "segment_anything2._C", 
+            sources=srcs_sam2, 
+            include_dirs=[extension_dir_sam],
+            define_macros=define_macros,
+            extra_compile_args=extra_compile_args
+        )
+    ]
+
+    return ext_modules
+
+def build_extensions():
+
+
+    with open("LICENSE", "r", encoding="utf-8") as f:
+        license = f.read()
+
+    HERE = pathlib.Path(__file__).parent
+    README = (HERE / "description.md").read_text()
+    setup(
+        name="groundino_samnet",
+        version="0.5.7",
+        author="Wilhelm David Buitrago Garcia",
+        url="https://github.com/ladmepaz/GSAMnet",
+        description="A SAM model with GroundingDINO model for feet segmentation",
+        long_description=README,
+        long_description_content_type="text/markdown",
+        license=license,
+        package_dir={"": "src"},
+        packages=find_namespace_packages(where="src", exclude=["segment_anything2/csrc"]),
+        include_package_data=True,
+        package_data={
+            "": ["segment_anything2/sam2_config/*.yaml"],
+            "groundino_samnet": ["description.md"],
+            "": ["mobilesamv2/weights/*.pt"]
+        },
+        install_requires=REQUIRED_PACKAGES,
+        ext_modules=get_extensions(),
+        cmdclass={"build_ext": BuildExtension},
+        #python_requires='==3.10',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.10"
+        ],
+        project_urls={
+            "GSAMnet": "https://github.com/ladmepaz/GSAMnet"
+        },
+    )
+
+
+if __name__ == "__main__":
+    build_extensions()
