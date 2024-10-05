3bd5aaafa77b8b269a6af0c5d902a8b6e9f76a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 04:03:10 +0000

    Add package aa-structures/2.15.0/aa_structures-2.15.0-py3-none-any.whl

---
+++ packages/aa-structures/aa_structures-2.15.0-py3-none-any.whl/structures/constants.py
@@ -0,0 +1,54 @@
+"""Constants for Structures."""
+
+from enum import IntEnum
+
+
+# Eve enums
+class EveAttributeId(IntEnum):
+    """An EVE attribute ID."""
+
+    SQUADRON_SIZE = 2215
+
+
+class EveCategoryId(IntEnum):
+    """An EVE category ID."""
+
+    ORBITAL = 46
+    STARBASE = 23
+    STRUCTURE = 65
+
+
+class EveGroupId(IntEnum):
+    """An EVE group ID."""
+
+    CITADEL = 1657
+    CONTROL_TOWER = 365
+    ENGINEERING_COMPLEX = 1404
+    FUEL_BLOCK = 1136
+    ICE_PRODUCT = 423
+    REFINERY = 1406
+    PLANET = 7
+    STRUCTURE_CITADEL_SERVICE_MODULE = 1321
+    UNCOMMON_MOON_ASTEROIDS = 1921
+    QUANTUM_CORES = 4086
+
+
+class EveTypeId(IntEnum):
+    """An EVE type ID."""
+
+    ASTRAHUS_UPWELL_QUANTUM_CORE = 56201
+    CALDARI_CONTROL_TOWER = 16213
+    CUSTOMS_OFFICE = 2233
+    IHUB = 32458
+    JUMP_GATE = 35841
+    LIQUID_OZONE = 16273
+    ORBITAL_SKYHOOK = 81080
+    NITROGEN_FUEL_BLOCK = 4051
+    STRONTIUM = 16275
+    TCU = 32226
+
+
+class EveCorporationId(IntEnum):
+    """An EVE corporation ID."""
+
+    DED = 1000137
