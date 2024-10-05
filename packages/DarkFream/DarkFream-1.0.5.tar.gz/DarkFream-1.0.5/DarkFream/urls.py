07aaf9a7665dd178b7a70b0f8d0c08e1dc184968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:04:13 +0000

    Add package DarkFream/1.0.5/DarkFream-1.0.5.tar.gz

---
+++ packages/DarkFream/DarkFream-1.0.5.tar.gz/DarkFream-1.0.5/DarkFream/urls.py
@@ -0,0 +1,43 @@
+from .template import LazyImport
+
+
+def path(path, view, name=None):
+    """
+    Returns a dictionary containing information about the route.
+
+    Args:
+        path (str): Path URL-адреса.
+        view (function): The function that handles the request to this path.
+        name (str, optional): Route name. Defaults to None.
+        methods (list, optional): List of HTTP methods supported by the route. Defaults to None.
+
+    Returns:
+        dict: Dictionary with keys 'path', 'view', 'name' and 'methods'.
+
+    Example:
+        >>> path('/home', my_view, 'home_page')
+        {'path': '/home', 'view': my_view, 'name': 'home_page', 'methods': None}
+        >>> path('', my_view, methods=['GET', 'POST'])
+        {'path': '', 'view': my_view, 'methods': ['GET', 'POST']}
+    """
+    if not path.startswith('/'):
+        path = '/' + path
+    return {'path': path, 'view': view, 'name': name}
+
+def include(path_url):
+    """
+    Returns a list of routes defined in the module specified in path_url.
+
+    Args:
+        path_url (str): Path to the module containing routes.
+
+    Returns:
+        list: List of routes defined in the module.
+
+    Example:
+        >>> include('myapp.urls')
+        [{'path': '/home', 'view': my_view, 'name': 'home_page'},
+         {'path': '/about', 'view': about_view, 'name': 'about_page'}]
+    """
+    urlpatterns = LazyImport(f'{path_url}', 'urlpatterns')
+    return urlpatterns
