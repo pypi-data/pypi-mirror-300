415961415e3b89375f4dae266b52d32c0f70d1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 04:04:04 +0000

    Add package mi-tabletqt/1.0.1/mi_tabletqt-1.0.1-py3-none-any.whl

---
+++ packages/mi-tabletqt/mi_tabletqt-1.0.1-py3-none-any.whl/tabletqt/graphics/image.py
@@ -0,0 +1,66 @@
+""" image.py -- Image element """
+
+import logging
+import tabletqt.element as element
+from tabletqt.geometry_types import Position, Rect_Size
+from tabletqt.exceptions import TabletBoundsExceeded
+
+from pathlib import Path
+from PyQt6.QtWidgets import QGraphicsPixmapItem
+from PyQt6.QtGui import QPixmap
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from tabletqt.layer import Layer
+
+_logger = logging.getLogger(__name__)
+
+class ImageE:
+    """
+    Manage the loading and rendering of Image Elements
+    """
+    @classmethod
+    def add(cls, layer: 'Layer', resource_path: Path, lower_left: Position, size: Rect_Size):
+        """
+        Adds the image
+
+        :param layer:
+        :param size:
+        :param resource_path: Path to an image file
+        :param lower_left:  Lower left corner of the image in Cartesian coordinates
+        """
+        # Flip lower left corner to device coordinates
+        try:
+            ll_dc = layer.Tablet.to_dc(Position(x=lower_left.x, y=lower_left.y))
+        except TabletBoundsExceeded:
+            _logger.warning(f"Lower left corner of image [{resource_path.name}] is outside Tablet draw region")
+            return
+
+        # Use upper left corner instead
+        ul = Position(x=ll_dc.x, y=ll_dc.y - size.height)
+
+        # Add it to the list
+        layer.Images.append(element.Image(resource_path=resource_path, upper_left=ul, size=size))
+        _logger.info(f'View>> Layer {layer.Name} registered resource at: {resource_path}')
+
+    @classmethod
+    def render(cls, layer: 'Layer'):
+        """
+        Render the Image Elements
+
+        :param layer: Draw on this Layer
+        """
+        for i in layer.Images:
+            if not i.resource_path.exists():
+                _logger.error(f'Image file [{i.resource_path}] not found')
+                continue
+
+            pixmap = QPixmap(str(i.resource_path))
+            if pixmap.isNull():
+                _logger.error(f'Image file [{i.resource_path}] could not be loaded as pixmap')
+                continue
+
+            pix_item = QGraphicsPixmapItem(pixmap)
+            pix_item.setPos(i.upper_left.x, i.upper_left.y)
+            layer.Scene.addItem(pix_item)
+            _logger.info(f"> Image at: ({i.upper_left.x}, {i.upper_left.y})")
---
+++ packages/mi-tabletqt/mi_tabletqt-1.0.1-py3-none-any.whl/tabletqt/graphics/polygon_se.py
@@ -0,0 +1,67 @@
+""" polygon_se.py -- Polygon Shape Element """
+
+import logging
+import tabletqt.element as element
+from tabletqt.geometry_types import Position
+
+from PyQt6.QtWidgets import QGraphicsPolygonItem
+from PyQt6.QtCore import QPointF
+from PyQt6.QtGui import QPolygonF
+from typing import TYPE_CHECKING, List
+from tabletqt.graphics.crayon_box import CrayonBox
+from tabletqt.graphics.line_segment import LineSegment
+
+if TYPE_CHECKING:
+    from tabletqt.layer import Layer
+
+_logger = logging.getLogger(__name__)
+class PolygonSE:
+    """
+    Manage the rendering of Polygon Shape Elements
+    """
+
+    @classmethod
+    def add(cls, layer: 'Layer', asset: str, vertices: List[Position]):
+        """
+        Add a closed polygon to the layer
+
+        :param layer: Draw on this layer
+        :param asset: Used to determine draw style
+        :param vertices: Polygon vertices in Tablet coordinates
+        """
+        # Flip each position to device coordinates
+        device_vertices = [layer.Tablet.to_dc(v) for v in vertices]
+
+        layer.Polygons.append(element.Polygon(
+            vertices= device_vertices,
+            border_style=layer.Presentation.Shape_presentation[asset],
+            fill=layer.Presentation.Closed_shape_fill[asset]
+        ))
+
+    @classmethod
+    def add_open(cls, layer: 'Layer', asset: str, vertices: List[Position]):
+        """
+        Add all of the line segments necessary to draw the polygon to our list of line segments
+
+        :param layer: Draw on this layer
+        :param asset: Used to look up the line style
+        :param vertices: A sequences of 2 or more vertices
+        """
+        for v1, v2 in zip(vertices, vertices[1:]):
+            assert len(vertices) > 1, "Open polygon has less than two vertices"
+            LineSegment.add(layer=layer, asset=asset, from_here=v1, to_there=v2)
+
+    @classmethod
+    def render(cls, layer: 'Layer'):
+        """Draw the closed non-rectangular shapes"""
+        for p in layer.Polygons:
+            _logger.info(f"> Polygon at: [{p.vertices}]")
+
+            pverts = [QPointF(*x) for x in p.vertices]
+            polygon = QPolygonF(pverts)
+            poly_item = QGraphicsPolygonItem(polygon)
+
+            # Set pen and brush
+            CrayonBox.choose_crayons(item=poly_item, border_style=p.border_style, fill=p.fill)
+
+            layer.Scene.addItem(poly_item)
