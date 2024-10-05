4988d58ad8f1eedae1a174858c9467314804965a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 06:03:13 +0000

    Add package agentdesk/0.2.98/agentdesk-0.2.98.tar.gz

---
+++ packages/agentdesk/agentdesk-0.2.98.tar.gz/agentdesk-0.2.98/agentdesk/__init__.py
@@ -0,0 +1,11 @@
+from .device import ConnectConfig, Desktop, ProvisionConfig, SimpleDesktop
+from .server.models import V1DesktopInstance, V1ProviderData
+
+__all__ = [
+    "Desktop",
+    "SimpleDesktop",
+    "ProvisionConfig",
+    "ConnectConfig",
+    "V1DesktopInstance",
+    "V1ProviderData",
+]
