10e2e5bc386d989773827f59a2b7ca6feb298fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:03:53 +0000

    Add package tjax/1.1.0/tjax-1.1.0.tar.gz

---
+++ packages/tjax/tjax-1.1.0.tar.gz/tjax-1.1.0/tjax/dataclasses.py
@@ -0,0 +1,14 @@
+# noqa: A005
+from ._src.dataclasses.dataclass import DataclassInstance, TDataclassInstance, dataclass
+from ._src.dataclasses.helpers import as_shallow_dict, field
+
+__all__ = ['DataclassInstance', 'TDataclassInstance', 'as_shallow_dict', 'dataclass', 'field']
+
+try:
+    import flax
+except ImportError:
+    pass
+else:
+    if flax.__version__ > '0.8':
+        from ._src.dataclasses.flax import DataClassModule, module_field
+        __all__ += ['DataClassModule', 'module_field']
