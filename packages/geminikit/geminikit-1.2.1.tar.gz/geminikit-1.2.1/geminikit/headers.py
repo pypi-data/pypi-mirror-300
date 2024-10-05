da087b515fc76e846d63e206c56ae7b877679308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:03:45 +0000

    Add package geminikit/1.2.1/geminikit-1.2.1.tar.gz

---
+++ packages/geminikit/geminikit-1.2.1.tar.gz/geminikit-1.2.1/geminikit/headers.py
@@ -0,0 +1,85 @@
+header_img_up = {
+    "X-Same-Domain": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    "Origin": "https://gemini.google.com",
+    "Referer": "https://gemini.google.com/",
+    "X-Goog-Upload-Command": "upload, finalize",
+    "X-Goog-Upload-Offset": "0",
+    "Content-Type": "application/octet-stream",
+    "X-Tenant-Id": "bard-storage"
+}
+
+header_img_int = {
+    'Authorization': 'Basic c2F2ZXM6cyNMdGhlNmxzd2F2b0RsN3J1d1U=',
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Dnt': '1',
+    'Push-Id': 'feeds/mcudyrk2a4khkz',
+    'Referer': 'https://gemini.google.com/',
+    'Sec-Ch-Ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'Sec-Ch-Ua-Arch': '',
+    'Sec-Ch-Ua-Bitness': '64',
+    'Sec-Ch-Ua-Full-Version': '114.0.5735.198',
+    'Sec-Ch-Ua-Full-Version-List': '"Not.A/Brand";v="8.0.0.0", "Chromium";v="114.0.5735.198", "Google Chrome";v="114.0.5735.198"',
+    'Sec-Ch-Ua-Mobile': '?1',
+    'Sec-Ch-Ua-Model': 'Nexus 5',
+    'Sec-Ch-Ua-Platform': 'Android',
+    'Sec-Ch-Ua-Platform-Version': '6.0',
+    'Sec-Ch-Ua-Wow64': '?0',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36',
+    'X-Goog-Upload-Command': 'start',
+
+    'X-Goog-Upload-Protocol': 'resumable',
+    'X-Tenant-Id': 'bard-storage',
+}
+
+
+header_main = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'priority': 'u=0, i',
+    'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    'sec-ch-ua-arch': '""',
+    'sec-ch-ua-bitness': '"64"',
+    'sec-ch-ua-form-factors': '',
+    'sec-ch-ua-full-version': '"129.0.6668.58"',
+    'sec-ch-ua-full-version-list': '"Google Chrome";v="129.0.6668.58", "Not=A?Brand";v="8.0.0.0", "Chromium";v="129.0.6668.58"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"Nexus 5"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"6.0"',
+    'sec-ch-ua-wow64': '?0',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36',
+    'x-browser-channel': 'stable',
+    'x-browser-copyright': 'Copyright 2024 Google LLC. All rights reserved.',
+    'x-browser-validation': 'vrdrOb79TZVKQF8a3V5ixRDvbQE=',
+    'x-browser-year': '2024',
+    'x-client-data': 'CIy2yQEIpLbJAQipncoBCLOKywEIkqHLAQiHoM0BCI2nzQEIwbbOAQj6wM4BCN3CzgEIy8TOAQiNxs4BCJLGzgEYnLHOARjDvc4B',
+}
+
+header_gen = {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Dnt': '1',
+    'Referer': 'https://gemini.google.com/',
+    'Sec-Ch-Ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'Sec-Ch-Ua-Arch': '""',
+    'Sec-Ch-Ua-Bitness': '"64"',
+    'Sec-Ch-Ua-Full-Version': '"114.0.5735.198"',
+    'Sec-Ch-Ua-Full-Version-List': '"Not.A/Brand";v="8.0.0.0", "Chromium";v="114.0.5735.198", "Google Chrome";v="114.0.5735.198"',
+    'Sec-Ch-Ua-Mobile': '?1',
+    'Sec-Ch-Ua-Model': '"Nexus 5"',
+    'Sec-Ch-Ua-Platform': '"Android"',
+    'Sec-Ch-Ua-Platform-Version': '"6.0"',
+    'Sec-Ch-Ua-Wow64': '?0',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36',
+    'X-Same-Domain': '1'
+}
+
+
+
