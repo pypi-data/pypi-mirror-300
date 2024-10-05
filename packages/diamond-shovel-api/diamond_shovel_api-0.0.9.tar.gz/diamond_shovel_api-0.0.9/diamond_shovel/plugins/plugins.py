9113ebb091080f538f4786ceb9a71bb2e966fb2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:02 +0000

    Add package diamond-shovel-api/0.0.9/diamond_shovel_api-0.0.9.tar.gz

---
+++ packages/diamond-shovel-api/diamond_shovel_api-0.0.9.tar.gz/diamond_shovel_api-0.0.9/diamond_shovel/plugins/plugins.py
@@ -0,0 +1,22 @@
+import configparser
+import io
+import pathlib
+from contextlib import contextmanager
+from typing import Generator
+
+
+class PluginInitContext:
+    @property
+    def config(self) -> configparser.ConfigParser:
+        ...
+
+    @property
+    def data_folder(self) -> pathlib.Path:
+        ...
+
+    @contextmanager
+    def open_resource(self, name: str) -> Generator[io.IOBase, None, None]:
+        ...
+
+    def extract_resource(self, name: str, replace: bool = False) -> None:
+        ...
