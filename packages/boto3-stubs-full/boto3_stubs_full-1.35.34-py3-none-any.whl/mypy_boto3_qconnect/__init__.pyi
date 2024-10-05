206ddc92ad2396de705df2d9c1877d139c618ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:08 +0000

    Add package boto3-stubs-full/1.35.34/boto3_stubs_full-1.35.34-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.35.34-py3-none-any.whl/mypy_boto3_qconnect/__init__.pyi
@@ -0,0 +1,72 @@
+"""
+Main interface for qconnect service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_qconnect import (
+        Client,
+        ListAssistantAssociationsPaginator,
+        ListAssistantsPaginator,
+        ListContentAssociationsPaginator,
+        ListContentsPaginator,
+        ListImportJobsPaginator,
+        ListKnowledgeBasesPaginator,
+        ListQuickResponsesPaginator,
+        QConnectClient,
+        QueryAssistantPaginator,
+        SearchContentPaginator,
+        SearchQuickResponsesPaginator,
+        SearchSessionsPaginator,
+    )
+
+    session = Session()
+    client: QConnectClient = session.client("qconnect")
+
+    list_assistant_associations_paginator: ListAssistantAssociationsPaginator = client.get_paginator("list_assistant_associations")
+    list_assistants_paginator: ListAssistantsPaginator = client.get_paginator("list_assistants")
+    list_content_associations_paginator: ListContentAssociationsPaginator = client.get_paginator("list_content_associations")
+    list_contents_paginator: ListContentsPaginator = client.get_paginator("list_contents")
+    list_import_jobs_paginator: ListImportJobsPaginator = client.get_paginator("list_import_jobs")
+    list_knowledge_bases_paginator: ListKnowledgeBasesPaginator = client.get_paginator("list_knowledge_bases")
+    list_quick_responses_paginator: ListQuickResponsesPaginator = client.get_paginator("list_quick_responses")
+    query_assistant_paginator: QueryAssistantPaginator = client.get_paginator("query_assistant")
+    search_content_paginator: SearchContentPaginator = client.get_paginator("search_content")
+    search_quick_responses_paginator: SearchQuickResponsesPaginator = client.get_paginator("search_quick_responses")
+    search_sessions_paginator: SearchSessionsPaginator = client.get_paginator("search_sessions")
+    ```
+"""
+
+from .client import QConnectClient
+from .paginator import (
+    ListAssistantAssociationsPaginator,
+    ListAssistantsPaginator,
+    ListContentAssociationsPaginator,
+    ListContentsPaginator,
+    ListImportJobsPaginator,
+    ListKnowledgeBasesPaginator,
+    ListQuickResponsesPaginator,
+    QueryAssistantPaginator,
+    SearchContentPaginator,
+    SearchQuickResponsesPaginator,
+    SearchSessionsPaginator,
+)
+
+Client = QConnectClient
+
+__all__ = (
+    "Client",
+    "ListAssistantAssociationsPaginator",
+    "ListAssistantsPaginator",
+    "ListContentAssociationsPaginator",
+    "ListContentsPaginator",
+    "ListImportJobsPaginator",
+    "ListKnowledgeBasesPaginator",
+    "ListQuickResponsesPaginator",
+    "QConnectClient",
+    "QueryAssistantPaginator",
+    "SearchContentPaginator",
+    "SearchQuickResponsesPaginator",
+    "SearchSessionsPaginator",
+)
