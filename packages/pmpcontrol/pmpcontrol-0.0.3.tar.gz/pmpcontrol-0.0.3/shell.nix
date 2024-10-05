067efeeb02dd6a6d15a7539d0e286a956c02017c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:03:47 +0000

    Add package pmpcontrol/0.0.3/pmpcontrol-0.0.3.tar.gz

---
+++ packages/pmpcontrol/pmpcontrol-0.0.3.tar.gz/pmpcontrol-0.0.3/shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSEnv  {
+  name = "python-env";
+  targetPkgs = pkgs: (with pkgs; [
+    python312Full
+    python312Packages.pip
+    python312Packages.virtualenv
+    alsa-lib
+  ]);
+  multiPkgs = pkgs: (with pkgs; [ ]);
+  runScript = ''
+    #!/bin/sh
+    if [ ! -d .venv ]; then
+      python -m venv .venv
+    fi
+    if which zsh &> /dev/null
+    source .venv/bin/activate
+    then
+        exec zsh
+    else
+        exec $SHELL
+    fi
+  '';
+}).env
+  
