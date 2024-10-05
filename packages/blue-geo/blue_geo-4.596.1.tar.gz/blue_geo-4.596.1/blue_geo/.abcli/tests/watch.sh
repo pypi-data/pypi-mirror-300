371a7eab43cfb127100f3da5f2631fb56d1047c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:45 +0000

    Add package blue-geo/4.596.1/blue_geo-4.596.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.596.1.tar.gz/blue_geo-4.596.1/blue_geo/.abcli/tests/watch.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+
+function test_blue_geo_watch() {
+    local options=$1
+
+    local list_of_targets=$(abcli_option "$options" target chilcotin-river-landslide-test+Leonardo-test)
+
+    local target
+    for target in $(echo $list_of_targets | tr + " "); do
+        abcli_log "🎯 $target"
+
+        local object_name=test_blue_geo_watch-$target-$(abcli_string_timestamp)
+
+        blue_geo_watch \
+            ,$options \
+            target=$target \
+            to=local \
+            - \
+            - \
+            $object_name
+        [[ $? -ne 0 ]] && return 1
+
+        local public_name=test_blue_geo_watch_v4-$target
+
+        abcli_clone \
+            ~download,~relate,~tags \
+            $object_name \
+            $public_name
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_publish \
+            tar \
+            $public_name
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_publish \
+            suffix=.gif \
+            $public_name
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
+    done
+
+    return 0
+}
---
+++ packages/blue-geo/blue_geo-4.596.1.tar.gz/blue_geo-4.596.1/blue_geo/watch/README.py
@@ -0,0 +1,107 @@
+from typing import List
+import os
+
+from blue_options import string
+from blue_objects import file
+from blue_objects.env import ABCLI_PUBLIC_PREFIX
+
+from blue_geo import REPO_NAME
+
+
+list_of_targets = {
+    "chilcotin-river-landslide": {
+        "test_blue_geo_watch_v4-chilcotin-river-landslide-test": [
+            f"[![bashtest](https://github.com/kamangir/{REPO_NAME}/actions/workflows/bashtest.yml/badge.svg)](https://github.com/kamangir/{REPO_NAME}/actions/workflows/bashtest.yml)"
+        ],
+        "geo-watch-2024-08-31-chilcotin-c": [
+            "L1C and L2A mixed",
+            "`2024-07-30/2024-08-09`",
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-199-11f9b5497ef0)",
+        ],
+        "geo-watch-2024-09-01-chilcotin-a": [
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-201-d64e9bb3716b)",
+        ],
+        "geo-watch-2024-09-01-chilcotin-c": [
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-202-d59ba811398b)",
+            "[on reddit](https://www.reddit.com/r/bash/comments/1f9cvyx/a_bash_python_tool_to_watch_a_target_in_satellite/).",
+        ],
+    },
+    "burning-man-2024": {
+        "geo-watch-2024-09-04-burning-man-2024-a": [
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-205-c272a95ce266)",
+        ]
+    },
+    "Mount-Etna": {
+        "geo-watch-2024-09-04-Mount-Etna-a": [
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-205-c272a95ce266)",
+        ]
+    },
+    "Fagradalsfjall": {
+        "geo-watch-2024-09-04-Fagradalsfjall-a": [
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-206-f7996520dc15)",
+        ]
+    },
+    "Jasper": {
+        "geo-watch-2024-09-06-Jasper-a": [
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-208-7063fca1423b)",
+        ]
+    },
+    "Leonardo": {
+        "test_blue_geo_watch_v4-Leonardo-test": [
+            f"[![bashtest](https://github.com/kamangir/{REPO_NAME}/actions/workflows/bashtest.yml/badge.svg)](https://github.com/kamangir/{REPO_NAME}/actions/workflows/bashtest.yml)"
+        ],
+        "geo-watch-2024-09-30-Leonardo-g": [
+            "[dev notes](https://medium.com/@arash-kamangir/%EF%B8%8F-conversations-with-ai-237-99db71023445)",
+        ],
+    },
+    "bellingcat-2024–09–27-nagorno-karabakh": {
+        "bellingcat-2024-09-27-nagorno-karabakh-2024-10-01-c-b": [
+            "[background](https://www.bellingcat.com/news/mena/2024/09/27/nagorno-karabakh-satellite-imagery-shows-city-wide-ransacking/)",
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-241-3e25857747a5)",
+        ],
+        "bellingcat-2024-09-27-nagorno-karabakh-b": [
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-244-a5f9b7959748)",
+        ],
+        "bellingcat-2024-09-27-nagorno-karabakh-6X-a": [
+            "[dev notes](https://arash-kamangir.medium.com/%EF%B8%8F-conversations-with-ai-245-18f6d15e5fbd)",
+        ],
+    },
+}
+
+targets_path = file.path(__file__)
+
+items: List[str] = []
+for target_name, list_of_objects in list_of_targets.items():
+    target_README = f"./targets/{target_name}.md"
+
+    target_title = target_name.replace("-", " ").title()
+
+    items += [
+        (
+            f"## [{target_title}]({target_README})"
+            if file.exists(os.path.join(targets_path, target_README))
+            else f"## {target_title}"
+        ),
+    ]
+
+    items += [
+        "- {}.".format(
+            ", ".join(
+                [
+                    f"[`{object_name}`]({ABCLI_PUBLIC_PREFIX}/{object_name}.tar.gz)",
+                    f"[gif]({ABCLI_PUBLIC_PREFIX}/{object_name}/{object_name}.gif)",
+                ]
+                + description
+            )
+        )
+        for object_name, description in list_of_objects.items()
+    ]
+
+    if list_of_objects:
+        last_object_name = list(list_of_objects.keys())[-1]
+        items += [
+            "",
+            f"![image]({ABCLI_PUBLIC_PREFIX}/{last_object_name}/{last_object_name}-2X.gif?raw=true&random={string.random()})",
+        ]
+
+    items += [""]
