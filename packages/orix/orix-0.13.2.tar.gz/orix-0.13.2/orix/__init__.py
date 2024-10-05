a8c27919357f39c97639927d560cab71a23811e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:04:17 +0000

    Add package orix/0.13.2/orix-0.13.2.tar.gz

---
+++ packages/orix/orix-0.13.2.tar.gz/orix-0.13.2/orix/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.13.2"
+# Sorted by line contributions (ideally excluding lines in notebook files)
+__credits__ = [
+    "Håkon Wiik Ånes",
+    "Ben Martineau",
+    "Paddy Harrison",
+    "Phillip Crout",
+    "Austin Gerlt",
+    "Duncan Johnstone",
+    "Niels Cautaerts",
+    "Anders Christian Mathisen",
+    "Zhou Xu",
+    "Carter Francis",
+    "Simon Høgås",
+    "Viljar Johan Femoen",
+    "Alessandra da Silva",
+    "Alexander Clausen",
+]
