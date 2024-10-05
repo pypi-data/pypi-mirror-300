8b074df1067e9d067ed2dc8ec3e81eb15b6e512f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 06:03:23 +0000

    Add package drift-shield/0.2/drift_shield-0.2.tar.gz

---
+++ packages/drift-shield/drift_shield-0.2.tar.gz/drift_shield-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="drift_shield",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        'pandas', 'scipy'
+    ],
+    author="Shanmukh Dara",
+    author_email="shanmukhdara@gmail.com",
+    description="A package to monitor and track data drift for ML models",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
