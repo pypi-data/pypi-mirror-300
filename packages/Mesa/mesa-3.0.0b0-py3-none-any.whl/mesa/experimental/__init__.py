f99d6d4f5c120fb2890c7f2eaccd6dc86ec1a7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:03:50 +0000

    Add package Mesa/3.0.0b0/mesa-3.0.0b0-py3-none-any.whl

---
+++ packages/Mesa/mesa-3.0.0b0-py3-none-any.whl/mesa/experimental/__init__.py
@@ -0,0 +1,7 @@
+"""Experimental init."""
+
+from mesa.experimental import cell_space
+
+from .solara_viz import JupyterViz, Slider, SolaraViz, make_text
+
+__all__ = ["cell_space", "JupyterViz", "SolaraViz", "make_text", "Slider"]
