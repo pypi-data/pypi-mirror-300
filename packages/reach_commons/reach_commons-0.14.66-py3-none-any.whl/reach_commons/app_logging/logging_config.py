2528588b85a9e6eff87c1cf8f528f46567600c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:07:39 +0000

    Add package reach_commons/0.14.66/reach_commons-0.14.66-py3-none-any.whl

---
+++ packages/reach_commons/reach_commons-0.14.66-py3-none-any.whl/reach_commons/app_logging/logging_config.py
@@ -0,0 +1,29 @@
+import logging
+
+
+class CustomPrefixFilter(logging.Filter):
+    def __init__(self, prefix="[ReachLogProcessor]"):
+        super().__init__()
+        self.prefix = prefix
+
+    def filter(self, record):
+        record.msg = f"{self.prefix} {record.msg}"
+        return True
+
+
+def setup_logger(prefix="[ReachLogProcessor]"):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    prefix_filter = CustomPrefixFilter(prefix)
+    logger.addFilter(prefix_filter)
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+
+logger = setup_logger()
