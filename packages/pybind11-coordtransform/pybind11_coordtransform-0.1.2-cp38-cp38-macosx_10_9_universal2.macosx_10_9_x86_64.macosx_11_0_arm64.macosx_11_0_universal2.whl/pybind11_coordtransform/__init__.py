bed9548dfcba858f1ea164f3036e30d645d43593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 04:03:03 +0000

    Add package pybind11-coordtransform/0.1.2/pybind11_coordtransform-0.1.2-cp38-cp38-macosx_10_9_universal2.macosx_10_9_x86_64.macosx_11_0_arm64.macosx_11_0_universal2.whl

---
+++ packages/pybind11-coordtransform/pybind11_coordtransform-0.1.2-cp38-cp38-macosx_10_9_universal2.macosx_10_9_x86_64.macosx_11_0_arm64.macosx_11_0_universal2.whl/pybind11_coordtransform/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from ._core import (
+    __doc__,
+    __version__,
+    bd09togcj02,
+    gcj02_to_wgs84_Nx2,
+    gcj02_to_wgs84_Nx3,
+    gcj02tobd09,
+    gcj02towgs84,
+    wgs84_to_gcj02_Nx2,
+    wgs84_to_gcj02_Nx3,
+    wgs84togcj02,
+)
+
+__all__ = [
+    "__doc__",
+    "__version__",
+    "bd09togcj02",
+    "gcj02tobd09",
+    "wgs84togcj02",
+    "gcj02towgs84",
+    "wgs84_to_gcj02_Nx3",
+    "wgs84_to_gcj02_Nx2",
+    "gcj02_to_wgs84_Nx3",
+    "gcj02_to_wgs84_Nx2",
+]
