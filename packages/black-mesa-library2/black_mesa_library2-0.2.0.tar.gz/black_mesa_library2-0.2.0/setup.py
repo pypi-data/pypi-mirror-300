76178ad2cf5a6616b4f6f5a5f28c05aa883d0327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 04:03:13 +0000

    Add package black-mesa-library2/0.2.0/black_mesa_library2-0.2.0.tar.gz

---
+++ packages/black-mesa-library2/black_mesa_library2-0.2.0.tar.gz/black_mesa_library2-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="black-mesa-library2",
+    version="0.2.0",
+    description="Super Secret Stuff",
+    author="D S",
+    author_email="dustin.silveri+poc1@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
