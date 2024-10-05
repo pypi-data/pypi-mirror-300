def5a770aa8072d16b88c585c0d7208f150012ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:04 +0000

    Add package saq/0.17.1/saq-0.17.1-py3-none-any.whl

---
+++ packages/saq/saq-0.17.1-py3-none-any.whl/saq/queue/postgres_ddl.py
@@ -0,0 +1,30 @@
+CREATE_JOBS_TABLE = """
+CREATE TABLE IF NOT EXISTS {jobs_table} (
+    key TEXT PRIMARY KEY,
+    lock_key SERIAL NOT NULL,
+    queued BIGINT NOT NULL DEFAULT EXTRACT(EPOCH FROM now()),
+    job BYTEA NOT NULL,
+    queue TEXT NOT NULL,
+    status TEXT NOT NULL,
+    scheduled BIGINT NOT NULL DEFAULT EXTRACT(EPOCH FROM now()),
+    expire_at BIGINT
+);
+"""
+
+CREATE_JOBS_DEQUEUE_INDEX = """
+CREATE INDEX IF NOT EXISTS saq_jobs_dequeue_idx ON {jobs_table} (status, queue, scheduled)
+"""
+
+CREATE_STATS_TABLE = """
+CREATE TABLE IF NOT EXISTS {stats_table} (
+    worker_id TEXT PRIMARY KEY,
+    stats JSONB,
+    expire_at BIGINT
+);
+"""
+
+DDL_STATEMENTS = [
+    CREATE_JOBS_TABLE,
+    CREATE_JOBS_DEQUEUE_INDEX,
+    CREATE_STATS_TABLE,
+]
