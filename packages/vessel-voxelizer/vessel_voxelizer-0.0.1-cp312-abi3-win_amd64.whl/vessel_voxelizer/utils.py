d247867b44b98c9f507d1371cc8726c81483bcdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 05:03:26 +0000

    Add package vessel-voxelizer/0.0.1/vessel_voxelizer-0.0.1-cp312-abi3-win_amd64.whl

---
+++ packages/vessel-voxelizer/vessel_voxelizer-0.0.1-cp312-abi3-win_amd64.whl/vessel_voxelizer/utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+
+def load_vessels(path):
+    vessels = pd.read_csv(path)
+
+    # number of vessel segments
+    N = len(vessels) - (vessels['parent_id'] == -1).sum()
+
+    ids = vessels['node_id'].to_numpy()
+    positions = vessels[['x', 'y', 'z']].to_numpy()
+    radii = vessels['radius'].to_numpy()
+    parent_id = vessels['parent_id'].to_numpy()
+
+    vessel_positions = np.zeros((N, 2, 3))
+    vessel_radii = np.zeros(N)
+
+    i_vessel = 0
+    for i in range(len(vessels)):
+        parent = parent_id[i]
+        if parent == -1:
+            continue
+        else:
+            vessel_positions[i_vessel] = positions[[parent_id[i], ids[i]]]
+            vessel_radii[i_vessel] = radii[i]
+            i_vessel += 1
+
+    return vessel_positions, vessel_radii
