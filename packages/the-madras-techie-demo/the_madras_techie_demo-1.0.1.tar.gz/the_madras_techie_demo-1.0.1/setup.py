8c74e84646bc06fc72393b5213a8555f3128a554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:03:46 +0000

    Add package the-madras-techie-demo/1.0.1/the_madras_techie_demo-1.0.1.tar.gz

---
+++ packages/the-madras-techie-demo/the_madras_techie_demo-1.0.1.tar.gz/the_madras_techie_demo-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+name='the_madras_techie_demo',
+version='1.0.1',
+packages=find_packages(),
+install_requires=[
+# Add dependencies here.
+# e.g. 'numpy>=1.11.1'
+],
+)
---
+++ packages/the-madras-techie-demo/the_madras_techie_demo-1.0.1.tar.gz/the_madras_techie_demo-1.0.1/the_madras_techie_demo/main.py
@@ -0,0 +1,17 @@
+def hello():
+    print("Hi from Madras Techie")
+
+def height():
+    print("We are all tall")    
+
+def weight():
+    print("We are all weight")
+
+def location():
+    print("We are worldly beings")    
+
+def job():
+    print("We work on the best possible works suitable for us")
+
+def sports():
+    print("We play our best suitable sports")    
