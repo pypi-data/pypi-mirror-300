54e4de6bbc3619e912335d03dae0e9c0afa13061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:03:48 +0000

    Add package bbot/2.1.0.4929rc0/bbot-2.1.0.4929rc0.tar.gz

---
+++ packages/bbot/bbot-2.1.0.4929rc0.tar.gz/bbot-2.1.0.4929rc0/bbot/modules/chaos.py
@@ -0,0 +1,46 @@
+from bbot.modules.templates.subdomain_enum import subdomain_enum_apikey
+
+
+class chaos(subdomain_enum_apikey):
+    watched_events = ["DNS_NAME"]
+    produced_events = ["DNS_NAME"]
+    flags = ["subdomain-enum", "passive", "safe"]
+    meta = {
+        "description": "Query ProjectDiscovery's Chaos API for subdomains",
+        "created_date": "2022-08-14",
+        "author": "@TheTechromancer",
+        "auth_required": True,
+    }
+    options = {"api_key": ""}
+    options_desc = {"api_key": "Chaos API key"}
+
+    base_url = "https://dns.projectdiscovery.io/dns"
+
+    async def ping(self):
+        url = f"{self.base_url}/example.com"
+        response = await self.api_request(url)
+        assert response.json()["domain"] == "example.com"
+
+    def prepare_api_request(self, url, kwargs):
+        kwargs["headers"]["Authorization"] = self.api_key
+        return url, kwargs
+
+    async def request_url(self, query):
+        _, domain = self.helpers.split_domain(query)
+        url = f"{self.base_url}/{domain}/subdomains"
+        return await self.api_request(url)
+
+    def parse_results(self, r, query):
+        j = r.json()
+        subdomains_set = set()
+        if isinstance(j, dict):
+            domain = j.get("domain", "")
+            if domain:
+                subdomains = j.get("subdomains", [])
+                for s in subdomains:
+                    s = s.lower().strip(".*")
+                    subdomains_set.add(s)
+                for s in subdomains_set:
+                    full_subdomain = f"{s}.{domain}"
+                    if full_subdomain and full_subdomain.endswith(f".{query}"):
+                        yield full_subdomain
