156a64306a2c566ba423d03ee36446c6e15e93fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:04:26 +0000

    Add package tgbsms/1.3.5/tgbsms-1.3.5-py3-none-any.whl

---
+++ packages/tgbsms/tgbsms-1.3.5-py3-none-any.whl/tgbsms/__init__.py
@@ -0,0 +1,7 @@
+from .enums import ParseMode
+from .exceptions import EmptyPayloadError
+from .exceptions import NoTokenError
+from .exceptions import WrongParseModeError
+from .tgbsms import send_message
+
+__all__ = ["send_message", "ParseMode", "WrongParseModeError", "NoTokenError", "EmptyPayloadError"]
---
+++ packages/tgbsms/tgbsms-1.3.5-py3-none-any.whl/tgbsms/tgbsms.py
@@ -0,0 +1,78 @@
+import logging
+from io import BytesIO
+
+import requests
+from environs import Env
+
+from tgbsms.enums import ParseMode
+from tgbsms.exceptions import EmptyPayloadError
+from tgbsms.exceptions import NoTokenError
+from tgbsms.exceptions import WrongParseModeError
+
+env = Env(eager=False)
+env.read_env()
+
+logger = logging.getLogger("tgbsms")
+
+ENV_ERROR_MSG = """
+Missing telegram_chat_id/telegram_bot_token. 
+Pass it to the function or set as an environment variable TELEGRAM_CHAT_ID/TELEGRAM_BOT_TOKEN
+"""
+
+NO_DATA_ERROR_MSG = "Payload is empty"
+
+
+def send_message(
+    text: str = "",
+    image: BytesIO | str | None = None,
+    parse_mode: ParseMode = "HTML",
+    telegram_chat_id: str | int | None = None,
+    telegram_bot_token: str | None = None,
+) -> None:
+    # Get the TOKEN from environment variables if it's not passed directly
+    telegram_bot_token = telegram_bot_token or env.str("TELEGRAM_BOT_TOKEN")
+    telegram_chat_id = telegram_chat_id or env.str("TELEGRAM_CHAT_ID")
+
+    # No credentials check
+    if telegram_bot_token is None or telegram_chat_id is None:
+        raise NoTokenError(ENV_ERROR_MSG)
+
+    # No data check
+    if not any([text, image]):
+        raise EmptyPayloadError(NO_DATA_ERROR_MSG)
+
+    # Wrong parse_mode check
+    try:
+        parse_mode = ParseMode[parse_mode]
+    except KeyError as e:
+        raise WrongParseModeError from e
+
+    method = "sendMessage" if image is None else "sendPhoto"
+    kword = "caption" if image else "text"
+    url = f"https://api.telegram.org/bot{telegram_bot_token}/{method}"
+
+    cropped_text = text[:4000]
+    data = {"chat_id": telegram_chat_id, kword: cropped_text, "parse_mode": parse_mode.value}
+
+    files = None
+    if image:
+        _data = {"photo": image}
+        if isinstance(image, str):
+            data.update(_data)
+        else:
+            files = _data
+
+    try:
+        response = requests.post(url, data, files=files)
+        response.raise_for_status()
+        
+        result = response.json().get("result")
+        msg_id = None
+        if result:
+            msg_id = result.get("message_id")
+            
+        logger.info(f"Message sent successfully (id: {msg_id})")
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error sending message: {e}")
+        logger.error(response.json())
