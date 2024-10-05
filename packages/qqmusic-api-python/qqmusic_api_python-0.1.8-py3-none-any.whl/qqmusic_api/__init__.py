aa9d88e7c41cf81089264d7950e9877baace9c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:03:46 +0000

    Add package qqmusic-api-python/0.1.8/qqmusic_api_python-0.1.8-py3-none-any.whl

---
+++ packages/qqmusic-api-python/qqmusic_api_python-0.1.8-py3-none-any.whl/qqmusic_api/__init__.py
@@ -0,0 +1,23 @@
+from . import album, login, lyric, mv, search, singer, song, songlist, top, user
+from .utils.credential import Credential
+from .utils.network import get_session, set_session
+from .utils.sync import sync
+
+__version__ = "0.1.8"
+
+__all__ = [
+    "album",
+    "Credential",
+    "get_session",
+    "login",
+    "lyric",
+    "mv",
+    "search",
+    "set_session",
+    "singer",
+    "song",
+    "songlist",
+    "sync",
+    "top",
+    "user",
+]
---
+++ packages/qqmusic-api-python/qqmusic_api_python-0.1.8-py3-none-any.whl/qqmusic_api/utils/sync.py
@@ -0,0 +1,27 @@
+"""同步执行异步函数
+
+代码来源: https://github.com/Nemo2011/bilibili-api
+"""
+
+import asyncio
+from collections.abc import Coroutine
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any
+
+
+def sync(coroutine: Coroutine) -> Any:
+    """同步执行异步函数
+
+    Args:
+        coroutine: 执行异步函数所创建的协程对象
+
+    Returns:
+        该协程对象的返回值
+    """
+    try:
+        asyncio.get_running_loop()
+    except RuntimeError:
+        return asyncio.run(coroutine)
+    else:
+        with ThreadPoolExecutor() as executor:
+            return executor.submit(asyncio.run, coroutine).result()
