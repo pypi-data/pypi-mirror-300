76ff8b22e0278c2b249a66a9dd8ba9cefb7c0db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 06:03:13 +0000

    Add package pypechain/0.0.40/pypechain-0.0.40.tar.gz

---
+++ packages/pypechain/pypechain-0.0.40.tar.gz/pypechain-0.0.40/pypechain/test/events/test_events.py
@@ -0,0 +1,90 @@
+"""Tests for overloading methods."""
+
+from __future__ import annotations
+
+import os
+
+import pytest
+from web3 import Web3
+from web3.logs import WARN
+
+from pypechain.test.events.types import EventsContract
+from pypechain.test.events.types.EventsTypes import EventAEvent, EventBEvent
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+project_root = os.path.dirname(os.path.dirname(current_path))
+
+
+@pytest.mark.usefixtures("process_contracts")
+class TestEvents:
+    """Tests events emitted from the contracts."""
+
+    @pytest.mark.skip()
+    def test_process_receipt(self, w3: Web3):
+        """Test that we can use event filters."""
+        deployed_contract = EventsContract.deploy(w3=w3, account=w3.eth.accounts[0])
+        hash_a = deployed_contract.functions.emitOneEvent(0, "0x0000000000000000000000000000000000000000").transact()
+        receipt_a = w3.eth.get_transaction_receipt(hash_a)
+        hash_b = deployed_contract.functions.emitTwoEvents(0, "0x0000000000000000000000000000000000000000").transact()
+        receipt_b = w3.eth.get_transaction_receipt(hash_b)
+
+        deployed_contract.events.EventA.process_receipt(receipt_a, errors=WARN)
+        deployed_contract.events.EventB.process_receipt(receipt_b, errors=WARN)
+
+    def test_get_typed_logs(self, w3):
+        """Test that we can get logs and the return is the type we expect."""
+        deployed_contract = EventsContract.deploy(w3=w3, account=w3.eth.accounts[0])
+        deployed_contract.functions.emitOneEvent(0, "0x0000000000000000000000000000000000000000").transact()
+        deployed_contract.functions.emitTwoEvents(1, "0x0000000000000000000000000000000000000000").transact()
+
+        event_a_logs = list(
+            deployed_contract.events.EventA.get_typed_logs(
+                argument_filters={"who": "0x0000000000000000000000000000000000000000"},
+                from_block=0,
+            )
+        )
+        event_b_logs = list(
+            deployed_contract.events.EventB.get_typed_logs(
+                from_block=0,
+            )
+        )
+
+        assert len(event_a_logs) == 2
+        assert isinstance(event_a_logs[0], EventAEvent)
+        assert event_a_logs[0].args.who == "0x0000000000000000000000000000000000000000"
+        assert event_a_logs[0].args.value == 0
+        assert isinstance(event_a_logs[1], EventAEvent)
+        assert event_a_logs[1].args.who == "0x0000000000000000000000000000000000000000"
+        assert event_a_logs[1].args.value == 1
+
+        assert len(list(event_b_logs)) == 1
+        assert isinstance(event_b_logs[0], EventBEvent)
+
+    def test_create_filter(self, w3):
+        """Test that we can use event filters."""
+        deployed_contract = EventsContract.deploy(w3=w3, account=w3.eth.accounts[0])
+        tx_hash = deployed_contract.functions.emitOneEvent(0, "0x0000000000000000000000000000000000000000").transact()
+        w3.eth.wait_for_transaction_receipt(tx_hash)
+
+        event_a_filter = deployed_contract.events.EventA.create_filter(
+            argument_filters={"who": "0x0000000000000000000000000000000000000000"},
+            from_block=0,
+        )
+        event_b_filter = deployed_contract.events.EventB.create_filter(
+            from_block=0,
+        )
+
+        a_events = event_a_filter.get_new_entries()
+        b_events = event_b_filter.get_new_entries()
+
+        assert len(a_events) == 1
+        assert len(b_events) == 0
+
+        tx_hash = deployed_contract.functions.emitTwoEvents(0, "0x0000000000000000000000000000000000000000").transact()
+        w3.eth.wait_for_transaction_receipt(tx_hash)
+
+        a_events = event_a_filter.get_new_entries()
+        b_events = event_b_filter.get_new_entries()
+
+        assert len(a_events) == 1
+        assert len(b_events) == 1
---
+++ packages/pypechain/pypechain-0.0.40.tar.gz/pypechain-0.0.40/snapshots/has_events.py
@@ -0,0 +1,228 @@
+
+
+class OverloadedTransferContractEvent(ContractEvent):
+    """ContractEvent for Transfer."""
+    # super() get_logs and create_filter methods are generic, while our version adds values & types
+    # pylint: disable=arguments-differ
+
+    # @combomethod destroys return types, so we are redefining functions as both class and instance
+    # pylint: disable=function-redefined
+
+    
+    
+    # pylint: disable=useless-parent-delegation
+    def __init__(self, *argument_names: tuple[str]) -> None:
+        super().__init__(*argument_names)
+
+    # We ignore types here for function redefinition
+    def get_typed_logs( # type: ignore
+        self: "OverloadedTransferContractEvent",
+        argument_filters: dict[str, Any] | None = None,
+        from_block: BlockIdentifier | None = None,
+        to_block: BlockIdentifier | None = None,
+        block_hash: HexBytes | None = None,
+    ) -> Iterable[TransferEvent]:
+        """Extension of `get_logs` that return a typed dataclass of the event."""
+        abi_events = super().get_logs(argument_filters=argument_filters, from_block=from_block, to_block=to_block, block_hash=block_hash)
+        # TODO there may be issues with this function if the user uses a middleware that changes event structure.
+        return [
+            TransferEvent(
+                log_index = abi_event.logIndex,
+                transaction_index = abi_event.transactionIndex,
+                transaction_hash = abi_event.transactionHash,
+                address = abi_event.address,
+                block_hash = abi_event.blockHash,
+                block_number = abi_event.blockNumber,
+
+                args = TransferEvent.TransferEventArgs(
+                    _from = abi_event.args["_from"],
+                    to = abi_event.args["to"],
+                    value = abi_event.args["value"],
+                ),
+
+            ) for abi_event in abi_events
+        ]
+
+    @classmethod
+    # We ignore types here for function redefinition
+    def get_typed_logs( # type: ignore
+        cls: Type["OverloadedTransferContractEvent"],
+        argument_filters: dict[str, Any] | None = None,
+        from_block: BlockIdentifier | None = None,
+        to_block: BlockIdentifier | None = None,
+        block_hash: HexBytes | None = None,
+    ) -> Iterable[TransferEvent]:
+        """Extension of `get_logs` that return a typed dataclass of the event."""
+        abi_events = super().get_logs(argument_filters=argument_filters, from_block=from_block, to_block=to_block, block_hash=block_hash)
+        # TODO there may be issues with this function if the user uses a middleware that changes event structure.
+        return [
+            TransferEvent(
+                log_index = abi_event.logIndex,
+                transaction_index = abi_event.transactionIndex,
+                transaction_hash = abi_event.transactionHash,
+                address = abi_event.address,
+                block_hash = abi_event.blockHash,
+                block_number = abi_event.blockNumber,
+
+                args = TransferEvent.TransferEventArgs(
+                    _from = abi_event.args["_from"],
+                    to = abi_event.args["to"],
+                    value = abi_event.args["value"],
+                ),
+
+            ) for abi_event in abi_events
+        ]
+
+    def create_filter( # type: ignore
+        self: "OverloadedTransferContractEvent",
+        *,  # PEP 3102
+        argument_filters: dict[str, Any] | None = None,
+        from_block: BlockIdentifier | None = None,
+        to_block: BlockIdentifier = "latest",
+        address: ChecksumAddress | None = None,
+        topics: Sequence[Any] | None = None,
+    ) -> LogFilter:
+        return cast(LogFilter, super().create_filter(argument_filters=argument_filters, from_block=from_block, to_block=to_block, address=address, topics=topics))
+
+    @classmethod
+    def create_filter( # type: ignore
+        cls: Type["OverloadedTransferContractEvent"],
+        *,  # PEP 3102
+        argument_filters: dict[str, Any] | None = None,
+        from_block: BlockIdentifier | None = None,
+        to_block: BlockIdentifier = "latest",
+        address: ChecksumAddress | None = None,
+        topics: Sequence[Any] | None = None,
+    ) -> LogFilter:
+        return cast(LogFilter, super().create_filter(argument_filters=argument_filters, from_block=from_block, to_block=to_block, address=address, topics=topics))
+
+
+
+class OverloadedApprovalContractEvent(ContractEvent):
+    """ContractEvent for Approval."""
+    # super() get_logs and create_filter methods are generic, while our version adds values & types
+    # pylint: disable=arguments-differ
+
+    # @combomethod destroys return types, so we are redefining functions as both class and instance
+    # pylint: disable=function-redefined
+
+    
+    
+    # pylint: disable=useless-parent-delegation
+    def __init__(self, *argument_names: tuple[str]) -> None:
+        super().__init__(*argument_names)
+
+    # We ignore types here for function redefinition
+    def get_typed_logs( # type: ignore
+        self: "OverloadedApprovalContractEvent",
+        argument_filters: dict[str, Any] | None = None,
+        from_block: BlockIdentifier | None = None,
+        to_block: BlockIdentifier | None = None,
+        block_hash: HexBytes | None = None,
+    ) -> Iterable[ApprovalEvent]:
+        """Extension of `get_logs` that return a typed dataclass of the event."""
+        abi_events = super().get_logs(argument_filters=argument_filters, from_block=from_block, to_block=to_block, block_hash=block_hash)
+        # TODO there may be issues with this function if the user uses a middleware that changes event structure.
+        return [
+            ApprovalEvent(
+                log_index = abi_event.logIndex,
+                transaction_index = abi_event.transactionIndex,
+                transaction_hash = abi_event.transactionHash,
+                address = abi_event.address,
+                block_hash = abi_event.blockHash,
+                block_number = abi_event.blockNumber,
+
+                args = ApprovalEvent.ApprovalEventArgs(
+                    owner = abi_event.args["owner"],
+                    spender = abi_event.args["spender"],
+                    value = abi_event.args["value"],
+                ),
+
+            ) for abi_event in abi_events
+        ]
+
+    @classmethod
+    # We ignore types here for function redefinition
+    def get_typed_logs( # type: ignore
+        cls: Type["OverloadedApprovalContractEvent"],
+        argument_filters: dict[str, Any] | None = None,
+        from_block: BlockIdentifier | None = None,
+        to_block: BlockIdentifier | None = None,
+        block_hash: HexBytes | None = None,
+    ) -> Iterable[ApprovalEvent]:
+        """Extension of `get_logs` that return a typed dataclass of the event."""
+        abi_events = super().get_logs(argument_filters=argument_filters, from_block=from_block, to_block=to_block, block_hash=block_hash)
+        # TODO there may be issues with this function if the user uses a middleware that changes event structure.
+        return [
+            ApprovalEvent(
+                log_index = abi_event.logIndex,
+                transaction_index = abi_event.transactionIndex,
+                transaction_hash = abi_event.transactionHash,
+                address = abi_event.address,
+                block_hash = abi_event.blockHash,
+                block_number = abi_event.blockNumber,
+
+                args = ApprovalEvent.ApprovalEventArgs(
+                    owner = abi_event.args["owner"],
+                    spender = abi_event.args["spender"],
+                    value = abi_event.args["value"],
+                ),
+
+            ) for abi_event in abi_events
+        ]
+
+    def create_filter( # type: ignore
+        self: "OverloadedApprovalContractEvent",
+        *,  # PEP 3102
+        argument_filters: dict[str, Any] | None = None,
+        from_block: BlockIdentifier | None = None,
+        to_block: BlockIdentifier = "latest",
+        address: ChecksumAddress | None = None,
+        topics: Sequence[Any] | None = None,
+    ) -> LogFilter:
+        return cast(LogFilter, super().create_filter(argument_filters=argument_filters, from_block=from_block, to_block=to_block, address=address, topics=topics))
+
+    @classmethod
+    def create_filter( # type: ignore
+        cls: Type["OverloadedApprovalContractEvent"],
+        *,  # PEP 3102
+        argument_filters: dict[str, Any] | None = None,
+        from_block: BlockIdentifier | None = None,
+        to_block: BlockIdentifier = "latest",
+        address: ChecksumAddress | None = None,
+        topics: Sequence[Any] | None = None,
+    ) -> LogFilter:
+        return cast(LogFilter, super().create_filter(argument_filters=argument_filters, from_block=from_block, to_block=to_block, address=address, topics=topics))
+
+
+
+class OverloadedContractEvents(ContractEvents):
+    """ContractEvents for the Overloaded contract."""
+
+    Transfer: OverloadedTransferContractEvent
+
+    Approval: OverloadedApprovalContractEvent
+
+
+    def __init__(
+        self,
+        abi: ABI,
+        w3: "Web3",
+        address: ChecksumAddress | None = None,
+    ) -> None:
+        super().__init__(abi, w3, address)
+        self.Transfer = cast(OverloadedTransferContractEvent, OverloadedTransferContractEvent.factory(
+            "Transfer",
+            w3=w3,
+            contract_abi=abi,
+            address=address,
+            event_name="Transfer"
+        ))
+        self.Approval = cast(OverloadedApprovalContractEvent, OverloadedApprovalContractEvent.factory(
+            "Approval",
+            w3=w3,
+            contract_abi=abi,
+            address=address,
+            event_name="Approval"
+        ))
+        
