24be9b1d4bb4bb80a8ea3e55863762cf9a762452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 06:03:13 +0000

    Add package node-hermes-core/1.0.0/node_hermes_core-1.0.0-py3-none-any.whl

---
+++ packages/node-hermes-core/node_hermes_core-1.0.0-py3-none-any.whl/node_hermes_core/nodes/depedency.py
@@ -0,0 +1,24 @@
+from typing import List
+
+import pydantic_yaml
+from hermes_core.loader import load_modules
+from pydantic import BaseModel, Field
+
+
+class HermesDependencies(BaseModel):
+    """
+    Provides a way to specify dependencies for the configuration file.
+    This is a subset of the configuration file that is loaded before the main configuration file.
+    """
+
+    modules: List[str] = Field(description="List of modules to import before loading the configuration")
+
+    @classmethod
+    def from_yaml(cls, file_path: str):
+        with open(file_path, "r") as file:
+            return pydantic_yaml.parse_yaml_raw_as(cls, file)
+
+    @staticmethod
+    def import_from_yaml(file_path: str):
+        config = HermesDependencies.from_yaml(file_path)
+        return load_modules(config.modules)
