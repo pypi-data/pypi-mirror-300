7b4b56f3393a117859d25d137e2087591a91d2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:35 +0000

    Add package opteryx/0.17.2a818/opteryx-0.17.2a818-cp311-cp311-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.17.2a818-cp311-cp311-win_amd64.whl/opteryx/compiled/functions/__init__.py
@@ -0,0 +1,5 @@
+from .ip_address import ip_in_cidr
+from .vectors import possible_match
+from .vectors import possible_match_indices
+from .vectors import tokenize_and_remove_punctuation
+from .vectors import vectorize
