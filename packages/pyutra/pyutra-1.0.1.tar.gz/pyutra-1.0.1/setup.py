f8b2c9d1f2683d1679bf9444f858db3d2e91082a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 04:03:37 +0000

    Add package pyutra/1.0.1/pyutra-1.0.1.tar.gz

---
+++ packages/pyutra/pyutra-1.0.1.tar.gz/pyutra-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyutra',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='This is a python library that is meant to try to be jack of all trades.',
+long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='TheDiamondOG',
+    author_email='thediamondogness@gmail.com',
+    url='https://github.com/thediamondog/pyutra',
+)
