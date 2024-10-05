3a8c6b8598681e62d1eb4d730124a7830c851f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:05:21 +0000

    Add package nixt/70/nixt-70.tar.gz

---
+++ packages/nixt/nixt-70.tar.gz/nixt-70/nixt/timer.py
@@ -0,0 +1,64 @@
+# This file is placed in the Public Domain.
+# pylint: disable=R,W0105,W0212,W0718,E1102
+
+
+"timers"
+
+
+import time
+import threading
+
+
+from .runtime  import launch, named
+
+
+class Timer:
+
+    "Timer"
+
+    def __init__(self, sleep, func, *args, thrname=None, **kwargs):
+        self.args  = args
+        self.func  = func
+        self.kwargs = kwargs
+        self.sleep = sleep
+        self.name  = thrname or kwargs.get("name", named(func))
+        self.state = {}
+        self.timer = None
+
+    def run(self):
+        "run the payload in a thread."
+        self.state["latest"] = time.time()
+        launch(self.func, *self.args)
+
+    def start(self):
+        "start timer."
+        timer = threading.Timer(self.sleep, self.run)
+        timer.name   = self.name
+        timer.sleep  = self.sleep
+        timer.state  = self.state
+        timer.func   = self.func
+        timer.state["starttime"] = time.time()
+        timer.state["latest"]    = time.time()
+        timer.start()
+        self.timer   = timer
+
+    def stop(self):
+        "stop timer."
+        if self.timer:
+            self.timer.cancel()
+
+
+class Repeater(Timer):
+
+    "Repeater"
+
+    def run(self):
+        launch(self.start)
+        super().run()
+
+
+def __dir__():
+    return (
+        'Repeater',
+        'Timer'
+    )
