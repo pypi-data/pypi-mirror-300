d6419183a21fd29717f1c102ff9ffab4a6b80d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:03:47 +0000

    Add package scrapegraphai/1.26.0b4/scrapegraphai-1.26.0b4-py3-none-any.whl

---
+++ packages/scrapegraphai/scrapegraphai-1.26.0b4-py3-none-any.whl/scrapegraphai/nodes/conditional_node.py
@@ -0,0 +1,50 @@
+"""
+Module for implementing the conditional node
+"""
+from typing import Optional, List
+from .base_node import BaseNode
+
+class ConditionalNode(BaseNode):
+    """
+    A node that determines the next step in the graph's execution flow based on 
+    the presence and content of a specified key in the graph's state. It extends 
+    the BaseNode by adding condition-based logic to the execution process.
+
+    This node type is used to implement branching logic within the graph, allowing 
+    for dynamic paths based on the data available in the current state.
+
+    It is expected that exactly two edges are created out of this node.
+    The first node is chosen for execution if the key exists and has a non-empty value,
+    and the second node is chosen if the key does not exist or is empty.
+
+    Attributes:
+        key_name (str): The name of the key in the state to check for its presence.
+
+    Args:
+        key_name (str): The name of the key to check in the graph's state. This is 
+                        used to determine the path the graph's execution should take.
+        node_name (str, optional): The unique identifier name for the node. Defaults 
+                                   to "ConditionalNode".
+
+    """
+
+    def __init__(self):
+        """
+        Initializes an empty ConditionalNode.
+        """
+        #super().__init__(node_name, "node", input, output, 2, node_config)
+        pass
+
+
+    def execute(self, state: dict) -> dict:
+        """
+        Checks if the specified key is present in the state and decides the next node accordingly.
+
+        Args:
+            state (dict): The current state of the graph.
+
+        Returns:
+            str: The name of the next node to execute based on the presence of the key.
+        """
+
+        pass
