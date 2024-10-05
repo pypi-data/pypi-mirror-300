d1067f2d649d1e47006494b7d67a553ea9aca082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:45 +0000

    Add package Ligare.platform/0.4.0/Ligare.platform-0.4.0-py3-none-any.whl

---
+++ packages/Ligare.platform/Ligare.platform-0.4.0-py3-none-any.whl/Ligare/platform/feature_flag/__init__.py
@@ -0,0 +1,15 @@
+from .caching_feature_flag_router import CachingFeatureFlagRouter
+from .caching_feature_flag_router import FeatureFlag as CacheFeatureFlag
+from .db_feature_flag_router import DBFeatureFlagRouter
+from .db_feature_flag_router import FeatureFlag as DBFeatureFlag
+from .feature_flag_router import FeatureFlag, FeatureFlagChange, FeatureFlagRouter
+
+__all__ = (
+    "FeatureFlagRouter",
+    "CachingFeatureFlagRouter",
+    "DBFeatureFlagRouter",
+    "FeatureFlag",
+    "CacheFeatureFlag",
+    "DBFeatureFlag",
+    "FeatureFlagChange",
+)
