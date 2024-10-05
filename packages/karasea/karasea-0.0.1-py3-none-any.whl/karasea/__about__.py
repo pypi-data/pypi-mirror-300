d417db521ef2a08ac6d94715b018d0a812f84073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:04:22 +0000

    Add package karasea/0.0.1/karasea-0.0.1-py3-none-any.whl

---
+++ packages/karasea/karasea-0.0.1-py3-none-any.whl/karasea/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Pacidus <pacidus@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ packages/karasea/karasea-0.0.1-py3-none-any.whl/karasea/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Pacidus <pacidus@gmail.com>
+#
+# SPDX-License-Identifier: MIT
