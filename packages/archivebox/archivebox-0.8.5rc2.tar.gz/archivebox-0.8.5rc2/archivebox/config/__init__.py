4b24279b0c7bf24604ad1e3fa1e010731d893a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:04:22 +0000

    Add package archivebox/0.8.5rc2/archivebox-0.8.5rc2.tar.gz

---
+++ packages/archivebox/archivebox-0.8.5rc2.tar.gz/archivebox-0.8.5rc2/archivebox/config/__init__.py
@@ -0,0 +1,27 @@
+__package__ = 'archivebox.config'
+
+from .constants import CONSTANTS, CONSTANTS_CONFIG, PACKAGE_DIR, DATA_DIR, ARCHIVE_DIR, VERSION
+from .defaults import (
+    SHELL_CONFIG,
+    STORAGE_CONFIG,
+    GENERAL_CONFIG,
+    SERVER_CONFIG,
+    ARCHIVING_CONFIG,
+    SEARCH_BACKEND_CONFIG,
+)
+
+
+__all__ = [
+    'CONSTANTS',
+    'PACKAGE_DIR',
+    'DATA_DIR',
+    'ARCHIVE_DIR',
+    'VERSION',
+    'SHELL_CONFIG',
+    'STORAGE_CONFIG',
+    'GENERAL_CONFIG',
+    'SERVER_CONFIG',
+    'ARCHIVING_CONFIG',
+    'SEARCH_BACKEND_CONFIG',
+    'CONSTANTS_CONFIG',
+]
