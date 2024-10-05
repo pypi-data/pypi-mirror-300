5cc75025aac11472541a2a9b62d7cb359e5dc5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:08:15 +0000

    Add package pylogicalprograms/1.1/pylogicalprograms-1.1-py3-none-any.whl

---
+++ packages/pylogicalprograms/pylogicalprograms-1.1-py3-none-any.whl/pylogicalprograms/__init__.py
@@ -0,0 +1,18 @@
+from .numberdigitcount import *
+from .factorial import *
+from .multiplicationtable import *
+from .integertoroman import *
+from .numbertoword import *
+from .binarysearch import *
+from .linearsearch import *
+from .swaptwonumbers import *
+from .zigzagconvertion import *
+from .missingnumber import *
+from .stringreverseandpalindrome import *
+from .fibonacciseries import *
+from .armstrongnumber import *
+
+__all__ = ['numberDigitCount', 'NumberDigitCountRecursion', 'factorial', 'multiplicationTableWithRange',
+           'multiplicationSpecificTable', 'integerToRoman', 'number_to_words', 'binarySearch', 'swapTwoNumWithTemp',
+           'swapTwoNumWithoutTemp', 'zigzagconvertion', 'missingNumber', 'linearSearch', 'stringReverse1', 'stringReverse2',
+           'stringPalindrome', 'numberPalindrome', 'fibonacciseries', 'fibonacciSeriesWithRecurtion', 'armstrongNumber']
