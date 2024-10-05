6c663d786b6c4ac44865ece7aa10a717564166e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:07:28 +0000

    Add package openlineage-sql/1.23.0/openlineage_sql-1.23.0.tar.gz

---
+++ packages/openlineage-sql/openlineage_sql-1.23.0.tar.gz/openlineage_sql-1.23.0/iface-py/script/build-macos.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Copyright 2018-2024 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+#
+# Build script for OpenLineage SQL parser.
+# It's assumed that it will be run on MacOS
+set -e
+
+if [ -x "$(command -v /usr/local/opt/python@3.8/bin/python3)" ]; then
+  /usr/local/opt/python@3.8/bin/python3 -m venv .env
+elif [ -x "$(command -v /usr/local/bin/python3.8)" ]; then
+  /usr/local/bin/python3.8 -m venv .env
+elif [ -x "$(command -v python3.8)" ]; then
+  python3.8 -m venv .env
+else
+  python -m venv .env
+fi
+
+source .env/bin/activate
+source $HOME/.cargo/env
+
+# Disable incremental compilation, since it causes issues.
+export CARGO_INCREMENTAL=0
+
+# Run test if indicated to do so.
+if [[ $RUN_TESTS = true ]]; then
+  cargo test --no-default-features
+  cargo clippy --all-targets --all-features -- -D warnings
+  cargo fmt -- --check
+fi
+
+# Build release wheels
+if [[ -d "./iface-py" ]]
+then
+  cd iface-py
+fi
+maturin build --target universal2-apple-darwin --out target/wheels --release --strip
+
+echo "Package build, trying to import"
+echo "Platform:"
+python -c "from distutils import util; print(util.get_platform())"
+# Verify that it imports and works properly
+python -m pip install openlineage-sql --no-index --find-links target/wheels --force-reinstall
+python -c "from openlineage_sql import parse, ColumnLineage; import sys; sys.exit(len(parse([\"SELECT b.a from b\"]).column_lineage) != 1)"
+echo "all good"
---
+++ packages/openlineage-sql/openlineage_sql-1.23.0.tar.gz/openlineage_sql-1.23.0/iface-py/script/setup-macos.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Copyright 2018-2024 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+#
+# Install script for building OpenLineage SQL parser.
+# It's assumed that it will be run on MacOS
+set -e
+
+echo "Installing homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+echo "Installing Python 3.8"
+brew install python@3.8
+
+echo "Installing Rust"
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+source $HOME/.cargo/env
+
+rustup target add aarch64-apple-darwin
+rustup target add x86_64-apple-darwin
+
+# Maturin is build tool that we're using. It can build python wheels based on standard Rust Cargo.toml.
+echo "Installing Maturin"
+if [ -x "$(command -v /usr/local/opt/python@3.8/bin/python3)" ]; then
+  /usr/local/opt/python@3.8/bin/python3 -m pip install maturin
+elif [ -x "$(command -v /usr/local/bin/python3.8)" ]; then
+  /usr/local/bin/python3.8 -m pip install maturin
+elif [ -x "$(command -v python3.8)" ]; then
+  python3.8 -m pip install maturin
+else
+  python -m pip install maturin
+fi
