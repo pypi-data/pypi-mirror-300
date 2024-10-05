9a83ae71ae5f360f77150d0edeb59c079f0cf052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 05:04:35 +0000

    Add package django-zero-theme/0.1.15/django-zero-theme-0.1.15.tar.gz

---
+++ packages/django-zero-theme/django-zero-theme-0.1.15.tar.gz/django-zero-theme-0.1.15/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-zero-theme',
+    version='0.1.15',
+    packages=find_packages(),
+    include_package_data=True,  # Include static and templates
+    license='MIT License',
+    description='A reusable Django app for ...',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/farjanul/django-zero-theme',
+    author='Farjanul Nayem',
+    author_email='mr.farjanul@gmail.com',
+    install_requires=[
+        'django>=3.2',
+        'django-import-export>=4.1.1',
+    ],
+    extras_require={
+        'dev': ['pytest', 'tox', 'black'],  # Optional dependencies for development
+    },
+    project_urls={
+        'Source': 'https://github.com/farjanul/django-zero-theme',
+        'Issue Tracker': 'https://github.com/farjanul/django-zero-theme/issues',  # GitHub Issues page
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
