1757851952241d24936cb41e126aeed5d690c903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:04:11 +0000

    Add package aws-cryptographic-material-providers/10.0.0/aws-cryptographic-material-providers-10.0.0.tar.gz

---
+++ packages/aws-cryptographic-material-providers/aws-cryptographic-material-providers-10.0.0.tar.gz/aws-cryptographic-material-providers-10.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="aws-cryptographic-material-providers",
+    version="10.0.0",
+    author="aws crypto",
+    author_email="aws-cryptography@aws.com",
+    description="aws-cryptographic-material-providers",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
