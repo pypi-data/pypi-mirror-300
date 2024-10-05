33e151812cc1083aa4906c4b441e8ef2b2c69366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:37 +0000

    Add package wavToText/0.0.3/wavToText-0.0.3.tar.gz

---
+++ packages/wavToText/wavToText-0.0.3.tar.gz/wavToText-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from typing import List
+from setuptools import find_packages, setup
+
+
+def get_requirements(requi_file='requirements.txt')->List[str]:
+    try:
+        with open(requi_file, 'r') as f:
+            requi = f.readlines()
+    except Exception as e:
+        raise Exception(f"Error reading {requi_file} file: {e}")
+        
+    requirements = [req.replace("\n", '') for req in requi]
+    if '-e .' in requirements:
+        requirements.remove('-e .')
+    print(requirements)
+    return requirements
+
+
+setup(
+    name = 'wavToText',
+    version = '0.0.3',
+    author = 'LAKHAL Badr',
+    author_email = 'lakhalbadr2@gmail.com',
+    description = 'Conevrting english wav audio into text.',
+    long_description = 'Voice recognition using a pretrained model called DeepSpeech v0.9.3 to convert English wav audio into text.',
+    packages = find_packages(),
+    install_requires = get_requirements(),
+    python_requires = '==3.9.10'
+)
