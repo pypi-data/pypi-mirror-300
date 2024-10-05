f58c2cf22129d367cd2a47f268a6b2ddf4ceb66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 05:03:41 +0000

    Add package spectquant/0.0.1/spectquant-0.0.1-py3-none-any.whl

---
+++ packages/spectquant/spectquant-0.0.1-py3-none-any.whl/spectquant/__init__.py
@@ -0,0 +1,25 @@
+# spectquant/__init__.py
+
+"""
+SpectQuant: \
+    A Specialized Package for Extracting Image Features \
+    for Cardiac Amyloidosis Quantification on SPECT.
+"""
+
+__version__ = "0.0.1"
+__author__ = "Markus Köfler"
+__all__ = [
+    "SUV",
+    "TBR",
+    "SegmentVolume",
+    "UptakeVol",
+    "SeptumVol",
+    "create_segs"
+]
+
+from .create_segs import create_segs
+from .suv import SUV
+from .tbr import TBR
+from .volume import SegmentVolume
+from .uptake import UptakeVol
+from .septum import SeptumVol
