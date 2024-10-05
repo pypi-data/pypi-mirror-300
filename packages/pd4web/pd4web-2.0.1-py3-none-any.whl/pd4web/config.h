f158f44133f465fbfdeebce750a4fed22fa691f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 05:04:24 +0000

    Add package pd4web/2.0.1/pd4web-2.0.1-py3-none-any.whl

---
+++ packages/pd4web/pd4web-2.0.1-py3-none-any.whl/pd4web/config.h
@@ -0,0 +1,17 @@
+// This is automatically generated code from pd4web.py script
+
+// Project Name
+#define PD4WEB_PROJECT_NAME "Test10"
+
+// Audio Config
+#define PD4WEB_CHS_IN 0
+#define PD4WEB_CHS_OUT 2
+#define PD4WEB_SR 48000
+
+// GUI Interface
+#define PD4WEB_GUI true
+#define PD4WEB_FPS 60
+#define PD4WEB_AUTO_THEME true
+
+// Midi
+#define PD4WEB_MIDI true
