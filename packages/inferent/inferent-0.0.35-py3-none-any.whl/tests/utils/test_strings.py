83b60a7f88f737f41857254e8d62ca36329b9600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:37 +0000

    Add package inferent/0.0.35/inferent-0.0.35-py3-none-any.whl

---
+++ packages/inferent/inferent-0.0.35-py3-none-any.whl/tests/utils/test_strings.py
@@ -0,0 +1,22 @@
+import unittest
+
+from inferent.utils import ct
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_ct(self):
+        ### edge cases
+        self.assertEqual(ct(None), "")
+        self.assertEqual(ct(""), "")
+
+        ### encoding
+        self.assertEqual(ct("foo\xfc"), "foo")  # Non UTF8
+        self.assertEqual(ct("foo\u0627"), "foo")  # UTF8, non-ascii
+        self.assertEqual(ct("f$oo, bar"), "foo bar")  # punctuation
+        self.assertEqual(ct("FOO BAR"), "foo bar")  # lowercase
+        self.assertEqual(ct("  foo  bar   "), "foo  bar")  # strip
+
+
+if __name__ == "__main__":
+    unittest.main()
