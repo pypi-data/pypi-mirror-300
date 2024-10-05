8a9066ed4a7e4c8ca20c742160eae67eab53deb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 04:03:18 +0000

    Add package ama-xiv-combat-sim/2024.10.4.3/ama_xiv_combat_sim-2024.10.4.3.tar.gz

---
+++ packages/ama-xiv-combat-sim/ama_xiv_combat_sim-2024.10.4.3.tar.gz/ama_xiv_combat_sim-2024.10.4.3/ama_xiv_combat_sim/simulator/skills/add_tank_skills_to_skill_library.py
@@ -0,0 +1,23 @@
+import ama_xiv_combat_sim.simulator.game_data.patch_655.class_skills.tank as patch655
+import ama_xiv_combat_sim.simulator.game_data.patch_70.class_skills.tank as patch70
+import ama_xiv_combat_sim.simulator.game_data.patch_701.class_skills.tank as patch701
+import ama_xiv_combat_sim.simulator.game_data.patch_705.class_skills.tank as patch705
+
+def add_tank_skills_to_skill_library(skill_library, version="6.55"):
+    match version:
+        case "6.55":
+            patch_use = patch655
+        case "7.0":
+            patch_use = patch70
+        case "7.01":
+            patch_use = patch701
+        case "7.05":
+            patch_use = patch705
+        case _:
+            raise RuntimeError(f"Bad version: {version}")
+
+    skill_library = patch_use.add_war_skills(skill_library)
+    skill_library = patch_use.add_gnb_skills(skill_library)
+    skill_library = patch_use.add_pld_skills(skill_library)
+    skill_library = patch_use.add_drk_skills(skill_library)
+    return skill_library
