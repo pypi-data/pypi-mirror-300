aed787d87abe2b4d120180a58172148e4e96773b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:03:57 +0000

    Add package prefect/3.0.5.dev3/prefect-3.0.5.dev3.tar.gz

---
+++ packages/prefect/prefect-3.0.5.dev3.tar.gz/prefect-3.0.5.dev3/src/prefect/server/ui/assets/usePageTitle-DL8xs9_z.js
@@ -0,0 +1,2 @@
+import{i as a,bA as c,m as i}from"./index-DOmrNEgj.js";function u(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>i(e)).filter(e=>e!==null).join(" • "));c(()=>document.title=o.value)}export{u};
+//# sourceMappingURL=usePageTitle-DL8xs9_z.js.map
