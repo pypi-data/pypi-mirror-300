c1dff3bcc2faca7d7221c804b8a6e2726055493e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 05:04:34 +0000

    Add package py-populate-dcim/0.1.2.1/py_populate_dcim-0.1.2.1-py3-none-any.whl

---
+++ packages/py-populate-dcim/py_populate_dcim-0.1.2.1-py3-none-any.whl/py_populate_dcim/api/views.py
@@ -0,0 +1,20 @@
+# api/views.py
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.parsers import FormParser
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication, TokenAuthentication
+
+from py_populate_dcim.api.models import RefreshRequest
+from .serializers import RefreshRequestSerializer
+
+
+class RefreshRequestViewSet(ModelViewSet):
+    """API viewset for triggering refresh of Etherflow's Python Populate DCIM program."""
+    # authentication_classes=[BasicAuthentication, TokenAuthentication]
+
+    queryset = RefreshRequest.objects.all()
+    serializer_class = RefreshRequestSerializer
+    parser_classes = [FormParser]
+    
+    # def post(self, request, format=None):
+    #     return Response({'received data': request.data})
