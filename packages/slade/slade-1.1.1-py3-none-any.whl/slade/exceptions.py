81e14bd7ca08864b3e0d04669a3f33e95d6b85e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:07:17 +0000

    Add package slade/1.1.1/slade-1.1.1-py3-none-any.whl

---
+++ packages/slade/slade-1.1.1-py3-none-any.whl/slade/exceptions.py
@@ -0,0 +1,9 @@
+
+
+class SladeException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class QueryFailedException(SladeException):
+    def __init__(self, message):
+        super().__init__(message)
