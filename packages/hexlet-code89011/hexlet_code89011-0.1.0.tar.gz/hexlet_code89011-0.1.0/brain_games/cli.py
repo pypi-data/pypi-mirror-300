9535e80e4afbfad3618704b84554339d8a1134cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 05:04:03 +0000

    Add package hexlet-code89011/0.1.0/hexlet_code89011-0.1.0.tar.gz

---
+++ packages/hexlet-code89011/hexlet_code89011-0.1.0.tar.gz/hexlet_code89011-0.1.0/brain_games/cli.py
@@ -0,0 +1,9 @@
+#приветствие пользователя
+def welcom_user():
+    print("Welcom ot the barain games!")
+    name = input("May I have your name? ")
+    print(f'Hello, {name}!')
+
+
+def welcome_user():
+    return None
