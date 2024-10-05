43a28cc1174cc4122692c49f9d1586c70d31ef0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:06:40 +0000

    Add package thecodecrate-pipeline/1.9.0/thecodecrate_pipeline-1.9.0-py3-none-any.whl

---
+++ packages/thecodecrate-pipeline/thecodecrate_pipeline-1.9.0-py3-none-any.whl/thecodecrate_pipeline/partials/with_pipeline_processor/processor.py
@@ -0,0 +1,38 @@
+from abc import abstractmethod
+import inspect
+from typing import Any, Protocol
+
+from .processor_interface import (
+    ProcessorInterface as ImplementsProcessorInterface,
+)
+from ..with_base.type_payload import TPayload
+from ..with_base.type_pipeline_callable import PipelineCallable
+
+
+class Processor(
+    ImplementsProcessorInterface[TPayload],
+    Protocol[TPayload],
+):
+    @abstractmethod
+    async def process(
+        self,
+        payload: TPayload,
+        stages: list[PipelineCallable[TPayload, ...]],
+        *args: Any,
+        **kwds: Any,
+    ) -> TPayload:
+        pass
+
+    async def _call_stage(
+        self,
+        payload: TPayload,
+        stage: PipelineCallable[TPayload, ...],
+        *args: Any,
+        **kwds: Any,
+    ) -> TPayload:
+        result = stage(payload, *args, **kwds)
+
+        if inspect.isawaitable(result):
+            return await result
+
+        return result
