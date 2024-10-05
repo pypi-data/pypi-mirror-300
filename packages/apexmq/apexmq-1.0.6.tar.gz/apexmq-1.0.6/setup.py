c1718a7853188b31abced9a2f17578445cd64b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 15:03:55 +0000

    Add package apexmq/1.0.6/apexmq-1.0.6.tar.gz

---
+++ packages/apexmq/apexmq-1.0.6.tar.gz/apexmq-1.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="apexmq",
+    version="1.0.6",
+    description="A developer-friendly library for integrating RabbitMQ with Django applications.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Kavindu Harshitha",
+    author_email="kavindu@apexkv.com",
+    license="Custom License",
+    license_files=("LICENSE",),
+    url="https://github.com/apexkv/apexmq",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "pika>=1.1.0",
+        "django>=3.0.0",
+    ],
+    python_requires=">=3.6",
+)
