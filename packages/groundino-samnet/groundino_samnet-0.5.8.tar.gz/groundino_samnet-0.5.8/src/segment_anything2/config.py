5e9ad548ed4aa4b1345dcf8474e76242416a7a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:04 +0000

    Add package groundino-samnet/0.5.8/groundino_samnet-0.5.8.tar.gz

---
+++ packages/groundino-samnet/groundino_samnet-0.5.8.tar.gz/groundino_samnet-0.5.8/src/segment_anything2/config.py
@@ -0,0 +1,6 @@
+SAM2_MODELS = {
+    "sam2_t": "facebook/sam2-hiera-tiny",
+    "sam2_s": "facebook/sam2-hiera-small",
+    "sam2_b_plus": "facebook/sam2-hiera-base-plus",
+    "sam2_l": "facebook/sam2-hiera-large"
+}
