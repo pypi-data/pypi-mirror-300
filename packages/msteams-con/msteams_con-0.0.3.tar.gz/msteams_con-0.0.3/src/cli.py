402fd8b6da08b5041510906f60b2332bc14096f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 14:04:01 +0000

    Add package msteams-con/0.0.3/msteams_con-0.0.3.tar.gz

---
+++ packages/msteams-con/msteams_con-0.0.3.tar.gz/msteams_con-0.0.3/src/cli.py
@@ -0,0 +1,31 @@
+"""Microsoft Teams Connector Command Line Interface."""
+
+import fire
+
+from card_templates import ReleaseCard
+from msteams_connector import MSTeamsConnector
+
+
+class Pipeline:
+  def __init__(
+    self,
+    hook_url: str | None = None,
+    version: str | None = None,
+    text: str | None = None,
+  ):
+    self.msteams_connector = MSTeamsConnector(hook_url)
+    self.release_card = ReleaseCard(version)
+    self.send_release_card = self.msteams_connector.send_payload(
+      self.release_card.generate_release_card()
+    )
+    self.send_text = self.msteams_connector.send_payload(
+      self.msteams_connector.generate_text_payload(text)
+    )
+
+
+def main():
+  fire.Fire(Pipeline)
+
+
+if __name__ == "__main__":
+  main()
