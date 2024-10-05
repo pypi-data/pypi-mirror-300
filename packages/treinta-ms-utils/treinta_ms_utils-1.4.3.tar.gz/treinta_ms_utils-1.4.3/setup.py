35a4bbfcf136005626a68b35b1f06b6730df839d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:08:32 +0000

    Add package treinta-ms-utils/1.4.3/treinta_ms_utils-1.4.3.tar.gz

---
+++ packages/treinta-ms-utils/treinta_ms_utils-1.4.3.tar.gz/treinta_ms_utils-1.4.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='treinta_ms_utils',
+    version='1.4.3',  # Incremento de la versión
+    author='cristian.pinela',
+    author_email='cristian.pinela@treinta.co',
+    packages=find_packages(),
+    license='LICENSE',
+    description='paquete que permite a los microservicios interaccionar con otros MS',
+    long_description=open('README.md').read(),  # Asegurarse de que README.md no tiene errores de sintaxis
+    long_description_content_type='text/markdown',  # Especifica que la descripción larga está en Markdown
+    install_requires=[
+        'boto3',
+        'botocore',
+        'pytz'
+    ]
+)
