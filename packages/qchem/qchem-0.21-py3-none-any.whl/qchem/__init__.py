6eba2f96d9206e5215cb114ab22370ada2b2662c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 14:03:46 +0000

    Add package qchem/0.21/qchem-0.21-py3-none-any.whl

---
+++ packages/qchem/qchem-0.21-py3-none-any.whl/qchem/__init__.py
@@ -0,0 +1,30 @@
+# Import and expose the submodules and classes
+#from .Parser import Parser
+#from .Enums.OrcaDensityFunctional import OrcaDensityFunctional
+#from .Enums.OrcaBasisSet import OrcaBasisSet
+#from .Enums.OrcaCalculationTypes import OrcaCalculationType
+#from .OrcaCalculation import OrcaCalculation
+#from .XYZFile import XYZFile
+#from .Molecule import Molecule
+#from .ClusterCalculation import ClusterCalculation
+#from .Data.Constants import CovalentRadiiConstants, AtomicMassConstants
+#
+## Optionally, you can also expose submodules as needed
+#from . import Enums
+#from . import Data
+#
+## List of publicly available symbols for easier access when using `import *`
+#__all__ = [
+#    "Parser",
+#    "OrcaDensityFunctional",
+#    "OrcaBasisSet",
+#    "OrcaCalculationType",
+#    "OrcaCalculation",
+#    "XYZFile",
+#    "Molecule",
+#    "ClusterCalculation",
+#    "CovalentRadiiConstants",
+#    "AtomicMassConstants",
+#    "Enums",
+#    "Data"
+#]
