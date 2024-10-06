dca4dcd7781625d29ee436849c109ed4e84972f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:46 +0000

    Add package hyper-bot/0.79.1/hyper-bot-0.79.1.tar.gz

---
+++ packages/hyper-bot/hyper-bot-0.79.1.tar.gz/hyper-bot-0.79.1/Hyper/Listener.py
@@ -0,0 +1,23 @@
+import atexit
+
+from Hyper import Configurator, Events
+from Hyper.Utils.Screens import color_txt, rgb
+
+config = Configurator.BotConfig.get("hyper-bot")
+
+if config.protocol == "OneBot":
+    from Hyper.Adapters.OneBot import *
+elif config.protocol == "Satori":
+    from Hyper.Adapters.Satori import *
+elif config.protocol == "Lagrange":
+    from Hyper.Adapters.LagrangePy import *
+elif config.protocol == "Kritor":
+    from Hyper.Adapters.Kritor import *
+
+Events.init()
+
+
+# @atexit.register
+# def not_running() -> None:
+#     if not listener_ran:
+#         print(color_txt("\n您没有运行监听器或客户端，HypeR Bot正在退出...\n", rgb(197, 37, 76)))
