927951e4d565a817bf5453bb77e8f0b2c3172173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:44 +0000

    Add package OAFuncs/0.0.24/OAFuncs-0.0.24.tar.gz

---
+++ packages/OAFuncs/OAFuncs-0.0.24.tar.gz/OAFuncs-0.0.24/OAFuncs/oa_cmap/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Liu Kun && 16031215@qq.com
+Date: 2024-09-17 16:09:20
+LastEditors: Liu Kun && 16031215@qq.com
+LastEditTime: 2024-10-06 18:10:04
+FilePath: \\Python\\My_Funcs\\OAFuncs\\OAFuncs\\oa_cmap\\__init__.py
+Description:  
+EditPlatform: vscode
+ComputerInfo: XPS 15 9510
+SystemInfo: Windows 11
+Python Version: 3.11
+'''
+
+
+
+from .oa_cmap import *
+
