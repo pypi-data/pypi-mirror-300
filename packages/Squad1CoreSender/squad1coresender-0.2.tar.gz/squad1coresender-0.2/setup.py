b0ed8e61f6de80da10873edd47dc5770d737f663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:53 +0000

    Add package Squad1CoreSender/0.2/squad1coresender-0.2.tar.gz

---
+++ packages/Squad1CoreSender/squad1coresender-0.2.tar.gz/squad1coresender-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Squad1CoreSender",  # Nombre del paquete
+    version="0.2",  # Versión del paquete
+    description="paquete utilizado para enviar mensajes entre los módulos y el core en un proyecto académico",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",  # Formato del README
+    author="matiasfelau",
+    author_email="matiasfelau@gmail.com",
+    url="https://github.com/matiasfelau/sender-python",  # URL del proyecto
+    packages=find_packages(),  # Encuentra todos los módulos automáticamente
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Tipo de licencia
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Versión mínima de Python requerida
+)
