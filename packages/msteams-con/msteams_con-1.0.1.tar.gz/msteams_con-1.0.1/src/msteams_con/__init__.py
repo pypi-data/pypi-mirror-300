fd8ee35490b269122b461817f4ac4be996f2b152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:32 +0000

    Add package msteams-con/1.0.1/msteams_con-1.0.1.tar.gz

---
+++ packages/msteams-con/msteams_con-1.0.1.tar.gz/msteams_con-1.0.1/src/msteams_con/__init__.py
@@ -0,0 +1,6 @@
+from .adaptive_card import AdaptiveCard
+from .logger import Logger
+from .msteams_connector import MSTeamsConnector
+
+__all__ = ["AdaptiveCard", "MSTeamsConnector", "Logger"]
+__version__ = "1.0.1"
