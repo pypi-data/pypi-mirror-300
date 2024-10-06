7942df73c420ed3abe93807313bb0406df70871b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:54 +0000

    Add package NUCS/0.10.1/NUCS-0.10.1-py3-none-any.whl

---
+++ packages/NUCS/NUCS-0.10.1-py3-none-any.whl/nucs/propagators/min_geq_propagator.py
@@ -0,0 +1,56 @@
+###############################################################################
+# __   _            _____    _____
+# | \ | |          / ____|  / ____|
+# |  \| |  _   _  | |      | (___
+# | . ` | | | | | | |       \___ \
+# | |\  | | |_| | | |____   ____) |
+# |_| \_|  \__,_|  \_____| |_____/
+#
+# Fast constraint solving in Python  - https://github.com/yangeorget/nucs
+#
+# Copyright 2024 - Yan Georget
+###############################################################################
+import numpy as np
+from numba import njit  # type: ignore
+from numpy.typing import NDArray
+
+from nucs.constants import MAX, MIN, PROP_CONSISTENCY, PROP_ENTAILMENT, PROP_INCONSISTENCY
+from nucs.numpy import new_triggers
+
+
+def get_complexity_min_geq(n: int, parameters: NDArray) -> float:
+    return 3 * n
+
+
+def get_triggers_min_geq(n: int, parameters: NDArray) -> NDArray:
+    """
+    Returns the triggers for this propagator.
+    :param n: the number of variables
+    :return: an array of triggers
+    """
+    triggers = new_triggers(n, False)
+    for i in range(n - 1):
+        triggers[i, MAX] = True
+    triggers[-1, MIN] = True
+    return triggers
+
+
+@njit(cache=True)
+def compute_domains_min_geq(domains: NDArray, parameters: NDArray) -> int:
+    """
+    Implements Min_i x_i >= x_{n-1}.
+    :param domains: the domains of the variables, x is an alias for domains
+    :param parameters: unused here
+    """
+    x = domains[:-1]
+    y = domains[-1]
+    if y[MAX] <= np.min(x[:, MIN]):
+        return PROP_ENTAILMENT
+    y[MAX] = min(y[MAX], np.min(x[:, MAX]))
+    if y[MIN] > y[MAX]:
+        return PROP_INCONSISTENCY
+    for i in range(len(x)):
+        x[i, MIN] = max(x[i, MIN], y[MIN])
+        if x[i, MAX] < x[i, MIN]:
+            return PROP_INCONSISTENCY
+    return PROP_CONSISTENCY
