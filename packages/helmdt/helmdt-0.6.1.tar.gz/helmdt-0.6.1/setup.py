010fe1b2d8afb9734148f3a0ebae1e1b2d98691a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:56 +0000

    Add package helmdt/0.6.1/helmdt-0.6.1.tar.gz

---
+++ packages/helmdt/helmdt-0.6.1.tar.gz/helmdt-0.6.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name="helmdt",
+    version="0.6.1",
+    author="Akaishi Toshiya",
+    author_email="Toshiya14@live.jp",
+    license="MIT",
+    description="A HELM Deployment toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toshiya14/helmdt",
+    py_modules=["helmdt"],
+    packages=find_packages(),
+    install_requires=[requirements],
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    entry_points="""
+        [console_scripts]
+        helmdt=helmdt:main
+    """,
+)
