63c8274f08c598868976332603a8fcc5896597bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:32 +0000

    Add package pylibrelinkup/0.2.1/pylibrelinkup-0.2.1.tar.gz

---
+++ packages/pylibrelinkup/pylibrelinkup-0.2.1.tar.gz/pylibrelinkup-0.2.1/src/pylibrelinkup/exceptions.py
@@ -0,0 +1,24 @@
+from .api_url import APIUrl
+
+
+class AuthenticationError(Exception):
+    """Raised when authentication fails."""
+
+    pass
+
+
+class RedirectError(Exception):
+    """Raised when a redirect is encountered during authentication. This is a signal to retry the request with the new region.
+    The new region is stored in the `region` attribute of the exception, which is an APIUrl enum value.
+    """
+
+    def __init__(self, region: APIUrl):
+        self.region = region
+        super().__init__(f"Redirected to {region}")
+
+
+class TermsOfUseError(Exception):
+    """Raised when the user needs to accept terms of use."""
+
+    def __init__(self):
+        super().__init__("User needs to accept terms of use. ")
---
+++ packages/pylibrelinkup/pylibrelinkup-0.2.1.tar.gz/pylibrelinkup-0.2.1/tests/test_client_authentication.py
@@ -0,0 +1,106 @@
+import json
+
+import pytest
+import requests
+import responses
+
+from pylibrelinkup.client import Client, AuthenticationError
+from pylibrelinkup.api_url import APIUrl
+from pylibrelinkup.exceptions import TermsOfUseError
+from pylibrelinkup.models.login import (
+    LoginResponse,
+)
+from tests.conftest import mocked_responses
+from tests.factories import LoginResponseFactory
+
+
+@pytest.mark.parametrize("api_url", APIUrl)
+def test_authenticate_raises_error_on_incorrect_login(
+    mocked_responses, api_url: APIUrl
+):
+    """Test that the authenticate method raises an error when the login fails."""
+    mocked_responses.add(
+        responses.POST,
+        f"{api_url.value}/llu/auth/login",
+        json={"not": "important"},
+        status=200,
+    )
+
+    client = Client(email="parp", password="parp", api_url=api_url)
+
+    with pytest.raises(AuthenticationError):
+        client.authenticate()
+
+
+@pytest.mark.parametrize("api_url", APIUrl)
+def test_authenticate_sets_token_on_correct_login(mocked_responses, api_url: APIUrl):
+    """Test that the authenticate method sets the token on a successful login."""
+    response = LoginResponseFactory.build()
+    response.data.authTicket.token = "parp"
+    assert isinstance(response, LoginResponse)
+
+    mocked_responses.add(
+        responses.POST,
+        f"{api_url.value}/llu/auth/login",
+        json=json.loads(response.model_dump_json()),
+        status=200,
+    )
+
+    client = Client(email="parp", password="parp", api_url=api_url)
+
+    client.authenticate()
+
+    assert client.token == "parp"
+
+
+@pytest.mark.parametrize("api_url", APIUrl)
+def test_authenticate_raises_error_on_invalid_response(
+    mocked_responses, api_url: APIUrl
+):
+    """Test that the authenticate method raises an error on invalid response."""
+    mocked_responses.add(
+        responses.POST,
+        f"{api_url.value}/llu/auth/login",
+        json={"invalid": "response"},
+        status=200,
+    )
+
+    client = Client(email="parp", password="parp", api_url=api_url)
+
+    with pytest.raises(AuthenticationError):
+        client.authenticate()
+
+
+@pytest.mark.parametrize("api_url", APIUrl)
+def test_authenticate_raises_error_on_http_error(mocked_responses, api_url: APIUrl):
+    """Test that the authenticate method raises an error on HTTP error."""
+    mocked_responses.add(
+        responses.POST,
+        f"{api_url.value}/llu/auth/login",
+        json={"error": "Unauthorized"},
+        status=401,
+    )
+
+    client = Client(email="parp", password="parp", api_url=api_url)
+
+    with pytest.raises(requests.exceptions.HTTPError):
+        client.authenticate()
+
+
+@pytest.mark.parametrize("api_url", APIUrl)
+def test_authenticate_raises_terms_of_use_error(
+    mocked_responses, api_url: APIUrl, terms_of_use_response_json
+):
+    """Test that the authenticate method raises a TermsOfUseError, when the user needs to accept terms of use."""
+
+    mocked_responses.add(
+        responses.POST,
+        f"{api_url.value}/llu/auth/login",
+        json=terms_of_use_response_json,
+        status=200,
+    )
+
+    client = Client(email="parp", password="parp", api_url=api_url)
+
+    with pytest.raises(TermsOfUseError):
+        client.authenticate()
