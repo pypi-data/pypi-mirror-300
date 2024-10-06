86a6a81db42864364a6821338c2e1fa91a80ebee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:32 +0000

    Add package OAFuncs/0.0.13/OAFuncs-0.0.13.tar.gz

---
+++ packages/OAFuncs/OAFuncs-0.0.13.tar.gz/OAFuncs-0.0.13/OAFuncs/OAFuncs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Liu Kun && 16031215@qq.com
+Date: 2024-09-17 16:18:38
+LastEditors: Liu Kun && 16031215@qq.com
+LastEditTime: 2024-09-17 16:18:40
+FilePath: \\Python\\My_Funcs\\OAFuncs\\OAFuncs\\OAFuncs.py
+Description:  
+EditPlatform: vscode
+ComputerInfo: XPS 15 9510
+SystemInfo: Windows 11
+Python Version: 3.11
+'''
+
+import OAFuncs
+
+if __name__ == '__main__':
+    print('OAFuncs')
+    # file.file()
+    # nc.nc()
+    # print('OAFuncs.py')
---
+++ packages/OAFuncs/OAFuncs-0.0.13.tar.gz/OAFuncs-0.0.13/OAFuncs/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Liu Kun && 16031215@qq.com
+Date: 2024-09-17 16:09:20
+LastEditors: Liu Kun && 16031215@qq.com
+LastEditTime: 2024-10-06 17:08:29
+FilePath: \\Python\\My_Funcs\\OAFuncs\\OAFuncs\\__init__.py
+Description:  
+EditPlatform: vscode
+ComputerInfo: XPS 15 9510
+SystemInfo: Windows 11
+Python Version: 3.11
+'''
+
+
+from OAFuncs import *
+from OAFuncs import oa_cmap, oa_data, oa_draw, oa_file, oa_nc
