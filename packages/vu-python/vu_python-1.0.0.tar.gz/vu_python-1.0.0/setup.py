30f99ee90268c9d8fa8c8571f6ff97a203523bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 14:03:41 +0000

    Add package vu-python/1.0.0/vu_python-1.0.0.tar.gz

---
+++ packages/vu-python/vu_python-1.0.0.tar.gz/vu_python-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vu_python',
+    version='1.0.0',
+    author='vu',
+    author_email='vupham.020502@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3'
+    ]
+)
