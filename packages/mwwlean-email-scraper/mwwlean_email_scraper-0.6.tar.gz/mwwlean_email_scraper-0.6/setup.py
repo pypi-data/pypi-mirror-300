da8b8a99e39977ba0561706a95575703a5dee591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 14:04:08 +0000

    Add package mwwlean-email-scraper/0.6/mwwlean_email_scraper-0.6.tar.gz

---
+++ packages/mwwlean-email-scraper/mwwlean_email_scraper-0.6.tar.gz/mwwlean_email_scraper-0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='mwwlean_email_scraper',
+    description="A Python tool that extracts public email addresses from websites.",
+    author='mwwlean',
+    version='0.6',
+    packages=find_packages(),
+    install_requires=[
+        'requests',     # Add requests as a dependency
+        'pyfiglet',
+    ],
+    entry_points={
+        "console_scripts":[
+            "email_scraper = mwwlean_email_scraper:main",
+        ],
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
