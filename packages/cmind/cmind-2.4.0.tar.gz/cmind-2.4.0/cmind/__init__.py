0e45685f06736ff00df8f2d3be082945d9dc8492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:23 +0000

    Add package cmind/2.4.0/cmind-2.4.0.tar.gz

---
+++ packages/cmind/cmind-2.4.0.tar.gz/cmind-2.4.0/cmind/__init__.py
@@ -0,0 +1,10 @@
+# Collective Mind init
+#
+# Written by Grigori Fursin
+
+__version__ = "2.4.0"
+
+from cmind.core import access
+from cmind.core import error
+from cmind.core import halt
+from cmind.core import CM
