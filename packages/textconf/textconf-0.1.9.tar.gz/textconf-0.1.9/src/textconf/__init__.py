7c535fb27d0c24010546dbbe48f95957db057e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:04:03 +0000

    Add package textconf/0.1.9/textconf-0.1.9.tar.gz

---
+++ packages/textconf/textconf-0.1.9.tar.gz/textconf-0.1.9/src/textconf/__init__.py
@@ -0,0 +1,11 @@
+"""Jinja2-based text generation from configuration objects.
+
+Provide a flexible and powerful way to generate text from
+configuration objects using Jinja2 templates.
+It allows for easy creation, updating, and rendering of configuration
+objects, with support for custom methods and dynamic content.
+"""
+
+from .config import BaseConfig, Renderable
+
+__all__ = ["BaseConfig", "Renderable"]
