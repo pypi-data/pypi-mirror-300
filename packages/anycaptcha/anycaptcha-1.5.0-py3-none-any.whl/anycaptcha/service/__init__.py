eda64c3e32c3e12b6d02aa4d10ea157ad1bfa75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:36 +0000

    Add package anycaptcha/1.5.0/anycaptcha-1.5.0-py3-none-any.whl

---
+++ packages/anycaptcha/anycaptcha-1.5.0-py3-none-any.whl/anycaptcha/service/__init__.py
@@ -0,0 +1,43 @@
+import enum
+from . import (
+    anti_captcha,
+    azcaptcha,
+    captcha_guru,
+    cptch_net,
+    deathbycaptcha,
+    rucaptcha,
+    twocaptcha,
+    multibot_captcha,
+    sctg_captcha,
+    capmonster
+)
+
+
+class Service(enum.Enum):
+    """CAPTCHA solving service enum"""
+
+    ANTI_CAPTCHA        = "anti-captcha.com"
+    AZCAPTCHA           = "azcaptcha.com"
+    CAPTCHA_GURU        = "cap.guru"
+    CPTCH_NET           = "cptch.net"
+    DEATHBYCAPTCHA      = "deathbycaptcha.com"
+    RUCAPTCHA           = "rucaptcha.com"
+    TWOCAPTCHA          = "2captcha.com"
+    MULTIBOT_CAPTCHA    = "multibot.in"
+    SCTG_CAPTCHA        = "api.sctg.xyz"
+    CAPMONSTER          = "capmonster.cloud"
+
+
+# supported CAPTCHA solving services
+SOLVING_SERVICE = {
+    Service.ANTI_CAPTCHA: anti_captcha,
+    Service.AZCAPTCHA: azcaptcha,
+    Service.CAPTCHA_GURU: captcha_guru,
+    Service.CPTCH_NET: cptch_net,
+    Service.DEATHBYCAPTCHA: deathbycaptcha,
+    Service.RUCAPTCHA: rucaptcha,
+    Service.TWOCAPTCHA: twocaptcha,
+    Service.MULTIBOT_CAPTCHA: multibot_captcha,
+    Service.SCTG_CAPTCHA: sctg_captcha,
+    Service.CAPMONSTER: capmonster
+}
