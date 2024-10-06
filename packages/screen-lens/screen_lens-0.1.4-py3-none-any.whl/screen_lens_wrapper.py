07773ae61e48afef4bd6f7cd8e51dcdc99f039fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:32 +0000

    Add package screen-lens/0.1.4/screen_lens-0.1.4-py3-none-any.whl

---
+++ packages/screen-lens/screen_lens-0.1.4-py3-none-any.whl/screen_lens_wrapper.py
@@ -0,0 +1,15 @@
+import subprocess
+import importlib.resources as pkg_resources
+import os
+
+def run_shell_script():
+    # Locate the shell script using importlib.resources
+    with pkg_resources.path('screen_lens.bin', 'screen_lens.sh') as script_path:
+        script_path = str(script_path)
+
+        if not os.path.exists(script_path):
+            print(f"Error: Script not found at {script_path}")
+            exit(1)
+
+        # Execute the shell script
+        subprocess.call([script_path])
