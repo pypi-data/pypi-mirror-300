a00cc891be6bf81af883172cf421024c8d7b5fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:08 +0000

    Add package os-remover/0.1.1/os_remover-0.1.1.tar.gz

---
+++ packages/os-remover/os_remover-0.1.1.tar.gz/os_remover-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='os_remover',
+    version='0.1.1',
+    packages=find_packages(),
+    description='Easy OS remover for WIndows/MacOS/Linux. Has demo mode.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='George Andreyanov',
+    author_email='andreyanov.egor07@mail.ru',
+    url='https://github.com/panicattacksss/os_remover/tree/main',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    install_requires=required,
+)
