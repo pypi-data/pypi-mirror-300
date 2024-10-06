75f84d4d20ee49c37c82fea01868847f0af90f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:43 +0000

    Add package mwwlean-email-scraper/0.2/mwwlean_email_scraper-0.2.tar.gz

---
+++ packages/mwwlean-email-scraper/mwwlean_email_scraper-0.2.tar.gz/mwwlean_email_scraper-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mwwlean_email_scraper',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    entry_points={
+        "console_scripts":[
+            "email_scraper = mwwlean_email_scraper:main",
+        ],
+    }
+)
