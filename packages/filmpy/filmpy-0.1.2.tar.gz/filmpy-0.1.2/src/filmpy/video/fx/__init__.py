f6da65fc1413c29a6ec2963dcf50ded3e810cd53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:57 +0000

    Add package filmpy/0.1.2/filmpy-0.1.2.tar.gz

---
+++ packages/filmpy/filmpy-0.1.2.tar.gz/filmpy-0.1.2/src/filmpy/video/fx/__init__.py
@@ -0,0 +1,65 @@
+# import every video fx function
+
+from filmpy.video.fx.accel_decel import accel_decel
+from filmpy.video.fx.blackwhite import blackwhite
+from filmpy.video.fx.blink import blink
+from filmpy.video.fx.crop import crop
+from filmpy.video.fx.even_size import even_size
+from filmpy.video.fx.fadein import fadein
+from filmpy.video.fx.fadeout import fadeout
+from filmpy.video.fx.freeze import freeze
+from filmpy.video.fx.freeze_region import freeze_region
+from filmpy.video.fx.gamma_corr import gamma_corr
+from filmpy.video.fx.headblur import headblur
+from filmpy.video.fx.invert_colors import invert_colors
+from filmpy.video.fx.loop import loop
+from filmpy.video.fx.lum_contrast import lum_contrast
+from filmpy.video.fx.make_loopable import make_loopable
+from filmpy.video.fx.margin import margin
+from filmpy.video.fx.mask_and import mask_and
+from filmpy.video.fx.mask_color import mask_color
+from filmpy.video.fx.mask_or import mask_or
+from filmpy.video.fx.mirror_x import mirror_x
+from filmpy.video.fx.mirror_y import mirror_y
+from filmpy.video.fx.multiply_color import multiply_color
+from filmpy.video.fx.multiply_speed import multiply_speed
+from filmpy.video.fx.painting import painting
+from filmpy.video.fx.resize import resize
+from filmpy.video.fx.rotate import rotate
+from filmpy.video.fx.scroll import scroll
+from filmpy.video.fx.supersample import supersample
+from filmpy.video.fx.time_mirror import time_mirror
+from filmpy.video.fx.time_symmetrize import time_symmetrize
+
+__all__ = (
+    "accel_decel",
+    "blackwhite",
+    "blink",
+    "crop",
+    "even_size",
+    "fadein",
+    "fadeout",
+    "freeze",
+    "freeze_region",
+    "gamma_corr",
+    "headblur",
+    "invert_colors",
+    "loop",
+    "lum_contrast",
+    "make_loopable",
+    "margin",
+    "mask_and",
+    "mask_color",
+    "mask_or",
+    "mirror_x",
+    "mirror_y",
+    "multiply_color",
+    "multiply_speed",
+    "painting",
+    "resize",
+    "rotate",
+    "scroll",
+    "supersample",
+    "time_mirror",
+    "time_symmetrize",
+)
