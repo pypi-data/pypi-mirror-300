4b209570eb5accfecdc4c614aa1bc3f7ab4a651c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 14:04:03 +0000

    Add package py3dep/0.18.0/py3dep-0.18.0.tar.gz

---
+++ packages/py3dep/py3dep-0.18.0.tar.gz/py3dep-0.18.0/tests/test_exceptions.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import pytest
+from shapely import Polygon
+
+import py3dep
+from py3dep.exceptions import InputTypeError, InputValueError
+
+DEF_CRS = 4326
+GEOM = Polygon(
+    [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
+)
+
+
+def test_wrong_bbox():
+    bbox = (1, 2, 3)
+    with pytest.raises(InputTypeError, match="tuple of length 4"):
+        _ = py3dep.check_3dep_availability(bbox)
+
+
+def test_wrong_layer():
+    geom = GEOM.bounds
+    with pytest.raises(InputValueError, match="DEM"):
+        _ = py3dep.get_map("None", geom, 1000)
+
+
+def test_wrong_crs():
+    geom = GEOM.bounds
+    with pytest.raises(InputValueError, match="crs"):
+        _ = py3dep.get_map("DEM", geom, 1000, DEF_CRS, "ESRI:102003")
