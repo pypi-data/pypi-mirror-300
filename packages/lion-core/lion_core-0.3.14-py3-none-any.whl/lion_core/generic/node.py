ebc52abc777ecffd3547b868e35281044356ded6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:01 +0000

    Add package lion-core/0.3.14/lion_core-0.3.14-py3-none-any.whl

---
+++ packages/lion-core/lion_core-0.3.14-py3-none-any.whl/lion_core/generic/node.py
@@ -0,0 +1,12 @@
+from lionabc import Relational
+
+from lion_core.generic.component import Component
+
+
+class Node(Component, Relational):
+    pass
+
+
+__all__ = ["Node"]
+
+# File: lion_core/generic/node.py
