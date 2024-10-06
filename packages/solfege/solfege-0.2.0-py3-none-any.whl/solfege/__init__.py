407fef2eaebc399a8d52fcb65037619d5cab50f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:23 +0000

    Add package solfege/0.2.0/solfege-0.2.0-py3-none-any.whl

---
+++ packages/solfege/solfege-0.2.0-py3-none-any.whl/solfege/__init__.py
@@ -0,0 +1,10 @@
+"""Library to provide tooling around scales and their solfège."""
+
+from solfege.note import Note
+from solfege.scale import Scale, ScaleType
+
+__all__ = [
+    Note.__name__,
+    Scale.__name__,
+    ScaleType.__name__,
+]
