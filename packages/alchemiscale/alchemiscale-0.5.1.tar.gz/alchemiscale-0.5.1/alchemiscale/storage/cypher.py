e19b3e164f601dc12c7122262da398fccda7082c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 14:03:43 +0000

    Add package alchemiscale/0.5.1/alchemiscale-0.5.1.tar.gz

---
+++ packages/alchemiscale/alchemiscale-0.5.1.tar.gz/alchemiscale-0.5.1/alchemiscale/storage/cypher.py
@@ -0,0 +1,30 @@
+from alchemiscale import ScopedKey
+from typing import List, Optional
+
+
+def cypher_list_from_scoped_keys(scoped_keys: List[Optional[ScopedKey]]) -> str:
+    """Generate a Cypher list structure from a list of ScopedKeys, ignoring NoneType entries.
+
+    Parameters
+    ----------
+    scoped_keys
+        List of ScopedKeys to generate the Cypher list
+
+    Returns
+    -------
+    str
+        Cypher list
+    """
+
+    if not isinstance(scoped_keys, list):
+        raise ValueError("`scoped_keys` must be a list of ScopedKeys")
+
+    data = []
+    for scoped_key in scoped_keys:
+        if scoped_key:
+            data.append('"' + str(scoped_key) + '"')
+    return "[" + ", ".join(data) + "]"
+
+
+def cypher_or(items):
+    return "|".join(items)
