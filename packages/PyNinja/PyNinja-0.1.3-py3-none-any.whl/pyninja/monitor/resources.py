228e80557e2e0b76b09904abc25bc7aa09c2fd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:04 +0000

    Add package PyNinja/0.1.3/PyNinja-0.1.3-py3-none-any.whl

---
+++ packages/PyNinja/PyNinja-0.1.3-py3-none-any.whl/pyninja/monitor/resources.py
@@ -0,0 +1,119 @@
+import asyncio
+import json
+import logging
+import os
+from typing import Dict, List
+
+import psutil
+
+from pyninja.features import operations
+from pyninja.modules import models
+
+LOGGER = logging.getLogger("uvicorn.default")
+
+
+def map_docker_stats(json_data: Dict[str, str]) -> Dict[str, str]:
+    """Map the JSON data to a dictionary.
+
+    Args:
+        json_data: JSON data from the docker stats command.
+
+    Returns:
+        Dict[str, str]:
+        Returns a dictionary with container stats.
+    """
+    return {
+        "Container ID": json_data.get("ID"),
+        "Container Name": json_data.get("Name"),
+        "CPU": json_data.get("CPUPerc"),
+        "Memory": json_data.get("MemPerc"),
+        "Memory Usage": json_data.get("MemUsage"),
+        "Block I/O": json_data.get("BlockIO"),
+        "Network I/O": json_data.get("NetIO"),
+    }
+
+
+def get_cpu_percent(cpu_interval: int) -> List[float]:
+    """Get CPU usage percentage.
+
+    Args:
+        cpu_interval: Interval to get the CPU performance.
+
+    Returns:
+        List[float]:
+        Returns a list of CPU percentages.
+    """
+    return psutil.cpu_percent(interval=cpu_interval, percpu=True)
+
+
+async def get_docker_stats() -> List[Dict[str, str]]:
+    """Run the docker stats command asynchronously and parse the output.
+
+    Returns:
+        List[Dict[str, str]]:
+        Returns a list of key-value pairs with the container stat and value.
+    """
+    process = await asyncio.create_subprocess_shell(
+        'docker stats --no-stream --format "{{json .}}"',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    if stderr:
+        LOGGER.debug(stderr.decode().strip())
+        return []
+    return [
+        map_docker_stats(json.loads(line))
+        for line in stdout.decode().strip().splitlines()
+    ]
+
+
+# noinspection PyProtectedMember
+async def get_system_metrics() -> Dict[str, dict]:
+    """Async handler for virtual memory, swap memory disk usage and CPU load averages.
+
+    Returns:
+        Dict[str, dict]:
+        Returns a nested dictionary.
+    """
+    m1, m5, m15 = os.getloadavg() or (None, None, None)
+    return dict(
+        memory_info=psutil.virtual_memory()._asdict(),
+        swap_info=psutil.swap_memory()._asdict(),
+        load_averages=dict(m1=m1, m5=m5, m15=m15),
+    )
+
+
+async def system_resources() -> Dict[str, dict]:
+    """Gather system resources including Docker stats asynchronously.
+
+    Returns:
+        Dict[str, dict]:
+        Returns a nested dictionary.
+    """
+    system_metrics_task = asyncio.create_task(get_system_metrics())
+    docker_stats_task = asyncio.create_task(get_docker_stats())
+    service_stats_task = asyncio.create_task(
+        operations.service_monitor(models.env.services)
+    )
+    process_stats_task = asyncio.create_task(
+        operations.process_monitor(models.env.processes)
+    )
+
+    # CPU percent check is a blocking call and cannot be awaited, so run it in a separate thread
+    loop = asyncio.get_event_loop()
+    cpu_usage_task = loop.run_in_executor(
+        models.EXECUTOR, get_cpu_percent, *(models.MINIMUM_CPU_UPDATE_INTERVAL,)
+    )
+
+    system_metrics = await system_metrics_task
+    docker_stats = await docker_stats_task
+    service_stats = await service_stats_task
+    process_stats = await process_stats_task
+    cpu_usage = await cpu_usage_task
+
+    system_metrics["cpu_usage"] = cpu_usage
+    system_metrics["docker_stats"] = docker_stats
+    system_metrics["service_stats"] = service_stats
+    system_metrics["process_stats"] = process_stats
+    return system_metrics
