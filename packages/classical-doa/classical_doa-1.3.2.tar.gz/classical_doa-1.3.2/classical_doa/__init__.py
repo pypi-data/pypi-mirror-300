92256ae537ea12fe06ba9ccc251081f4513f1cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:10 +0000

    Add package classical-doa/1.3.2/classical_doa-1.3.2.tar.gz

---
+++ packages/classical-doa/classical_doa-1.3.2.tar.gz/classical_doa-1.3.2/classical_doa/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+__version__ = "1.3.2"
+__author__ = "Qian Xu"
+
+warnings.warn(
+    "classical_doa has been deprecated. Please use my new project doa_py: https://github.com/zhiim/doa_py."
+)
