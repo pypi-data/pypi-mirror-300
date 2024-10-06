d5ed98018b017b080e48ad5bc361bd6fe1d1e752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:33 +0000

    Add package mtmai/0.3.1052/mtmai-0.3.1052.tar.gz

---
+++ packages/mtmai/mtmai-0.3.1052.tar.gz/mtmai-0.3.1052/mtmai/agents/tools/tools.py
@@ -0,0 +1,95 @@
+from datetime import date, datetime
+from typing import Optional
+
+from langchain_core.runnables import RunnableConfig
+from langchain_core.tools import tool
+
+from mtmai.core.logging import get_logger
+from typing import Callable, Literal
+
+from langchain_core.messages import ToolMessage
+from pydantic import BaseModel, Field
+
+logger = get_logger()
+
+
+# Primary Assistant
+class ToFlightBookingAssistant(BaseModel):
+    """Transfers work to a specialized assistant to handle flight updates and cancellations."""
+
+    request: str = Field(
+        description="Any necessary followup questions the update flight assistant should clarify before proceeding."
+    )
+
+
+class ToBookCarRental(BaseModel):
+    """Transfers work to a specialized assistant to handle car rental bookings."""
+
+    location: str = Field(
+        description="The location where the user wants to rent a car."
+    )
+    start_date: str = Field(description="The start date of the car rental.")
+    end_date: str = Field(description="The end date of the car rental.")
+    request: str = Field(
+        description="Any additional information or requests from the user regarding the car rental."
+    )
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "location": "Basel",
+                "start_date": "2023-07-01",
+                "end_date": "2023-07-05",
+                "request": "I need a compact car with automatic transmission.",
+            }
+        }
+
+@tool
+def search_flights(
+    departure_airport: Optional[str] = None,
+    arrival_airport: Optional[str] = None,
+    start_time: Optional[date | datetime] = None,
+    end_time: Optional[date | datetime] = None,
+    limit: int = 20,
+) -> list[dict]:
+    """Search for flights based on departure airport, arrival airport, and departure time range."""
+    logger.info(
+        f"调用工具： search_flights: {departure_airport}, {arrival_airport}, {start_time}, {end_time}, {limit}"
+    )
+    return [{"title": "flight", "id": "id1"}]
+
+
+@tool
+def update_ticket_to_new_flight(
+    ticket_no: str, new_flight_id: int, *, config: RunnableConfig
+) -> str:
+    """Update the user's ticket to a new valid flight."""
+    configuration = config.get("configurable", {})
+    passenger_id = configuration.get("passenger_id", None)
+    if not passenger_id:
+        raise ValueError("No passenger ID configured.")
+
+    # TODO: 操作数据库实现具体逻辑
+    return "Ticket successfully updated to new flight."
+
+
+@tool
+def cancel_ticket(ticket_no: str, *, config: RunnableConfig) -> str:
+    """Cancel the user's ticket and remove it from the database."""
+    configuration = config.get("configurable", {})
+    passenger_id = configuration.get("passenger_id", None)
+    if not passenger_id:
+        raise ValueError("No passenger ID configured.")
+    # TODO: 操作数据库实现具体逻辑
+    return "Ticket successfully cancelled."
+
+
+update_flight_safe_tools = [search_flights]
+update_flight_sensitive_tools = [update_ticket_to_new_flight, cancel_ticket]
+update_flight_tools = update_flight_safe_tools + update_flight_sensitive_tools
+# update_flight_tools = update_flight_safe_tools + update_flight_sensitive_tools
+
+
+develop_safe_tools = [search_flights]
+develop_sensitive_tools = []
+develop_tools = develop_safe_tools + develop_sensitive_tools
