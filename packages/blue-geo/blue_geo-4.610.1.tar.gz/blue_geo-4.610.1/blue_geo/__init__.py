5c1feb789c6fc604ab61e689acab6ce5c7ce71ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 14:03:45 +0000

    Add package blue-geo/4.610.1/blue_geo-4.610.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.610.1.tar.gz/blue_geo-4.610.1/blue_geo/__init__.py
@@ -0,0 +1,15 @@
+# to avoid import complications.
+# from blue_objects.env import ABCLI_PUBLIC_PREFIX
+ABCLI_PUBLIC_PREFIX = "https://kamangir-public.s3.ca-central-1.amazonaws.com"
+
+NAME = "blue_geo"
+
+ICON = "🌐"
+
+DESCRIPTION = f"{ICON} AI for a Blue Planet."
+
+VERSION = "4.610.1"
+
+REPO_NAME = "blue-geo"
+
+MARQUEE = f"{ABCLI_PUBLIC_PREFIX}/2024-01-06-20-39-46-73614/2024-01-06-20-39-46-73614-2X.gif?raw=true"
