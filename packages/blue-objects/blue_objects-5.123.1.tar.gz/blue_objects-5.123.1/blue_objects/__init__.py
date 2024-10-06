2a293975c12095166321795b9e1ca89743b9aa59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:49 +0000

    Add package blue-objects/5.123.1/blue_objects-5.123.1.tar.gz

---
+++ packages/blue-objects/blue_objects-5.123.1.tar.gz/blue_objects-5.123.1/blue_objects/__init__.py
@@ -0,0 +1,13 @@
+NAME = "blue_objects"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} data objects for Bash."
+
+VERSION = "5.123.1"
+
+REPO_NAME = "blue-objects"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/raw/main/blue-objects/marquee.png?raw=true"
+)
