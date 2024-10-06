af2f1957f19aa799886b24fc8aff467985e896bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:37 +0000

    Add package indexify/0.2.10/indexify-0.2.10.tar.gz

---
+++ packages/indexify/indexify-0.2.10.tar.gz/indexify-0.2.10/indexify/functions_sdk/image.py
@@ -0,0 +1,34 @@
+class Image:
+    def __init__(self):
+        self._image_name = None
+
+        self._tag = "latest"
+
+        self._base_image = "python:3.10.15-slim-bookworm"
+
+        self._run_strs = []
+
+    def name(self, image_name):
+        self._image_name = image_name
+        return self
+
+    def tag(self, tag):
+        self._tag = tag
+        return self
+
+    def base_image(self, base_image):
+        self._base_image = base_image
+        return self
+
+    def run(self, run_str):
+        self._run_strs.append(run_str)
+        return self
+
+
+DEFAULT_IMAGE = (
+    Image()
+    .name("tensorlake/indexify-executor-default")
+    .base_image("python:3.10.15-slim-bookworm")
+    .tag("latest")
+    .run("pip install indexify")
+)
