b63ea68dd3120753a4e4d7a009a872d2e9693355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:42 +0000

    Add package SkyMapMod/0.0.6/skymapmod-0.0.6.tar.gz

---
+++ packages/SkyMapMod/skymapmod-0.0.6.tar.gz/skymapmod-0.0.6/src/SkyMapMod/airglow.py
@@ -0,0 +1,9 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+
+def spectrum():
+    from .airglow_spectrum import wave, intensity
+    print(wave, intensity)
---
+++ packages/SkyMapMod/skymapmod-0.0.6.tar.gz/skymapmod-0.0.6/src/SkyMapMod/airglow_spectrum.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+wave = np.array([1, 2, 3, 4, 5])
+intensity = np.array([5, 4, 3, 2, 1])
