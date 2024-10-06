86c93dcd693c70f41949035be6e1a9426c5f842f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:47 +0000

    Add package frouros/0.9.0/frouros-0.9.0.tar.gz

---
+++ packages/frouros/frouros-0.9.0.tar.gz/frouros-0.9.0/setup.py
@@ -0,0 +1,54 @@
+"""Package setup file."""
+
+import codecs
+
+import toml
+from setuptools import find_packages, setup
+
+
+def long_description() -> str:
+    """Read and return README as long description."""
+    with codecs.open("README.md", encoding="utf-8-sig") as f:
+        return f.read()
+
+
+# ground truth package metadata is loaded from pyproject.toml
+# for context see:
+#   - [PEP 621 -- Storing project metadata in pyproject.toml]
+#     (https://www.python.org/dev/peps/pep-0621)
+pyproject = toml.load("pyproject.toml")
+
+
+def setup_package() -> None:
+    """Set up package."""
+    setup(
+        author_email=pyproject["project"]["authors"][0]["email"],
+        author=pyproject["project"]["authors"][0]["name"],
+        description=pyproject["project"]["description"],
+        extras_require=pyproject["project"]["optional-dependencies"],
+        include_package_data=True,
+        install_requires=pyproject["project"]["dependencies"],
+        keywords=pyproject["project"]["keywords"],
+        classifiers=pyproject["project"]["classifiers"],
+        # FIXME: Find a way not to hardcode license
+        license="BSD-3-Clause",
+        long_description=long_description(),
+        long_description_content_type="text/markdown",
+        maintainer_email=pyproject["project"]["maintainers"][0]["email"],
+        maintainer=pyproject["project"]["maintainers"][0]["name"],
+        name=pyproject["project"]["name"],
+        packages=find_packages(
+            where=".",
+            exclude=["tests", "tests.*"],
+        ),
+        project_urls=pyproject["project"]["urls"],
+        python_requires=pyproject["project"]["requires-python"],
+        setup_requires=pyproject["build-system"]["requires"],
+        url=pyproject["project"]["urls"]["repository"],
+        version=pyproject["project"]["version"],
+        zip_safe=False,
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
