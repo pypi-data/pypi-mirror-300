f9e16befefa3d4f34aa638308842bccaaf2b4292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 03:11:36 +0000

    Add package algorithmetn/0.3/algorithmetn-0.3.tar.gz

---
+++ packages/algorithmetn/algorithmetn-0.3.tar.gz/algorithmetn-0.3/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='algorithmetn',
+    version='0.3',
+    packages=find_packages(),
+    description='A package for basic list operations and algorithms.',
+    author='Jaballah Karim',
+    author_email='lhackerk233@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
