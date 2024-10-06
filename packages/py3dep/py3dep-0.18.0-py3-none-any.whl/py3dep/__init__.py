8301e813ad3839879926a0fbddc5de65d5233977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 14:04:03 +0000

    Add package py3dep/0.18.0/py3dep-0.18.0-py3-none-any.whl

---
+++ packages/py3dep/py3dep-0.18.0-py3-none-any.whl/py3dep/__init__.py
@@ -0,0 +1,44 @@
+"""Top-level package for Py3DEP."""
+
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+
+from py3dep import exceptions
+from py3dep.print_versions import show_versions
+from py3dep.py3dep import (
+    add_elevation,
+    check_3dep_availability,
+    elevation_bycoords,
+    elevation_bygrid,
+    elevation_profile,
+    get_dem,
+    get_dem_vrt,
+    get_map,
+    query_3dep_sources,
+    static_3dep_dem,
+)
+from py3dep.utils import deg2mpm, fill_depressions
+
+try:
+    __version__ = version("py3dep")
+except PackageNotFoundError:
+    __version__ = "999"
+
+__all__ = [
+    "fill_depressions",
+    "get_map",
+    "check_3dep_availability",
+    "query_3dep_sources",
+    "deg2mpm",
+    "elevation_bycoords",
+    "elevation_bygrid",
+    "elevation_profile",
+    "static_3dep_dem",
+    "get_dem",
+    "get_dem_vrt",
+    "add_elevation",
+    "show_versions",
+    "exceptions",
+    "__version__",
+]
