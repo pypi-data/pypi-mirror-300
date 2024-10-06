7a2ae63564c1a7dc3d0dc7fa17b0813a2fa4979f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 14:03:59 +0000

    Add package SendEmailPy3/1.0.6/sendemailpy3-1.0.6.tar.gz

---
+++ packages/SendEmailPy3/sendemailpy3-1.0.6.tar.gz/sendemailpy3-1.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='SendEmailPy3',
+    packages=['send_email_smtp'],
+    version="1.0.6",
+    license="MIT",
+    description="Easy email sending for Python 3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Goutham Narayana Pedinedi",
+    author_email="codspecialops@gmail.com",
+    url="https://github.com/GouthamOfTheNP/send_email",
+    keywords=["send", "email", "python",],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
