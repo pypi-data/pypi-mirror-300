887859d27560a7763c10a54225f8cb628835e954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:44 +0000

    Add package modelo-linear-brasil/0.0.2/modelo_linear_brasil-0.0.2.tar.gz

---
+++ packages/modelo-linear-brasil/modelo_linear_brasil-0.0.2.tar.gz/modelo_linear_brasil-0.0.2/README.md
@@ -0,0 +1,2 @@
+# regressao_linear_brasil
+Neste repositório, disponibilizo um pacote em português para desenvolvimento de modelos de regressão linear. O pacote contém as principais métricas de validação como método da classe RegressaoLinearBRC, as quais são MAE, MSE, RMSE, R² e R² Ajustado.
---
+++ packages/modelo-linear-brasil/modelo_linear_brasil-0.0.2.tar.gz/modelo_linear_brasil-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modelo_linear_brasil',
+    version='0.0.2',
+    packages=find_packages(),
+    description='Uma classe para Regressão Linear em português, contendo as principais métricas da abordagem como métodos.',
+    author='Bruno Rodrigues Carloto',
+    author_email='brunnocarlotosjob@gmail.com',
+    url='https://github.com/bruno-rodrigues-carloto/regressao-linear-brasil/modelo_linear_brasil.py',
+    python_requires='>=3.6',
+)
