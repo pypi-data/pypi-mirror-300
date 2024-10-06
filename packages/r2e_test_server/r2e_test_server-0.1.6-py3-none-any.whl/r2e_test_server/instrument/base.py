fbe85cf99aa176f5ab495c153cfacf6d06dddbb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:03:41 +0000

    Add package r2e_test_server/0.1.6/r2e_test_server-0.1.6-py3-none-any.whl

---
+++ packages/r2e_test_server/r2e_test_server-0.1.6-py3-none-any.whl/r2e_test_server/instrument/base.py
@@ -0,0 +1,57 @@
+import json
+import inspect
+import functools
+
+
+class Instrumenter:
+    def __init__(self):
+        self.current_frame = None
+        self.previous_frame = None
+        self.output = None
+        pass
+
+    def instrument(self, func):
+        """Wrap the given function with the instrumentation logic."""
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            self.current_frame = inspect.currentframe()
+            if self.current_frame is not None:
+                self.previous_frame = self.current_frame.f_back
+
+            self.before_call(func, *args, **kwargs)
+            self.output = func(*args, **kwargs)
+            self.after_call(func, *args, **kwargs)
+            return self.output
+
+        return wrapper
+
+    def instrument_method(self, class_obj, method):
+        """Wrap the given method with the instrumentation logic."""
+        # get the method from the class
+        method = getattr(class_obj, method)
+
+        # instrument the method and set it back to the class
+        setattr(class_obj, method.__name__, self.instrument(method))
+        return class_obj
+
+    def caller_info(self):
+        """Return the caller's information."""
+        if self.previous_frame is None:
+            return None
+
+        caller_info = inspect.getframeinfo(self.previous_frame)
+        caller_info = {"func_name": caller_info[2], "lineno": caller_info[1]}
+        return caller_info
+
+    def before_call(self, func, *args, **kwargs):
+        """Hook method for doing something before the original function call."""
+        pass
+
+    def after_call(self, func, *args, **kwargs):
+        """Hook method for doing something after the original function call."""
+        pass
+
+    def dump_logs(self, file_path: str):
+        """Dump the collected information, if any, to a file."""
+        pass
---
+++ packages/r2e_test_server/r2e_test_server-0.1.6-py3-none-any.whl/r2e_test_server/testing/loader.py
@@ -0,0 +1,72 @@
+from typing import Any, Dict, List, Tuple
+from unittest import TestLoader, TestSuite, TestCase
+
+from r2e_test_server.testing.cleaner import R2ETestCleaner
+
+
+class R2ETestLoader:
+
+    @staticmethod
+    def load_tests(
+        test_cases: Dict[str, str], funclass_names: List[str], nspace: Dict[str, Any]
+    ) -> Tuple[Dict[str, TestSuite], Dict[str, Any]]:
+        """Load the test cases into test suites."""
+
+        test_suites = {}
+
+        for test_id, test_case in test_cases.items():
+            test_suite = R2ETestLoader.load_test(test_case, funclass_names, nspace)
+            test_suites[test_id] = test_suite
+
+        return test_suites, nspace
+
+    @staticmethod
+    def load_test(
+        test_case: str, funclass_names: List[str], nspace: Dict[str, Any]
+    ) -> TestSuite:
+        """Load a test case into a test suite and add it to the namespace."""
+
+        for funclass_name in funclass_names:
+            ref_name = f"reference_{funclass_name}"
+
+            try:
+                test_case = R2ETestCleaner.clean_test_case(
+                    test_case, funclass_name, ref_name
+                )
+            except Exception as e:
+                print("[ERROR] Could not load test case!")
+                raise
+
+        try:
+            R2ETestLoader.add_test_to_namespace(test_case, nspace)
+
+            test_suite, test_classes = R2ETestLoader.create_test_suite(nspace)
+            R2ETestLoader.clean_namespace(nspace, test_classes)
+        except Exception as e:
+            print("[ERROR] Could not load test case!")
+            raise
+        return test_suite
+
+    @staticmethod
+    def add_test_to_namespace(test_case: str, nspace: Dict[str, Any]):
+        """Add the test case to the namespace."""
+        exec(test_case, nspace, nspace)
+
+    @staticmethod
+    def create_test_suite(nspace: Dict[str, Any]) -> Tuple[TestSuite, List[str]]:
+        """Create a test suite from the test case."""
+        loader, test_suite = TestLoader(), TestSuite()
+        test_classes = []
+
+        for name, obj in nspace.items():
+            if isinstance(obj, type) and issubclass(obj, TestCase):
+                test_suite.addTest(loader.loadTestsFromTestCase(obj))
+                test_classes.append(name)
+
+        return test_suite, test_classes
+
+    @staticmethod
+    def clean_namespace(nspace: Dict[str, Any], test_classes: List[str]):
+        """Remove the test classes from the namespace."""
+        for name in test_classes:
+            nspace.pop(name, None)
