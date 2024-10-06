ced008c6781ea51e9996416e174add31d5c5442b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 04:04:10 +0000

    Add package accents-remover/0.1.2/accents_remover-0.1.2.tar.gz

---
+++ packages/accents-remover/accents_remover-0.1.2.tar.gz/accents_remover-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r') as file:
+    readme = file.read()
+ 
+setup(
+    name='accents_remover',
+    version='0.1.2',
+    license='MIT License',
+    author='Lincoln Oliver',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author_email='lincolnolive@gmail.com',
+    keywords='remove accents',
+    description='A function to remove string accents withouth changing other characters',
+    packages=['src.accents_remover'],
+    install_requires=[],
+    url='https://github.com/lincolnolivr/accents_remover-pylib.git'
+)
