26bec5e18524d07828f8cb97fd9bc87e64c5a68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-06 15:03:37 +0000

    Add package OAFuncs/0.0.22/OAFuncs-0.0.22.tar.gz

---
+++ packages/OAFuncs/OAFuncs-0.0.22.tar.gz/OAFuncs-0.0.22/OAFuncs/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Liu Kun && 16031215@qq.com
+Date: 2024-09-17 16:09:20
+LastEditors: Liu Kun && 16031215@qq.com
+LastEditTime: 2024-10-06 17:52:20
+FilePath: \\Python\\My_Funcs\\OAFuncs\\OAFuncs\\__init__.py
+Description:  
+EditPlatform: vscode
+ComputerInfo: XPS 15 9510
+SystemInfo: Windows 11
+Python Version: 3.11
+'''
+
+__all__ = ['oa_cmap', 'oa_data', 'oa_draw', 'oa_file', 'oa_nc']
+
+from OAFuncs import *
