1542013871e413b7c813df1785bbd36d88bd3ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:11:00 +0000

    Add package reglin-brasil/0.0.0.0/reglin_brasil-0.0.0.0.tar.gz

---
+++ packages/reglin-brasil/reglin_brasil-0.0.0.0.tar.gz/reglin_brasil-0.0.0.0/README.md
@@ -0,0 +1,2 @@
+# Regressao-Linear-Brasil
+Neste repositório, disponibilizo um pacote em português para desenvolvimento de modelos de regressão linear. O pacote contém as principais métricas de validação como método da classe RegressaoLinearBRC, as quais são MAE, MSE, RMSE, R² e R² Ajustado.
---
+++ packages/reglin-brasil/reglin_brasil-0.0.0.0.tar.gz/reglin_brasil-0.0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reglin_brasil',
+    version='0.0.0.0',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    description='Uma classe para Regressão Linear em português, contendo as principais métricas da abordagem como métodos.',
+    author='Bruno Rodrigues Carloto',
+    author_email='brunnocarlotosjob@gmail.com',
+    url='https://github.com/bruno-rodrigues-carloto/Regressao-Linear-Brasil',
+    python_requires='>=3.6',
+)
