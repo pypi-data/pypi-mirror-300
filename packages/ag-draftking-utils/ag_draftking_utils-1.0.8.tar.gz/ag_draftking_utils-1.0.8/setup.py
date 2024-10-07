eb882cc27dc10e6ab160746c14ee0e92cb043d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 14:04:30 +0000

    Add package ag-draftking-utils/1.0.8/ag_draftking_utils-1.0.8.tar.gz

---
+++ packages/ag-draftking-utils/ag_draftking_utils-1.0.8.tar.gz/ag_draftking_utils-1.0.8/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '1.0.8'
+DESCRIPTION = 'AG Draftking Utilities'
+LONG_DESCRIPTION = 'Simple Utility Functions called in most places'
+
+# Setting up
+setup(
+    name="ag_draftking_utils",
+    version=VERSION,
+    author="no thanks",
+    author_email="giantsrule93@yahoo.com>",
+    url="https://github.com/akashgoyal119/DraftKingUtils",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'sqlalchemy',
+        'pymysql',
+        'selenium',
+        ### packages needed but ones we don't want to include in build
+        ### due to a large file size... 
+        #'boto3',
+        #'pandas',
+        #'sklearn',
+        #'lightgbm'
+    ],
+    keywords=['python', 'sql'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
