da869229b3830b4ce83840bc7b6accb945aa918a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:04:28 +0000

    Add package pip-latest/0.1/pip-latest-0.1.tar.gz

---
+++ packages/pip-latest/pip-latest-0.1.tar.gz/pip-latest-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pip-latest',
+    version='0.1',
+    packages=find_packages(),
+    description='A package to upgrade pip to the latest version',
+    author='Babith G',
+    author_email='babithg@gmail.com',
+    url='https://github.com/babithg/pip-latest',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
