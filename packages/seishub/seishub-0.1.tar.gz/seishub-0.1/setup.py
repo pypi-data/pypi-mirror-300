e8d49d0cf2a5135df3d23cbcdb14ab91da0ed52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 14:05:03 +0000

    Add package seishub/0.1/seishub-0.1.tar.gz

---
+++ packages/seishub/seishub-0.1.tar.gz/seishub-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seishub',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here,
+        # for example: 'requests>=2.0',
+    ],
+    # Add all other necessary metadata
+)
+
