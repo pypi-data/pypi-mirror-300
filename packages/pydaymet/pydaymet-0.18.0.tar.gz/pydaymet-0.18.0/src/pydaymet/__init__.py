94f985d9199cb21c64257ffa05534c81c1a9a64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:54 +0000

    Add package pydaymet/0.18.0/pydaymet-0.18.0.tar.gz

---
+++ packages/pydaymet/pydaymet-0.18.0.tar.gz/pydaymet-0.18.0/src/pydaymet/__init__.py
@@ -0,0 +1,28 @@
+"""Top-level package for PyDaymet."""
+
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+
+from pydaymet import exceptions
+from pydaymet.core import Daymet, separate_snow
+from pydaymet.pet import potential_et
+from pydaymet.print_versions import show_versions
+from pydaymet.pydaymet import get_bycoords, get_bygeom, get_bystac
+
+try:
+    __version__ = version("pydaymet")
+except PackageNotFoundError:
+    __version__ = "999"
+
+__all__ = [
+    "Daymet",
+    "separate_snow",
+    "get_bycoords",
+    "get_bygeom",
+    "get_bystac",
+    "potential_et",
+    "show_versions",
+    "exceptions",
+    "__version__",
+]
