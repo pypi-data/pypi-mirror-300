6ddcbe65141ce2b7909870b4ea8cf3b56e10ba92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:37 +0000

    Add package pypdf-table-extraction/0.0.2/pypdf_table_extraction-0.0.2.tar.gz

---
+++ packages/pypdf-table-extraction/pypdf_table_extraction-0.0.2.tar.gz/pypdf_table_extraction-0.0.2/camelot/backends/ghostscript_backend.py
@@ -0,0 +1,21 @@
+class GhostscriptBackend:
+
+    def convert(self, pdf_path, png_path, resolution=300):
+        try:
+            import ghostscript
+        except RuntimeError:
+            raise OSError(
+                "Ghostscript is not installed. You can install it using the instructions"
+                " here: https://pypdf-table-extraction.readthedocs.io/en/latest/user/install-deps.html"
+            )
+
+        gs_command = [
+            "gs",
+            "-q",
+            "-sDEVICE=png16m",
+            "-o",
+            png_path,
+            f"-r{resolution}",
+            pdf_path,
+        ]
+        ghostscript.Ghostscript(*gs_command)
