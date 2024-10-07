bd8ae62f62e850ccf2016f2d56feb93cb08d95a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 14:05:01 +0000

    Add package OpenGIS/0.0.3/opengis-0.0.3.tar.gz

---
+++ packages/OpenGIS/opengis-0.0.3.tar.gz/opengis-0.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="OpenGIS",
+      version="0.0.3",
+      description="OpenGIS",
+      packages=["OpenGIS"],
+      author="wzj",
+      author_email="773598627@qq.com")
