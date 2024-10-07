65c8d90071e14ccdc1d226d6903abe86292e474b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:04:00 +0000

    Add package kelian/0.1.1/kelian-0.1.1-py3-none-any.whl

---
+++ packages/kelian/kelian-0.1.1-py3-none-any.whl/kelian/__init__.py
@@ -0,0 +1,49 @@
+# __init__.py
+
+"""
+kelian: Une bibliothèque Python de bout de code utiles
+-----------------------------------------------------
+Description:
+    Cette bibliothèque permet d'être plus rapide dans le développement Python en évitant de réinventer la roue.
+    Elle est composée de nombreux bouts de code couramment utilisés, tels que des fonctions d'utilitaires, des 
+    algorithmes classiques, des manipulations de données, et bien plus encore, afin de simplifier le développement
+    et améliorer la productivité.
+
+Auteur:
+    Kelian
+
+Licence:
+    MIT License (voir LICENSE pour plus de détails)
+
+Version:
+    0.1.0
+"""
+
+__version__ = "0.1.0"
+__author__ = "Kelian"
+__license__ = "MIT"
+
+from .encryption import alpha2dict, list2dict, encrypt, decrypt, encrypt_by_list, decrypt_by_list
+from .system import get_processor_details, get_motherboard_details, get_gpu_details, get_monitor_details, get_cd_drive_details, get_mouse_details, get_speaker_details, get_keyboard_details, get_hard_disk_details, get_ram_details
+from .utils import string2hash
+
+# Définir ce qui sera importé lors d'un 'from package import *'
+__all__ = [
+    "alpha2dict", 
+    "list2dict", 
+    "encrypt", 
+    "decrypt", 
+    "encrypt_by_list", 
+    "decrypt_by_list", 
+    "get_processor_details", 
+    "get_motherboard_details", 
+    "get_gpu_details", 
+    "get_monitor_details", 
+    "get_cd_drive_details", 
+    "get_mouse_details", 
+    "get_speaker_details", 
+    "get_keyboard_details", 
+    "get_hard_disk_details", 
+    "get_ram_details", 
+    "string2hash"
+]
