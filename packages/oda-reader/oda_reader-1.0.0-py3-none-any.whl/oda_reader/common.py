10731cce8bf8d8ce27b1f4f1fc984354fc895692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:04:32 +0000

    Add package oda-reader/1.0.0/oda_reader-1.0.0-py3-none-any.whl

---
+++ packages/oda-reader/oda_reader-1.0.0-py3-none-any.whl/oda_reader/common.py
@@ -0,0 +1,107 @@
+import logging
+from copy import deepcopy
+from io import StringIO
+from pathlib import Path
+
+import pandas as pd
+import requests
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+logger = logging.getLogger("oda_importer")
+
+
+class ImporterPaths:
+    """Class to store the paths to the data and output folders."""
+
+    project = Path(__file__).resolve().parent.parent
+    scripts = project / "oda_reader"
+    schemas = scripts / "schemas"
+    mappings = schemas / "mappings"
+
+
+def text_to_stringIO(response: requests.models.Response) -> StringIO:
+    """Convert the content of a response to bytes.
+
+    Args:
+        response (requests.models.Response): The response object from the API.
+
+    Returns:
+        StringIO: The content of the response as a stringIO object.
+
+    """
+    # Use BytesIO to handle the binary stream data
+    return StringIO(response.text)
+
+
+def get_data_from_api(url: str, compressed: bool = True) -> requests.models.Response:
+    """Download a CSV file from an API endpoint and return it as a DataFrame.
+
+    Args:
+        url (str): The URL of the API endpoint.
+        compressed (bool): Whether the data is fetched compressed. Strongly recommended.
+
+    Returns:
+        requests.models.Response: The response object from the API.
+    """
+
+    # Set the headers with gzip encoding (if required)
+    if compressed:
+        headers = {"Accept-Encoding": "gzip"}
+    else:
+        headers = {}
+
+    # Fetch the data with headers
+    logger.info(f"Fetching data from {url}")
+    response = requests.get(url, headers=headers)
+
+    if (response.status_code == 404) and (response.text == "NoRecordsFound"):
+        raise ConnectionError("No data found for the selected parameters.")
+
+    if (response.status_code == 500) and (response.text.find("not set to") > 0):
+        url = url.replace("public", "dcd-public")
+        response = requests.get(url, headers=headers)
+
+    # Ensure the request was successful
+    response.raise_for_status()
+
+    return response
+
+
+def api_response_to_df(
+    url: str, read_csv_options: dict = None, compressed: bool = True
+) -> pd.DataFrame:
+    """Download a CSV file from an API endpoint and return it as a DataFrame.
+
+    Args:
+        url (str): The URL of the API endpoint.
+        read_csv_options (dict): Options to pass to `pd.read_csv`.
+        compressed (bool): Whether the data is fetched compressed. Strongly recommended.
+
+    Returns:
+        pd.DataFrame: The data as a DataFrame.
+
+    """
+    # Set default options for read_csv
+    if read_csv_options is None:
+        read_csv_options = {}
+
+    # If asked for uncompressed data, return the data as is
+    if not compressed:
+        return pd.read_csv(url, **read_csv_options)
+
+    # Fetch the data from the API with compression headers
+    response = get_data_from_api(url=url, compressed=compressed)
+
+    # Convert the content to stringIO
+    data = text_to_stringIO(response)
+
+    # Return the data as a DataFrame
+    try:
+        d_ = deepcopy(data)
+        return pd.read_csv(d_, **read_csv_options)
+    except ValueError:
+        read_csv_options["dtype"]["CHANNEL"] = "string[pyarrow]"
+        read_csv_options["dtype"]["MODALITY"] = "string[pyarrow]"
+        read_csv_options["dtype"]["MD_DIM"] = "string[pyarrow]"
+        return pd.read_csv(data, **read_csv_options)
