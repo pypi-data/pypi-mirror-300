dc2775abf57cb614de3330f2b3d157928c60dd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:04:05 +0000

    Add package beamngpy/1.30/beamngpy-1.30-py2.py3-none-any.whl

---
+++ packages/beamngpy/beamngpy-1.30-py2.py3-none-any.whl/beamngpy-1.30.dist-info/AUTHORS.rst
@@ -0,0 +1,8 @@
+============
+Contributors
+============
+
+* BeamNG GmbH <info@beamng.gmbh>
+* Marc Müller <mmueller@beamng.gmbh>
+* Adam Ivora <aivora@beamng.gmbh>
+* David Stark <dstark@beamng.gmbh>
---
+++ packages/beamngpy/beamngpy-1.30-py2.py3-none-any.whl/beamngpy/__init__.py
@@ -0,0 +1,34 @@
+"""
+BeamNGPy API module.
+
+.. moduleauthor:: Marc Müller <mmueller@beamng.gmbh>
+.. moduleauthor:: Pascale Maul <pmaul@beamng.gmbh>
+"""
+
+import os
+
+from beamngpy.beamng import BeamNGpy
+from beamngpy.logging import config_logging, set_up_simple_logging
+from beamngpy.misc import vec3
+from beamngpy.misc.quat import angle_to_quat
+from beamngpy.scenario import Scenario, ScenarioObject, StaticObject
+from beamngpy.scenario.level import Level
+from beamngpy.scenario.procedural import (
+    ProceduralBump,
+    ProceduralCone,
+    ProceduralCube,
+    ProceduralCylinder,
+    ProceduralMesh,
+    ProceduralRing,
+)
+from beamngpy.scenario.road import MeshRoad, Road
+from beamngpy.vehicle import Vehicle
+
+
+def read(fil):
+    fil = os.path.join(os.path.dirname(__file__), fil)
+    with open(fil, encoding="utf-8") as f:
+        return f.read()
+
+
+__version__ = read("version.txt")
