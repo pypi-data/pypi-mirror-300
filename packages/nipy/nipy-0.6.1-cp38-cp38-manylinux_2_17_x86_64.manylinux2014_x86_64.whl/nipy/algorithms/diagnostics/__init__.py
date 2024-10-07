bdd0aff95a30994a02a1c73f01670ed6685bedc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:03:53 +0000

    Add package nipy/0.6.1/nipy-0.6.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/nipy/nipy-0.6.1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/nipy/algorithms/diagnostics/__init__.py
@@ -0,0 +1,8 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+# Initialization for diagnostics package
+
+from ..utils import pca
+from .screens import screen
+from .timediff import time_slice_diffs
+from .tsdiffplot import plot_tsdiffs, plot_tsdiffs_image
