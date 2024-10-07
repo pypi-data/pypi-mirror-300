227bb7ac5cb16275f8cc495fe4828f32adcd5b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:58 +0000

    Add package codebase-digest/0.1.37/codebase_digest-0.1.37.tar.gz

---
+++ packages/codebase-digest/codebase_digest-0.1.37.tar.gz/codebase_digest-0.1.37/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="codebase-digest",
+    version='0.1.37',
+    author="Kamil Stanuch",
+    description="Consolidates and analyzes codebases for insights.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kamilstanuch/codebase-digest",
+    packages=find_packages(exclude=["tests*"]),
+    package_data={
+        "codebase_digest": ["prompt_library/*.md"],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="code analysis, codebase, consolidation, visualization",
+    install_requires=[
+        "tiktoken",
+        "colorama",
+        "PyGithub",
+        "pyperclip",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cdigest=codebase_digest.app:main",
+        ],
+    },
+    python_requires='>=3.6',
+    include_package_data=True,
+)
