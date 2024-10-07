707b0d1f9283f8d2aa1a9182b1f9a1583b7f8fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:11:04 +0000

    Add package piver/0.0.1/piver-0.0.1.tar.gz

---
+++ packages/piver/piver-0.0.1.tar.gz/piver-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+NAME = "piver"
+DESCRIPTION = """
+A Python library for reading and managing configuration files.
+""".replace("\n", "")
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name=NAME,
+    version="0.0.1",
+    author="lfcypo",
+    author_email="lfcypo@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lfcypo/piver",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['toml']
+)
