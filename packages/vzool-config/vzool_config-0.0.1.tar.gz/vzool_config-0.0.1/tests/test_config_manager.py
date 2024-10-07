d0680da9dced6b4656e745342c72b7cea5038c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:03:59 +0000

    Add package vzool-config/0.0.1/vzool_config-0.0.1.tar.gz

---
+++ packages/vzool-config/vzool_config-0.0.1.tar.gz/vzool_config-0.0.1/tests/test_config_manager.py
@@ -0,0 +1,5 @@
+from vzool_config import ConfigManager
+
+
+def test_config_manager():
+    ConfigManager.test(debug=True)
---
+++ packages/vzool-config/vzool_config-0.0.1.tar.gz/vzool_config-0.0.1/vzool_config/__init__.py
@@ -0,0 +1,10 @@
+# Importing necessary classes and functions from the main module
+from vzool_config.config_manager import (
+    ConfigManager,
+)
+
+# Version information for the module
+__version__ = "0.0.1"
+__all__ = [
+    "ConfigManager",
+]
