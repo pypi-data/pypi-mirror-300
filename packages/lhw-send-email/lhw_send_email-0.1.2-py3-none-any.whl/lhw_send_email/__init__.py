724344715a53cee4175eeb20764d3e54f01eef13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:04:03 +0000

    Add package lhw-send-email/0.1.2/lhw_send_email-0.1.2-py3-none-any.whl

---
+++ packages/lhw-send-email/lhw_send_email-0.1.2-py3-none-any.whl/lhw_send_email/__init__.py
@@ -0,0 +1,19 @@
+'''
+encoding:   -*- coding: utf-8 -*-
+@Time           :  2024/9/27 8:05
+@Project_Name   :  SendEmail
+@Author         :  lhw
+@File_Name      :  __init__.py
+
+功能描述
+
+实现步骤
+
+'''
+from .send import *
+
+__all__ = [
+    'SendEmail',
+]
+
+
