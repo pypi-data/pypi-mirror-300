487c3506eb16fb33acdc71fe31c7bf8e2d2db6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:47 +0000

    Add package pynhd/0.18.0/pynhd-0.18.0.tar.gz

---
+++ packages/pynhd/pynhd-0.18.0.tar.gz/pynhd-0.18.0/src/pynhd/__init__.py
@@ -0,0 +1,77 @@
+"""Top-level package for PyNHD."""
+
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+
+from pynhd import exceptions
+from pynhd.core import AGRBase, GeoConnex, ScienceBase
+from pynhd.network_tools import (
+    NHDTools,
+    enhd_flowlines_nx,
+    flowline_resample,
+    flowline_xsection,
+    mainstem_huc12_nx,
+    network_resample,
+    network_xsection,
+    nhdflw2nx,
+    nhdplus_l48,
+    prepare_nhdplus,
+    topoogical_sort,
+    vector_accumulation,
+)
+from pynhd.nhdplus_derived import (
+    StreamCat,
+    enhd_attrs,
+    epa_nhd_catchments,
+    nhd_fcode,
+    nhdplus_attrs,
+    nhdplus_attrs_s3,
+    nhdplus_h12pp,
+    nhdplus_vaa,
+    streamcat,
+)
+from pynhd.print_versions import show_versions
+from pynhd.pynhd import HP3D, NHD, NLDI, NHDPlusHR, PyGeoAPI, WaterData, pygeoapi
+
+try:
+    __version__ = version("pynhd")
+except PackageNotFoundError:
+    __version__ = "999"
+
+__all__ = [
+    "prepare_nhdplus",
+    "topoogical_sort",
+    "vector_accumulation",
+    "flowline_resample",
+    "network_resample",
+    "flowline_xsection",
+    "network_xsection",
+    "nhdflw2nx",
+    "enhd_flowlines_nx",
+    "mainstem_huc12_nx",
+    "nhdplus_l48",
+    "StreamCat",
+    "streamcat",
+    "NLDI",
+    "NHDTools",
+    "AGRBase",
+    "GeoConnex",
+    "ScienceBase",
+    "NHDPlusHR",
+    "NHD",
+    "HP3D",
+    "PyGeoAPI",
+    "pygeoapi",
+    "WaterData",
+    "nhd_fcode",
+    "nhdplus_attrs",
+    "nhdplus_attrs_s3",
+    "epa_nhd_catchments",
+    "enhd_attrs",
+    "nhdplus_vaa",
+    "nhdplus_h12pp",
+    "show_versions",
+    "exceptions",
+    "__version__",
+]
