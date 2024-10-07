f4b4ee7f66cd725a0d75af1654cb6fe2db421133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:04:14 +0000

    Add package stockvip/1.0.1/stockvip-1.0.1.tar.gz

---
+++ packages/stockvip/stockvip-1.0.1.tar.gz/stockvip-1.0.1/README.md
@@ -0,0 +1,19 @@
+# Thư viện `stockvip`
+
+**`stockvip`** là một thư viện Python mạnh mẽ hỗ trợ người dùng trong việc lấy và phân tích dữ liệu cổ phiếu | Chỉ số chứng khoán | Chứng quyền | Phái sinh | Trái phiếu | v.v... từ các sàn giao dịch tại Việt Nam. Thư viện này cung cấp cách kết nối đơn giản để người dùng dễ dàng truy xuất và làm việc với dữ liệu thị trường chứng khoán.
+
+## Các tính năng chính
+
+- Lấy dữ liệu lịch sử giao dịch của các mã cổ phiếu | chỉ số | chứng quyền | phái sinh | chỉ số tài chính - thông tin doanh nghiệp | trái phiếu v.v...
+- Cung cấp dữ liệu về giá cổ phiếu, khối lượng giao dịch, tự doanh, mua bán khối ngoại, báo cáo tài chính và nhiều chỉ số tài chính khác.
+- Đơn giản hóa việc kết nối và truy xuất dữ liệu tài chính.
+- Bên cạnh đó thư viện hỗ trợ tính toán các chỉ số phân tích kỹ thuật
+
+**Tài liệu chi tiết**: [click here](https://stockvip.readthedocs.io/vi/latest/index.html) **Read the Docs**.
+
+## Cài đặt
+
+Bạn có thể cài đặt thư viện **`stockvip`** thông qua `pip`:
+
+```bash
+pip install stockvip
---
+++ packages/stockvip/stockvip-1.0.1.tar.gz/stockvip-1.0.1/setup.py
@@ -0,0 +1,50 @@
+# setup.py
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+
+extensions = [
+    Extension(
+        "stockvip.config.config",  # Tên module cho file config.c
+        ["stockvip/config/config.c"]  # Đường dẫn đến file config.c
+    ),
+    Extension(
+        "stockvip.config.ensure_logged_in",  # Tên module cho file ensure_logged_in.c
+        ["stockvip/config/ensure_logged_in.c"]  # Đường dẫn đến file ensure_logged_in.c
+    ),
+    Extension(
+        "stockvip.process_client.login",  # Tên module cho file login.c
+        ["stockvip/process_client/login.c"]  # Đường dẫn đến file login.c
+    ),
+    Extension(
+        "stockvip.process_stock.ohlcv",  # Tên module cho file trong thư mục stock/price
+        ["stockvip/process_stock/ohlcv.c"]  # Đường dẫn đến file price.c
+    )
+]
+
+setup(
+    name="stockvip",  # Tên gói sử dụng dấu gạch dưới
+    version="1.0.1",
+    author="stockvip.vn",
+    author_email="support@stockvip.vn",
+    description="Dữ liệu thị trường chứng khoán Việt Nam",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://stockvip.vn",
+    include_package_data=True,
+    packages=find_packages(),
+    ext_modules=cythonize(
+        extensions, 
+        compiler_directives={
+            'language_level': "3",
+            "embedsignature":True}),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        "pandas==2.1.2",
+        "requests",
+    ],
+)
