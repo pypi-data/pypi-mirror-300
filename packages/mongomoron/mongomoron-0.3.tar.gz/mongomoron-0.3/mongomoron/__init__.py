9630c2c5fa44292feceed1ff3dc5e8d50eb58766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 14:04:56 +0000

    Add package mongomoron/0.3/mongomoron-0.3.tar.gz

---
+++ packages/mongomoron/mongomoron-0.3.tar.gz/mongomoron-0.3/mongomoron/__init__.py
@@ -0,0 +1,5 @@
+from .mongomoron import DatabaseConnection, Collection, index, insert_one, \
+    insert_many, update, update_one, update_many, delete, query, query_one, \
+    and_, not_, nor_, or_, aggregate, sum_, min_, max_, avg, std, push_, \
+    accumulator, push_unique, first, last, percentile, median, filter_, cond, \
+    switch, cmp, dict_, list_, document
