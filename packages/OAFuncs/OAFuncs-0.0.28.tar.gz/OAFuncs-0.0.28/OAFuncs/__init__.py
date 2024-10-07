a18e996c82d5a0a4cd7ee9fabb6deedfafde07c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:53 +0000

    Add package OAFuncs/0.0.28/OAFuncs-0.0.28.tar.gz

---
+++ packages/OAFuncs/OAFuncs-0.0.28.tar.gz/OAFuncs-0.0.28/OAFuncs/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Liu Kun && 16031215@qq.com
+Date: 2024-09-17 16:09:20
+LastEditors: Liu Kun && 16031215@qq.com
+LastEditTime: 2024-10-06 18:44:51
+FilePath: \\Python\\My_Funcs\\OAFuncs\\OAFuncs\\__init__.py
+Description:  
+EditPlatform: vscode
+ComputerInfo: XPS 15 9510
+SystemInfo: Windows 11
+Python Version: 3.11
+'''
+
+# 会导致OAFuncs直接导入所有函数，不符合模块化设计
+from .oa_cmap import *
+from .oa_data import *
+from .oa_draw import *
+from .oa_file import *
+from .oa_nc import *
+
+# import oa_cmap
+# import oa_data
+# import oa_draw
+# import oa_file
+# import oa_nc
