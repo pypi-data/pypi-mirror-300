907888c6fdb505e74c802ff6506ade2541239b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 14:04:58 +0000

    Add package OpenGIS/0.0.1/opengis-0.0.1.tar.gz

---
+++ packages/OpenGIS/opengis-0.0.1.tar.gz/opengis-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="opengis",
+      version="0.0.1",
+      description="deo tools",
+      packages=["skgeo"],
+      author="wzj",
+      author_email="773598627@qq.com")
