a6eac1e8524a2327d9cf342efc08c295f1d6ee69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:04:24 +0000

    Add package opengis/0.0.4/opengis-0.0.4.tar.gz

---
+++ packages/opengis/opengis-0.0.4.tar.gz/opengis-0.0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="opengis",
+      version="0.0.4",
+      description="opengis",
+      packages=["opengis"],
+      author="wang",
+      author_email="773598627@qq.com")
