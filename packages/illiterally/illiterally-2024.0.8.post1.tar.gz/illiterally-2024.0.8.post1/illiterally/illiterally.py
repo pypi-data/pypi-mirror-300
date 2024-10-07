f8de8e67493989ef2916aba4d2725ec052a73d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 05:03:40 +0000

    Add package illiterally/2024.0.8.post1/illiterally-2024.0.8.post1.tar.gz

---
+++ packages/illiterally/illiterally-2024.0.8.post1.tar.gz/illiterally-2024.0.8.post1/illiterally/illiterally.py
@@ -0,0 +1,46 @@
+# 🚀 Illiterally Implementation
+from typing import *
+import dataclasses
+import slugify
+import jinja2
+import emoji
+import io
+import os
+
+from illiterally.log import Log, Indent
+from illiterally.block import Block,BlockReader
+from illiterally.state import State
+
+# 🚀 Entry Point
+def illiterally( source_files: list[str], template_files: list[str], block_template: str, output_dir: str='./output', source_prefix: Optional[str]=None, template_prefix: Optional[str]=None, left: str=None, right: str=None, suppress: bool=False ):
+    S = State(
+        source_files = source_files,
+        template_files = template_files,
+        block_template = block_template, 
+        output_dir = output_dir,
+        source_prefix = source_prefix,
+        template_prefix = template_prefix,
+        left = left,
+        right = right,
+        suppress = suppress
+    )
+
+    # build a list of all slugs in the source files
+    blocks,duplicates = S.parse_blocks()
+    if len(duplicates) > 0:
+        # duplicates found, de-duplicate
+        blocks,duplicates = S.parse_blocks(duplicates)
+        if len(duplicates) > 0:
+            # duplicates still found. What the...
+            return 1
+
+    # now go over all of the template files and activate
+    # any blocks that they will render
+    S.activate_blocks_from_templates( blocks )
+    S.render_blocks_from_templates( blocks )
+
+    return 0
+
+# 🚗
+
+# 🚗
