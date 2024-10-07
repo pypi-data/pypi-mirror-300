781150f9dca1e89e54db3237140a7de892ab3448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 05:03:46 +0000

    Add package openSIMS/0.5.5/openSIMS-0.5.5-py3-none-any.whl

---
+++ packages/openSIMS/openSIMS-0.5.5-py3-none-any.whl/openSIMS/API/Process.py
@@ -0,0 +1,27 @@
+import copy
+import openSIMS as S
+from . import Geochron, Stable
+
+class Samples:
+
+    def __init__(self,simplex,method):
+        self.pars = simplex.get_pars(method)
+        self.method = method
+        self.samples = copy.copy(simplex.samples)
+
+class GeochronSamples(Samples,Geochron.Geochron,Geochron.Processor):
+    pass
+
+class StableSamples(Samples,Stable.Stable,Stable.Processor):
+    pass
+
+def get_samples(simplex,method=None):
+    if method is None:
+        method = S.list_methods()[0]
+    datatype = S.settings(method)['type']
+    if datatype == 'geochron':
+        return GeochronSamples(simplex,method)
+    elif datatype == 'stable':
+        return StableSamples(simplex,method)
+    else:
+        raise ValueError('Unrecognised data type')
