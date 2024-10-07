c3c495a4949ee8cefa0aad04b733b420fa266aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:51 +0000

    Add package rabity/0.4.8/rabity-0.4.8-py3-none-any.whl

---
+++ packages/rabity/rabity-0.4.8-py3-none-any.whl/rabity/bot.py
@@ -0,0 +1,45 @@
+from flask import Flask, session, render_template
+import RunwayLib
+import os
+import colorama
+from colorama import Fore, Style
+
+app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'templates'))
+class bot():
+
+    def page(name, template = None):
+        @app.route(f"/{name}")
+        def create(template = None):
+            """Creates a Page"""
+            if template is None:
+                print(Fore.RED + "[ERROR]" + Fore.BLUE + " No Template Found" + Style.RESET_ALL)
+            else:
+                print(print(Fore.YELLOW + "[INFO]" + Fore.BLUE + f" Running the Bot With " + Fore.YELLOW + template + Fore.BLUE + " directory" + Style.RESET_ALL))
+                return render_template(template)
+        return create(template)
+    @app.route("/")
+    def home(template="index.html"):
+        """Checks for the Template availability"""
+        with app.app_context():
+            if not template:
+                print(Fore.RED + "[ERROR]" + Fore.BLUE + " No Template Found" + Style.RESET_ALL)
+                #template = "index.html"
+                print(Fore.YELLOW + "[INFO]" + Fore.BLUE + " Using default template" + Style.RESET_ALL)
+                #app.run(port=port, debug=debug)
+            else:
+                print(Fore.YELLOW + "[INFO]" + Fore.BLUE + f"Running the Bot With " + Fore.YELLOW + template + Fore.BLUE + " directory" + Style.RESET_ALL)
+                return render_template(template)
+    
+
+    @app.route("/")
+    def run(port, debug = False):
+        
+        
+        #if template is None:
+            #template = "index.html"
+            #print(Fore.YELLOW + "[INFO]" + Fore.BLUE + " Using default template" + Style.RESET_ALL)
+            #app.run(port=port, debug=debug)
+        #else:
+            #print(Fore.YELLOW + "[INFO]" + Fore.BLUE + f"Running the Bot With " + Fore.YELLOW + template + Fore.BLUE + " directory" + Style.RESET_ALL)
+        app.run(port=port, debug=debug)
+        print(f"bot is running on http://localhost:{port}")
