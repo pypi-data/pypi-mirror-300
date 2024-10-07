16836dc9e0b434d255d1b45292ad1871f283b701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 14:04:07 +0000

    Add package Mopidy-Spotify/5.0.0a3/Mopidy_Spotify-5.0.0a3-py3-none-any.whl

---
+++ packages/Mopidy-Spotify/Mopidy_Spotify-5.0.0a3-py3-none-any.whl/mopidy_spotify/__init__.py
@@ -0,0 +1,52 @@
+import pathlib
+
+import pkg_resources
+from mopidy import config, ext
+
+__version__ = pkg_resources.get_distribution("Mopidy-Spotify").version
+
+
+class Extension(ext.Extension):
+    dist_name = "Mopidy-Spotify"
+    ext_name = "spotify"
+    version = __version__
+
+    def get_default_config(self):
+        return config.read(pathlib.Path(__file__).parent / "ext.conf")
+
+    def get_config_schema(self):
+        schema = super().get_config_schema()
+
+        schema["username"] = config.Deprecated()  # since 5.0
+        schema["password"] = config.Deprecated()  # since 5.0
+
+        schema["client_id"] = config.String()
+        schema["client_secret"] = config.Secret()
+
+        schema["bitrate"] = config.Integer(choices=(96, 160, 320))
+        schema["volume_normalization"] = config.Boolean()
+        schema["private_session"] = config.Deprecated()  # since 5.0
+
+        schema["timeout"] = config.Integer(minimum=0)
+
+        schema["cache_dir"] = config.Deprecated()  # since 2.0
+        schema["settings_dir"] = config.Deprecated()  # since 2.0
+
+        schema["allow_cache"] = config.Boolean()
+        schema["cache_size"] = config.Integer(minimum=0)
+
+        schema["allow_network"] = config.Deprecated()  # since 5.0
+        schema["allow_playlists"] = config.Boolean()
+
+        schema["search_album_count"] = config.Integer(minimum=0, maximum=200)
+        schema["search_artist_count"] = config.Integer(minimum=0, maximum=200)
+        schema["search_track_count"] = config.Integer(minimum=0, maximum=200)
+
+        schema["toplist_countries"] = config.Deprecated()  # since 5.0
+
+        return schema
+
+    def setup(self, registry):
+        from mopidy_spotify.backend import SpotifyBackend
+
+        registry.add("backend", SpotifyBackend)
---
+++ packages/Mopidy-Spotify/Mopidy_Spotify-5.0.0a3-py3-none-any.whl/mopidy_spotify/images.py
@@ -0,0 +1,121 @@
+import itertools
+import logging
+import operator
+import urllib.parse
+
+from mopidy_spotify.browse import BROWSE_DIR_URIS
+from mopidy_spotify.translator import web_to_image
+
+_API_MAX_IDS_PER_REQUEST = 50
+
+_cache = {}  # (type, id) -> [Image(), ...]
+
+logger = logging.getLogger(__name__)
+
+
+def get_images(web_client, uris):
+    result = {}
+    uri_type_getter = operator.itemgetter("type")
+    uris = (_parse_uri(u) for u in uris)
+    uris = sorted((u for u in uris if u), key=uri_type_getter)
+    for uri_type, group in itertools.groupby(uris, uri_type_getter):
+        batch = []
+        for uri in group:
+            if uri["key"] in _cache:
+                result[uri["uri"]] = _cache[uri["key"]]
+            elif uri_type == "playlist":
+                result.update(_process_uri(web_client, uri))
+            else:
+                batch.append(uri)
+                if len(batch) >= _API_MAX_IDS_PER_REQUEST:
+                    result.update(_process_uris(web_client, uri_type, batch))
+                    batch = []
+        result.update(_process_uris(web_client, uri_type, batch))
+    return result
+
+
+def _parse_uri(uri):
+    if uri in BROWSE_DIR_URIS:
+        return  # These are internal to the extension.
+    try:
+        parsed_uri = urllib.parse.urlparse(uri)
+        uri_type, uri_id = None, None
+
+        if parsed_uri.scheme == "spotify":
+            fragments = parsed_uri.path.split(":")
+            if len(fragments) < 2:
+                raise ValueError("Too few fragments")
+            uri_type, uri_id = parsed_uri.path.split(":")[:2]
+        elif parsed_uri.scheme in ("http", "https"):
+            if parsed_uri.netloc in ("open.spotify.com", "play.spotify.com"):
+                uri_type, uri_id = parsed_uri.path.split("/")[1:3]
+
+        supported_types = ("track", "album", "artist", "playlist")
+        if uri_type:
+            if uri_type not in supported_types:
+                logger.warning(f"Unsupported image type '{uri_type}' in {repr(uri)}")
+                return
+            elif uri_id:
+                return {
+                    "uri": uri,
+                    "type": uri_type,
+                    "id": uri_id,
+                    "key": (uri_type, uri_id),
+                }
+        raise ValueError("Unknown error")
+    except Exception as e:
+        logger.exception(f"Could not parse {repr(uri)} as a Spotify URI ({e})")
+
+
+def _process_uri(web_client, uri):
+    data = web_client.get(f"{uri['type']}s/{uri['id']}")
+    _cache[uri["key"]] = tuple(web_to_image(i) for i in data.get("images") or [])
+    return {uri["uri"]: _cache[uri["key"]]}
+
+
+def _process_uris(web_client, uri_type, uris):
+    result = {}
+    ids = [u["id"] for u in uris]
+    ids_to_uris = {u["id"]: u for u in uris}
+
+    if not uris:
+        return result
+
+    data = web_client.get(uri_type + "s", params={"ids": ",".join(ids)})
+    for item in (
+        data.get(
+            uri_type + "s",
+        )
+        or []
+    ):
+        if not item:
+            continue
+
+        if "linked_from" in item:
+            item_id = item["linked_from"].get("id")
+        else:
+            item_id = item.get("id")
+        uri = ids_to_uris.get(item_id)
+        if not uri:
+            continue
+
+        if uri["key"] not in _cache:
+            if uri_type == "track":
+                if "album" not in item:
+                    continue
+                album = _parse_uri(item["album"].get("uri"))
+                if not album:
+                    continue
+                album_key = album["key"]
+                if album_key not in _cache:
+                    _cache[album_key] = tuple(
+                        web_to_image(i) for i in item["album"].get("images") or []
+                    )
+                _cache[uri["key"]] = _cache[album_key]
+            else:
+                _cache[uri["key"]] = tuple(
+                    web_to_image(i) for i in item.get("images") or []
+                )
+        result[uri["uri"]] = _cache[uri["key"]]
+
+    return result
