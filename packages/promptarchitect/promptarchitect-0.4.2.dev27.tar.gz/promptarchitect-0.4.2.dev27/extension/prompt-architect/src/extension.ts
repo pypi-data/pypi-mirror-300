d8d33bdb7a0020fc0a30aca5c60aec851f467e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:04:17 +0000

    Add package promptarchitect/0.4.2.dev27/promptarchitect-0.4.2.dev27.tar.gz

---
+++ packages/promptarchitect/promptarchitect-0.4.2.dev27.tar.gz/promptarchitect-0.4.2.dev27/extension/prompt-architect/src/extension.ts
@@ -0,0 +1,95 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as path from "path";
+import * as vscode from "vscode";
+import {
+  collectPromptFiles,
+  Node,
+  PromptFileNode,
+  PromptInputNode,
+  TestSpecificationNode,
+} from "./validation/discovery";
+import { mergeNodeTree } from "./validation/session";
+
+type RunnableNode = PromptFileNode | TestSpecificationNode | PromptInputNode;
+
+let discoveredTests = new Map<string, RunnableNode>();
+
+/**
+ * Activate the extension in vscode.
+ * @param context The extension context
+ */
+export function activate(context: vscode.ExtensionContext) {
+  const testController = vscode.tests.createTestController(
+    "promptarchitect.testController",
+    "Prompt Architect"
+  );
+
+  testController.createRunProfile(
+    "Run tests",
+    vscode.TestRunProfileKind.Run,
+    async (request) => {
+      let nodes: RunnableNode[];
+
+      if (request.include) {
+        // Select only the nodes that the user has selected
+        nodes = request.include.map((node) => {
+          if (!discoveredTests.has(node.id)) {
+            throw new Error(`Node ${node.id} not found in discovered tests`);
+          }
+
+          return discoveredTests.get(node.id)!;
+        });
+      } else {
+        nodes = Array.from(discoveredTests.values());
+      }
+
+      console.log("Running tests", nodes);
+    }
+  );
+
+  testController.refreshHandler = async () => {
+    const nodeTree = await collectPromptFiles();
+    mergeNodeTree(testController, nodeTree);
+    cacheDiscoveredTests(nodeTree);
+  };
+
+  testController.resolveHandler = async (item) => {
+    let nodeTree: Node[];
+
+    if (item) {
+      const workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath;
+      const relativePath = path.relative(workspaceFolder, item.uri!.fsPath);
+
+      nodeTree = await collectPromptFiles(relativePath);
+    } else {
+      nodeTree = await collectPromptFiles();
+    }
+
+    mergeNodeTree(testController, nodeTree);
+    cacheDiscoveredTests(nodeTree);
+  };
+
+  context.subscriptions.push(testController);
+}
+
+function cacheDiscoveredTests(nodeTree: Node[], clearCache = true) {
+  if (clearCache) {
+    discoveredTests = new Map();
+  }
+
+  for (const node of nodeTree) {
+    if (node.type !== "directory") {
+      discoveredTests.set(node.id, node);
+    }
+
+    if (node.type !== "promptInput") {
+      cacheDiscoveredTests(node.children, false);
+    }
+  }
+}
+
+/**
+ * Deactivate the extension
+ */
+export function deactivate() {}
