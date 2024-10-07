803bbf03b1430f614f4309d323f7c0f4722e132c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:03:50 +0000

    Add package bellande-format/0.1.4/bellande_format-0.1.4.tar.gz

---
+++ packages/bellande-format/bellande_format-0.1.4.tar.gz/bellande_format-0.1.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bellande_format",
+    version="0.1.4",
+    description="Bellande Format is a file format type",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="RonaldsonBellande",
+    author_email="ronaldsonbellande@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+    ],
+    keywords=["package", "setuptools"],
+    python_requires=">=3.0",
+    extras_require={
+        "dev": ["pytest", "pytest-cov[all]", "mypy", "black"],
+    },
+    entry_points={
+        'console_scripts': [
+            'Bellande_Format = bellande_parser.bellande_parser:Bellande_Format',
+        ],
+    },
+    project_urls={
+        "Home": "https://github.com/Architecture-Mechanism/bellande_format",
+        "Homepage": "https://github.com/Architecture-Mechanism/bellande_format",
+        "documentation": "https://github.com/Architecture-Mechanism/bellande_format",
+        "repository": "https://github.com/Architecture-Mechanism/bellande_format",
+    },
+)
