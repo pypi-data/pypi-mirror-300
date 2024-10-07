b81d29e4066414ae46bde17b54f94d968011a3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:04:13 +0000

    Add package slurmgen/3.11.0/slurmgen-3.11.0.tar.gz

---
+++ packages/slurmgen/slurmgen-3.11.0.tar.gz/slurmgen-3.11.0/slurmgen/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module with base import.
+"""
+
+__author__ = "Thomas Guillod"
+__copyright__ = "Thomas Guillod - Dartmouth College"
+__license__ = "BSD License"
+
+from slurmgen.script import *
