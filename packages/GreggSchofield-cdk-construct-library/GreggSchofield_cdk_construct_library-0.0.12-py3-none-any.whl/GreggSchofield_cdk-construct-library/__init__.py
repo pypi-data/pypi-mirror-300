f7bbf9bcef21dbc38f389c8ce3d712888b140be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:04:12 +0000

    Add package GreggSchofield-cdk-construct-library/0.0.12/GreggSchofield_cdk_construct_library-0.0.12-py3-none-any.whl

---
+++ packages/GreggSchofield-cdk-construct-library/GreggSchofield_cdk_construct_library-0.0.12-py3-none-any.whl/GreggSchofield_cdk-construct-library/__init__.py
@@ -0,0 +1,67 @@
+r'''
+# replace this
+'''
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import typeguard
+from importlib.metadata import version as _metadata_package_version
+TYPEGUARD_MAJOR_VERSION = int(_metadata_package_version('typeguard').split('.')[0])
+
+def check_type(argname: str, value: object, expected_type: typing.Any) -> typing.Any:
+    if TYPEGUARD_MAJOR_VERSION <= 2:
+        return typeguard.check_type(argname=argname, value=value, expected_type=expected_type) # type:ignore
+    else:
+        if isinstance(value, jsii._reference_map.InterfaceDynamicProxy): # pyright: ignore [reportAttributeAccessIssue]
+           pass
+        else:
+            if TYPEGUARD_MAJOR_VERSION == 3:
+                typeguard.config.collection_check_strategy = typeguard.CollectionCheckStrategy.ALL_ITEMS # type:ignore
+                typeguard.check_type(value=value, expected_type=expected_type) # type:ignore
+            else:
+                typeguard.check_type(value=value, expected_type=expected_type, collection_check_strategy=typeguard.CollectionCheckStrategy.ALL_ITEMS) # type:ignore
+
+from ._jsii import *
+
+import constructs as _constructs_77d1e7e8
+
+
+class PlatformQueue(
+    _constructs_77d1e7e8.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="cdk-construct-library.PlatformQueue",
+):
+    def __init__(self, scope: _constructs_77d1e7e8.Construct, id: builtins.str) -> None:
+        '''
+        :param scope: -
+        :param id: -
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__3ee2d7fdd62d69b291ffb23d1131c1d60c75d391305be02337377100a5565642)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        jsii.create(self.__class__, self, [scope, id])
+
+
+__all__ = [
+    "PlatformQueue",
+]
+
+publication.publish()
+
+def _typecheckingstub__3ee2d7fdd62d69b291ffb23d1131c1d60c75d391305be02337377100a5565642(
+    scope: _constructs_77d1e7e8.Construct,
+    id: builtins.str,
+) -> None:
+    """Type checking stubs"""
+    pass
