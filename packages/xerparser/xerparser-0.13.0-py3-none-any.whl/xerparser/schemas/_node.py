bb7e4af1ad2bc76c65d0e861d4969f66f489d2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:48 +0000

    Add package xerparser/0.13.0/xerparser-0.13.0-py3-none-any.whl

---
+++ packages/xerparser/xerparser-0.13.0-py3-none-any.whl/xerparser/schemas/_node.py
@@ -0,0 +1,107 @@
+from typing import Iterator, Self
+
+
+class Node:
+    """A class to represent a node in a `Tree Data Structure`."""
+
+    def __init__(self, uid: str, code: str, name: str, parent_id: str) -> None:
+        self.uid: str = uid
+        """Unique Table ID"""
+        self.code: str = code
+        """Code or ID for Node"""
+        self.name: str = name
+        """Name or description of Node"""
+        self.parent_id: str = parent_id
+        """Parent Unique Table ID"""
+        self._parent: Self | None = None
+        self._children: list[Self] = []
+
+    def __eq__(self, __o: Self) -> bool:
+        self._validate(__o)
+        return self.full_code == __o.full_code
+
+    def __hash__(self) -> int:
+        return hash(self.full_code)
+
+    def __gt__(self, __o: Self) -> bool:
+        return self.full_code > self._validate(__o).full_code
+
+    def __lt__(self, __o: Self) -> bool:
+        return self.full_code < self._validate(__o).full_code
+
+    def __str__(self) -> str:
+        return f"{self.full_code} - {self.name}"
+
+    def addChild(self, child: Self) -> None:
+        """Add child node"""
+        self._children.append(self._validate(child))
+
+    @property
+    def children(self) -> list[Self]:
+        """List of children nodes"""
+        return self._children
+
+    @property
+    def depth(self) -> int:
+        """
+        Length of the path to the root node (i.e., root path).
+        Root node with have a depth of 0.
+        """
+        return len(self.lineage) - 1
+
+    @property
+    def height(self) -> int:
+        """
+        Length of the longest downward path to a leaf.
+        Leaves with have a height of 0.
+        """
+        return max([child.depth for child in self.traverse_children()]) - self.depth
+
+    @property
+    def full_code(self) -> str:
+        """Node code/ID including parent codes/IDs"""
+        if not self.parent:
+            return self.code
+        return f"{self.parent.full_code}.{self.code}"
+
+    @property
+    def lineage(self) -> list[Self]:
+        if not self._parent:
+            return [self]
+        return self._parent.lineage + [self]
+
+    @property
+    def parent(self) -> Self | None:
+        """Parent node"""
+        return self._parent
+
+    @parent.setter
+    def parent(self, value: Self | None) -> None:
+        if value is None:
+            return
+        self._parent = self._validate(value)
+
+    def traverse_parents(self) -> Iterator[Self]:
+        """Iterate through parents to root."""
+        yield self
+        if self.parent:
+            yield from self.parent.traverse_parents()
+
+    def traverse_children(self) -> Iterator[Self]:
+        """Iterate through children to leaves."""
+        yield self
+        for child in self.children:
+            yield from child.traverse_children()
+
+    def _validate(self, _o2) -> Self:
+        if type(self) is not type(_o2):
+            raise TypeError(f"Expected {type(self)}; got {type(_o2)}")
+        return _o2
+
+
+def build_tree(nodes: dict[str, Node]) -> dict[str, Node]:
+    for node in nodes.values():
+        if parent := nodes.get(node.parent_id):
+            node.parent = parent
+            parent.addChild(node)
+    return nodes
---
+++ packages/xerparser/xerparser-0.13.0-py3-none-any.whl/xerparser/schemas/rsrc.py
@@ -0,0 +1,27 @@
+# xerparser
+# rsrc.py
+
+from typing import Any
+
+from xerparser.schemas._node import Node
+from xerparser.schemas.udftype import UDFTYPE
+
+
+class RSRC(Node):
+    """
+    A class to represent a Resource.
+    """
+
+    def __init__(self, **data: str) -> None:
+        from xerparser.schemas.taskrsrc import TASKRSRC
+
+        super().__init__(
+            data["rsrc_id"],
+            data["rsrc_short_name"],
+            data["rsrc_name"],
+            data["parent_rsrc_id"],
+        )
+        self.clndr_id: str = data["clndr_id"]
+        self.type: str = data["rsrc_type"]
+        self.user_defined_fields: dict[UDFTYPE, Any] = {}
+        self.task_rsrcs: list[TASKRSRC] = []
