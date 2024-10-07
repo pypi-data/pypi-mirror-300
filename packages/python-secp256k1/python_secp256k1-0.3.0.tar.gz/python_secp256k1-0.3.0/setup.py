2c0eea3a9f141c3622a4d7f1129e92583e56fda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:35 +0000

    Add package python-secp256k1/0.3.0/python_secp256k1-0.3.0.tar.gz

---
+++ packages/python-secp256k1/python_secp256k1-0.3.0.tar.gz/python_secp256k1-0.3.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+__version__ = "0.3.0"
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md")) as f:
+    README = f.read()
+
+install_requires = []
+
+setup(
+    name="python-secp256k1",
+    version=__version__,
+    license="MIT",
+    author="scg",
+    author_email="scgbckbone@proton.me",
+    description="Ctypes Python3 FFI bindings for libsecp256k1",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+    ],
+    url="https://github.com/scgbckbone/python-secp256k1",
+    keywords=[
+        "bitcoin",
+        "secp256k1",
+        "ecdsa",
+        "schnorr",
+    ],
+    packages=["pysecp256k1", "pysecp256k1.low_level"],
+    zip_safe=False,
+    install_requires=install_requires,
+    test_suite="tests",
+    extras_require={"typing_extensions": ["typing_extensions>=3.6"]},
+)
