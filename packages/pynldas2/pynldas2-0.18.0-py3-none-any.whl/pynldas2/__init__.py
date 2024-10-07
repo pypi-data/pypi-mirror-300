16539c97f8761a2cae0843abc7dcbd225b445bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:03:52 +0000

    Add package pynldas2/0.18.0/pynldas2-0.18.0-py3-none-any.whl

---
+++ packages/pynldas2/pynldas2-0.18.0-py3-none-any.whl/pynldas2/__init__.py
@@ -0,0 +1,23 @@
+"""Top-level package."""
+
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+
+from pynldas2 import exceptions
+from pynldas2.print_versions import show_versions
+from pynldas2.pynldas2 import get_bycoords, get_bygeom, get_grid_mask
+
+try:
+    __version__ = version("pynldas2")
+except PackageNotFoundError:
+    __version__ = "999"
+
+__all__ = [
+    "get_bycoords",
+    "get_grid_mask",
+    "get_bygeom",
+    "show_versions",
+    "exceptions",
+    "__version__",
+]
