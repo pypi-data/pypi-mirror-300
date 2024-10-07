9ab041bf149e26898c2d78be170ce94fd591a3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:56 +0000

    Add package jureap/0.2.0/jureap-0.2.0.tar.gz

---
+++ packages/jureap/jureap-0.2.0.tar.gz/jureap-0.2.0/test/bin/test_log.py
@@ -0,0 +1,26 @@
+# --------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# --------------------------------------------------------------------------------------------------
+
+import jureap
+
+
+def test_log_01():
+
+    input_file = "test/share/log/01.input.csv"
+    output_file = "test/share/log/01.output.json"
+    environment = "test/share/log/01.env.file"
+    input_version = 1
+    output_version = 1
+
+    env_json = jureap.detail.env_file_to_dict(environment)
+    jureap.log.log(input_file, output_file, env_json, input_version, output_version)
+
+    with open(output_file, "r") as file:
+        output = file.read()
+
+    with open("test/share/log/01.expected.json", "r") as file:
+        expected = file.read()
+
+    assert output == expected
