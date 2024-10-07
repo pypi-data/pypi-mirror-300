c9252018ebdbbe9cba3ec0e803a95b0fb4dbf6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:43 +0000

    Add package rabity/0.4.4/rabity-0.4.4-py3-none-any.whl

---
+++ packages/rabity/rabity-0.4.4-py3-none-any.whl/rabity/bot.py
@@ -0,0 +1,45 @@
+from flask import Flask, session, render_template
+import RunwayLib
+import os
+import colorama
+from colorama import Fore, Style
+
+app = Flask(__name__)
+class bot():
+
+    def page(name):
+        @app.route(f"/{name}")
+        def create(template = None):
+            """Creates a Page"""
+            if template is None:
+                print(Fore.RED + "[ERROR]" + Fore.BLUE + " No Template Found" + Style.RESET_ALL)
+            else:
+                print(print(Fore.YELLOW + "[INFO]" + Fore.BLUE + f"Running the Bot With " + Fore.YELLOW + template + Fore.BLUE + " directory" + Style.RESET_ALL))
+                return render_template(template)
+        return create
+    @app.route("/")
+    def home(template = None):
+        """Checks for the Template avalability"""
+        if template is None:
+            #template = "index.html"
+            print(Fore.YELLOW + "[INFO]" + Fore.BLUE + " Using default template" + Style.RESET_ALL)
+            #app.run(port=port, debug=debug)
+        else:
+            print(Fore.YELLOW + "[INFO]" + Fore.BLUE + f"Running the Bot With " + Fore.YELLOW + template + Fore.BLUE + " directory" + Style.RESET_ALL)
+        return render_template(template)
+    
+
+    @app.route("/")
+    def run(token, port, template = None, debug = False):
+        
+        
+        #if template is None:
+            #template = "index.html"
+            #print(Fore.YELLOW + "[INFO]" + Fore.BLUE + " Using default template" + Style.RESET_ALL)
+            #app.run(port=port, debug=debug)
+        #else:
+            #print(Fore.YELLOW + "[INFO]" + Fore.BLUE + f"Running the Bot With " + Fore.YELLOW + template + Fore.BLUE + " directory" + Style.RESET_ALL)
+        app.run(port=port, debug=debug)
+        print(f"bot is running on http://localhost:{port}")
+bot.run(token=13, port="1333")
+
