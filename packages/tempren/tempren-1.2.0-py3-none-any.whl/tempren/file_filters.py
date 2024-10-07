adff601ed3e88616963926726bc4458617aa3b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:58 +0000

    Add package tempren/1.2.0/tempren-1.2.0-py3-none-any.whl

---
+++ packages/tempren/tempren-1.2.0-py3-none-any.whl/tempren/file_filters.py
@@ -0,0 +1,98 @@
+import fnmatch
+import logging
+import re
+from abc import ABC, abstractmethod
+from typing import Callable
+
+from tempren.evaluation import evaluate_expression
+from tempren.exceptions import ExpressionEvaluationError, TemplateEvaluationError
+from tempren.primitives import File, Pattern
+
+
+class FileFilter(ABC):
+    @abstractmethod
+    def __call__(self, file: File) -> bool:
+        raise NotImplementedError()
+
+
+class FileFilterInverter(FileFilter):
+    def __init__(self, original_filter: Callable[[File], bool]):
+        self.original_filter = original_filter
+
+    def __call__(self, file: File) -> bool:
+        return not self.original_filter(file)
+
+
+class RegexFileFilter(FileFilter, ABC):
+    pattern: re.Pattern
+    ignore_case: bool
+
+    def __init__(self, pattern: str, ignore_case: bool = False):
+        flags = 0
+        if ignore_case:
+            flags |= re.IGNORECASE
+        self.pattern = re.compile(pattern, flags)
+        self.ignore_case = ignore_case
+
+
+class RegexFilenameFileFilter(RegexFileFilter):
+    def __call__(self, file: File) -> bool:
+        match = self.pattern.match(file.relative_path.name)
+        return match is not None
+
+
+class RegexPathFileFilter(RegexFileFilter):
+    def __call__(self, file: File) -> bool:
+        match = self.pattern.match(str(file.relative_path))
+        return match is not None
+
+
+class GlobFileFilter(FileFilter, ABC):
+    pattern: str
+    ignore_case: bool
+
+    def __init__(self, pattern: str, ignore_case: bool = False):
+        self.pattern = pattern
+        self.ignore_case = ignore_case
+        if self.ignore_case:
+            self.pattern = self.pattern.lower()
+
+
+class GlobFilenameFileFilter(GlobFileFilter):
+    def __call__(self, file: File) -> bool:
+        if self.ignore_case:
+            return fnmatch.fnmatch(file.relative_path.name.lower(), self.pattern)
+        return fnmatch.fnmatchcase(file.relative_path.name, self.pattern)
+
+
+class GlobPathFileFilter(GlobFileFilter):
+    def __call__(self, file: File) -> bool:
+        if self.ignore_case:
+            return fnmatch.fnmatch(str(file.relative_path).lower(), self.pattern)
+        return fnmatch.fnmatchcase(str(file.relative_path), self.pattern)
+
+
+class TemplateFileFilter(FileFilter):
+    log: logging.Logger
+    pattern: Pattern
+
+    def __init__(self, pattern: Pattern):
+        self.log = logging.getLogger(__name__)
+        self.pattern = pattern
+
+    def __call__(self, file: File) -> bool:
+        self.log.debug("Rendering filter template for '%s'", file)
+        rendered_expression = self.pattern.process_as_expression(file)
+        self.log.debug("Evaluating filter expression '%s'", rendered_expression)
+        try:
+            evaluation_result = evaluate_expression(rendered_expression)
+            self.log.debug("Evaluation result '%s'", repr(evaluation_result))
+            return bool(evaluation_result)
+        except ExpressionEvaluationError as evaluation_error:
+            assert self.pattern.source_representation is not None
+            raise TemplateEvaluationError(
+                file,
+                self.pattern.source_representation,
+                evaluation_error.expression,
+                evaluation_error.message,
+            )
---
+++ packages/tempren/tempren-1.2.0-py3-none-any.whl/tempren/tags/hash.py
@@ -0,0 +1,69 @@
+import hashlib
+import zlib
+from pathlib import Path
+from typing import Optional
+
+from tempren.primitives import File, Tag
+
+CHUNK_SIZE = 4096
+
+
+def _calculate_hash(algorithm, path: Path, chunk_size: int) -> str:
+    with open(path, "rb") as f:
+        for chunk in iter(lambda: f.read(chunk_size), b""):
+            algorithm.update(chunk)
+    return algorithm.hexdigest()
+
+
+class Md5Tag(Tag):
+    """MD5 hash of the file"""
+
+    require_context = False
+
+    def process(self, file: File, context: Optional[str]) -> str:
+        assert context is None
+        return _calculate_hash(hashlib.md5(), file.absolute_path, CHUNK_SIZE)
+
+
+class Sha1Tag(Tag):
+    """SHA1 hash of the file"""
+
+    require_context = False
+
+    def process(self, file: File, context: Optional[str]) -> str:
+        assert context is None
+        return _calculate_hash(hashlib.sha1(), file.absolute_path, CHUNK_SIZE)
+
+
+class Sha256Tag(Tag):
+    """SHA256 hash of the file"""
+
+    require_context = False
+
+    def process(self, file: File, context: Optional[str]) -> str:
+        assert context is None
+        return _calculate_hash(hashlib.sha256(), file.absolute_path, CHUNK_SIZE)
+
+
+class Sha224Tag(Tag):
+    """SHA224 hash of the file"""
+
+    require_context = False
+
+    def process(self, file: File, context: Optional[str]) -> str:
+        assert context is None
+        return _calculate_hash(hashlib.sha224(), file.absolute_path, CHUNK_SIZE)
+
+
+class Crc32Tag(Tag):
+    """CRC32 hash of the file"""
+
+    require_context = False
+
+    def process(self, file: File, context: Optional[str]) -> str:
+        assert context is None
+        hash_value = 0
+        with open(file.absolute_path, "rb") as f:
+            for chunk in iter(lambda: f.read(CHUNK_SIZE), b""):
+                hash_value = zlib.crc32(chunk, hash_value)
+        return f"{hash_value:08x}"
