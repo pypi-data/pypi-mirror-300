41753fb8ebac9ec8253053637cb628f768e8fc38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:53 +0000

    Add package sales-forecasting/0.3.1/sales_forecasting-0.3.1.tar.gz

---
+++ packages/sales-forecasting/sales_forecasting-0.3.1.tar.gz/sales_forecasting-0.3.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sales_forecasting',  # Replace with your custom package name
+    version='0.3.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        'numpy',
+        'joblib'
+    ],
+    description="A sales forecasting package based on date features",
+    author="Anik",
+    author_email="anik.dip@gmail.com",
+)
