11a1a71825451b8da1fdb216cb48eefd77470208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:03:52 +0000

    Add package classifier-free-guidance-pytorch/0.7.1/classifier_free_guidance_pytorch-0.7.1-py3-none-any.whl

---
+++ packages/classifier-free-guidance-pytorch/classifier_free_guidance_pytorch-0.7.1-py3-none-any.whl/classifier_free_guidance_pytorch/__init__.py
@@ -0,0 +1,16 @@
+from classifier_free_guidance_pytorch.classifier_free_guidance_pytorch import (
+    NullConditioner,
+    TextConditioner,
+    AttentionTextConditioner,
+    TextEmbeddingReturner
+)
+
+from classifier_free_guidance_pytorch.classifier_free_guidance_pytorch import (
+    classifier_free_guidance,
+    classifier_free_guidance_class_decorator
+)
+
+from classifier_free_guidance_pytorch.open_clip import OpenClipAdapter
+from classifier_free_guidance_pytorch.t5 import T5Adapter
+from classifier_free_guidance_pytorch.bge import BGEAdapter
+
