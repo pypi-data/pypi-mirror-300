5d96d3807bde69fe7dbb34453d8bb8058d1974c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:04:07 +0000

    Add package fossbot-lib-real/0.1.3.5/fossbot_lib_real-0.1.3.5.tar.gz

---
+++ packages/fossbot-lib-real/fossbot_lib_real-0.1.3.5.tar.gz/fossbot_lib_real-0.1.3.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import os
+
+setup(
+   name='fossbot_lib_real',
+   version= os.getenv('PYPI_VERSION').replace('v','').strip(),
+   author='Christos Chronis & Manousos Linardakis',
+   author_email='chronis@hua.gr',
+   packages= ['fossbot_lib/common/data_structures','fossbot_lib/common/interfaces','fossbot_lib/parameters_parser','fossbot_lib/real_robot/'],
+   description='FossBot Simulator Library')
+
