7fefab7984b3277487c58faa6be9b27effc6f15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 15:04:07 +0000

    Add package decaylanguage/0.18.5/decaylanguage-0.18.5-py3-none-any.whl

---
+++ packages/decaylanguage/decaylanguage-0.18.5-py3-none-any.whl/decaylanguage/utils/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2018-2024, Eduardo Rodrigues and Henry Schreiner.
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://github.com/scikit-hep/decaylanguage for details.
+
+from __future__ import annotations
+
+from .errors import LineFailure
+from .particleutils import charge_conjugate_name
+from .utilities import (
+    DescriptorFormat,
+    filter_lines,
+    iter_flatten,
+    split,
+)
+
+__all__ = (
+    "DescriptorFormat",
+    "LineFailure",
+    "iter_flatten",
+    "split",
+    "filter_lines",
+    "charge_conjugate_name",
+)
+
+
+def __dir__() -> tuple[str, ...]:
+    return __all__
