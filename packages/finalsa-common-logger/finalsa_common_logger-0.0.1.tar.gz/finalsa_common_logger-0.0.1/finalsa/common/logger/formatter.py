fdbd6161f000ba1d0b75274cd56cde5527a06462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 04:04:02 +0000

    Add package finalsa-common-logger/0.0.1/finalsa_common_logger-0.0.1.tar.gz

---
+++ packages/finalsa-common-logger/finalsa_common_logger-0.0.1.tar.gz/finalsa_common_logger-0.0.1/finalsa/common/logger/formatter.py
@@ -0,0 +1,21 @@
+from pythonjsonlogger import jsonlogger
+from datetime import datetime, timezone
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(
+            log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+        if log_record.get('correlation_id'):
+            log_record['correlation_id'] = str(log_record['correlation_id'])
+
+
+formatter = CustomJsonFormatter('%(timestamp)s %(level)s %(name)s %(message)s')
---
+++ packages/finalsa-common-logger/finalsa_common_logger-0.0.1.tar.gz/finalsa_common_logger-0.0.1/tests/test_version.py
@@ -0,0 +1,5 @@
+from finalsa.common.logger import (__version__)
+
+
+def test_version():
+    assert __version__ is not None
