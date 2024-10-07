5fbdf81a704f88cc86e3730cf57ac5eeeed6c368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-07 03:10:51 +0000

    Add package athlon-flex-api/0.1.4/athlon_flex_api-0.1.4.tar.gz

---
+++ packages/athlon-flex-api/athlon_flex_api-0.1.4.tar.gz/athlon_flex_api-0.1.4/src/athlon_flex_api/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for Athlon Flex API."""
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_format = logging.Formatter(
+    fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(log_format)
+logger.addHandler(console_handler)
+
+__all__ = ["logger", "api", "vehicles_clusters"]
