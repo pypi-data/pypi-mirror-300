95bcfa15a3cd7af2ce8d911bc575d2a1fd032411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:04:55 +0000

    Add package django-markdown-editor-next/0.4/django_markdown_editor_next-0.4.tar.gz

---
+++ packages/django-markdown-editor-next/django_markdown_editor_next-0.4.tar.gz/django_markdown_editor_next-0.4/setup.py
@@ -0,0 +1,40 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='django-markdown-editor-next',
+    version='0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='A Django package to support Markdown text editor in Django admin',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/aliahadmd/django-markdown-editor-next',
+    author='Ali',
+    author_email='ali@aliahad.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'Django>=2.2',
+        'markdown2>=2.4.0',
+        'Pillow>=8.2.0',  # For image handling
+    ],
+    
+)
