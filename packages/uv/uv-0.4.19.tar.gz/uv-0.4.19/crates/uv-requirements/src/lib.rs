3cbd4539af792348de88ad7b06d45b252154df06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 14:05:33 +0000

    Add package uv/0.4.19/uv-0.4.19.tar.gz

---
+++ packages/uv/uv-0.4.19.tar.gz/uv-0.4.19/crates/uv-requirements/src/lib.rs
@@ -0,0 +1,12 @@
+pub use crate::lookahead::*;
+pub use crate::source_tree::*;
+pub use crate::sources::*;
+pub use crate::specification::*;
+pub use crate::unnamed::*;
+
+mod lookahead;
+mod source_tree;
+mod sources;
+mod specification;
+mod unnamed;
+pub mod upgrade;
