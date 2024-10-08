aa6dc14dad3e437b09e89c891d29da5d60490390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 15:04:55 +0000

    Add package graspi-igraph/0.0.0/graspi_igraph-0.0.0.tar.gz

---
+++ packages/graspi-igraph/graspi_igraph-0.0.0.tar.gz/graspi_igraph-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "graspi_igraph",
+    author = "Wenqi Zheng",
+    author_email = "wenqizhe@buffalo.edu",
+    description = "Utilize Python-igraph to produce similar functionality as GraSPI",
+    packages = find_packages(),
+    classifiers = ["Programming Language :: Python"]
+    
+)
