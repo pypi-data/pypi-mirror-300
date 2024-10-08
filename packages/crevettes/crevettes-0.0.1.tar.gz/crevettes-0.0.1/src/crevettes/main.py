8dde2e2673b59d274b499b3f65407d6b1549c609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 14:05:59 +0000

    Add package crevettes/0.0.1/crevettes-0.0.1.tar.gz

---
+++ packages/crevettes/crevettes-0.0.1.tar.gz/crevettes-0.0.1/src/crevettes/main.py
@@ -0,0 +1,23 @@
+import sys
+
+from crevettes import harvester
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: crevettes thread_id")
+        sys.exit(1)
+
+    # Get thread ID from command-line argument.
+    thread_id = sys.argv[1]
+
+    # Fetch thread and comments.
+    submission, comments = harvester.fetch_reddit_thread(thread_id)
+
+    # Write thread metadata data to csv.
+    harvester.write_to_csv(submission, comments, thread_id)
+    print(f"CSV saved as reddit_thread_{thread_id}.csv")
+
+
+if __name__ == "__main__":
+    main()
