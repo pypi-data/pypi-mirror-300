a8223bdbe4d8657989ced48fef08ceb6361607fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 15:06:42 +0000

    Add package bbot/2.1.0.4939rc0/bbot-2.1.0.4939rc0.tar.gz

---
+++ packages/bbot/bbot-2.1.0.4939rc0.tar.gz/bbot-2.1.0.4939rc0/bbot/modules/securitytrails.py
@@ -0,0 +1,38 @@
+from bbot.modules.templates.subdomain_enum import subdomain_enum_apikey
+
+
+class securitytrails(subdomain_enum_apikey):
+    watched_events = ["DNS_NAME"]
+    produced_events = ["DNS_NAME"]
+    flags = ["subdomain-enum", "passive", "safe"]
+    meta = {
+        "description": "Query the SecurityTrails API for subdomains",
+        "created_date": "2022-07-03",
+        "author": "@TheTechromancer",
+        "auth_required": True,
+    }
+    options = {"api_key": ""}
+    options_desc = {"api_key": "SecurityTrails API key"}
+
+    base_url = "https://api.securitytrails.com/v1"
+
+    async def setup(self):
+        self.limit = 100
+        return await super().setup()
+
+    async def ping(self):
+        url = f"{self.base_url}/ping?apikey={{api_key}}"
+        r = await self.api_request(url)
+        resp_content = getattr(r, "text", "")
+        assert getattr(r, "status_code", 0) == 200, resp_content
+
+    async def request_url(self, query):
+        url = f"{self.base_url}/domain/{query}/subdomains?apikey={{api_key}}"
+        response = await self.api_request(url)
+        return response
+
+    def parse_results(self, r, query):
+        j = r.json()
+        if isinstance(j, dict):
+            for host in j.get("subdomains", []):
+                yield f"{host}.{query}"
