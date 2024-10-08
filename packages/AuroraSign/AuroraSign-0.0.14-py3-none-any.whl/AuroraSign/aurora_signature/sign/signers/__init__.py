fa934d12be2ea9b965a3e8e1d1cbc69692e4ed34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 14:05:03 +0000

    Add package AuroraSign/0.0.14/AuroraSign-0.0.14-py3-none-any.whl

---
+++ packages/AuroraSign/AuroraSign-0.0.14-py3-none-any.whl/AuroraSign/aurora_signature/sign/signers/__init__.py
@@ -0,0 +1,22 @@
+"""
+This package houses the part of .aurora_signature that produces digital signatures.
+It contains modules for creating ``SignedData`` CMS objects, embedding them
+in PDF files, and for handling PDF-specific document signing needs.
+"""
+
+from .functions import async_sign_pdf, sign_pdf
+
+from .pdf_cms import SimpleSigner
+from .pdf_signer import PdfSignatureMetadata
+
+from .constants import (
+    DEFAULT_MD,
+)
+
+__all__ = [
+    'SimpleSigner',
+    'PdfSignatureMetadata',
+    'sign_pdf',
+    'async_sign_pdf',
+    'DEFAULT_MD'
+]
