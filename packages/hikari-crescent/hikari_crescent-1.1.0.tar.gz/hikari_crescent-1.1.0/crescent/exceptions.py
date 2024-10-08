63ff106ffaf907f0333de906e1e36046d2bd907b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 14:05:13 +0000

    Add package hikari-crescent/1.1.0/hikari_crescent-1.1.0.tar.gz

---
+++ packages/hikari-crescent/hikari_crescent-1.1.0.tar.gz/hikari_crescent-1.1.0/crescent/exceptions.py
@@ -0,0 +1,28 @@
+from typing import Sequence
+
+__all__: Sequence[str] = (
+    "CrescentException",
+    "AlreadyRegisteredError",
+    "PluginAlreadyLoadedError",
+    "PermissionsError",
+)
+
+
+class CrescentException(Exception):
+    """Base Exception for all exceptions Crescent throws"""
+
+
+class AlreadyRegisteredError(CrescentException):
+    """Command or exception catch function was already registered"""
+
+
+class PluginAlreadyLoadedError(CrescentException):
+    """A plugin is attempted to be loaded but the plugin manager already loaded the plugin."""
+
+
+class PermissionsError(CrescentException):
+    """Raise when a permission is declared in a subcommand"""
+
+
+class InteractionAlreadyAcknowledgedError(CrescentException):
+    """Raise when an interaction is already acknowledged"""
