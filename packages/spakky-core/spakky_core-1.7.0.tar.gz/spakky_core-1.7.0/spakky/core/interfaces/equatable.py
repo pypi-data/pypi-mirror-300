b11df278e4b38a3b711e40e3b19427878cefcce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 16:04:32 +0000

    Add package spakky-core/1.7.0/spakky_core-1.7.0.tar.gz

---
+++ packages/spakky-core/spakky_core-1.7.0.tar.gz/spakky_core-1.7.0/spakky/core/interfaces/equatable.py
@@ -0,0 +1,20 @@
+import sys
+from abc import abstractmethod
+from typing import TypeVar, Protocol, runtime_checkable
+
+if sys.version_info >= (3, 11):
+    from typing import Self  # pragma: no cover
+else:
+    from typing_extensions import Self  # pragma: no cover
+
+
+@runtime_checkable
+class IEquatable(Protocol):
+    @abstractmethod
+    def __eq__(self, __value: Self) -> bool: ...
+
+    @abstractmethod
+    def __hash__(self) -> int: ...
+
+
+EquatableT = TypeVar("EquatableT", bound=IEquatable)
