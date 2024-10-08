27919d0c2ca8264ecc5567f97b720a5aa27975c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 16:04:55 +0000

    Add package amilib/0.3.9/amilib-0.3.9.tar.gz

---
+++ packages/amilib/amilib-0.3.9.tar.gz/amilib-0.3.9/amilib/ami_corpus.py
@@ -0,0 +1,272 @@
+"""
+downstream parser for pygetpapers
+"""
+import datetime
+import json
+from collections import defaultdict
+from pathlib import Path
+
+import lxml
+import lxml.etree as ET
+from lxml.html import HTMLParser
+
+# from amilib.ami_bib import SAVED, QUERY, STARTDATE, ENDDATE
+from amilib.ami_html import HtmlLib, HtmlUtil
+from amilib.ami_util import AmiJson, AmiUtil
+# from amilib.ami_bib import DOI, AUTHOR_STRING, JOURNAL_INFO_TITLE, PUB_YEAR, ABS_TEXT, SAVED_CONFIG_INI, Pygetpapers
+from amilib.util import Util
+
+EUPMC_RESULTS_JSON = "eupmc_results.json"
+DATATABLES_HTML = "datatables.html"
+SAVED_CONFIG_INI = "saved_config.ini"  # TODO cyclic import
+
+logger = Util.get_logger(__name__)
+
+
+class AmiCorpus():
+    """
+
+    """
+    EUPMC_TRANSFORM = {
+        "doi": {
+            "url": {
+                "prefix": "https://www.doi.org/",
+            }
+        },
+        "authorString": {
+            "text": {
+                "split": ",",
+            }
+        },
+        "abstractText": {
+            "text": {
+                "truncate": 200,
+            }
+        },
+        "pmcid": {
+            "url": {
+                "prefix": "https://europepmc.org/betaSearch?query=",
+            }
+
+        }
+
+    }
+
+    def __init__(self, indir=None):
+        """
+
+        """
+        self.indir = indir
+        self.eupmc_results = None
+
+    def get_datatables(self):
+        """
+        create a JQuery datatables from eumpc_reults.json
+        """
+        self.get_or_create_eupmc_results()
+        if self.eupmc_results is None:
+            logger.error(f"no {EUPMC_RESULTS_JSON}, so cannot create datatables at present")
+            return
+
+    def get_or_create_eupmc_results(self):
+        f"""
+        gets file {EUPMC_RESULTS_JSON} from directory
+        :return: {EUPMC_RESULTS_JSON} from directory or None if not exists
+        """
+        if self.eumpc_results is None:
+            if not Path(self.indir).is_dir():
+                logger.error(f"not a directory {self.indir}")
+                return None
+            self.eupmc_results = Path(self.eupmc_results, EUPMC_RESULTS_JSON)
+            if not self.eupmc_results.exists():
+                self.eupmc_results = None
+        return self.eupmc_results
+
+    @classmethod
+    def make_datatables(cls, indir, outdir=None, outfile_h=None):
+        """
+        creates a JQuery.datatables (HTML) file from an AmiCorpus
+        May make this method a member function later
+        :param indir: directory with corpus (normally created by pygetpapers)
+        :param outdir: output for datatables (if omitted uses indir)
+        :param outfile_h: the HTML file ceated (may be changed)
+        """
+        from amilib.ami_bib import SAVED_CONFIG_INI
+
+        if outdir is None:
+            outdir = indir
+        if outfile_h is None:
+            outfile_h = Path(outdir, DATATABLES_HTML)
+        # wanted_keys = [PMCID, DOI, TITLE, AUTHOR_STRING, JOURNAL_INFO_TITLE, PUB_YEAR, ABS_TEXT]
+        config_ini = Path(indir, SAVED_CONFIG_INI)
+        infile = Path(indir, EUPMC_RESULTS_JSON)
+        outdir.mkdir(parents=True, exist_ok=True)
+        datatables = True
+        AmiCorpus.read_json_create_write_html_table(
+            infile, outfile_h, wanted_keys=None, datatables=datatables, table_id=None, config_ini=config_ini)
+
+    @classmethod
+    def read_json_create_write_html_table(
+            cls, infile, outfile_h, wanted_keys,
+            styles=None, datatables=None, table_id=None, config_ini=None):
+        """
+        read pygetpapers output, select columns and create HTML table
+        :param infile: eumpc_results from pygetpapers
+        :param outfile_h: HTML table to write
+        :param wanted_keys: column headings to select
+        :param styles: datatables styles
+        :param datatables: datatables
+        :param table_id: id links script to table (default "my_table")
+        :param config_ini: adds info from saved_config.ini - mainly the query
+        :return: html table
+        """
+        # TODO cyclic imports
+        from amilib.ami_bib import PMCID, TITLE
+        from amilib.ami_bib import DOI, AUTHOR_STRING, JOURNAL_INFO_TITLE, PUB_YEAR, ABS_TEXT
+        if styles is None:
+            styles = ["td {border:solid 1px black;}"]
+        if table_id is None:
+            table_id = "my_table"
+        if wanted_keys is None:
+            wanted_keys = [PMCID, DOI, TITLE, AUTHOR_STRING, JOURNAL_INFO_TITLE, PUB_YEAR, ABS_TEXT]
+
+        assert infile.exists(), f"infile {infile} must exist"
+        with open(infile, "r") as f:
+            jsonx = json.load(f)
+        # look for all papers
+        papers = jsonx.get("papers")
+        assert papers is not None, f"cannot find papers"
+        # specific keys we want
+        dict_by_id = AmiJson.create_json_table(papers, wanted_keys)
+        htmlx, table = HtmlLib.create_html_table(
+            dict_by_id, transform_dict=AmiCorpus.EUPMC_TRANSFORM,
+            styles=styles, datatables=datatables, table_id=table_id
+        )
+        cls.add_query_as_caption(config_ini, table)
+
+        HtmlUtil.write_html_elem(htmlx, outfile_h, debug=True)
+
+    @classmethod
+    def add_query_as_caption(cls, config_ini, table):
+        """
+        add query string from config.ini to table caption
+        :param config_ini: saved_config.ini file
+        :param table: to add caption to
+        """
+        from amilib.ami_bib import SAVED, QUERY, STARTDATE, ENDDATE
+        if config_ini is not None:
+            config, _ = AmiUtil.get_config_and_section_names(config_ini)
+            saved_section = config[SAVED]
+            query = saved_section[QUERY]
+            startdate = saved_section[STARTDATE]
+            if startdate == 'False':
+                startdate = "..."
+            enddate = saved_section[ENDDATE]
+            if enddate == 'False':
+                enddate = datetime.date.today()
+
+            caption = ET.SubElement(table, "caption")
+            caption.text = f"query: {query}"
+            if startdate:
+                caption.text += f"; start: {startdate}"
+            if enddate:
+                caption.text += f"; end: {enddate}"
+
+    @classmethod
+    def create_hit_html(cls, infiles, phrases=None, outfile=None, xpath=None, debug=False):
+        all_paras = []
+        all_dict = dict()
+        hit_dict = defaultdict(list)
+        if type(phrases) is not list:
+            phrases = [phrases]
+        for infile in infiles:
+            assert Path(infile).exists(), f"{infile} does not exist"
+            html_tree = lxml.etree.parse(str(infile), HTMLParser())
+            paras = HtmlLib.find_paras_with_ids(html_tree, xpath=xpath)
+            all_paras.extend(paras)
+
+            # this does the search
+            para_id_by_phrase_dict = HtmlLib.create_para_ohrase_dict(paras, phrases)
+            if len(para_id_by_phrase_dict) > 0:
+                if debug:
+                    print(f"para_phrase_dict {para_id_by_phrase_dict}")
+                cls.add_hit_with_filename_and_para_id(all_dict, hit_dict, infile, para_id_by_phrase_dict)
+        if debug:
+            print(f"para count~: {len(all_paras)}")
+        outfile = Path(outfile)
+        outfile.parent.mkdir(exist_ok=True, parents=True)
+        html1 = cls.create_html_from_hit_dict(hit_dict)
+        if outfile:
+            with open(outfile, "w") as f:
+                if debug:
+                    print(f" hitdict {hit_dict}")
+                HtmlLib.write_html_file(html1, outfile, debug=True)
+        return html1
+
+    @classmethod
+    def add_hit_with_filename_and_para_id(cls, all_dict, hit_dict, infile, phrase_by_para_id_dict):
+        """adds non-empty hits in hit_dict and all to all_dict
+        :param all_dict: accumulates para_phrase_dict by infile
+
+        TODO - move to amilib
+        """
+        item_paras = [item for item in phrase_by_para_id_dict.items() if len(item[1]) > 0]
+        if len(item_paras) > 0:
+            all_dict[infile] = phrase_by_para_id_dict
+            for para_id, hits in phrase_by_para_id_dict.items():
+                for hit in hits:
+                    # TODO should write file with slashes (on Windows we get %5C)
+                    infile_s = f"{infile}"
+                    infile_s = infile_s.replace("\\", "/")
+                    infile_s = infile_s.replace("%5C", "/")
+                    url = f"{infile_s}#{para_id}"
+                    hit_dict[hit].append(url)
+
+
+    @classmethod
+    def add_hit_with_filename_and_para_id(cls, all_dict, hit_dict, infile, para_phrase_dict):
+        """adds non-empty hits in hit_dict and all to all_dict
+        :param all_dict: accumulates para_phrase_dict by infile
+
+        TODO - move to amilib
+        """
+        item_paras = [item for item in para_phrase_dict.items() if len(item[1]) > 0]
+        if len(item_paras) > 0:
+            all_dict[infile] = para_phrase_dict
+            for para_id, hits in para_phrase_dict.items():
+                for hit in hits:
+                    # TODO should write file with slashes (on Windows we get %5C)
+                    infile_s = f"{infile}"
+                    infile_s = infile_s.replace("\\", "/")
+                    infile_s = infile_s.replace("%5C", "/")
+                    url = f"{infile_s}#{para_id}"
+                    hit_dict[hit].append(url)
+
+    @classmethod
+    def create_html_from_hit_dict(cls, hit_dict):
+        html = HtmlLib.create_html_with_empty_head_body()
+        body = HtmlLib.get_body(html)
+        ul = ET.SubElement(body, "ul")
+        for term, hits in hit_dict.items():
+            li = ET.SubElement(ul, "li")
+            p = ET.SubElement(li, "p")
+            p.text = term
+            ul1 = ET.SubElement(li, "ul")
+            for hit in hits:
+                # TODO manage hits with Paths
+                # on windows some hits have "%5C' instead of "/"
+                hit = str(hit).replace("%5C", "/")
+                li1 = ET.SubElement(ul1, "li")
+                a = ET.SubElement(li1, "a")
+                a.text = hit.replace("/html_with_ids.html", "")
+                ss = "ipcc/"
+                try:
+                    idx = a.text.index(ss)
+                except Exception as e:
+                    print(f"cannot find substring {ss} in {a}")
+                    continue
+                a.text = a.text[idx + len(ss):]
+                a.attrib["href"] = hit
+        return html
+
+
---
+++ packages/amilib/amilib-0.3.9.tar.gz/amilib-0.3.9/test/test_bib.py
@@ -0,0 +1,177 @@
+import ast
+import collections
+import configparser
+import json
+import logging
+from pathlib import Path
+import pandas as pd
+
+from amilib.ami_bib import (SAVED, SAVED_CONFIG_INI, SECTION_KEYS, API, LIMIT, QUERY, STARTDATE, XML, \
+                            EUPMC_RESULTS_JSON, PMCID, ABS_TEXT, EPMC_KEYS, JOURNAL_INFO, DOI, TITLE, AUTHOR_STRING,
+                            PUB_YEAR, JOURNAL_INFO_TITLE, Pygetpapers)
+from amilib.ami_html import HtmlUtil, HtmlLib
+from amilib.ami_util import AmiJson, AmiUtil
+from amilib.ami_corpus import AmiCorpus
+from amilib.amix import AmiLib
+from amilib.file_lib import FileLib
+from amilib.util import Util
+from test.resources import Resources
+from test.test_all import AmiAnyTest
+
+logger = Util.get_logger(__name__)
+
+class AmiBibliographyTest(AmiAnyTest):
+    """
+
+    """
+    @classmethod
+    def convert_csv_to_bib(cls):
+        """
+
+        """
+
+def df_toupper(s):
+    return f"<a href='{s}`>s</a>"
+
+def df_truncate(s):
+    return f"{s[:200]}..."
+
+class DictParser(ast.NodeVisitor):
+    def visit_Dict(self,node):
+        keys,values = node.keys,node.values
+        keys = [n.s for n in node.keys]
+        values = [n.s for n in node.values]
+        self.od = collections.OrderedDict(zip(keys, values))
+
+def df_unpack_dict(json_string):
+    """
+    this is messy. I think we need a recursive descent parser
+    """
+    print(f"DIKT {json_string}")
+    try:
+        # dikt = json.loads(s)
+    # or
+        # decoder = json.JSONDecoder(object_pairs_hook=collections.OrderedDict)
+        # dikt = decoder.decode(json_string)
+    # or
+        dp = DictParser()
+        dp.visit(ast.parse(json_string))
+        dikt = dp.od
+    except Exception as e:
+        print(f"EXC {e}")
+        return "None"
+    print(f"dikt {type(dikt)} {dikt.keys()}")
+    title = dikt.get("journal").get("title")
+    return title
+
+HTML_WITH_IDS = "html_with_ids"
+
+class PygetpapersTest(AmiAnyTest):
+    """
+    manage output from pygetpapers
+    """
+
+    def test_edit_csv(self):
+        """
+        reade *.csv created by pygetpapers
+        not working because of lack of OrderedDict parser
+        """
+        infile = Path(Resources.TEST_RESOURCES_DIR, "csv", "frictionless", "europe_pmc.csv")
+        outdir = Path(Resources.TEMP_DIR, "csv", "frictionless")
+        outdir.mkdir(parents=True, exist_ok=True)
+        outfile = Path(outdir, "europe_pmc.csv")
+        outfile_h = Path(outdir, "europe_pmc.html")
+        assert infile.exists()
+        df = pd.read_csv(infile)
+
+        keys = df.keys()
+        key_list = keys.to_list()
+        assert EPMC_KEYS == keys.to_list()
+        df[PMCID] = df[PMCID].apply(df_toupper)
+        df[ABS_TEXT] = df[ABS_TEXT].apply(df_truncate)
+        df[JOURNAL_INFO] = df[JOURNAL_INFO].apply(df_unpack_dict)
+        df2 = df[[PMCID, DOI, TITLE, AUTHOR_STRING, JOURNAL_INFO, PUB_YEAR, ABS_TEXT]]
+
+        with open(outfile, "w") as f:
+            f.write(df2.to_csv())
+        with open(outfile_h, "w") as f:
+            f.write(df2.to_html(escape="False"))
+
+    def test_make_html_table_from_json(self):
+        """
+        assume JSON is an implicit table
+        """
+        effective_level = logger.getEffectiveLevel()
+        logger.setLevel(logging.INFO)
+
+        project_name = "district_heating"
+        indir = Path(Resources.TEST_RESOURCES_DIR, "json", project_name)
+        outdir = Path(Resources.TEMP_DIR, "json", project_name)
+        outfile_h = Path(outdir, "europe_pmc.html")
+
+        AmiCorpus.make_datatables(indir, outdir, outfile_h)
+        logger.setLevel(effective_level)
+
+    def test_make_datatables_cli(self):
+        """
+
+        """
+        indir = Path(Resources.TEST_RESOURCES_DIR, "json", "district_heating")
+        args = ["HTML", "--indir", str(indir), "--operation", "DATATABLES"]
+        amilib = AmiLib()
+        amilib.run_command(args)
+
+    def test_read_pygetpapers_config(self):
+        """
+        reads pygetpapers saved_config.ini and extracts meta/data
+        """
+        inpath = Path(Resources.TEST_RESOURCES_DIR, "pygetpapers", SAVED_CONFIG_INI)
+        assert inpath.exists()
+        saved_section, section_names = Pygetpapers.get_saved_section(inpath)
+
+        assert type(section_names) is list
+        assert section_names == [SAVED]
+        assert type(saved_section) is configparser.SectionProxy
+
+        assert SECTION_KEYS == saved_section.keys()
+        assert saved_section.get(API) == "europe_pmc"
+        assert saved_section.get(LIMIT) == '20'
+        assert saved_section.get(QUERY) == "'district heating'"
+        assert saved_section.get(STARTDATE) == "False"
+        assert saved_section.get(XML) == "True"
+
+    def test_search_all_chapters_with_query_words(self, outfile=None):
+        """
+        read chapter, search for words and return list of paragraphs/ids in which they occur
+        simple, but requires no server
+        """
+        query = "south_asia"
+        indir = Path(Resources.TEST_RESOURCES_DIR, 'ipcc')
+        outfile = Path(indir, f"{query}.html")
+        debug = False
+        globstr = f"{str(indir)}/**/{HTML_WITH_IDS}.html"
+        infiles = FileLib.posix_glob(globstr, recursive=True)
+        assert 2 == len(infiles)
+        phrases = [
+            "bananas",
+            "South Asia",
+        ]
+        html1 = AmiCorpus.create_hit_html(infiles, phrases=phrases, outfile=outfile, debug=debug)
+        assert html1 is not None
+        assert len(html1.xpath("//p")) > 0
+
+    def test_search_all_chapters_with_query_words_commandline(self, outfile=None):
+        """
+        read chapter, search for words and return list of paragraphs/ids in which they occur
+        simple, but requires no server
+        """
+        query = "south_asia"
+        path = Path(Resources.TEST_RESOURCES_DIR, 'ipcc')
+        outfile = Path(path, f"{query}.html")
+        debug = False
+        infiles = FileLib.posix_glob(f"{str(path)}/**/{HTML_WITH_IDS}.html", recursive=True)
+        phrases = [
+            "bananas",
+            "South Asia"
+        ]
+        html1 = AmiCorpus.create_hit_html(infiles, phrases=phrases, outfile=outfile, debug=debug)
