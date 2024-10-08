186bbd661744623a9725f6cdaefe52ec4d779680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 14:05:37 +0000

    Add package dice-calc/0.0.4/dice_calc-0.0.4.tar.gz

---
+++ packages/dice-calc/dice_calc-0.0.4.tar.gz/dice_calc-0.0.4/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='dice_calc',
+    version='0.0.4',
+    author='Ar-Kareem',
+    description='Advanced Calculator for Dice',
+    package_dir={'dice_calc': 'src'},
+    packages=['dice_calc'],    
+
+
+
+)
