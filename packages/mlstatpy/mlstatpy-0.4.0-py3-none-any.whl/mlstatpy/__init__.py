09db245fbacb623517d16e32b58c593e1cefc61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 07:04:20 +0000

    Add package mlstatpy/0.4.0/mlstatpy-0.4.0-py3-none-any.whl

---
+++ packages/mlstatpy/mlstatpy-0.4.0-py3-none-any.whl/mlstatpy/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.4.0"
+__author__ = "Xavier Dupré"
+__github__ = "https://github.com/sdpython/mlstatpy"
+__url__ = "https://sdpython.github.io/doc/mlstatpy/dev/"
+__license__ = "MIT License"
+
+
+def check(log=False):
+    """
+    Checks the library is working.
+    It raises an exception.
+    If you want to disable the logs:
+
+    :param log: if True, display information, otherwise
+    :return: 0 or exception
+    """
+    return True
