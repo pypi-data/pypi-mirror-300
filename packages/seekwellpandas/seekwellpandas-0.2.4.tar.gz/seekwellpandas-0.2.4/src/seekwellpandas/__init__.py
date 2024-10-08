657c906ae2184d6d4a3a497ffc93d149a26b2cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 04:04:20 +0000

    Add package seekwellpandas/0.2.4/seekwellpandas-0.2.4.tar.gz

---
+++ packages/seekwellpandas/seekwellpandas-0.2.4.tar.gz/seekwellpandas-0.2.4/src/seekwellpandas/__init__.py
@@ -0,0 +1,23 @@
+from .methods import (
+    select,
+    where_,
+    group_by,
+    having,
+    order_by,
+    limit,
+    join_,
+    union,
+    distinct,
+    intersect,
+    difference,
+    with_column,
+    rename_column,
+    cast,
+    drop_column,
+    unpivot,
+    group_having
+)
+
+from .methods import __all__
+
+__version__ = "0.2.4"
