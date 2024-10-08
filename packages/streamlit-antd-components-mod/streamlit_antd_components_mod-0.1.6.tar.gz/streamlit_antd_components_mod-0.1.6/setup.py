c6455bf0e994aeea13d8491ae416fc66d4ddebca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 14:05:00 +0000

    Add package streamlit-antd-components-mod/0.1.6/streamlit_antd_components_mod-0.1.6.tar.gz

---
+++ packages/streamlit-antd-components-mod/streamlit_antd_components_mod-0.1.6.tar.gz/streamlit_antd_components_mod-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from os.path import dirname
+from os.path import join
+import setuptools
+
+
+def readme() -> str:
+    """Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top
+    level README file and 2) it's easier to type in the README file than to put
+    a raw string in below.
+    :return: content of README.md
+    """
+    return open(join(dirname(__file__), "readme.md")).read()
+
+
+setuptools.setup(
+    name="streamlit-antd-components-mod",
+    version="0.1.6",
+    author="kennethqzw",
+    description="streamlit custom components of Antd Design and Mantine, modded from jihaoran's streamlit-antd-components",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cocopuffff/StreamlitAntdComponents",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=[
+        "streamlit >= 1.12.0",
+    ],
+)
