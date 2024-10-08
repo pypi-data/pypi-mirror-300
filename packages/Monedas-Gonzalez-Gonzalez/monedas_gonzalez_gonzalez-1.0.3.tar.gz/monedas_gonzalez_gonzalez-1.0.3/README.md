1bea59d073f302bc26df50fd94326b2ce4813e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 04:04:02 +0000

    Add package Monedas-Gonzalez-Gonzalez/1.0.3/monedas_gonzalez_gonzalez-1.0.3.tar.gz

---
+++ packages/Monedas-Gonzalez-Gonzalez/monedas_gonzalez_gonzalez-1.0.3.tar.gz/monedas_gonzalez_gonzalez-1.0.3/README.md
@@ -0,0 +1,8 @@
+Este proyecto tiene como finalidad dar indicaciones a inversores
+de compra y venta según el precio de las criptomonedas en cada momento.
+Además ofrece la posibilidad de poder elegir entre diferentes criptomonedas
+haciéndolo así un programa más completo para el usuario.
+
+Para su utilización, basta con ejecutar el archivo main.py y elegir entre las
+difrentes criptomonedas ofrecidas. A continuación, el programa 
+mostrará el precio junto con las indicaciones de compra o venta.
---
+++ packages/Monedas-Gonzalez-Gonzalez/monedas_gonzalez_gonzalez-1.0.3.tar.gz/monedas_gonzalez_gonzalez-1.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Monedas_Gonzalez_Gonzalez',
+    version='1.0.3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'krakenex',
+        'pandas',
+        'matplotlib',
+    ],
+    entry_points={
+        'console_scripts': [
+            'analizador-bandas-bollinger=src.main:main',
+        ],
+    },
+    test_suite='tests',
+    author='Luis González',
+    author_email='lgg2002419@gmail.com',
+    description='Soporte para análisis de bandas de Bollinger en criptomonedas',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/LuisGonzalez2002/ProyectoGonzalez_Gonzalez',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.12',
+    ],
+    python_requires='>=3.12',
+)
