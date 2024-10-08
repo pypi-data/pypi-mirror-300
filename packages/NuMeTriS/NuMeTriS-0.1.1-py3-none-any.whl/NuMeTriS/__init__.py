d949789748fa71fd105fd43b4c7621326263439f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 16:04:46 +0000

    Add package NuMeTriS/0.1.1/NuMeTriS-0.1.1-py3-none-any.whl

---
+++ packages/NuMeTriS/NuMeTriS-0.1.1-py3-none-any.whl/NuMeTriS/__init__.py
@@ -0,0 +1,8 @@
+from .Utility_Functions import *
+from .Utility_Functions_sparse import *
+
+from .Graph_Class import *
+from .Sparse_Graph_Class import *
+
+__version__ = '0.1.0.'
+__author__ = 'Marzio Di Vece (marzio.divece@imtlucca.it, m.divece@cbs.nl, mrzdiv.91@gmail.com)'
