52b7f430f7ba74f37fe3c7fb8f91d778669fa0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 16:04:43 +0000

    Add package sendalert/0.1.8/sendalert-0.1.8.tar.gz

---
+++ packages/sendalert/sendalert-0.1.8.tar.gz/sendalert-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="sendalert",
+    version="0.1.8",
+    packages=["sendalert"],
+    install_requires=[
+        "requests",
+    ],
+    author="philz1337x",
+    author_email="python@okasi.de",
+    description="A simple Python package for sending alerts using SendAlert.io",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/philz1337x/sendalert-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
