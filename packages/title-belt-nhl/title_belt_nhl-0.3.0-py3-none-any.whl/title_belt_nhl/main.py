1eeb3bdd3c5cbb3b00794442077cd15efc909237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 07:04:15 +0000

    Add package title-belt-nhl/0.3.0/title_belt_nhl-0.3.0-py3-none-any.whl

---
+++ packages/title-belt-nhl/title_belt_nhl-0.3.0-py3-none-any.whl/title_belt_nhl/main.py
@@ -0,0 +1,68 @@
+import rich_click as click
+
+from title_belt_nhl.schedule import Schedule
+
+team_option = click.option(
+    "--team", default="VAN", required=True, help="Team abbrev. (ex: CHI)."
+)
+season_option = click.option(
+    "--season",
+    default=None,
+    required=False,
+    help="Defaults to current season if not specified. Example: 20242025.",
+)
+
+
+@click.group()
+@team_option
+@season_option
+@click.pass_context
+def cli(ctx, team, season):
+    click.echo(f"Calculating shortest path for {team} to challenge for the belt...")
+
+    schedule = Schedule(team, season)
+    ctx.ensure_object(dict)
+    ctx.obj["schedule"] = schedule
+
+
+@cli.command()
+@click.pass_context
+def path(ctx):
+    schedule: Schedule = ctx.obj["schedule"]
+    team = schedule.team
+    holder = schedule.belt_holder
+
+    click.echo("=============================================================")
+    click.echo(f"CURRENT SEASON: {schedule.get_season_pretty()}")
+    click.echo(f"CURRENT BELT HOLDER: {holder}")
+
+    if team == holder:
+        click.echo(f"{team} ALREADY HAS THE BELT!")
+    else:
+        path_matches = schedule.find_nearest_path_games()
+        click.echo(f"{len(path_matches)} GAMES UNTIL `{team}` HAS A SHOT AT THE BELT")
+        for match in path_matches:
+            click.echo(f"\t{match.date_obj} | {match.belt_holder} -> {match}")
+
+
+@cli.command()
+@click.pass_context
+def path_alt(ctx):
+    schedule: Schedule = ctx.obj["schedule"]
+    team = schedule.team
+    holder = schedule.belt_holder
+
+    click.echo("=============================================================")
+    click.echo(f"CURRENT SEASON: {schedule.get_season_pretty()}")
+    click.echo(f"CURRENT BELT HOLDER: {holder}")
+
+    if team == holder:
+        click.echo(f"{team} ALREADY HAS THE BELT!")
+    else:
+        path_matches = schedule.find_nearest_path_v2()
+        if path_matches is None:
+            click.echo(f"NO PATH FOUND FOR `{team}`")
+        else:
+            click.echo(f"{len(path_matches)} GAMES UNTIL `{team}` HAS A SHOT AT THE BELT")
+            for match in path_matches:
+                click.echo(f"\t{match.date_obj} | {match.belt_holder} -> {match}")
