39b54b7f12bc3219e94e892a7d86c620da6a9d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 15:06:37 +0000

    Add package opengis/1.5.2/opengis-1.5.2.tar.gz

---
+++ packages/opengis/opengis-1.5.2.tar.gz/opengis-1.5.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="opengis",
+      version="1.5.2",
+      description="opengis",
+      packages=["opengis"],
+      author="zijie wang",
+      author_email="773598627@qq.com")
