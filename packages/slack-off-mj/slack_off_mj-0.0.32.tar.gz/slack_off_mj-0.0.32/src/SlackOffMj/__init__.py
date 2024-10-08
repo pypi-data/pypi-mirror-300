afa08af61951e67eafb4ab4a4565f644272b4004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 15:05:57 +0000

    Add package slack-off-mj/0.0.32/slack_off_mj-0.0.32.tar.gz

---
+++ packages/slack-off-mj/slack_off_mj-0.0.32.tar.gz/slack_off_mj-0.0.32/src/SlackOffMj/__init__.py
@@ -0,0 +1,4 @@
+from .reconciliation_source import XlsxDataSource
+from .reconciliation_rule import OrganizationRule
+from .reconcilization_merge import DefaultMerge
+from .reconciliation_to_excel import ToExcel
