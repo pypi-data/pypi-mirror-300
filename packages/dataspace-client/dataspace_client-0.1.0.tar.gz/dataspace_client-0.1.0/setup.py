83ea48c69304e077caaa44f3be9bfa67bde59643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:04:11 +0000

    Add package dataspace-client/0.1.0/dataspace_client-0.1.0.tar.gz

---
+++ packages/dataspace-client/dataspace_client-0.1.0.tar.gz/dataspace_client-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='dataspace-client',
+version='0.1.0',
+author='Anton Gustafsson',
+author_email='anton.gustafsson@ri.se',
+description='This is a tool for connecting to a dataspace created with publish subscribe paradigm. ',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
