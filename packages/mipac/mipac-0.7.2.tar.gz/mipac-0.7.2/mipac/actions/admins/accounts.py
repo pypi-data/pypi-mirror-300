c07e3f3b1c475726bb8f6efce6ffae16195f1cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 15:05:35 +0000

    Add package mipac/0.7.2/mipac-0.7.2.tar.gz

---
+++ packages/mipac/mipac-0.7.2.tar.gz/mipac-0.7.2/mipac/actions/admins/accounts.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from mipac.abstract.action import AbstractAction
+from mipac.http import HTTPClient, Route
+from mipac.models.user import CreatedUser
+from mipac.types.user import ICreatedUser
+
+if TYPE_CHECKING:
+    from mipac.client import ClientManager
+
+
+class AdminAccountActions(AbstractAction):
+    def __init__(self, *, session: HTTPClient, client: ClientManager):
+        self._session: HTTPClient = session
+        self._client: ClientManager = client
+
+    async def create(self, username: str, password: str) -> CreatedUser:
+        """ユーザーを作成します
+
+        Endpoint: `/api/admin/accounts/create`
+
+        Parameters
+        ----------
+        username : str
+            ユーザー名
+        password : str
+            パスワード
+
+        Returns
+        -------
+        CreatedUser
+            作成されたユーザー
+        """
+        data = {"username": username, "password": password}
+        raw_created_user: ICreatedUser = await self._session.request(
+            Route("POST", "/api/admin/accounts/create"),
+            json=data,
+        )
+        return CreatedUser(raw_created_user, client=self._client)
+
+    async def delete(self, *, user_id: str) -> bool:
+        """対象のユーザーを削除します
+
+        Endpoint: `/api/admin/accounts/delete`
+
+        Parameters
+        ----------
+        user_id : str
+            対象のユーザーID
+
+        Returns
+        -------
+        bool
+            成功ならTrue
+        """
+        res = await self._session.request(
+            Route("POST", "/api/admin/accounts/delete"),
+            json={"userId": user_id},
+            auth=True,
+            lower=True,
+        )
+        return bool(res)
