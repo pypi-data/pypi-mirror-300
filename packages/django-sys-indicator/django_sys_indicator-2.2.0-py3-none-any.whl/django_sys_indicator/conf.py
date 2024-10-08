1facce8f041897512557fabf98d837042b372adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 14:05:44 +0000

    Add package django-sys-indicator/2.2.0/django_sys_indicator-2.2.0-py3-none-any.whl

---
+++ packages/django-sys-indicator/django_sys_indicator-2.2.0-py3-none-any.whl/django_sys_indicator/conf.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import Dict
+from typing import Sequence
+
+from django.conf import settings as dj_settings
+
+DEFAULT_COLOURS = {
+    # Format here is colour, border colour
+    "red": ("#c50000", "#daa"),
+    "blue": ("#006fc4", "#aad"),
+    "green": ("#009e00", "#ada"),
+    "purple": ("#800080", "#dad"),
+    "orange": ("#ff7700", "#ffb57d"),
+}
+
+
+class Settings:
+    """Lazy settings"""
+
+    @property
+    def SYSTEM_INDICATOR_ENABLED(self) -> bool:  # noqa: N802
+        return getattr(dj_settings, "SYSTEM_INDICATOR_ENABLED", False)
+
+    @property
+    def SYSTEM_INDICATOR_COLOR(self) -> str:  # noqa: N802
+        return getattr(dj_settings, "SYSTEM_INDICATOR_COLOR", "green")
+
+    @property
+    def SYSTEM_INDICATOR_LABEL(self) -> str:  # noqa: N802
+        return getattr(dj_settings, "SYSTEM_INDICATOR_LABEL", "localhost")
+
+    @property
+    def SYSTEM_INDICATOR_COLORS(self) -> dict[str, tuple[str, str]]:  # noqa: N802
+        return getattr(dj_settings, "SYSTEM_INDICATOR_COLORS", DEFAULT_COLOURS)
+
+    @property
+    def SYSTEM_INDICATOR_EXCLUSIONS(self) -> Sequence[str]:  # noqa: N802
+        return getattr(dj_settings, "SYSTEM_INDICATOR_EXCLUSIONS", [])
+
+
+settings = Settings()
