8049cc5fd4d6347dd9913f13d602bb6225c448e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 07:04:17 +0000

    Add package l2-data-utils/0.1.1/l2_data_utils-0.1.1.tar.gz

---
+++ packages/l2-data-utils/l2_data_utils-0.1.1.tar.gz/l2_data_utils-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='l2_data_utils',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'pyspark',
+        'delta-spark',
+    ],
+)
