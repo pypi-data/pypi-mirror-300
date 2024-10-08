bf6fb2c5306058c5031f044aa9ae237ed243dfd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:05:10 +0000

    Add package arvi/0.1.18/arvi-0.1.18-py3-none-any.whl

---
+++ packages/arvi/arvi-0.1.18-py3-none-any.whl/arvi/config.py
@@ -0,0 +1,14 @@
+# whether to return self from (some) RV methods
+return_self = False
+
+# whether to check internet connection before querying DACE
+check_internet = False
+
+# make all DACE requests without using a .dacerc file
+request_as_public = False
+
+# whether to adjust instrument means before gls by default
+adjust_means_gls = True
+
+# debug
+debug = False
