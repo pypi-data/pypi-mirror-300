fedb6e828994dc25c4dd3043dbb1628cb5c174fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 03:10:55 +0000

    Add package argus-htmx-frontend/0.12/argus_htmx_frontend-0.12.tar.gz

---
+++ packages/argus-htmx-frontend/argus_htmx_frontend-0.12.tar.gz/argus_htmx_frontend-0.12/src/argus_htmx/tailwindtheme/tailwind.config.template.js
@@ -0,0 +1,24 @@
+const projectPaths = [
+    '../templates/**/*.html',
+    './**/templates/**/*.html',
+    'src/argus_htmx/templates/**/*.html'
+];
+
+const contentPaths = [...projectPaths];
+console.log(`tailwindcss will scan ${contentPaths}`);
+
+module.exports = {
+    content: contentPaths,
+    theme: {
+        borderWidth: {
+            DEFAULT: '2px',
+        },
+        ...{{ themeoverride }},
+    },
+    daisyui: {
+        themes: {{ daisyuithemes }},
+    },
+    plugins: [
+        require("daisyui"),
+    ]
+}
