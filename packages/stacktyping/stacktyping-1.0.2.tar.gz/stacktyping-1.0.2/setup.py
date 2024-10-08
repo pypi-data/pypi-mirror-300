39e58690872ad0e2773cca9fef02af2891e2ac20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 03:10:10 +0000

    Add package stacktyping/1.0.2/stacktyping-1.0.2.tar.gz

---
+++ packages/stacktyping/stacktyping-1.0.2.tar.gz/stacktyping-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='stacktyping',
+    version='1.0.2',
+    author='ChewangNeko',
+    author_email='2745283841@qq.com',
+    description='Stack developed based on C++',
+    py_modules=['stacktyping'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'colorama',
+    ],
+)
