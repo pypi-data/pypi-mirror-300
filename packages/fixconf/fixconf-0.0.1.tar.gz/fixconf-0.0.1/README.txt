a6b65a4851c03783328eb59771da683da120651a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 16:04:28 +0000

    Add package fixconf/0.0.1/fixconf-0.0.1.tar.gz

---
+++ packages/fixconf/fixconf-0.0.1.tar.gz/fixconf-0.0.1/README.txt
@@ -0,0 +1,3 @@
+# for parsing quick fix configuration file
+python setup.py sdist bdist_wheel
+twine upload dist/*
---
+++ packages/fixconf/fixconf-0.0.1.tar.gz/fixconf-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fixconf',
+    version='0.0.1',
+    author='tangjicheng',
+    author_email='tangjch15@gmail.com',
+    description='Parser for quick fix configuration file',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
