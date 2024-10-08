94b0140cfe77c938222395ffb789a3bbd3c45fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 04:04:12 +0000

    Add package weasel-make/0.3.2/weasel_make-0.3.2.tar.gz

---
+++ packages/weasel-make/weasel_make-0.3.2.tar.gz/weasel_make-0.3.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+   name='weasel-make',
+   version='0.3.2',
+   packages=find_packages(),
+   install_requires=[],
+   entry_points={
+       'console_scripts': [
+           'weasel=weasel_make.weasel:main',
+       ],
+   },
+   # Metadata
+   author='Mirror12k',
+   description='A Makefile-compatibile Build Tool',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   license='MIT',
+   keywords='make build-tool weasel makefile',
+   url='https://github.com/mirror12k/weasel-make',
+   include_package_data=True,
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+   ],
+)
