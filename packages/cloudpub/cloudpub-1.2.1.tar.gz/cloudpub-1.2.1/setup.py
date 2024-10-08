85a0a2827a3fdcaa45495323c00bcedd58d23563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:04:21 +0000

    Add package cloudpub/1.2.1/cloudpub-1.2.1.tar.gz

---
+++ packages/cloudpub/cloudpub-1.2.1.tar.gz/cloudpub-1.2.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloudpub',
+    description='Services for publishing products in cloud environments',
+    version='1.2.1',
+    keywords='stratosphere cloudpub cloudpublish',
+    author='Jonathan Gangi',
+    author_email='jgangi@redhat.com',
+    url='https://github.com/release-engineering/cloudpub',
+    license='GPLv3+',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    install_requires=[
+        'attrs',
+        'deepdiff',
+        'requests',
+        'tenacity',
+        "boto3>=1.34.117",
+    ],
+    zip_safe=False,
+)
