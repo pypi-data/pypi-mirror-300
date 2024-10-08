334e4d5186d52065156f6ba089ef953456582713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 03:10:52 +0000

    Add package dizest/4.0.2/dizest-4.0.2-py3-none-any.whl

---
+++ packages/dizest/dizest-4.0.2-py3-none-any.whl/dizest/core/app.py
@@ -0,0 +1,28 @@
+class App:
+    def __init__(self, workflow, app_id):
+        self.workflow = workflow
+        self.__package__ = workflow.__package__['apps'][app_id]
+
+    # update
+    def update(self, **kwargs):
+        for key in kwargs:
+            self.__package__[key] = kwargs[key]
+    
+    # getter
+    def id(self):
+        return self.__package__['id']
+
+    def title(self):
+        return self.__package__['title']
+    
+    def code(self):
+        return self.__package__['code']
+
+    def api(self):
+        return self.__package__['api']
+
+    def outputs(self):
+        return [x['name'] for x in self.__package__['outputs']]
+
+    def inputs(self):
+        return self.__package__['inputs']
