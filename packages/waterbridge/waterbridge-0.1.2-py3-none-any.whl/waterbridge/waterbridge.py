26abd232c7a6b823da14b3b748297a129ae3315c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:04:46 +0000

    Add package waterbridge/0.1.2/waterbridge-0.1.2-py3-none-any.whl

---
+++ packages/waterbridge/waterbridge-0.1.2-py3-none-any.whl/waterbridge/waterbridge.py
@@ -0,0 +1,37 @@
+import subprocess
+import sys
+
+def run_script(script_name):
+    try:
+        result = subprocess.run(['python', script_name], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while running {script_name}: {e}")
+
+def main():
+    if len(sys.argv) != 2:
+        print("""
+How to run: waterbridge -1 | -2 | -3
+
+-1: Identify water bridges in RNA–protein complexes.
+    - For this, ensure that the output files from HBPLUS are placed in the current directory.
+
+-2: Analyze the output of option -1 and identify the topology of the water bridges, such as A1-w-N1, A1-w-N2, and others.
+
+-3: Provide nomenclature for the A1-w-N1 topology only.
+""")
+        sys.exit(1)
+
+    arg = sys.argv[1]
+
+    if arg == '-1':
+        run_script('1_Identification of water bridges.py')
+    elif arg == '-2':
+        run_script('2_Topological classification of water bridges.py')
+    elif arg == '-3':
+        run_script('3_Nomenclature of A1-w-N1 waterbridges.py')
+    else:
+        print("Please enter a valid argument; -1, -2, or -3.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
