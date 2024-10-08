45a1231fc4d2cd199e7958dfe62e5df5612140c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 04:04:21 +0000

    Add package greyhorse/0.4.19.1/greyhorse-0.4.19.1-py3-none-any.whl

---
+++ packages/greyhorse/greyhorse-0.4.19.1-py3-none-any.whl/greyhorse/app/entities/controllers.py
@@ -0,0 +1,45 @@
+from typing import Any, get_type_hints, override
+
+from greyhorse.result import Ok, Result
+
+from ..abc.collectors import MutNamedCollector, NamedCollector
+from ..abc.controllers import Controller, ControllerError, OperatorMember
+
+
+def operator(resource_type: type):
+    def decorator(func: classmethod):
+        class_name, method_name = func.__qualname__.split('.')
+        hints = get_type_hints(func, include_extras=True)
+        ret_type = hints.pop('return', None)
+        func.__operator__ = OperatorMember(
+            class_name, method_name, resource_type, method=func, params=hints, ret_type=ret_type
+        )
+        return func
+
+    return decorator
+
+
+class SyncController(Controller):
+    @override
+    def setup(self, collector: NamedCollector[type, Any]) -> Result[bool, ControllerError]:
+        return Ok(True)
+
+    @override
+    def teardown(
+        self, collector: MutNamedCollector[type, Any]
+    ) -> Result[bool, ControllerError]:
+        return Ok(True)
+
+
+class AsyncController(Controller):
+    @override
+    async def setup(
+        self, collector: NamedCollector[type, Any]
+    ) -> Result[bool, ControllerError]:
+        return Ok(True)
+
+    @override
+    async def teardown(
+        self, collector: MutNamedCollector[type, Any]
+    ) -> Result[bool, ControllerError]:
+        return Ok(True)
