8a9971db5df99e6a244d66fa7a36b4e235d8a282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 15:06:42 +0000

    Add package bbot/2.1.0.4939rc0/bbot-2.1.0.4939rc0-py3-none-any.whl

---
+++ packages/bbot/bbot-2.1.0.4939rc0-py3-none-any.whl/bbot/modules/hunterio.py
@@ -0,0 +1,70 @@
+from bbot.modules.templates.subdomain_enum import subdomain_enum_apikey
+
+
+class hunterio(subdomain_enum_apikey):
+    watched_events = ["DNS_NAME"]
+    produced_events = ["EMAIL_ADDRESS", "DNS_NAME", "URL_UNVERIFIED"]
+    flags = ["passive", "email-enum", "subdomain-enum", "safe"]
+    meta = {
+        "description": "Query hunter.io for emails",
+        "created_date": "2022-04-25",
+        "author": "@TheTechromancer",
+        "auth_required": True,
+    }
+    options = {"api_key": ""}
+    options_desc = {"api_key": "Hunter.IO API key"}
+
+    base_url = "https://api.hunter.io/v2"
+    limit = 100
+
+    async def ping(self):
+        url = f"{self.base_url}/account?api_key={{api_key}}"
+        r = await self.api_request(url)
+        resp_content = getattr(r, "text", "")
+        assert getattr(r, "status_code", 0) == 200, resp_content
+
+    async def handle_event(self, event):
+        query = self.make_query(event)
+        for entry in await self.query(query):
+            email = entry.get("value", "")
+            sources = entry.get("sources", [])
+            if email:
+                email_event = self.make_event(email, "EMAIL_ADDRESS", event)
+                if email_event:
+                    await self.emit_event(
+                        email_event,
+                        context=f'{{module}} queried Hunter.IO API for "{query}" and found {{event.type}}: {{event.data}}',
+                    )
+                    for source in sources:
+                        domain = source.get("domain", "")
+                        if domain:
+                            await self.emit_event(
+                                domain,
+                                "DNS_NAME",
+                                email_event,
+                                context=f"{{module}} originally found {email} at {{event.type}}: {{event.data}}",
+                            )
+                        url = source.get("uri", "")
+                        if url:
+                            await self.emit_event(
+                                url,
+                                "URL_UNVERIFIED",
+                                email_event,
+                                context=f"{{module}} originally found {email} at {{event.type}}: {{event.data}}",
+                            )
+
+    async def query(self, query):
+        emails = []
+        url = (
+            f"{self.base_url}/domain-search?domain={query}&api_key={{api_key}}" + "&limit={page_size}&offset={offset}"
+        )
+        agen = self.api_page_iter(url, page_size=self.limit)
+        try:
+            async for j in agen:
+                new_emails = j.get("data", {}).get("emails", [])
+                if not new_emails:
+                    break
+                emails += new_emails
+        finally:
+            agen.aclose()
+        return emails
---
+++ packages/bbot/bbot-2.1.0.4939rc0-py3-none-any.whl/bbot/modules/leakix.py
@@ -0,0 +1,50 @@
+from bbot.modules.templates.subdomain_enum import subdomain_enum_apikey
+
+
+class leakix(subdomain_enum_apikey):
+    watched_events = ["DNS_NAME"]
+    produced_events = ["DNS_NAME"]
+    flags = ["subdomain-enum", "passive", "safe"]
+    options = {"api_key": ""}
+    # NOTE: API key is not required (but having one will get you more results)
+    options_desc = {"api_key": "LeakIX API Key"}
+    meta = {
+        "description": "Query leakix.net for subdomains",
+        "created_date": "2022-07-11",
+        "author": "@TheTechromancer",
+    }
+
+    base_url = "https://leakix.net"
+
+    async def setup(self):
+        ret = await super(subdomain_enum_apikey, self).setup()
+        self.headers = {"Accept": "application/json"}
+        self.api_key = self.config.get("api_key", "")
+        if self.api_key:
+            self.headers["api-key"] = self.api_key
+            return await self.require_api_key()
+        return ret
+
+    def prepare_api_request(self, url, kwargs):
+        if self.api_key:
+            kwargs["headers"]["api-key"] = self.api_key
+        return url, kwargs
+
+    async def ping(self):
+        url = f"{self.base_url}/host/1.2.3.4.5"
+        r = await self.helpers.request(url)
+        resp_content = getattr(r, "text", "")
+        assert getattr(r, "status_code", 0) != 401, resp_content
+
+    async def request_url(self, query):
+        url = f"{self.base_url}/api/subdomains/{self.helpers.quote(query)}"
+        response = await self.api_request(url)
+        return response
+
+    def parse_results(self, r, query=None):
+        json = r.json()
+        if json:
+            for entry in json:
+                subdomain = entry.get("subdomain", "")
+                if subdomain:
+                    yield subdomain
