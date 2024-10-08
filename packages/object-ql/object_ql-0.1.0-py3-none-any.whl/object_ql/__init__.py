006f6694e80bb1a817e6dd4c0a40bf43dd0de328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:04:58 +0000

    Add package object-ql/0.1.0/object_ql-0.1.0-py3-none-any.whl

---
+++ packages/object-ql/object_ql-0.1.0-py3-none-any.whl/object_ql/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# **************************************************************
+# Object Query Language, for Gramps and others
+#
+# Copyright (c) Douglas Blank
+# MIT License
+#
+# Largely based on https://github.com/DavidMStraub/gramps-ql
+# **************************************************************
+
+from __future__ import annotations  # can be removed at 3.8 EOL
+
+from .oql import (
+    ObjectQuery,
+    apply,
+    iter_objects,
+    match,
+    parse,
+)
