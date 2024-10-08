e9976583e3e634ea5bb2d4dbe9ee617da2b9236f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:04:12 +0000

    Add package stablehlo-coreml-experimental/0.0.6/stablehlo_coreml_experimental-0.0.6-py3-none-any.whl

---
+++ packages/stablehlo-coreml-experimental/stablehlo_coreml_experimental-0.0.6-py3-none-any.whl/stablehlo_coreml/__init__.py
@@ -0,0 +1,5 @@
+from .converter import convert
+from .passes.utils import register_optimizations, DEFAULT_HLO_PIPELINE
+
+__version__ = "0.0.6"
+__all__ = ['convert', 'register_optimizations', 'DEFAULT_HLO_PIPELINE']
