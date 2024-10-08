5e40d5a5575f3930d760e622ed05805c416ac167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 05:04:53 +0000

    Add package image-processing-lfrossinhole/0.0.1/image_processing_lfrossinhole-0.0.1.tar.gz

---
+++ packages/image-processing-lfrossinhole/image_processing_lfrossinhole-0.0.1.tar.gz/image_processing_lfrossinhole-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing_lfrossinhole",
+    version="0.0.1",
+    author="Lucas Feitosa",
+    description="Image Processing Package using Skimage",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lfrossinhole/image-processing-package",
+    packages=find_packages(),
+    istall_requires=requirements,
+    python_requires='>=3.5',
+)
