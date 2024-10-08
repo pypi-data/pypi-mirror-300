78d7e8fba7448657e106c6eafc6d5c64670312e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 16:04:36 +0000

    Add package mngs/1.8.0/mngs-1.8.0-py3-none-any.whl

---
+++ packages/mngs/mngs-1.8.0-py3-none-any.whl/mngs/dsp/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+from . import PARAMS, add_noise, filt, norm, reference, utils
+from ._crop import crop
+from ._demo_sig import demo_sig
+from ._detect_ripples import detect_ripples
+
+# from ._ensure_3d import ensure_3d
+from ._hilbert import hilbert
+
+# from ._listen import listen
+from ._misc import ensure_3d
+from ._mne import get_eeg_pos
+from ._modulation_index import modulation_index
+from ._pac import pac
+from ._psd import psd
+from ._resample import resample
+from ._time import time
+from ._transform import to_segments, to_sktime_df
+from ._wavelet import wavelet
---
+++ packages/mngs/mngs-1.8.0-py3-none-any.whl/mngs/plt/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# from . import colors
+from ._PARAMS import PARAMS
+
+_ = None
+from . import ax
+from ._add_hue import add_hue
+from ._annotated_heatmap import annotated_heatmap
+from ._colors import (
+    cycle_color,
+    rgb2rgba,
+    rgba2hex,
+    to_hex,
+    to_rgb,
+    to_rgba,
+    update_alpha,
+    gradiate_color,
+)
+from ._configure_mpl import configure_mpl
+from ._draw_a_cube import draw_a_cube
+from ._get_RGBA_from_colormap import get_RGBA_from_colormap
+from ._grid_image import grid_image
+from ._interp_colors import interp_colors
+from ._mk_colorbar import mk_colorbar
+from ._mk_patches import mk_patches
+from ._subplots._SubplotsManager import subplots
+from ._tpl import termplot
