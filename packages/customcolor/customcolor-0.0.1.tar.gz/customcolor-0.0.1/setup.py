ea73b18cebadf2049ec55ab4f27e35e019cc528b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 03:10:29 +0000

    Add package customcolor/0.0.1/customcolor-0.0.1.tar.gz

---
+++ packages/customcolor/customcolor-0.0.1.tar.gz/customcolor-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+	name='customcolor',
+	version='0.0.1',
+	description='Библиотека для красивого вывода юнит-тестов в консоли',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	author='Kirill',
+	author_email='gimpelcomp@gmail.com',
+	packages=['customcolor'],
+	install_requires=[],
+	python_requires='>=3.11.4',
+	license='MIT',
+
+)
