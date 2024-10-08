d32387a8f29e5ad315cf070e8b3fed923cf2d3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 05:04:25 +0000

    Add package augmented-matrix/1.0/augmented_matrix-1.0.tar.gz

---
+++ packages/augmented-matrix/augmented_matrix-1.0.tar.gz/augmented_matrix-1.0/augmented_matrix/augmented_matrix.py
@@ -0,0 +1,177 @@
+import numpy as np
+
+
+class NoSolutionError(Exception):
+    pass
+
+
+class AugmentedMatrix:
+    """
+    A class that represents an augmented matrix
+    """
+    def __init__(self, matrix, constraint=None, /, **kwargs):
+        """
+
+        :param matrix: An m*n 2D matrix
+        :param constraint: An m*1 vector that's the constraint of the augmented matrix
+        :param kwargs: kwargs for initializing the np array
+        """
+
+        self._matrix = np.asarray(matrix, **kwargs)
+
+        if constraint is not None:
+            constraint = np.asarray(constraint, **kwargs)
+            self._matrix = np.append(self._matrix, [[i] for i in constraint], axis=1)
+
+    @property
+    def matrix(self) -> np.ndarray:
+        """
+        :return: the augmented matrix
+        """
+        return self._matrix
+
+    def set_matrix(self, matrix):
+        self._matrix = matrix
+
+    def check_valid_solution(self):
+        """
+        Checks if the matrix is consistent
+        :raises NoSolutionError: If the matrix is not consistent
+        """
+        matrix = self._matrix
+        for row in matrix:
+            if all(x == 0 for x in row[:-1]):
+                if row[-1] != 0:
+                    raise NoSolutionError('No solution exists!')
+
+    def check_upper_triangular(self) -> bool:
+        """
+        Checks if the matrix is in upper triangular form
+        :returns True: if the matrix is in upper triangular form
+        :returns False: if the matrix is not in upper triangular form
+        """
+        matrix = self._matrix
+        row, column = 0, 0
+        while row < (matrix.shape[0] - 1) and column < matrix.shape[1]:
+            if all(x == 0 for x in matrix[:, column][row + 1:]):
+                row += 1
+                column += 1
+            else:
+                return False
+
+        return True
+
+    def partial_pivot(self) -> np.ndarray:
+        """
+        Transforms the matrix into upper triangular form by partial pivoting
+
+        :return: the upper triangular form of the matrix
+        """
+        matrix = self._matrix
+        row, column = 0, 0
+        while row < (row_count := matrix.shape[0]) and column < matrix.shape[1]:
+            # Use the highest absolute value as pivot
+            max_row_index = row
+            for i in range(row, matrix.shape[0]):
+                if abs(matrix[i][column]) > abs(matrix[max_row_index][column]):
+                    max_row_index = i
+
+            if row != max_row_index:
+                matrix[[row, max_row_index]] = matrix[[max_row_index, row]]
+
+            pivot = matrix[row][column]
+            if pivot != 0:
+                for i in range(row + 1, row_count):
+                    leading_value = matrix[i][column]
+                    if leading_value == 0:
+                        continue
+                    factor = -1 * pivot / leading_value
+                    matrix[i] = factor * matrix[i] + matrix[row]
+
+            column += 1
+            row = column
+
+        self._matrix = matrix
+        return matrix
+
+    def simplify_echelon(self) -> np.ndarray:
+        """
+        Simplifies the upper triangular form so that the pivots are 1
+        """
+        matrix = self._matrix
+        # Simplify the echelon form
+        for row_index in range(matrix.shape[0]):
+            pivot = 1
+            for element in matrix[row_index]:
+                if element != 0:
+                    pivot = element
+                    break
+            matrix[row_index] = matrix[row_index] / pivot
+
+        self._matrix = matrix
+        return matrix
+
+    def reduce_echelon(self) -> np.ndarray:
+        """
+        Transforms the matrix from upper triangular form to reduced echelon form
+        :raise: NoSolutionException: if the matrix has no solution
+        """
+        if not self.check_upper_triangular():
+            self.partial_pivot()
+
+        self.simplify_echelon()
+
+        matrix = self._matrix
+        for row_index in range(matrix.shape[0] - 1, -1, -1):
+            pivot_coord = None
+            for element_index in range(matrix.shape[1] - 1):
+                if matrix[row_index][element_index] != 0:
+                    pivot_coord = [row_index, element_index]
+                    break
+
+            if pivot_coord is None:
+                continue
+            else:
+                for i in range(row_index):
+                    leading = matrix[i][pivot_coord[1]]
+                    if leading == 0:
+                        continue
+
+                    pivot = matrix[pivot_coord[0]][pivot_coord[1]]
+                    factor = -1 * leading / pivot
+                    matrix[i] = factor * matrix[pivot_coord[0]] + matrix[i]
+
+        self._matrix = matrix
+        return matrix
+
+    def solve(self, return_type=float) -> np.ndarray:
+        """
+        Solves the augmented matrix
+        :returns self._matrix: the row-reduced matrix
+        :raises NoSolutionException: if the matrix has no solution
+        """
+        self.partial_pivot()
+        self.reduce_echelon()
+        self.check_valid_solution()
+        return self._matrix.astype(return_type)
+
+    def is_square(self) -> bool:
+        """
+        Checks if the matrix is square (the dimensions are equal and the matrix is 2D)
+        :returns True: if the matrix is square
+        :returns False: if the matrix is not square
+        """
+        shape = self._matrix.shape
+        return len(shape) == 2 and shape[0] == shape[1]
+
+    def determinant(self):
+        """
+        Calculates the determinant of the matrix
+        :return: a value indicating the determinant of this matrix
+        :raises LinAlgError: if the matrix is not square
+        """
+
+        return np.linalg.det(self._matrix.astype(float))
+
+
+__all__ = ['AugmentedMatrix', 'NoSolutionError']
---
+++ packages/augmented-matrix/augmented_matrix-1.0.tar.gz/augmented_matrix-1.0/test/test_augmented_matrix.py
@@ -0,0 +1,103 @@
+import unittest
+from augmented_matrix import AugmentedMatrix, NoSolutionError
+from fractions import Fraction
+import numpy as np
+
+
+class TestAugmentedMatrix(unittest.TestCase):
+    @staticmethod
+    def _to_fraction(original):
+        return [[Fraction(j).limit_denominator() for j in i] for i in original]
+
+    def _check_matrix_equal(self, unsolved, expected, constraint=None):
+        if constraint is not None:
+            constraint = [Fraction(j).limit_denominator() for j in constraint]
+        m = AugmentedMatrix(self._to_fraction(unsolved), constraint)
+        m.solve()
+        self.assertEqual(m.matrix.tolist(), self._to_fraction(expected))
+
+    def _check_determinant(self, matrix, expected_determinant):
+        m = AugmentedMatrix(matrix)
+        self.assertEqual(m.determinant(), expected_determinant)
+
+    def test_augmented_matrix(self):
+        original_matrix = [
+            [3, 5, -1, 10],
+            [1, 4, 1, 7],
+            [9, 0, 2, 1]
+        ]
+
+        expected = [
+            [1, 0, 0, 0.2],
+            [0, 1, 0, 1.8],
+            [0, 0, 1, -0.4]
+        ]
+        self._check_matrix_equal(original_matrix, expected)
+
+    def test_call_reduce_echelon_when_non_echelon(self):
+        m = AugmentedMatrix(self._to_fraction([
+            [1, 2, 3, 4],
+            [0, 1, 2, 2],
+            [0, 1, 1, 3]
+        ]))
+        m.reduce_echelon()
+        expected = [
+            [1, 0, 0, -1],
+            [0, 1, 0, 4],
+            [0, 0, 1, -1]
+        ]
+        self.assertEqual(m.matrix.tolist(), expected)
+
+    def test_augmented_matrix_no_solution(self):
+        original = [
+            [1, 2, 3],
+            [1, 2, 4]
+        ]
+        with self.assertRaises(NoSolutionError):
+            self._check_matrix_equal(original, [])
+
+    def test_augmented_matrix_with_free_variable(self):
+        original_matrix = [
+            [1, 2, 3],
+            [2, 4, 6]
+        ]
+        expected = [
+            [1, 2, 3],
+            [0, 0, 0]
+        ]
+        self._check_matrix_equal(original_matrix, expected)
+
+    def test_augmented_matrix_with_constraint(self):
+        original_matrix = [
+            [3, 5, -1],
+            [1, 4, 1],
+            [9, 0, 2]
+        ]
+        constraint = [10, 7, 1]
+        expected = [
+            [1, 0, 0, 0.2],
+            [0, 1, 0, 1.8],
+            [0, 0, 1, -0.4]
+        ]
+        self._check_matrix_equal(original_matrix, expected, constraint=constraint)
+
+    def test_determinant(self):
+        matrix = [
+            [2, -8, 6, 8],
+            [3, -9, 5, 10],
+            [-3, 0, 1, -2],
+            [1, -4, 0, 6]
+        ]
+        self._check_determinant(matrix, -36)
+
+    def test_non_square_matrix_determinant_raise_error(self):
+        matrix = [
+            [1, 2, 3],
+            [4, 5, 6]
+        ]
+        with self.assertRaises(np.linalg.LinAlgError):
+            self._check_determinant(matrix, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
