0a40aa6c3691ea77c2af3c1bb3a9e2678e8ec91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 05:04:50 +0000

    Add package mici/0.3.0/mici-0.3.0-py3-none-any.whl

---
+++ packages/mici/mici-0.3.0-py3-none-any.whl/mici/__init__.py
@@ -0,0 +1,16 @@
+"""MCMC samplers based on simulating Hamiltonian dynamics on a manifold."""
+
+__authors__ = "Matt Graham"
+__license__ = "MIT"
+
+import mici.adapters
+import mici.autodiff
+import mici.integrators
+import mici.interop
+import mici.matrices
+import mici.samplers
+import mici.solvers
+import mici.stagers
+import mici.states
+import mici.systems
+import mici.transitions
