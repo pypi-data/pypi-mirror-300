ac6b113407a99d5e44a922d7365b61de6fae901b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 15:05:07 +0000

    Add package drop-columns-by-treshold/0.0.1/drop_columns_by_treshold-0.0.1.tar.gz

---
+++ packages/drop-columns-by-treshold/drop_columns_by_treshold-0.0.1.tar.gz/drop_columns_by_treshold-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name = "drop_columns_by_treshold",
+    version = "0.0.1",
+    author = "Ganbaatar Bold",
+    author_email = "elmerganbaa@gmail.com",
+    description = "drop_columns_by_treshold",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ganbaaelmer/drop_columns_by_treshold.git",
+    project_urls = {
+        "Bug Tracker": "https://github.com/ganbaaelmer/drop_columns_by_treshold.git",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6"
+)
---
+++ packages/drop-columns-by-treshold/drop_columns_by_treshold-0.0.1.tar.gz/drop_columns_by_treshold-0.0.1/src/drop_columns_by_treshold/drop_columns_by_treshold.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import seaborn as sns # data visualization library
+import matplotlib.pyplot as plt # plotting library
+
+#v0.0.1
+def drop_columns_by_treshold(file_name, nan_percent):
+    df = pd.read_csv(file_name)
+    sns.heatmap(df.isnull(), cbar=False, cmap='CMRmap')
+    plt.title('before drop columns by treshold: missing values showing through heatmap')
+    plt.show()
+
+    # billing_subscriber.info()
+    print("before delete columns count: ", len(df.columns.tolist()))
+    print("before delete columns: ", df.columns.tolist())
+
+    # Find out the columns that need to be dropped for a given threshold:
+    def get_nan_cols(df, nan_percent):
+        threshold = len(df.index) * nan_percent
+        return [c for c in df.columns if df[c].isnull().sum() >= threshold]
+
+    
+
+    cols_to_del = get_nan_cols(df, nan_percent)
+    cols_to_del
+
+    print("columns to delete: ", cols_to_del)
+
+    # drop NA with threshold
+    #billing_subscriber = billing_subscriber.dropna(thresh=10000 ,axis=1)
+
+    #drop columns with too many NA empty values
+    df = df.drop(cols_to_del, axis=1)
+    print("remaining columns count: ", len(df.columns.tolist()))
+    print("remaining columns: ", df.columns.tolist())
+    print("\ntotal row count: ", df.shape[0])
+
+    missing_info = df.isna().sum().to_frame()
+    missing_info.columns = ["NA count"]
+
+    print("missing_info: ", missing_info)
+
+    sns.heatmap(df.isnull(), cbar=False, cmap='CMRmap')
+    plt.title('after dropped columns by treshold: missing values showing through heatmap')
+    plt.show()
+
+
+    return df
+
+
+
+# test
+file_name = "billing_subscriber.csv"
+threshold = 0.1
+
+#df = drop_columns_by_treshold(file_name, threshold)
