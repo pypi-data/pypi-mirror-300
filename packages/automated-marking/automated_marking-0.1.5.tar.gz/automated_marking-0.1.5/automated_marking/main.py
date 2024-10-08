4c18804f791a65513d178563ebc82e9c4e394b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 03:10:26 +0000

    Add package automated-marking/0.1.5/automated_marking-0.1.5.tar.gz

---
+++ packages/automated-marking/automated_marking-0.1.5.tar.gz/automated_marking-0.1.5/automated_marking/main.py
@@ -0,0 +1,9 @@
+# automated_marking/main.py
+
+from .repo_processing import process_repos  # Import your main processing function
+
+def main():
+    process_repos()  # Call the main processing function
+
+if __name__ == "__main__":
+    main()  # Call the main function when executed directly
