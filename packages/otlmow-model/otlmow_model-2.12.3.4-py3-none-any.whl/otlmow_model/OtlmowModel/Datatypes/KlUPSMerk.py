851b333525864d6bc9ab8734ce9f4d518340b757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 15:05:08 +0000

    Add package otlmow-model/2.12.3.4/otlmow_model-2.12.3.4-py3-none-any.whl

---
+++ packages/otlmow-model/otlmow_model-2.12.3.4-py3-none-any.whl/otlmow_model/OtlmowModel/Datatypes/KlUPSMerk.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+from otlmow_model.OtlmowModel.BaseClasses.KeuzelijstField import KeuzelijstField
+from otlmow_model.OtlmowModel.BaseClasses.KeuzelijstWaarde import KeuzelijstWaarde
+
+
+# Generated with OTLEnumerationCreator. To modify: extend, do not edit
+class KlUPSMerk(KeuzelijstField):
+    """Het merk van de UPS."""
+    naam = 'KlUPSMerk'
+    label = 'UPS merk'
+    objectUri = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#KlUPSMerk'
+    definition = 'Het merk van de UPS.'
+    status = 'ingebruik'
+    codelist = 'https://wegenenverkeer.data.vlaanderen.be/id/conceptscheme/KlUPSMerk'
+    options = {
+        'apc': KeuzelijstWaarde(invulwaarde='apc',
+                                label='APC',
+                                status='ingebruik',
+                                definitie='APC',
+                                objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlUPSMerk/apc'),
+        'delta': KeuzelijstWaarde(invulwaarde='delta',
+                                  label='Delta',
+                                  status='ingebruik',
+                                  definitie='Delta',
+                                  objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlUPSMerk/delta'),
+        'phoenix-contact': KeuzelijstWaarde(invulwaarde='phoenix-contact',
+                                            label='Phoenix Contact',
+                                            status='ingebruik',
+                                            definitie='Phoenix Contact',
+                                            objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlUPSMerk/phoenix-contact'),
+        'prostar': KeuzelijstWaarde(invulwaarde='prostar',
+                                    label='Prostar',
+                                    status='ingebruik',
+                                    definitie='Prostar',
+                                    objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlUPSMerk/prostar'),
+        'pulse-dimension': KeuzelijstWaarde(invulwaarde='pulse-dimension',
+                                            label='Pulse Dimension',
+                                            status='ingebruik',
+                                            definitie='Pulse Dimension',
+                                            objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlUPSMerk/pulse-dimension')
+    }
+
+    @classmethod
+    def create_dummy_data(cls):
+        return cls.create_dummy_data_keuzelijst(cls.options)
+
