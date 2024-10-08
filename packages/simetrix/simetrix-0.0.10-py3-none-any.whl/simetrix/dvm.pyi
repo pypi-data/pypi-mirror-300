921d014af2860635cbb80ad555d17298ab74bb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 03:10:35 +0000

    Add package simetrix/0.0.10/simetrix-0.0.10-py3-none-any.whl

---
+++ packages/simetrix/simetrix-0.0.10-py3-none-any.whl/simetrix/dvm.pyi
@@ -0,0 +1,113 @@
+# SPDX-FileCopyrightText: 2024-present SIMetrix Technologies Ltd, SIMPLIS Technologies Inc.
+#
+# SPDX-License-Identifier: SIMetrix and SIMetrix/SIMPLIS End User Licence Agreement
+
+"""SIMetrix and SIMetrix/SIMPLIS Python Support for DVM
+
+Provides DVM specific functionality for SIMetrix/SIMPLIS Python integration.
+
+This requires a running licensed instance of SIMetrix or SIMetrix/SIMPLIS.
+
+Additional documentation can be found at:
+https://www.simetrix.co.uk/documentation/
+"""
+
+from . import simetrix as simetrix
+from enum import Enum
+
+Status = Enum('Status', ['Fail', 'Pass', 'Warn'])
+TestStatus = Enum('TestStatus', ['Fail', 'Pass', 'Warn'])
+
+class ControlSymbol(simetrix.schematic.Instance):
+    def circuitDescription(self) -> str: ...
+    def circuitName(self) -> str: ...
+
+class LogTestDataResult:
+    def measurement(self) -> str: ...
+    def target(self) -> str: ...
+    def topology(self) -> str: ...
+    def value(self) -> str: ...
+
+class LogTestData:
+    def executed(self) -> str: ...
+    def logPath(self) -> str: ...
+    def progress(self) -> tuple[int, int]: ...
+    def rawData(self) -> list[str]: ...
+    def reportPath(self) -> str: ...
+    def results(self) -> list[LogTestDataResult]: ...
+    def rstatus(self) -> str: ...
+    def simulator(self) -> str: ...
+    def status(self) -> str: ...
+
+class LogFile:
+    def data(self, label: str) -> LogTestData: ...
+
+class ProcessReturn:
+    def createScalar(self, name: str, value: str) -> None: ...
+    def createSpecification(self, name: str, status: Status, description: str) -> None: ...
+    def createStatistic(self, name: str, value: str) -> None: ...
+    def createStatisticSpecification(self, name: str, status: Status, description: str) -> None: ...
+
+class TestState:
+    def label(self) -> str: ...
+    def logData(self) -> LogTestData: ...
+    def promoteGraph(self, name: str, weight: int = None, fuzzyLogic: bool = None) -> None: ...
+    def promoteScalar(self, name: str, value: str, newName: str = None) -> None: ...
+    def reportDirectory(self) -> str: ...
+
+class TestContext:
+    """Provides contextual information about a currently executing DVM test."""
+    def label(self) -> str: 
+        """Returns the label for the test.
+        
+        Returns
+        -------
+        str
+            Label for the test.
+        """
+        ...
+    def logData(self) -> LogTestData: ...
+    def promoteGraph(self, name: str, weight: int = None, fuzzyLogic: bool = None) -> None: 
+        """Adds a graph to the overview report.
+
+        This allows for multiple graphs to be listed within the report.
+        
+        Parameters
+        ----------
+        name : str
+            Name of a DVM-generated graph to promote.
+        weight : int, optional
+            A number that indicates the order in which you want the graph to appear with the higher numbered graphs appearing first in the report.
+        fuzzyLogic : bool, optional
+            States whether the provided name is only an approximation, where if set to true the name will be searched for in the actual graph names in report.txt.        
+        """
+        ...
+    def promoteScalar(self, name: str, value: str, newName: str = None) -> None: 
+        """Adds a scalar to the overview report. 
+
+        This allows for custom values to be specified within the report.
+        
+        Parameters
+        ----------
+        name : str
+            Name of the scalar to add.
+        value : str
+            String value for the scalar to add.
+        newName : str, optional
+            New name for the added scalar.        
+        """
+        ...
+    def reportDirectory(self) -> str: 
+        """Returns the path of the directory containing the report.
+        
+        Returns
+        -------
+        str
+            Path of the directory containing the report.
+        """
+        ...
+    def createScalar(self, name: str, value: str) -> None: 
+        ...
+    def createSpecification(self, name: str, status: TestStatus, description: str) -> None: ...
+    def createStatistic(self, name: str, value: str) -> None: ...
+    def createStatisticSpecification(self, name: str, status: TestStatus, description: str) -> None: ...
---
+++ packages/simetrix/simetrix-0.0.10-py3-none-any.whl/simetrix/schematic.pyi
@@ -0,0 +1,123 @@
+# SPDX-FileCopyrightText: 2024-present SIMetrix Technologies Ltd, SIMPLIS Technologies Inc.
+#
+# SPDX-License-Identifier: SIMetrix and SIMetrix/SIMPLIS End User Licence Agreement
+
+from . import simetrix as simetrix
+
+"""SIMetrix and SIMetrix/SIMPLIS Python Support for Schematics.
+
+Provides schematic specific functionality for SIMetrix/SIMPLIS Python integration.
+
+This requires a running licensed instance of SIMetrix or SIMetrix/SIMPLIS.
+
+Additional documentation can be found at:
+https://www.simetrix.co.uk/documentation/
+"""
+
+class AbstractSchematicObject:
+    """Base class for all schematic objects.
+    
+    Schematic objects are objects that exist within a schematic. They have a unique to the schematic
+    handle, along with a set of properties.    
+    """
+    def handle(self) -> str: 
+        """Returns the object handle.
+        
+        Returns
+        -------
+        str
+            Handle for this object.        
+        """
+        ...
+    def hasProperty(self, name: str) -> bool: 
+        """Returns whether the object has a property with the given name.
+        
+        Properties are compared using a case-insensitive comparison of their names.
+
+        Parameters
+        ----------
+        name : str
+            Name of the property to search for.
+
+        Returns
+        -------
+        bool
+            True if a property with the specified name exists in this object, false otherwise.        
+        """        
+        ...
+
+    def properties(self) -> simetrix.Properties: 
+        """Returns the set of properties for this object.
+        
+        Returns
+        -------
+        simetrix.Properties
+            Set of properties for this object.
+        """    
+        ...
+
+    def propertyNames(self) -> list[str]: 
+        """Returns a list of all property names for this object.
+        
+        Returns
+        -------
+        list[str]
+            List of property names for this object.        
+        """
+        ...
+
+    def propertyValue(self, name: str) -> str: 
+        """Returns the value for the property with the given name.
+        
+        Properties within this object are searched for using a case-insensitive comparison of the names.
+
+        Returns
+        -------
+        str
+            Value for the property in this object with the given name, or None if there is no property with the given name in this object.        
+        """
+        ...
+
+    def schematic(self) -> Schematic: 
+        """Returns the Schematic that this object is containined within.
+        
+        Returns
+        -------
+        Schematic
+            The Schematic containing this object.        
+        """
+        ...
+
+
+    def setProperty(self, name: str, value: str) -> None: 
+        """Sets a property for this object with the given name and value.
+        
+        If a property already exists within this object with the same name as provided, the existing property 
+        is overwritten with the new property. Otherwise if no property exists with the provided name, a new 
+        property is added to this object.
+        
+        Properties are compared using a case-insensitive comparison of their names.
+
+        Parameters
+        ----------
+        name : str
+            Name of the property.
+        value : 
+            Value of the property.
+        """
+        ...
+
+class Instance(AbstractSchematicObject):
+    """Represents an instance of a particular symbol within a schematic."""
+    def symbolName(self) -> str: 
+        """Returns the name of the symbol.
+        
+        Returns
+        -------
+        str
+            Name of the associated symbol.
+        """
+        ...
+
+
+    
