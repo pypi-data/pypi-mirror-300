5a8380933d6c701222dd6abd488c8719bceefdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 16:04:43 +0000

    Add package rws-ddlpy/0.6.0/rws_ddlpy-0.6.0.tar.gz

---
+++ packages/rws-ddlpy/rws_ddlpy-0.6.0.tar.gz/rws_ddlpy-0.6.0/ddlpy/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Data Distributie Laag. Service from Rijkswaterstaat for distributing water quantity data.."""
+
+__author__ = """Fedor Baart"""
+__email__ = 'fedor.baart@deltares.nl'
+__version__ = '0.6.0'
+
+from ddlpy.ddlpy import locations
+from ddlpy.ddlpy import (measurements, 
+                         measurements_latest, 
+                         measurements_available, 
+                         measurements_amount,
+                         )
+from ddlpy.utils import (simplify_dataframe,
+                         dataframe_to_xarray)
+
+__all__ = ['locations', 
+           'measurements',
+           'measurements_latest', 
+           'measurements_available', 
+           'measurements_amount',
+           'simplify_dataframe',
+           'dataframe_to_xarray'
+           ]
---
+++ packages/rws-ddlpy/rws_ddlpy-0.6.0.tar.gz/rws_ddlpy-0.6.0/tests/test_cli.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 07:44:47 2024
+
+@author: veenstra
+"""
+
+import os
+from click.testing import CliRunner
+from ddlpy import cli
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.cli)
+    assert result.exit_code == 0
+    assert 'Show this message and exit.' in result.output
+    help_result = runner.invoke(cli.cli, ['--help'])
+    assert help_result.exit_code == 0
+    assert 'Show this message and exit.' in help_result.output
+
+    locations_command = 'locations --grootheid-code WATHTE --station HOEKVHLD'
+    locations_result = runner.invoke(cli.cli, locations_command.split())
+    assert locations_result.exit_code == 0
+    file_locs = "locations.json"
+    assert os.path.exists(file_locs)
+    
+    measurements_command = 'measurements 2023-01-01 2023-01-03'
+    measurements_result = runner.invoke(cli.cli, measurements_command.split())
+    assert measurements_result.exit_code == 0
+    file_meas = "HOEKVHLD_OW_cm_WATHTE_NVT_NAP_NVT.csv"
+    assert os.path.exists(file_meas)
+    
+    # cleanup
+    os.remove(file_locs)
+    os.remove(file_meas)
