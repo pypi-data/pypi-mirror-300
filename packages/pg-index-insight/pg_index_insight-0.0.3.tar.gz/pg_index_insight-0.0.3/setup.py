e02ef702e691dcdb89035c6b21b8df5cb92ae7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 04:04:34 +0000

    Add package pg-index-insight/0.0.3/pg_index_insight-0.0.3.tar.gz

---
+++ packages/pg-index-insight/pg_index_insight-0.0.3.tar.gz/pg_index_insight-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pg_index_insight',  # The name of your package
+    version='0.0.3',  # Initial version
+    author='Huseyin Demir',
+    author_email='huseyin.d3r@gmail.com',
+    description='A Python CLI tool to analyze PostgreSQL indexes',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kylorend3r/pg_index_insight',  # Link to your project
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'click',  # List your dependencies here
+        'psycopg2',
+        'tabulate'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pg_index_insight = pg_index_insight.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',  # Python version compatibility
+)
