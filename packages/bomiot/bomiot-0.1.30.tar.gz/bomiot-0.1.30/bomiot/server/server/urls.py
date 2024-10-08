d2793d6cba87647e4c502e92cc0f9de411e62bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 05:04:49 +0000

    Add package bomiot/0.1.30/bomiot-0.1.30.tar.gz

---
+++ packages/bomiot/bomiot-0.1.30.tar.gz/bomiot-0.1.30/bomiot/server/server/urls.py
@@ -0,0 +1,37 @@
+import os
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic.base import TemplateView
+from django.contrib.staticfiles.views import serve
+from django.views.static import serve as static_serve
+from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from . import views
+
+def return_static(request, path, insecure=True, **kwargs):
+    return serve(request, path, insecure, **kwargs)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('favicon.ico', views.favicon, name='favicon'),
+    re_path('^css/.*$', views.statics, name='css'),
+    re_path('^js/.*$', views.statics, name='js'),
+    re_path('^assets/.*$', views.statics, name='assets'),
+    re_path('^statics/.*$', views.statics, name='statics'),
+    re_path('^fonts/.*$', views.statics, name='fonts'),
+    re_path('^icons/.*$', views.statics, name='icons'),
+    re_path(r'^static/(?P<path>.*)$', return_static, name='static'),
+    re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+urlpatterns += [
+    path('api/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/debug/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/', SpectacularRedocView.as_view(url_name='schema'), name='docs'),
+]
+
+print(1, os.environ.get('RUN_MAIN'))
