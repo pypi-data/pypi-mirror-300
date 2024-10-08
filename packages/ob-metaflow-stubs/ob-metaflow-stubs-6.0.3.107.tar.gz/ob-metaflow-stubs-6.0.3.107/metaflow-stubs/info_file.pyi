b93d8a3959c5807faa1d06d0a7357c88aa29f105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:05:28 +0000

    Add package ob-metaflow-stubs/6.0.3.107/ob-metaflow-stubs-6.0.3.107.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.107.tar.gz/ob-metaflow-stubs-6.0.3.107/metaflow-stubs/info_file.pyi
@@ -0,0 +1,16 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.24.1+obcheckpoint(0.0.11);ob(v1)                              #
+# Generated on 2024-10-07T18:39:02.359086                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+CURRENT_DIRECTORY: str
+
+INFO_FILE: str
+
+def read_info_file():
+    ...
+
