314a4cabbb2eb93a7af7b10483bc6a9d102af930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 05:04:53 +0000

    Add package data-generation-hyper/0.0.3/data_generation_hyper-0.0.3.tar.gz

---
+++ packages/data-generation-hyper/data_generation_hyper-0.0.3.tar.gz/data_generation_hyper-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data_generation_hyper",
+    version="0.0.3",
+    author="Cesare Bidini, Onuralp Guvercin, Emin Yuksel, Mevlut",
+    author_email="cesare.bidini@gmail.com",
+    description="Library for synthetic data generation",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cesbid/data_generation",  # Your repository URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
