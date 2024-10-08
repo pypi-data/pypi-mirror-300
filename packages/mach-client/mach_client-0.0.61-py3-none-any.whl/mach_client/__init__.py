bc330055863a21e28272e18316666a514bc8a70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 14:06:28 +0000

    Add package mach-client/0.0.61/mach_client-0.0.61-py3-none-any.whl

---
+++ packages/mach-client/mach_client-0.0.61-py3-none-any.whl/mach_client/__init__.py
@@ -0,0 +1,40 @@
+from datetime import datetime as _datetime
+import logging as _logging
+import sys as _sys
+
+from .client import GasEstimate, MachClient, Order, Quote, WalletPoints, client
+from .constants import ChainId, Scanner
+from .data_types import Chain, Token
+
+__all__ = [
+    "Chain",
+    "ChainId",
+    "GasEstimate",
+    "MachClient",
+    "Order",
+    "Quote",
+    "Scanner",
+    "Token",
+    "WalletPoints",
+    "client",
+]
+
+
+def _make_logger(name: str) -> _logging.Logger:
+    logger = _logging.getLogger(name)
+
+    if logger.hasHandlers():
+        return logger
+
+    logger.setLevel(_logging.DEBUG)
+
+    handler = _logging.StreamHandler(_sys.stdout)
+    handler.setLevel(_logging.DEBUG)
+    logger.addHandler(handler)
+
+    logger.info(f"START {name} {_datetime.today()}")
+
+    return logger
+
+
+_make_logger("mach-client")
