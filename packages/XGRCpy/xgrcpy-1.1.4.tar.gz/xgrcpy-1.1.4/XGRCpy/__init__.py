8fbd3992084df5fc528f20ee25961ec67552335d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 16:04:34 +0000

    Add package XGRCpy/1.1.4/xgrcpy-1.1.4.tar.gz

---
+++ packages/XGRCpy/xgrcpy-1.1.4.tar.gz/xgrcpy-1.1.4/XGRCpy/__init__.py
@@ -0,0 +1,11 @@
+# Import key functions and classes
+from .XGRCpy import XGRCViewTable
+from .XGRCpy import XGRCSPView
+from .XGRCPowerBI import XGRCViewTablePowerBI
+from .XGRCPowerBI import XGRCViewSPPowerBI
+
+# Optional: Define a version number for the package
+__version__ = "1.1.4"
+
+# Optional: Define the package's public interface
+__all__ = ["XGRCViewTable","XGRCSPView","XGRCViewTablePowerBI","XGRCViewSPPowerBI"]
