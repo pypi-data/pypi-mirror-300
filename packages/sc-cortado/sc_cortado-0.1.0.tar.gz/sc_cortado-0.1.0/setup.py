ccf3171e22d63668dfb28e2a64c2f518b1e199a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 07:04:22 +0000

    Add package sc-cortado/0.1.0/sc_cortado-0.1.0.tar.gz

---
+++ packages/sc-cortado/sc_cortado-0.1.0.tar.gz/sc_cortado-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="sc_cortado",
+    version="0.1.0",
+    author="Musaddiq Lodi",
+    author_email="lodimk2@vcu.edu",
+    description="CORTADO: hill Climbing Optimization foR cell-Type specific mArker gene DiscOvery",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "pandas",
+        "scikit-learn",
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lodimk2/cortado",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
