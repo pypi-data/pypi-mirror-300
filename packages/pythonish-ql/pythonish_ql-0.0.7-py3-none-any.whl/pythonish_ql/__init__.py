e984a6d50fb251cff486f1a92b5b96659f5a9c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 06:05:28 +0000

    Add package pythonish-ql/0.0.7/pythonish_ql-0.0.7-py3-none-any.whl

---
+++ packages/pythonish-ql/pythonish_ql-0.0.7-py3-none-any.whl/pythonish_ql/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# **************************************************************
+# Pythonish Query Language, for Gramps and others
+#
+# Copyright (c) Douglas Blank
+# MIT License
+#
+# Largely based on https://github.com/DavidMStraub/gramps-ql
+# **************************************************************
+
+from .pythonish_ql import (
+    PythonishQuery,
+    apply,
+    iter_objects,
+    match,
+    parse,
+)
