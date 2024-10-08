4ec49f06e5bee966e992153a33d958be7ef52d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 04:04:25 +0000

    Add package ipylgbst/0.3.0/ipylgbst-0.3.0.tar.gz

---
+++ packages/ipylgbst/ipylgbst-0.3.0.tar.gz/ipylgbst-0.3.0/src/__tests__/index.spec.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+// Add any needed widget imports here (or from controls)
+// import {} from '@jupyter-widgets/base';
+
+import { createTestModel } from './utils';
+
+import { LegoBoostModel } from '../widget';
+
+describe('Example', () => {
+  describe('LegoBoostModel', () => {
+    it('should be createable', () => {
+      const model = createTestModel(LegoBoostModel);
+      expect(model).toBeInstanceOf(LegoBoostModel);
+    });
+  });
+});
