c1117237e3ead1c82589a813e569a103ca39b755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-08 05:04:52 +0000

    Add package tempren/1.2.6/tempren-1.2.6-py3-none-any.whl

---
+++ packages/tempren/tempren-1.2.6-py3-none-any.whl/tempren/template/generators.py
@@ -0,0 +1,36 @@
+import logging
+from abc import ABC
+from pathlib import Path
+
+from tempren.primitives import File, PathGenerator, Pattern
+from tempren.template.exceptions import InvalidFilenameError
+
+
+class TemplateGenerator(PathGenerator, ABC):
+    log: logging.Logger
+    pattern: Pattern
+
+    def __init__(self, pattern: Pattern):
+        self.log = logging.getLogger(__name__)
+        self.log.debug("Creating template generator with template: %s", pattern)
+        self.pattern = pattern
+
+    def generate_replacement(self, file: File) -> str:
+        self.log.debug("Rendering template for '%s'", file.relative_path)
+        rendered_template = self.pattern.process(file)
+        self.log.debug("Rendered template: '%s'", rendered_template)
+        return rendered_template
+
+
+class TemplateNameGenerator(TemplateGenerator):
+    def generate(self, file: File) -> Path:
+        new_name = self.generate_replacement(file)
+        try:
+            return file.relative_path.with_name(new_name)
+        except ValueError:
+            raise InvalidFilenameError(new_name)
+
+
+class TemplatePathGenerator(TemplateGenerator):
+    def generate(self, file: File) -> Path:
+        return Path(self.generate_replacement(file))
