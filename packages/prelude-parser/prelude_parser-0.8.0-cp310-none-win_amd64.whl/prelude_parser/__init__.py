bceb4d27679c3ec42cc3d76adb01373d12c4fbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 07:04:29 +0000

    Add package prelude-parser/0.8.0/prelude_parser-0.8.0-cp310-none-win_amd64.whl

---
+++ packages/prelude-parser/prelude_parser-0.8.0-cp310-none-win_amd64.whl/prelude_parser/__init__.py
@@ -0,0 +1,20 @@
+from prelude_parser._prelude_parser import (
+    parse_site_native_file,
+    parse_site_native_string,
+    parse_subject_native_file,
+    parse_subject_native_string,
+    parse_user_native_file,
+    parse_user_native_string,
+)
+from prelude_parser.parser import parse_to_classes, parse_to_dict
+
+__all__ = [
+    "parse_site_native_file",
+    "parse_site_native_string",
+    "parse_subject_native_file",
+    "parse_subject_native_string",
+    "parse_user_native_file",
+    "parse_user_native_string",
+    "parse_to_classes",
+    "parse_to_dict",
+]
