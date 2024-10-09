195741ee682cb07460c9c972a3f09e56744c6922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 17:09:34 +0000

    Add package moneysnake/0.0.7/moneysnake-0.0.7-py3-none-any.whl

---
+++ packages/moneysnake/moneysnake-0.0.7-py3-none-any.whl/moneysnake/__init__.py
@@ -0,0 +1,8 @@
+from .client import MB_URL as MB_URL
+from .client import MB_VERSION_ID as MB_VERSION_ID
+from .client import post_request as post_request
+from .client import set_admin_id as set_admin_id
+from .client import set_timeout as set_timeout
+from .client import set_token as set_token
+from .contact import Contact as Contact
+from .contact import ContactPerson as ContactPerson
---
+++ packages/moneysnake/moneysnake-0.0.7-py3-none-any.whl/moneysnake/contact.pyi
@@ -0,0 +1,45 @@
+from .client import post_request as post_request
+from .custom_field_model import CustomFieldModel as CustomFieldModel
+from pydantic import BaseModel
+
+class ContactPerson(BaseModel):
+    firstname: str | None
+    lastname: str | None
+
+class Contact(CustomFieldModel):
+    company_name: str | None
+    address1: str | None
+    address2: str | None
+    zipcode: str | None
+    city: str | None
+    country: str | None
+    phone: str | None
+    delivery_method: str | None
+    customer_id: str | None
+    tax_number: str | None
+    firstname: str | None
+    lastname: str | None
+    chamber_of_commerce: str | None
+    bank_account: str | None
+    send_invoices_to_attention: str | None
+    send_invoices_to_email: str | None
+    send_estimates_to_attention: str | None
+    send_estimates_to_email: str | None
+    sepa_active: bool
+    sepa_iban: str | None
+    sepa_iban_account_name: str | None
+    sepa_bic: str | None
+    sepa_mandate_id: str | None
+    sepa_mandate_date: str | None
+    sepa_sequence_type: str | None
+    si_identifier_type: str | None
+    si_identifier: str | None
+    invoice_workflow_id: int | None
+    estimate_workflow_id: int | None
+    email_ubl: bool
+    direct_debit: bool
+    contact_person: ContactPerson | None
+    type: str | None
+    from_checkout: bool
+    @staticmethod
+    def find_by_customer_id(customer_id: str) -> Contact: ...
