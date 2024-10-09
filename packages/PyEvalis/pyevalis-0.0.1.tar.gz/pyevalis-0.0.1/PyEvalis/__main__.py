cbc580cfead9cf334b5a441562aa3df4b78431b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:43 +0000

    Add package PyEvalis/0.0.1/pyevalis-0.0.1.tar.gz

---
+++ packages/PyEvalis/pyevalis-0.0.1.tar.gz/pyevalis-0.0.1/PyEvalis/__main__.py
@@ -0,0 +1,16 @@
+import DecisionTree
+from pyevalis import *
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Error: Incorrect number of parameters.")
+        print("Usage: pyevalis <inputFileName> <resultFileName>")
+    else:
+        inputFileName = sys.argv[1]
+        resultFileName = sys.argv[2]
+        #DecisionTree.DecisionTree(inputFileName, resultFileName)
+
+
+if __name__ == "__main__":
+    main()
