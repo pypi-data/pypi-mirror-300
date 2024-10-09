1db0be34ebc268544a492f5ec05809e3cddd07d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 16:07:03 +0000

    Add package mtmai-client/0.3.1099/mtmai_client-0.3.1099.tar.gz

---
+++ packages/mtmai-client/mtmai_client-0.3.1099.tar.gz/mtmai_client-0.3.1099/mtmai_client/models/text_2_image_request.py
@@ -0,0 +1,80 @@
+from typing import Any, Dict, List, Type, TypeVar, Union, cast
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="Text2ImageRequest")
+
+
+@_attrs_define
+class Text2ImageRequest:
+    """
+    Attributes:
+        prompt (str): Prompt must be at least 10 characters.
+        model (Union[None, Unset, str]):
+    """
+
+    prompt: str
+    model: Union[None, Unset, str] = UNSET
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        prompt = self.prompt
+
+        model: Union[None, Unset, str]
+        if isinstance(self.model, Unset):
+            model = UNSET
+        else:
+            model = self.model
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "prompt": prompt,
+            }
+        )
+        if model is not UNSET:
+            field_dict["model"] = model
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        prompt = d.pop("prompt")
+
+        def _parse_model(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        model = _parse_model(d.pop("model", UNSET))
+
+        text_2_image_request = cls(
+            prompt=prompt,
+            model=model,
+        )
+
+        text_2_image_request.additional_properties = d
+        return text_2_image_request
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
