abb899736f3b35686317a7deb8f35c8cc5472937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:06:52 +0000

    Add package gqxls/0.0.1/gqxls-0.0.1.tar.gz

---
+++ packages/gqxls/gqxls-0.0.1.tar.gz/gqxls-0.0.1/README.md
@@ -0,0 +1,21 @@
+# gqxls
+
+*<u>A package for daily ues.</u>*
+
+## A. Functions
+
+### 1. Convert PDF to image
+
+### 2. Get the specified directory suffix file absolute directory
+
+### 3. Generate specified random string
+
+## B. Long term plan
+The package requires the following free softwares to work as expected:
+- pillow
+- secrets
+- pymupdf
+- typing
+
+
+
---
+++ packages/gqxls/gqxls-0.0.1.tar.gz/gqxls-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi (if you dont need delete it)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# you need to change all these
+VERSION = '0.0.1'
+DESCRIPTION = 'Consolidation package for daily use'
+LONG_DESCRIPTION = 'gqxls is an integration package for daily use'
+
+setup(
+    name="gqxls",
+    version=VERSION,
+    author="S Liao",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pillow','secrets','pymupdf','typing'],
+    keywords=['python', 'gqxls'],
+    python_requires='>=3.6',
+
+)
