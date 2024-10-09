ed4fc1c014b3b6656c6d83ce43413feaa7ffeca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:23 +0000

    Add package patchwork-cli/0.0.68/patchwork_cli-0.0.68-py3-none-any.whl

---
+++ packages/patchwork-cli/patchwork_cli-0.0.68-py3-none-any.whl/patchwork/steps/ScanSemgrep/typed.py
@@ -0,0 +1,13 @@
+from typing_extensions import Annotated, TypedDict
+
+from patchwork.common.utils.step_typing import StepTypeConfig
+
+
+class ScanSemgrepInputs(TypedDict, total=False):
+    sarif_file_path: Annotated[str, StepTypeConfig(is_config=True, is_path=True)]
+    sarif_values: str
+    semgrep_extra_args: Annotated[str, StepTypeConfig(is_config=True)]
+
+
+class ScanSemgrepOutputs(TypedDict):
+    sarif_values: dict
