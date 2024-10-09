a0edc6384ea5eb86c742f476f13a46398ed05b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:05:24 +0000

    Add package yc-profiler-dev/0.7/yc_profiler_dev-0.7.tar.gz

---
+++ packages/yc-profiler-dev/yc_profiler_dev-0.7.tar.gz/yc_profiler_dev-0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yc-profiler-dev',
+    version='0.7',
+    packages=find_packages(exclude=['tests*']),
+    description='yCrash Python Agent',
+    install_requires=['pyyaml', 'requests', 'memory_profiler','psutil'],
+    url='https://ycrash.io',
+    author='yCrash Team',
+    author_email='mahesh@tier1app.com'
+)
---
+++ packages/yc-profiler-dev/yc_profiler_dev-0.7.tar.gz/yc_profiler_dev-0.7/ycrash/system/system_profiler.py
@@ -0,0 +1,59 @@
+"""
+    yCrash System Profiler: System Profiler
+    This service is reponsible for retrieving and uploading system data to yCrash
+"""
+from datetime import datetime
+
+from commons.common import run_command
+from commons.webclient import upload_json_data
+import json
+from commons.common import getKey, getServerUrl
+from .system_info import SystemInfo
+from .system_commands import BaseCommands
+from log.ycrash_logger import ycrash_logger
+
+
+PS_DUMP_ENDPOINT = "/yc-receiver?dt=ps"
+DF_DUMP_ENDPOINT = "/yc-receiver?dt=df"
+NETSTAT_DUMP_ENDPOINT = "/yc-receiver?dt=ns"
+KERNAL_DUMP_ENDPOINT = "/yc-receiver?dt=kernal"
+TOP_DUMP_ENDPOINT = "/yc-receiver?dt=top"
+TOPH_DUMP_ENDPOINT = "/yc-receiver?dt=toph"
+DMSEG_DUMP_ENDPOINT = "/yc-receiver?dt=dmesg"
+PING_DUMP_ENDPOINT = "/yc-receiver?dt=ping"
+VMSTAT_DUMP_ENDPOINT = "/yc-receiver?dt=vmstat"
+
+
+"""
+    Runs all system commands for the OS.
+"""
+def export_all_system_data(config):
+    system_info = SystemInfo()
+    ycrash_logger.log(f"System Info {system_info}")
+    commands_manager = BaseCommands().get_commands(system_info.get_os_name())
+    capture_and_upload_system_data(config, PS_DUMP_ENDPOINT, commands_manager.PS)
+    capture_and_upload_system_data(config, DF_DUMP_ENDPOINT, commands_manager.Disk)
+    capture_and_upload_system_data(config, NETSTAT_DUMP_ENDPOINT, commands_manager.NetStat)
+    capture_and_upload_system_data(config, TOP_DUMP_ENDPOINT, commands_manager.Top)
+    capture_and_upload_system_data(config, TOPH_DUMP_ENDPOINT, commands_manager.TopH)
+    capture_and_upload_system_data(config, DMSEG_DUMP_ENDPOINT, commands_manager.DMesg2)
+    capture_and_upload_system_data(config, KERNAL_DUMP_ENDPOINT, commands_manager.KernelParam)
+    capture_and_upload_system_data(config, VMSTAT_DUMP_ENDPOINT, commands_manager.VMState)
+    capture_and_upload_system_data(config, PING_DUMP_ENDPOINT, commands_manager.Ping)
+
+
+"""
+    Executes each system data and export the data to yCrash server
+"""
+def capture_and_upload_system_data(config, endpoint, command):
+    start_time = datetime.now()
+    data = {
+        "data": run_command(command),
+    }
+    end_time = datetime.now()
+    time_took = end_time - start_time
+    system_details_json = json.dumps(data, indent=4)
+    ycrash_logger.debug("Uploading system data...")
+    upload_json_data('application/json', getKey(config), getServerUrl(config, endpoint),
+                     system_details_json)
+    ycrash_logger.debug(f"Time taken for command {command} is {time_took.total_seconds()}")
