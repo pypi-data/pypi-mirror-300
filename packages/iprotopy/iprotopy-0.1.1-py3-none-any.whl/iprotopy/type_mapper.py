4d70bf08815a7faadf291c728214e41cfd714d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 07:05:18 +0000

    Add package iprotopy/0.1.1/iprotopy-0.1.1-py3-none-any.whl

---
+++ packages/iprotopy/iprotopy-0.1.1-py3-none-any.whl/iprotopy/type_mapper.py
@@ -0,0 +1,31 @@
+from _ast import alias
+from typing import Tuple, Union
+
+from imports import ImportFrom
+
+from iprotopy.import_types import AstImport
+
+
+class TypeMapper:
+    def __init__(self):
+        self._standard_types_mapping = {
+            'string': 'str',
+            'int64': 'int',
+            'int32': 'int',
+            'double': 'float',
+            'bool': 'bool',
+        }
+
+        self._google_types_mapping = {
+            'google.protobuf.Timestamp': (
+                'datetime',
+                ImportFrom(module='datetime', names=[alias(name='datetime')], level=0),
+            )
+        }
+
+    def map(self, proto_type: str) -> Tuple[str, Union[AstImport, None]]:
+        if proto_type in self._standard_types_mapping:
+            return self._standard_types_mapping[proto_type], None
+        if proto_type in self._google_types_mapping:
+            return self._google_types_mapping[proto_type]
+        raise ValueError(f'Unknown type {proto_type}')
