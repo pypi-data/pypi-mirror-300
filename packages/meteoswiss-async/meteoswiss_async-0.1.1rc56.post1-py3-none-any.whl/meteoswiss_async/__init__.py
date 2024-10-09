697bbabd701d22fe49a80207d3d8616b3643d7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:12 +0000

    Add package meteoswiss-async/0.1.1rc56.post1/meteoswiss_async-0.1.1rc56.post1-py3-none-any.whl

---
+++ packages/meteoswiss-async/meteoswiss_async-0.1.1rc56.post1-py3-none-any.whl/meteoswiss_async/__init__.py
@@ -0,0 +1,10 @@
+"""Python Package Template"""
+
+from __future__ import annotations
+
+__version__ = "0.1.1""-rc56-post1"
+__author___ = "albertomontesg <al.montes.gomez@gmail.com>"
+
+from .client import *  # noqa: F403
+from .errors import *  # noqa: F403
+from .model import *  # noqa: F403
