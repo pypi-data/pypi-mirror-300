f71eb74df8ea8f9a79b0eba9dd4393dd2b8bf360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:06:58 +0000

    Add package utubes/0.0.50b2/utubes-0.0.50b2.tar.gz

---
+++ packages/utubes/utubes-0.0.50b2.tar.gz/utubes-0.0.50b2/Utubes/functions/__init__.py
@@ -0,0 +1,12 @@
+from .function05 import BKeys, UKeys
+from .function12 import DownloadSR
+from .function01 import DownloadER
+from .function02 import Extractors
+from .function08 import Instagram
+from .function03 import FormatoR
+from .function04 import Filename
+from .function07 import Quality
+from .function09 import Finders
+from .function06 import Dlinks
+from .function11 import Cancel
+from .function10 import Glink
---
+++ packages/utubes/utubes-0.0.50b2.tar.gz/utubes-0.0.50b2/Utubes/scripts/en.py
@@ -0,0 +1,11 @@
+
+class Scripted(object):
+    DATA01 = ""
+    DATA11 = " "
+    DATA05 = "NA"
+    DATA06 = "tmp"
+    DATA12 = ".tmp"
+    DATA07 = "{}.{}"
+    DATA10 = "{}.{}"
+    DATA08 = "Unknown"
+    DATA09 = "Unknown.tmp"
