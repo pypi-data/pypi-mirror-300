02322cd32ebe826bf6dff378724efb270c9a2aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:05:15 +0000

    Add package pharaoh-report/0.7.4/pharaoh_report-0.7.4.tar.gz

---
+++ packages/pharaoh-report/pharaoh_report-0.7.4.tar.gz/pharaoh_report-0.7.4/src/pharaoh/sphinx_app.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import typing as t
+from pathlib import Path
+
+from sphinx.application import Sphinx
+
+if t.TYPE_CHECKING:  # We get circular imports otherwise
+    from pharaoh.project import PharaohProject
+    from pharaoh.templating.second_level.template_env import PharaohTemplateEnv
+
+
+class PharaohSphinx(Sphinx):
+    # Set by entrypoint of Sphinx plugin "asset_ext"
+    # ...sphinx_ext.asset_ext.setup()
+    pharaoh_proj: PharaohProject
+    # Set by entrypoint of Sphinx plugin "jinja_ext"
+    # ...jinja_ext.setup()
+    pharaoh_te: PharaohTemplateEnv
+
+    @property
+    def assets_dir(self):
+        return Path(self.builder.outdir) / "pharaoh_assets"
