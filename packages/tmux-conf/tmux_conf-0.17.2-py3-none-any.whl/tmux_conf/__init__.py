9d66cd6ec67928c0842aaa558448a951de370c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:07:14 +0000

    Add package tmux-conf/0.17.2/tmux_conf-0.17.2-py3-none-any.whl

---
+++ packages/tmux-conf/tmux_conf-0.17.2-py3-none-any.whl/tmux_conf/__init__.py
@@ -0,0 +1,13 @@
+#  -*- mode: python; mode: fold -*-
+#
+#  Copyright (c) 2022-2024: Jacob.Lundqvist@gmail.com
+#  License: MIT
+#
+#  Part of https://github.com/jaclu/tmux-conf
+#
+
+"""base packet imports"""
+
+from .tmux_conf import TmuxConfig
+
+__all__ = ["TmuxConfig"]
