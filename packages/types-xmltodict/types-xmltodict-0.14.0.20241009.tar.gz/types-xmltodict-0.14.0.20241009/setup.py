5e74c2e0d346d56743c903563b48729eedb41ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:23 +0000

    Add package types-xmltodict/0.14.0.20241009/types-xmltodict-0.14.0.20241009.tar.gz

---
+++ packages/types-xmltodict/types-xmltodict-0.14.0.20241009.tar.gz/types-xmltodict-0.14.0.20241009/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+name = "types-xmltodict"
+description = "Typing stubs for xmltodict"
+long_description = '''
+## Typing stubs for xmltodict
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`xmltodict`](https://github.com/martinblech/xmltodict) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`xmltodict`.
+
+This version of `types-xmltodict` aims to provide accurate annotations
+for `xmltodict==0.14.*`.
+The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/xmltodict. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit
+[`4b11151a0c70a09049c69d8fe69351a739472fea`](https://github.com/python/typeshed/commit/4b11151a0c70a09049c69d8fe69351a739472fea) and was tested
+with mypy 1.11.2, pyright 1.1.383, and
+pytype 2024.9.13.
+'''.lstrip()
+
+setup(name=name,
+      version="0.14.0.20241009",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/xmltodict.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['xmltodict-stubs'],
+      package_data={'xmltodict-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
