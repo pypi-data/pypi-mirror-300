bb0ee8391cdcc2f474abceb5eee803834e300cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:06:18 +0000

    Add package fence-llm/0.0.15/fence_llm-0.0.15.tar.gz

---
+++ packages/fence-llm/fence_llm-0.0.15.tar.gz/fence_llm-0.0.15/fence/agents/base.py
@@ -0,0 +1,61 @@
+"""
+Base Agent class
+"""
+
+import logging
+from abc import abstractmethod
+
+from fence import LLM
+from fence.links import logger as link_logger
+
+logger = logging.getLogger(__name__)
+
+# Suppress the link logger
+link_logger.setLevel("INFO")
+
+
+class BaseAgent:
+    """Base Agent class"""
+
+    def __init__(
+        self,
+        identifier: str | None = None,
+        model: LLM = None,
+        description: str | None = None,
+        environment: dict | None = None,
+    ):
+        """
+        Initialize the Agent object.
+
+        :param identifier: An identifier for the agent. If none is provided, the class name will be used.
+        :param model: An LLM model object.
+        :param description: A description of the agent.
+        :param environment: A dictionary of environment variables to pass to delegates and tools.
+        """
+        self.environment = environment or {}
+        self.identifier = identifier or self.__class__.__name__
+        self.model = model
+        self.description = description
+
+    @abstractmethod
+    def run(self, prompt: str) -> str:
+        """
+        Run the agent
+
+        :param prompt: The initial prompt to feed to the LLM
+        """
+        raise NotImplementedError
+
+    def format_toml(self):
+        """
+        Returns a TOML-formatted key-value pair of the agent name,
+        the description (docstring) of the agent.
+        """
+
+        # Preformat the arguments
+        toml_string = f"""[[agents]]
+agent_name = "{self.identifier}"
+agent_description = "{self.description or self.__doc__}"
+"""
+
+        return toml_string
