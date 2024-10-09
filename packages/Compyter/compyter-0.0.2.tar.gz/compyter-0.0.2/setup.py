c2cf4af0080c1d0194c57bb8aec373a74cdf73e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:14 +0000

    Add package Compyter/0.0.2/compyter-0.0.2.tar.gz

---
+++ packages/Compyter/compyter-0.0.2.tar.gz/compyter-0.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="compyter",              # Package name
+    version="0.0.2",                # Initial release version
+    packages=find_packages(),       # Automatically find and include all packages
+    install_requires=[            # Optional: List dependencies (if any    install_requires=[
+        'bs4',    # Example dependency
+        'black'  # Another example
+    ],
+    description="A component parser for html",
+    long_description=open('README.org').read(),  # Read long description from file
+    long_description_content_type='text/plain',  # Specify Markdown as the format
+    url="https://github.com/spynets/compyte",  # GitHub repo or package URL
+    author="Alfred Roos",             # Your name
+    author_email="alfred@stensatter.se",  # Your contact email
+    license="MIT",                  # License type (e.g., MIT)
+    classifiers=[                   # Additional metadata about your package
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',        # Python version compatibility
+
+)
