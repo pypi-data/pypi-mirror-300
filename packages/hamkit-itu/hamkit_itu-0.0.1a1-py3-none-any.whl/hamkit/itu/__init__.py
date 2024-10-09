8cf7a91b16b342bbb5dfb3a66b9539ecb5a2c1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:21 +0000

    Add package hamkit-itu/0.0.1a1/hamkit_itu-0.0.1a1-py3-none-any.whl

---
+++ packages/hamkit-itu/hamkit_itu-0.0.1a1-py3-none-any.whl/hamkit/itu/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2024-present Adam Fourney <adam.fourney@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from ._itu import (
+    ITU_PREFIXES,
+    ItuPrefix,
+    call_sign_to_country,
+    prefix_to_countries,
+    country_to_prefixes,
+)
