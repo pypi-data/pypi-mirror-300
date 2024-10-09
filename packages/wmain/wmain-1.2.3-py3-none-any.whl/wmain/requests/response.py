7cf65b061e96b9c85749fc7cb48af75504c60457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:23 +0000

    Add package wmain/1.2.3/wmain-1.2.3-py3-none-any.whl

---
+++ packages/wmain/wmain-1.2.3-py3-none-any.whl/wmain/requests/response.py
@@ -0,0 +1,65 @@
+from requests import Response
+import json
+import re
+
+
+class WResponse:
+
+    def __init__(self, response: Response) -> None:
+        self.response = response
+        charset: re.Match = re.search(
+            r'charset=["\']?([a-z0-9-]*)["\']?', response.text
+        )
+        self.response.encoding = (
+            charset.group(1) if charset else response.apparent_encoding
+        )
+
+    def __repr__(self) -> str:
+        return f"<WResponse [{self.status_code}]>"
+    
+    def __str__(self) -> str:
+        return f"<WResponse [{self.status_code}]>"
+    
+    @property
+    def json(self) -> json:
+        return self.response.json()
+    
+    @property
+    def status_code(self) -> int:
+        return self.response.status_code
+
+    @property
+    def content(self) -> bytes:
+        return self.response.content
+
+    @property
+    def text(self) -> str:
+        return self.response.text
+
+    @property
+    def headers(self) -> dict:
+        return self.response.headers
+
+    @property
+    def cookies(self) -> dict:
+        return self.response.cookies
+
+    @property
+    def url(self) -> str:
+        return self.response.url
+
+    @property
+    def encoding(self) -> str:
+        return self.response.encoding
+
+    @property
+    def is_redirect(self) -> bool:
+        return self.response.is_redirect
+
+    @property
+    def history(self) -> list:
+        return self.response.history
+    
+    def save(self, filename: str) -> None:
+        with open(filename, 'wb') as f:
+            f.write(self.content)
