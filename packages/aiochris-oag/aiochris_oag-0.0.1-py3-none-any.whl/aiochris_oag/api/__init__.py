9c91161a1eea8bb94674d85d1828a82a33543480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 05:05:46 +0000

    Add package aiochris-oag/0.0.1/aiochris_oag-0.0.1-py3-none-any.whl

---
+++ packages/aiochris-oag/aiochris_oag-0.0.1-py3-none-any.whl/aiochris_oag/api/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+
+# import apis into api package
+from aiochris_oag.api.auth_token_api import AuthTokenApi
+from aiochris_oag.api.chris_admin_api import ChrisAdminApi
+from aiochris_oag.api.chrisinstance_api import ChrisinstanceApi
+from aiochris_oag.api.comments_api import CommentsApi
+from aiochris_oag.api.computeresources_api import ComputeresourcesApi
+from aiochris_oag.api.default_api import DefaultApi
+from aiochris_oag.api.downloadtokens_api import DownloadtokensApi
+from aiochris_oag.api.filebrowser_api import FilebrowserApi
+from aiochris_oag.api.grouppermissions_api import GrouppermissionsApi
+from aiochris_oag.api.groups_api import GroupsApi
+from aiochris_oag.api.note_api import NoteApi
+from aiochris_oag.api.pacs_api import PacsApi
+from aiochris_oag.api.pipelines_api import PipelinesApi
+from aiochris_oag.api.plugininstances_api import PlugininstancesApi
+from aiochris_oag.api.plugins_api import PluginsApi
+from aiochris_oag.api.publicfeeds_api import PublicfeedsApi
+from aiochris_oag.api.schema_api import SchemaApi
+from aiochris_oag.api.search_api import SearchApi
+from aiochris_oag.api.taggings_api import TaggingsApi
+from aiochris_oag.api.tags_api import TagsApi
+from aiochris_oag.api.userfiles_api import UserfilesApi
+from aiochris_oag.api.userpermissions_api import UserpermissionsApi
+from aiochris_oag.api.users_api import UsersApi
+
