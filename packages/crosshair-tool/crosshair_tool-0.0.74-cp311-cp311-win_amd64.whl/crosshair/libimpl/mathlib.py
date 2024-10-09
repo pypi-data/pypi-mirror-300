0da319f01208bd4b4d53f227a910b002de92728e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:46 +0000

    Add package crosshair-tool/0.0.74/crosshair_tool-0.0.74-cp311-cp311-win_amd64.whl

---
+++ packages/crosshair-tool/crosshair_tool-0.0.74-cp311-cp311-win_amd64.whl/crosshair/libimpl/mathlib.py
@@ -0,0 +1,139 @@
+import math
+import sys
+
+import z3
+
+from crosshair import NoTracing, register_patch
+from crosshair.core import with_realized_args
+from crosshair.libimpl.builtinslib import (
+    PreciseIeeeSymbolicFloat,
+    RealBasedSymbolicFloat,
+    SymbolicBool,
+    SymbolicIntable,
+)
+from crosshair.z3util import z3Not, z3Or
+
+if sys.version_info >= (3, 9):
+
+    def _gcd(a=0, b=0):
+        while b:
+            a, b = b, a % b
+        return abs(a)
+
+else:  # (arguments were required in Python <= 3.8)
+
+    def _gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return abs(a)
+
+
+def _isfinite(x):
+    with NoTracing():
+        if isinstance(x, (SymbolicIntable, RealBasedSymbolicFloat)):
+            return True
+        elif isinstance(x, PreciseIeeeSymbolicFloat):
+            return SymbolicBool(z3Not(z3Or(z3.fpIsNaN(x.var), z3.fpIsInf(x.var))))
+        else:
+            return math.isfinite(x)
+
+
+def _isnan(x):
+    with NoTracing():
+        if isinstance(x, (SymbolicIntable, RealBasedSymbolicFloat)):
+            return False
+        elif isinstance(x, PreciseIeeeSymbolicFloat):
+            return SymbolicBool(z3.fpIsNaN(x.var))
+        else:
+            return math.isnan(x)
+
+
+def _isinf(x):
+    with NoTracing():
+        if isinstance(x, (SymbolicIntable, RealBasedSymbolicFloat)):
+            return False
+        elif isinstance(x, PreciseIeeeSymbolicFloat):
+            return SymbolicBool(z3.fpIsInf(x.var))
+        else:
+            return math.isinf(x)
+
+
+_FUNCTIONS_WITH_REALIZATION = [
+    # TODO: we could attempt to implement some of these in the SMT solver
+    "acos",
+    "acosh",
+    "asin",
+    "asinh",
+    "atan",
+    "atan2",
+    "atanh",
+    "ceil",
+    "comb",
+    "copysign",
+    "cos",
+    "cosh",
+    "degrees",
+    "dist",
+    "erf",
+    "erfc",
+    "exp",
+    "expm1",
+    "fabs",
+    "factorial",
+    "floor",
+    "fmod",
+    "frexp",
+    "fsum",
+    "gamma",
+    "hypot",
+    "isclose",
+    "isqrt",
+    "ldexp",
+    "lgamma",
+    "log",
+    "log10",
+    "log1p",
+    "log2",
+    "modf",
+    "perm",
+    "pow",
+    "prod",
+    "radians",
+    "remainder",
+    "sin",
+    "sinh",
+    "sqrt",
+    "tan",
+    "tanh",
+    "trunc",
+]
+
+if sys.version_info >= (3, 9):
+    _FUNCTIONS_WITH_REALIZATION.extend(
+        [
+            "lcm",
+            "nextafter",
+            "ulp",
+        ]
+    )
+
+if sys.version_info >= (3, 11):
+    _FUNCTIONS_WITH_REALIZATION.extend(
+        [
+            "cbrt",
+            "exp2",
+        ]
+    )
+
+if sys.version_info >= (3, 12):
+    _FUNCTIONS_WITH_REALIZATION.append("sumprod")
+
+
+def make_registrations():
+    register_patch(math.gcd, _gcd)
+    register_patch(math.isfinite, _isfinite)
+    register_patch(math.isnan, _isnan)
+    register_patch(math.isinf, _isinf)
+    for fn_name in _FUNCTIONS_WITH_REALIZATION:
+        fn = getattr(math, fn_name)
+        register_patch(fn, with_realized_args(fn))
