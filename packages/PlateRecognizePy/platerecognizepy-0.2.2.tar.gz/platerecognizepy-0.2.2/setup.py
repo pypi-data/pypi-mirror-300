91756c27874e2705106aa0f47856d2037ab8005b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 16:06:14 +0000

    Add package PlateRecognizePy/0.2.2/platerecognizepy-0.2.2.tar.gz

---
+++ packages/PlateRecognizePy/platerecognizepy-0.2.2.tar.gz/platerecognizepy-0.2.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="PlateRecognizePy",
+    version="0.2.2",
+    author="VJ",
+    author_email="",
+    description="A Python library for license plate detection and recognition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/techbyvj/PlateRecognizePy",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "pytesseract",
+        "easyocr",
+        "opencv-python",
+        "numpy",
+        "ultralytics",
+    ],
+)
