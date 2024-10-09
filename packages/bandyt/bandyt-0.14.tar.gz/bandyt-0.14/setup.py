697a7e9a928617e419a691aeeba6a869cfd2626c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 14:04:53 +0000

    Add package bandyt/0.14/bandyt-0.14.tar.gz

---
+++ packages/bandyt/bandyt-0.14.tar.gz/bandyt-0.14/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bandyt',                   # Your module's name
+    version='0.14',                      # Initial release version
+    packages=['bandyt'],           # Automatically find packages in your project
+    install_requires=[                  # Dependencies
+        'numpy',
+        'pandas',
+        'seaborn',
+        'matplotlib',
+        'pydot',
+        'igraph'
+    ],
+    description='A brief description of your module',
+    long_description=open('README.md').read(),  # Optional: Content of your README file
+    long_description_content_type='text/markdown',  # Optional: README file type
+    url='https://github.com/bandyt-group/bandyt',  # URL to your GitHub repository
+    author='Elizaveta Mukhaleva, Babgen Manookian, Grigoriy Gogoshin, Nagarajan Vaidehi, Andrei Rodin, Sergio Branciamore',
+    author_email='bandyt-group@gmail.com',
+    license='MIT',                      # License type
+    classifiers=[                       # Optional metadata
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',            # Minimum Python version requirement
+)
