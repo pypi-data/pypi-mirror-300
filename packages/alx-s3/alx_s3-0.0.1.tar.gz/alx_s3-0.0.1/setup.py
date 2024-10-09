6d5117f9b219e1be5d37cbac978cab5a50880118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:06:25 +0000

    Add package alx-s3/0.0.1/alx_s3-0.0.1.tar.gz

---
+++ packages/alx-s3/alx_s3-0.0.1.tar.gz/alx_s3-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alx-s3",
+    version="0.0.1",
+    author="sairamn",
+    author_email="sairam.n90@gmail.com",
+    description="Dummy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your-username/my-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
