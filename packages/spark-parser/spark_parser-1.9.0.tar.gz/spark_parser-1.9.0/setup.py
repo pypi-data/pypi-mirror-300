1bd9c1bbddc712f6025ec5398bacc7ab6d814df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:05:39 +0000

    Add package spark-parser/1.9.0/spark_parser-1.9.0.tar.gz

---
+++ packages/spark-parser/spark_parser-1.9.0.tar.gz/spark_parser-1.9.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Setup script for the 'spark_parser' distribution."""
+
+# To the extent possible we make this file look more like a
+# configuration file rather than code like setup.py. I find putting
+# configuration stuff in the middle of a function call in setup.py,
+# which for example requires commas in between parameters, is a little
+# less elegant than having it here with reduced code, albeit there
+# still is some room for improvement.
+
+# Things that change more often go here.
+copyright = """
+Copyright (C) 2016, 2022, 2024 Rocky Bernstein <rb@dustyfeet.com>.
+"""
+
+from setuptools import setup
+
+setup()
