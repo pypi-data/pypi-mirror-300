5489db32868d90b17a2efbb15092aeac789f591a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 05:05:46 +0000

    Add package chris-oag/0.0.1/chris_oag-0.0.1.tar.gz

---
+++ packages/chris-oag/chris_oag-0.0.1.tar.gz/chris_oag-0.0.1/chris_oag/api/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+
+# import apis into api package
+from chris_oag.api.auth_token_api import AuthTokenApi
+from chris_oag.api.chris_admin_api import ChrisAdminApi
+from chris_oag.api.chrisinstance_api import ChrisinstanceApi
+from chris_oag.api.comments_api import CommentsApi
+from chris_oag.api.computeresources_api import ComputeresourcesApi
+from chris_oag.api.default_api import DefaultApi
+from chris_oag.api.downloadtokens_api import DownloadtokensApi
+from chris_oag.api.filebrowser_api import FilebrowserApi
+from chris_oag.api.grouppermissions_api import GrouppermissionsApi
+from chris_oag.api.groups_api import GroupsApi
+from chris_oag.api.note_api import NoteApi
+from chris_oag.api.pacs_api import PacsApi
+from chris_oag.api.pipelines_api import PipelinesApi
+from chris_oag.api.plugininstances_api import PlugininstancesApi
+from chris_oag.api.plugins_api import PluginsApi
+from chris_oag.api.publicfeeds_api import PublicfeedsApi
+from chris_oag.api.schema_api import SchemaApi
+from chris_oag.api.search_api import SearchApi
+from chris_oag.api.taggings_api import TaggingsApi
+from chris_oag.api.tags_api import TagsApi
+from chris_oag.api.userfiles_api import UserfilesApi
+from chris_oag.api.userpermissions_api import UserpermissionsApi
+from chris_oag.api.users_api import UsersApi
+
