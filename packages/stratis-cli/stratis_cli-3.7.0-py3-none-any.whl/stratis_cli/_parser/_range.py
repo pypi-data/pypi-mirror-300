e8c8083d7dc6239be728f35f59df0c6f24f37c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:05:09 +0000

    Add package stratis-cli/3.7.0/stratis_cli-3.7.0-py3-none-any.whl

---
+++ packages/stratis-cli/stratis_cli-3.7.0-py3-none-any.whl/stratis_cli/_parser/_range.py
@@ -0,0 +1,86 @@
+# Copyright 2021 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Definition of filesystem actions to display in the CLI.
+"""
+
+# isort: STDLIB
+import argparse
+import re
+
+# isort: THIRDPARTY
+from justbytes import B, GiB, KiB, MiB, PiB, Range, TiB
+
+_RANGE_RE = re.compile(r"^(?P<magnitude>[0-9]+)(?P<units>([KMGTP]i)?B)$")
+
+_SIZE_SPECIFICATION = (
+    "Size must be specified using the format <magnitude><units> where "
+    "<magnitude> is a decimal integer value and <units> is any binary "
+    'unit specifier in the set "B", "KiB", "MiB", "GiB", "TiB", '
+    'and "PiB".'
+)
+
+
+def _unit_map(unit_specifier):
+    """
+    A simple function that maps a unit_specifier string
+    call to a justbytes unit designation.
+
+    :param str unit_specifier: the string
+    :returns: the corresponding justbytes units
+    :rtype: justbytes.BinaryUnit
+    """
+    if unit_specifier == "B":
+        return B
+    if unit_specifier == "KiB":
+        return KiB
+    if unit_specifier == "MiB":
+        return MiB
+    if unit_specifier == "GiB":
+        return GiB
+    if unit_specifier == "TiB":
+        return TiB
+    if unit_specifier == "PiB":
+        return PiB
+    assert False, f'Unknown unit specifier "{unit_specifier}"'
+
+
+def parse_range(values):
+    """
+    Parse a range value.
+
+    :param str values: string to parse
+    """
+    match = _RANGE_RE.search(values)
+    if match is None:
+        raise argparse.ArgumentTypeError(
+            f"Ill-formed size specification: {_SIZE_SPECIFICATION}"
+        )
+
+    (magnitude, unit) = (match.group("magnitude"), match.group("units"))
+
+    units = _unit_map(unit)
+
+    return Range(int(magnitude), units)
+
+
+class RejectAction(argparse.Action):
+    """
+    Just reject any use of the option.
+    """
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        raise argparse.ArgumentError(
+            self, f"Option {option_string} can not be assigned to or set."
+        )
