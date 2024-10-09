c10659d4dbf8ee9c128b15bd3b5cfb5365ebca42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 14:04:59 +0000

    Add package leaderbot/0.0.3/leaderbot-0.0.3.tar.gz

---
+++ packages/leaderbot/leaderbot-0.0.3.tar.gz/leaderbot-0.0.3/leaderbot/evaluate/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileType: SOURCE
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the license found in the LICENSE.txt file in the root
+# directory of this source tree.
+
+
+from .model_selection import model_selection
+from .goodness_of_fit import goodness_of_fit
+from .generalization import generalization
+
+__all__ = ['model_selection', 'goodness_of_fit', 'generalization']
