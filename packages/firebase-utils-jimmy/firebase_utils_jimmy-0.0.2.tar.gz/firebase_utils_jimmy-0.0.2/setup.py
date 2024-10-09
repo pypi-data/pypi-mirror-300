c5b19a6b7269bfb7a2bc58f653f4c24c6ec20d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:28 +0000

    Add package firebase-utils-jimmy/0.0.2/firebase_utils_jimmy-0.0.2.tar.gz

---
+++ packages/firebase-utils-jimmy/firebase_utils_jimmy-0.0.2.tar.gz/firebase_utils_jimmy-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='firebase_utils_jimmy',
+  version='0.0.2',
+  author='Jimmy Q',
+  author_email='jaime.quinones.t@gmail.com',
+  description='Librería con funciones utiles y seguras para la interacción con Firestore.',
+  long_description=open("README.md").read(),
+  url='https://github.com/Serialk89/lib-python-base.git',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.11',
+  install_requires=[
+    'firebase-admin'
+  ],
+)
