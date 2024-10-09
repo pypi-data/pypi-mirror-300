2942e7c21f2f2621c81419431fd5662205904dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 14:05:10 +0000

    Add package mit-21m387/1.0.7/mit_21m387-1.0.7-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/mit-21m387/mit_21m387-1.0.7-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/fmplib/__init__.py
@@ -0,0 +1,13 @@
+__title__ = 'fmplib'
+__author__ = 'Eran Egozy'
+
+from .util import *
+from .unit1 import *
+from .unit2 import *
+from .unit3 import *
+from .unit4 import *
+from .unit5 import *
+from .unit6 import *
+from .unit7 import *
+from .unit8 import *
+from .unit9 import *
