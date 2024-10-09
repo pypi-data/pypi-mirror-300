491ede9bdc6af0f91e096997f9d2e79951d92d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 05:07:08 +0000

    Add package lessgo/1.0/lessgo-1.0.tar.gz

---
+++ packages/lessgo/lessgo-1.0.tar.gz/lessgo-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lessgo',
+    version='1.0',
+    author='gskills',
+    description='A simple lessgo package',
+    packages=find_packages(),
+)
