8ed0904002cb1815c43264d173dedfc99a75b8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:26 +0000

    Add package gramps-webapi/2.4.3/gramps_webapi-2.4.3.tar.gz

---
+++ packages/gramps-webapi/gramps_webapi-2.4.3.tar.gz/gramps_webapi-2.4.3/setup.py
@@ -0,0 +1,72 @@
+#
+# Gramps Web API - A RESTful API for the Gramps genealogy program
+#
+# Copyright (C) 2020-2024      David Straub
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    README = readme_file.read()
+
+with open("gramps_webapi/_version.py") as version_file:
+    exec(version_file.read())
+
+
+REQUIREMENTS = [
+    "gramps-desktop[GUI,i18n]==5.2.*",
+    "Click>=7.0",
+    "Flask>=2.1.0",
+    "Flask-Caching>=2.0.0",
+    "Flask-Compress",
+    "Flask-Cors",
+    "Flask-JWT-Extended>=4.2.1, !=4.4.0, !=4.4.1",
+    "Flask-Limiter>=2.9.0",
+    "Flask-SQLAlchemy",
+    "marshmallow>=3.13.0",
+    "webargs",
+    "SQLAlchemy",
+    "pdf2image",
+    "Pillow",
+    "bleach[css]>=5.0.0",
+    "jsonschema",
+    "ffmpeg-python",
+    "boto3",
+    "alembic",
+    "celery[redis]",
+    "Unidecode",
+    "pytesseract",
+    "gramps-ql>=0.3.0",
+    "sifts>=0.8.2",
+]
+
+setup(
+    author="Gramps Development Team",
+    url="https://github.com/gramps-project/web-api",
+    python_requires=">=3.5",
+    description="A RESTful web API for the Gramps genealogical database.",
+    license="AGPL v3 or greater",
+    install_requires=REQUIREMENTS,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    name="gramps-webapi",
+    packages=find_packages(include=["gramps_webapi", "gramps_webapi.*"]),
+    version=__version__,
+    zip_safe=False,
+)
