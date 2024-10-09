a1e5027560a640a4b94d75ffdf2bdbc2fca914f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 05:05:47 +0000

    Add package ocdiff/0.0.33/ocdiff-0.0.33-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ocdiff/ocdiff-0.0.33-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ocdiff/__init__.pyi
@@ -0,0 +1,12 @@
+def html_diff(
+    a: str,
+    b: str,
+    context_lines: int | None = None,
+    max_total_width: int | None = None,
+) -> str: ...
+def console_diff(
+    a: str,
+    b: str,
+    context_lines: int | None = None,
+    max_total_width: int | None = None,
+) -> str: ...
