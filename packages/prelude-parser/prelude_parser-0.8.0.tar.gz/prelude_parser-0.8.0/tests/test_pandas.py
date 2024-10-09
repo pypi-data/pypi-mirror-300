1f38ed94cb59427535d3a07178453f5bc73dbefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 07:04:29 +0000

    Add package prelude-parser/0.8.0/prelude_parser-0.8.0.tar.gz

---
+++ packages/prelude-parser/prelude_parser-0.8.0.tar.gz/prelude_parser-0.8.0/tests/test_pandas.py
@@ -0,0 +1,44 @@
+import sys
+
+import pytest
+
+if sys.version_info >= (3, 9):
+    import pandas as pd
+
+    from prelude_parser.pandas import to_dataframe
+
+
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="Pandas only supports 3.9+")
+def test_pandas_to_dataframe(test_file_1):
+    result = to_dataframe(test_file_1)
+    data = {
+        "base_form": [
+            "communications.form.name.communications",
+            "communications.form.name.communications",
+        ],
+        "communications_made": ["Yes", "Yes"],
+        "form_group": ["Communications", "Communications"],
+        "form_number": [None, None],
+        "form_state": ["In-Work", "In-Work"],
+        "form_title": ["Communications", "Communications"],
+        "patient_id": [1681574905819, 1681574994823],
+        "patient_name": ["ABC-001", "ABC-002"],
+        "site_id": [1681574834910, 1681574834910],
+        "site_name": ["Some Site", "Some Site"],
+        "study_name": ["PBS", "PBS"],
+    }
+    expected = pd.DataFrame.from_dict(data)
+    result = result.reindex(sorted(result.columns), axis=1)
+    assert expected.equals(result)
+
+
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="Pandas only supports 3.9+")
+def test_pandas_to_dataframe_short_names(test_file_4):
+    result = to_dataframe(test_file_4, short_names=True)
+    data = {
+        "sitename": ["Some Site", "Another Site"],
+        "studyname": ["PBS", "PBS"],
+    }
+    expected = pd.DataFrame.from_dict(data)
+    result = result.reindex(sorted(result.columns), axis=1)
+    assert expected.equals(result)
