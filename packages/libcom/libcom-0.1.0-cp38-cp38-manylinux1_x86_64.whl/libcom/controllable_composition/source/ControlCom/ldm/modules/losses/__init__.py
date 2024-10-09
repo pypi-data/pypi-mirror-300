5fb94bbd81df65015d0cb7f07da345e7a1659d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:15 +0000

    Add package libcom/0.1.0/libcom-0.1.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/libcom/libcom-0.1.0-cp38-cp38-manylinux1_x86_64.whl/libcom/controllable_composition/source/ControlCom/ldm/modules/losses/__init__.py
@@ -0,0 +1,1 @@
+from libcom.controllable_composition.source.ControlCom.ldm.modules.losses.contperceptual import LPIPSWithDiscriminator
---
+++ packages/libcom/libcom-0.1.0-cp38-cp38-manylinux1_x86_64.whl/libcom/objectstitch/__init__.py
@@ -0,0 +1,4 @@
+# change to your lib name
+from .objectstitch import Mure_ObjectStitchModel
+
+__all__ = ['Mure_ObjectStitchModel']
