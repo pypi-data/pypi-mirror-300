b660bd8337512df5ea93dd645d9d9a38b046c08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:06:29 +0000

    Add package nonebot-plugin-weather-rank/0.1.7/nonebot_plugin_weather_rank-0.1.7.tar.gz

---
+++ packages/nonebot-plugin-weather-rank/nonebot_plugin_weather_rank-0.1.7.tar.gz/nonebot_plugin_weather_rank-0.1.7/nonebot_plugin_weather_rank/utils/schema.py
@@ -0,0 +1,9 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class Weather(Model):
+    id = fields.IntField(True, generated=True)
+    group_id = fields.TextField()
+    location_code = fields.TextField()
+    location_name = fields.TextField()
