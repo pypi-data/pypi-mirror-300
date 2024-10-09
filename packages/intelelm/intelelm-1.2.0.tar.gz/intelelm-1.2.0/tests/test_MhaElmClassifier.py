4292dac410b9f8555682417f3e660b00202cdbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:26 +0000

    Add package intelelm/1.2.0/intelelm-1.2.0.tar.gz

---
+++ packages/intelelm/intelelm-1.2.0.tar.gz/intelelm-1.2.0/tests/test_MhaElmClassifier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:45, 15/08/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+from intelelm import MhaElmClassifier
+
+
+def test_MhaElmClassifier_class():
+    X = np.random.rand(100, 6)
+    y = np.random.randint(0, 2, size=100)
+
+    opt_paras = {"name": "GA", "epoch": 10, "pop_size": 30}
+    model = MhaElmClassifier(layer_sizes=(10, ), act_name="elu", obj_name="AS", optim="BaseGA",
+                             optim_paras=opt_paras, verbose=False, seed=42)
+    model.fit(X, y)
+    pred = model.predict(X)
+    assert MhaElmClassifier.SUPPORTED_CLS_OBJECTIVES == model.SUPPORTED_CLS_OBJECTIVES
+    assert pred[0] in (0, 1)
