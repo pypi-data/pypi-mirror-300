d5cee8fbff0bf3656844e46d69e75853624fcab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:06:02 +0000

    Add package astc-encoder-py/0.1.5/astc_encoder_py-0.1.5-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/astc-encoder-py/astc_encoder_py-0.1.5-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/astc_encoder/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.1.5"
+
+from .enum import (
+    ASTCConfigFlags,
+    ASTCProfile,
+    ASTCQualityPreset,
+    ASTCSwizzleComponentSelector,
+    ASTCType,
+)
+
+from .encoder import (
+    ASTCConfig,
+    ASTCContext,
+    ASTCImage,
+    ASTCSwizzle,
+    ASTCError,
+)
