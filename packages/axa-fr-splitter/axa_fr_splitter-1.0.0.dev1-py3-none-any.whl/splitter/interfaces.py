68dfe9d2417e605c49fea4506efaff148e16f575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:05:21 +0000

    Add package axa-fr-splitter/1.0.0.dev1/axa_fr_splitter-1.0.0.dev1-py3-none-any.whl

---
+++ packages/axa-fr-splitter/axa_fr_splitter-1.0.0.dev1-py3-none-any.whl/splitter/interfaces.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import BinaryIO, Protocol
+from collections.abc import Iterable
+
+from splitter.mime_reader import IMimeReader
+from splitter.file import File, FileOrError
+
+
+class IFileHandler(Protocol):
+    def split_document(
+        self,
+        file_info: File | str | Path | BinaryIO | bytes,
+        filename: str | None = None,
+    ) -> Iterable[FileOrError]:
+        ...
+
+    def is_supported(
+        self,
+        file_info: File | str | Path | BinaryIO | bytes,
+        filename: str | None = None,
+    ) -> bool:
+        ...
+
+
+class IExtensionHandler(Protocol):
+    def to_files(self, file: File) -> Iterable[FileOrError]:
+        ...
+
+    def to_handler(
+        self,
+        mime_reader: IMimeReader | None = None,
+        extensions: Iterable[str] | None = None,
+        mime_types: Iterable[str] | None = None,
+    ) -> IFileHandler:
+        ...
