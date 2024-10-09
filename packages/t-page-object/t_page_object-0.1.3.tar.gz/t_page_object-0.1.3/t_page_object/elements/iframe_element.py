8f2193ca45731b7080579a6a77e670ef8f62cb39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 05:05:40 +0000

    Add package t-page-object/0.1.3/t_page_object-0.1.3.tar.gz

---
+++ packages/t-page-object/t_page_object-0.1.3.tar.gz/t_page_object-0.1.3/t_page_object/elements/iframe_element.py
@@ -0,0 +1,8 @@
+"""Frame element module."""
+from ..base_element import BaseElement
+
+
+class IFrameElement(BaseElement):
+    """Class for frame element model."""
+
+    pass
---
+++ packages/t-page-object/t_page_object-0.1.3.tar.gz/t_page_object-0.1.3/tests/test_t_page_object.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Tests for `t_page_object` package."""
+import unittest
+
+
+class TestTPageObject(unittest.TestCase):
+    """Smoke tests of the package."""
+
+    def test_example(self):
+        """Example test."""
+        assert True
