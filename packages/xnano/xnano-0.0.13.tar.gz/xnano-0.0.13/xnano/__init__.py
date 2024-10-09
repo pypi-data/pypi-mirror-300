de49e5c39a3b9a1344a77106b8515765203ae199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 16:06:39 +0000

    Add package xnano/0.0.13/xnano-0.0.13.tar.gz

---
+++ packages/xnano/xnano-0.0.13.tar.gz/xnano-0.0.13/xnano/__init__.py
@@ -0,0 +1,58 @@
+__all__ = [
+    # Core
+    "Completions",
+    "completion",
+    "InstructorMode",
+    "BaseModel",
+    "Field",
+
+    # llm
+    "classifier",
+    "coder",
+    "extractor",
+    "function",
+    "generator",
+    "validator",
+    "planner",
+    "prompter",
+    "qa",
+    "solver",
+    "query",
+    # nlp/data
+    "chunker",
+    "reader",
+
+    # multimodal
+    "image",
+    "audio",
+    "transcribe"
+]
+
+
+from .main import (
+    Completions,
+    completion,
+    InstructorMode,
+    BaseModel,
+    Field
+)
+
+
+from .lib import (
+    classifier,
+    coder,
+    extractor,
+    function,
+    generator,
+    validator,
+    planner,
+    prompter,
+    qa,
+    solver,
+    chunker,
+    reader,
+    image,
+    audio,
+    transcribe,
+    query,
+)
