8c97930c955e79e9acb3bb8e85efa33f81b04172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:20 +0000

    Add package picows/1.4.1/picows-1.4.1-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/picows/picows-1.4.1-cp311-cp311-musllinux_1_2_aarch64.whl/picows/__init__.py
@@ -0,0 +1,29 @@
+from .picows import (
+    WSError,
+    WSMsgType,
+    WSCloseCode,
+    WSFrame,
+    WSTransport,
+    WSListener,
+    WSUpgradeRequest,
+    ws_connect,
+    ws_create_server,
+    PICOWS_DEBUG_LL
+)
+
+
+__all__ = [
+    'WSError',
+    'WSMsgType',
+    'WSCloseCode',
+    'WSFrame',
+    'WSTransport',
+    'WSListener',
+    'WSUpgradeRequest',
+    'ws_connect',
+    'ws_create_server',
+    'PICOWS_DEBUG_LL'
+]
+
+__version__ = "1.4.1"
+__author__ = "Taras Kozlov"
