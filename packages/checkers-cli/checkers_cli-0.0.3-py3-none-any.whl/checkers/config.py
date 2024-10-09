fa216c33adba83f058c36cc79943bfc1404c1886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 07:04:50 +0000

    Add package checkers-cli/0.0.3/checkers_cli-0.0.3-py3-none-any.whl

---
+++ packages/checkers-cli/checkers_cli-0.0.3-py3-none-any.whl/checkers/config.py
@@ -0,0 +1,35 @@
+from typing import Optional
+import os
+import toml
+from pydantic import BaseModel, ValidationError
+from .exceptions import ConfigFileNotFoundException, ConfigFileInvalid
+
+
+class Config(BaseModel):
+    dbt_project_dir: str = os.getcwd()
+    api_host: str = "https://www.getcheckers.com/api"
+
+    @property
+    def manifest_path(self):
+        return os.path.join(self.dbt_project_dir, "target", "manifest.json")
+
+    def dump(self, out):
+        with open(out, "w") as fh:
+            toml.dump(self.model_dump(), fh)
+
+
+def load_config(path: Optional[str] = None, **overrides) -> Config:
+    if path is None:
+        return Config(**overrides)
+    elif not os.path.exists(path):
+        raise ConfigFileNotFoundException(f"No config file found at {path}")
+    else:
+        raw_config = toml.load(open(path, "r"))
+
+    raw_config.update(overrides)
+
+    try:
+        config = Config(**raw_config)
+        return config
+    except ValidationError as err:
+        raise ConfigFileInvalid(str(err))
