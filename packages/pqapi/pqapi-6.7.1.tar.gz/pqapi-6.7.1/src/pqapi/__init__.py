bd451798d92f3996288ad222aa4aa9e9ce8b8c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:07:00 +0000

    Add package pqapi/6.7.1/pqapi-6.7.1.tar.gz

---
+++ packages/pqapi/pqapi-6.7.1.tar.gz/pqapi-6.7.1/src/pqapi/__init__.py
@@ -0,0 +1,47 @@
+from .api import (
+    AnswerResponse,
+    agent_query,
+    async_add_user,
+    async_agent_query,
+    async_delete_bibliography,
+    async_get_bibliography,
+    async_get_feedback,
+    async_get_query,
+    async_send_feedback,
+    async_submit_agent_job,
+    check_dois,
+    delete_bibliography,
+    get_bibliography,
+    get_query_request,
+    submit_agent_job,
+    upload_file,
+    upload_paper,
+)
+from .models import (
+    QueryRequest,
+    UploadMetadata,
+)
+from .version import __version__
+
+__all__ = [
+    "AnswerResponse",
+    "QueryRequest",
+    "UploadMetadata",
+    "__version__",
+    "agent_query",
+    "async_add_user",
+    "async_agent_query",
+    "async_delete_bibliography",
+    "async_get_bibliography",
+    "async_get_feedback",
+    "async_get_query",
+    "async_send_feedback",
+    "async_submit_agent_job",
+    "check_dois",
+    "delete_bibliography",
+    "get_bibliography",
+    "get_query_request",
+    "submit_agent_job",
+    "upload_file",
+    "upload_paper",
+]
