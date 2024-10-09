563eb2571f6a334d199a0bc2d38c7718e6270fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 14:05:34 +0000

    Add package pyjson5/1.6.7/pyjson5-1.6.7.tar.gz

---
+++ packages/pyjson5/pyjson5-1.6.7.tar.gz/pyjson5-1.6.7/docs/conf.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath('..'))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.inheritance_diagram',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.autosectionlabel',
+    'myst_parser',
+]
+
+language = "en"
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = 'PyJSON5'
+copyright = '2018-2024, René Kijewski'
+author = 'René Kijewski'
+
+with open('../src/VERSION.inc', 'rt') as f:
+    version = eval(f.read().strip())
+    release = version
+
+language = None
+exclude_patterns = []
+pygments_style = 'sphinx'
+todo_include_todos = False
+
+html_theme = 'furo'
+htmlhelp_basename = 'PyJSON5doc'
+
+display_toc = True
+autodoc_default_flags = ['members']
+autosummary_generate = True
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3.12', None),
+}
+
+inheritance_graph_attrs = {
+    'size': '"6.0, 8.0"',
+    'fontsize': 32,
+    'bgcolor': 'transparent',
+}
+inheritance_node_attrs = {
+    'color': 'black',
+    'fillcolor': 'white',
+    'style': '"filled,solid"',
+}
+inheritance_edge_attrs = {
+    'penwidth': 1.2,
+    'arrowsize': 0.8,
+}
---
+++ packages/pyjson5/pyjson5-1.6.7.tar.gz/pyjson5-1.6.7/pyjson5.pyx
@@ -0,0 +1,42 @@
+# distutils: language = c++
+# cython: embedsignature = True, language_level = 3, warn.undeclared = True, warn.unreachable = True, warn.maybe_uninitialized = True
+
+# Copyright 2018-2024 René Kijewski <pypi.org@k6i.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include 'src/_imports.pyx'
+include 'src/_constants.pyx'
+
+include 'src/_exceptions.pyx'
+include 'src/_exceptions_decoder.pyx'
+include 'src/_exceptions_encoder.pyx'
+include 'src/_raise_decoder.pyx'
+include 'src/_raise_encoder.pyx'
+
+include 'src/_unicode.pyx'
+
+include 'src/_reader_ucs.pyx'
+include 'src/_reader_callback.pyx'
+include 'src/_readers.pyx'
+include 'src/_decoder.pyx'
+
+include 'src/_writers.pyx'
+include 'src/_writer_reallocatable.pyx'
+include 'src/_writer_callback.pyx'
+include 'src/_writer_noop.pyx'
+include 'src/_encoder_options.pyx'
+include 'src/_encoder.pyx'
+
+include 'src/_exports.pyx'
+include 'src/_legacy.pyx'
