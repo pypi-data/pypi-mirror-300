03e90d08bebed15d006286ca79b0cb43e987fbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:49 +0000

    Add package dissentty-vk/0.3/dissentty_vk-0.3-py3-none-any.whl

---
+++ packages/dissentty-vk/dissentty_vk-0.3-py3-none-any.whl/dissentty_vk/__init__.py
@@ -0,0 +1,16 @@
+# my_vk_bot/__init__.py
+
+__version__ = '0.3'
+__author__ = 'dissentty'
+__license__ = 'MIT'
+
+# Импорт ключевых компонентов библиотеки
+from dissentty_vk.api import VkAPI
+from dissentty_vk.bot import VkBot
+
+# Опциональная инициализация при импорте библиотеки
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info(f"Библиотека {__name__} версии {__version__} инициализирована")
