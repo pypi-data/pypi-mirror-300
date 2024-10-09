8524c20ac0478384772faba535e7545cbced5001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:05:31 +0000

    Add package hellodipesh/0.3.0/hellodipesh-0.3.0.tar.gz

---
+++ packages/hellodipesh/hellodipesh-0.3.0.tar.gz/hellodipesh-0.3.0/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='hellodipesh',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    description='A package to count nulls and -1s in PySpark DataFrames.',
+    author='Dipesh Singh',
+    author_email='prodipesh12@gmail.com',
+    url='https://github.com/prodipesh/hellodipesh',  # Update if needed
+)
