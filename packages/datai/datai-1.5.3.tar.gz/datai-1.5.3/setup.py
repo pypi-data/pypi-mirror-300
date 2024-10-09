0beeef9c6a84c948796b9cd3b1b8d2c4aff382c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 07:05:11 +0000

    Add package datai/1.5.3/datai-1.5.3.tar.gz

---
+++ packages/datai/datai-1.5.3.tar.gz/datai-1.5.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='datai',
+    version='1.5.3',
+    description='A library for data visualization and cleaning',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='M Ans',
+    author_email='m.ans.cs@outlook.com',
+    url='https://github.com/Anschoudary/datai',  # Replace with your actual URL
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'setuptools',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
