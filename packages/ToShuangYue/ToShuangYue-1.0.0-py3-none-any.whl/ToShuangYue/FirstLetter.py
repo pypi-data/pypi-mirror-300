a3f217b7a11422799d9ee34db93370bed487ec34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:14 +0000

    Add package ToShuangYue/1.0.0/ToShuangYue-1.0.0-py3-none-any.whl

---
+++ packages/ToShuangYue/ToShuangYue-1.0.0-py3-none-any.whl/ToShuangYue/FirstLetter.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def heart():
+    t=np.linspace(0,2*np.pi,100)
+    x=16*np.sin(t)**3
+    y=13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t)
+    plt.plot(x,y,color='red')
+    plt.axis('equal')
+    plt.axis('off')
+    plt.savefig('Heart2ShuangYue.pdf')
+    plt.show()
