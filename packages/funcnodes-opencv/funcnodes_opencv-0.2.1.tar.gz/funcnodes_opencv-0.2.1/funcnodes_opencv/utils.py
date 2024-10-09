87663d18ec8fc44fac1f8b56b33f423549d4195f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 05:07:35 +0000

    Add package funcnodes-opencv/0.2.1/funcnodes_opencv-0.2.1.tar.gz

---
+++ packages/funcnodes-opencv/funcnodes_opencv-0.2.1.tar.gz/funcnodes_opencv-0.2.1/funcnodes_opencv/utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def normalize(img: np.ndarray, to_uint8: bool = True):
+    img = img.astype(float)
+    img -= img.min()
+    img /= img.max()
+    if to_uint8:
+        img *= 255
+        img = img.astype(np.uint8)
+    return img
+
+
+def gen_lut():
+    """
+    Generate a label colormap compatible with opencv lookup table, based on
+    Rick Szelski algorithm in `Computer Vision: Algorithms and Applications`,
+    appendix C2 `Pseudocolor Generation`.
+    :Returns:
+      color_lut : opencv compatible color lookup table
+    """
+    tobits = lambda x, o: np.array(list(np.binary_repr(x, 24)[o::-3]), np.uint8)
+    arr = np.arange(256)
+    r = np.concatenate([np.packbits(tobits(x, -3)) for x in arr])
+    g = np.concatenate([np.packbits(tobits(x, -2)) for x in arr])
+    b = np.concatenate([np.packbits(tobits(x, -1)) for x in arr])
+    return np.concatenate([[[b]], [[g]], [[r]]]).T
+
+
+LUT = gen_lut()
