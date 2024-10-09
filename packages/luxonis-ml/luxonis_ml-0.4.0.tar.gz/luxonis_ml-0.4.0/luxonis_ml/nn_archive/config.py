446d18f09f4991eb989cbd15453a8e938a2cfdb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 14:05:10 +0000

    Add package luxonis-ml/0.4.0/luxonis_ml-0.4.0.tar.gz

---
+++ packages/luxonis-ml/luxonis_ml-0.4.0.tar.gz/luxonis_ml-0.4.0/luxonis_ml/nn_archive/config.py
@@ -0,0 +1,31 @@
+from typing import Literal
+
+from pydantic import Field
+
+from luxonis_ml.utils import BaseModelExtraForbid
+
+from .model import Model
+
+CONFIG_VERSION = Literal["1.0", "1.1"]
+
+
+class Config(BaseModelExtraForbid):
+    """The main class of the multi/single-stage model config scheme
+    (multi- stage models consists of interconnected single-stage
+    models).
+
+    @type config_version: str
+    @ivar config_version: Static variable representing the version of
+        the config scheme.
+    @type model: Model
+    @ivar model: A Model object representing the neural network used in
+        the archive.
+    """
+
+    config_version: CONFIG_VERSION = Field(
+        ...,
+        description="Static variable representing the version of the config scheme.",
+    )
+    model: Model = Field(
+        description="A Model object representing the neural network used in the archive."
+    )
