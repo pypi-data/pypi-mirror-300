533b7468da37f705f40e946266751025c2a385a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 16:07:06 +0000

    Add package catocli/1.0.1/catocli-1.0.1-py3-none-any.whl

---
+++ packages/catocli/catocli-1.0.1-py3-none-any.whl/build/lib/build/lib/cato/__main__.py
@@ -0,0 +1,11 @@
+import sys
+from .Utils import clidriver
+
+def main():
+	if sys.argv.__len__() == 1:
+		print('Usage: cato -h')
+	else:
+		sys.exit(clidriver.main(sys.argv[1:]))
+
+if __name__ == '__main__':
+	main()
