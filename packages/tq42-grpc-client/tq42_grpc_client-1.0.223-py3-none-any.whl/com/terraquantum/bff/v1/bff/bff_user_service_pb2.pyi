3c912cf3c329742f5f33fb25db5098e54c75feb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 17:08:39 +0000

    Add package tq42-grpc-client/1.0.223/tq42_grpc_client-1.0.223-py3-none-any.whl

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.223-py3-none-any.whl/com/terraquantum/bff/v1/bff/bff_user_service_pb2.pyi
@@ -0,0 +1,5 @@
+from com.terraquantum.bff.v1.bff import list_users_pb2 as _list_users_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
