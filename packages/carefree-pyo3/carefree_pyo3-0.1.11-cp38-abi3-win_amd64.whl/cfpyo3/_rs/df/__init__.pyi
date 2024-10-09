d653c6caa2f0b5efedbd5b5a358cb502b0a0ce14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 17:08:51 +0000

    Add package carefree-pyo3/0.1.11/carefree_pyo3-0.1.11-cp38-abi3-win_amd64.whl

---
+++ packages/carefree-pyo3/carefree_pyo3-0.1.11-cp38-abi3-win_amd64.whl/cfpyo3/_rs/df/__init__.pyi
@@ -0,0 +1,39 @@
+import numpy as np
+
+from typing import Tuple
+from typing import Generic
+from typing import TypeVar
+
+COLUMNS_NBYTES: int
+
+TDF = TypeVar("TDF")
+
+class IOs(Generic[TDF]):
+    def save(self, path: str) -> None: ...
+    @staticmethod
+    def load(path: str) -> TDF: ...
+
+class Ops:
+    def mean_axis1(self) -> np.ndarray: ...
+    def corr_with_axis1(self, other: np.ndarray) -> np.ndarray: ...
+
+class DataFrameF64(IOs[DataFrameF64], Ops):
+    @staticmethod
+    def new(
+        index: np.ndarray,
+        columns: np.ndarray,
+        values: np.ndarray,
+    ) -> DataFrameF64: ...
+    @property
+    def index(self) -> np.ndarray: ...
+    @property
+    def columns(self) -> np.ndarray: ...
+    @property
+    def values(self) -> np.ndarray: ...
+    @property
+    def shape(self) -> Tuple[int, int]: ...
+    def with_data(self, values: np.ndarray) -> DataFrameF64: ...
+    def to_owned(self) -> OwnedDataFrameF64: ...
+
+class OwnedDataFrameF64(IOs[OwnedDataFrameF64], Ops):
+    def to_py(self) -> DataFrameF64: ...
