7ac259eba89ebf81dbf5a81bcb3a0c15471d991c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:05:16 +0000

    Add package rmediator/0.2.2/rmediator-0.2.2-py3-none-any.whl

---
+++ packages/rmediator/rmediator-0.2.2-py3-none-any.whl/rmediator/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.2.2"
+
+from rmediator.mediator import Mediator
+
+__all__ = [
+    "Mediator",
+]
---
+++ packages/rmediator/rmediator-0.2.2-py3-none-any.whl/rmediator/decorators/request.py
@@ -0,0 +1,39 @@
+from types import UnionType
+from typing import Annotated, Union
+
+from typing_extensions import Doc
+
+
+def request(
+    response_type: Annotated[
+        Union[type, UnionType, None],
+        Doc(
+            "The type of the response expected from the request, or None if no response is expected."
+        ),
+    ]
+):
+    """
+    Decorate a class to register a request class with a specified response type.
+
+    Args:
+        response_type (Annotated[Union[type, None], Doc]): The type of the response expected from the request, or None if no response is expected.
+
+    Returns:
+        decorator: The decorator function.
+
+    Example:
+    ```python
+    @request(ResponseType)
+    class MyRequest:
+        # Define the request class
+        pass
+    ```
+    """
+
+    def decorator(cls: type):
+        cls._response = response_type
+        cls._rmediator_decorated_request = True
+
+        return cls
+
+    return decorator
