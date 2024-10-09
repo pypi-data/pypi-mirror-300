6d1b72ce69ffc1e276cb83e77e890c77c388b4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:07:14 +0000

    Add package pokazowka/1.1.1/pokazowka-1.1.1.tar.gz

---
+++ packages/pokazowka/pokazowka-1.1.1.tar.gz/pokazowka-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="pokazowka",
+    version="1.1.1",
+    author="gmalek",
+    description="Pakiet testowy",
+    url="http://example.com",
+    install_requires=[],
+    packages=['test_package'],
+
+)
