f838c3495d252f7da4774af6ca184c5631c10c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:09 +0000

    Add package flexpipe/0.1.0/flexpipe-0.1.0.tar.gz

---
+++ packages/flexpipe/flexpipe-0.1.0.tar.gz/flexpipe-0.1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flexpipe",
+    version="0.1.000",
+    author="Gaëtan Desrues",
+    description="Flexible Pipeline Processing Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
