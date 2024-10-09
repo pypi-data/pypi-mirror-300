2c2457980b1074e6069ad292221dcdcc02b7fb1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 16:07:12 +0000

    Add package moneysnake/0.0.4/moneysnake-0.0.4.tar.gz

---
+++ packages/moneysnake/moneysnake-0.0.4.tar.gz/moneysnake-0.0.4/src/moneysnake/__init__.py
@@ -0,0 +1,20 @@
+from client import (
+    MB_URL,
+    MB_VERSION_ID,
+    post_request,
+    set_admin_id,
+    set_timeout,
+    set_token,
+)
+from contact import Contact, ContactPerson
+
+__all__ = [
+    "MB_URL",
+    "MB_VERSION_ID",
+    "set_admin_id",
+    "set_token",
+    "set_timeout",
+    "post_request",
+    "Contact",
+    "ContactPerson",
+]
