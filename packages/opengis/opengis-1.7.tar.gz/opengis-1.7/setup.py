70109939cb69a5f8ae9456c586c0ad810d23240c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:39 +0000

    Add package opengis/1.7/opengis-1.7.tar.gz

---
+++ packages/opengis/opengis-1.7.tar.gz/opengis-1.7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="opengis",
+      version="1.7",
+      description="gis tools",
+      packages=["opengis"],
+      author="zijie wang",
+      author_email="773598627@qq.com",
+      install_requires=["numpy"])
