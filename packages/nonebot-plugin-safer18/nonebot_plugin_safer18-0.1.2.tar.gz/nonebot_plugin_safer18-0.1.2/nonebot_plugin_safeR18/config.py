008ff9fc0948940b15ecb86d7aa0a00c6c5b0247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 14:04:58 +0000

    Add package nonebot-plugin-safer18/0.1.2/nonebot_plugin_safer18-0.1.2.tar.gz

---
+++ packages/nonebot-plugin-safer18/nonebot_plugin_safer18-0.1.2.tar.gz/nonebot_plugin_safer18-0.1.2/nonebot_plugin_safeR18/config.py
@@ -0,0 +1,14 @@
+from nonebot import get_plugin_config, get_driver
+from pydantic import BaseModel
+
+
+class ScopedConfig(BaseModel):
+    save_path: str = "./data/safeR18"
+
+
+class Config(BaseModel):
+    safer18: ScopedConfig = ScopedConfig()
+
+
+global_config = get_driver().config
+plugin_config = get_plugin_config(Config).safer18
