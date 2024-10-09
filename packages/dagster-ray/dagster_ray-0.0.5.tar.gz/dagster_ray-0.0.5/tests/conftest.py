b12724b30e15a5e2aa319f55801430541ecb88d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 03:09:28 +0000

    Add package dagster-ray/0.0.5/dagster_ray-0.0.5.tar.gz

---
+++ packages/dagster-ray/dagster_ray-0.0.5.tar.gz/dagster_ray-0.0.5/tests/conftest.py
@@ -0,0 +1,24 @@
+import logging
+from typing import Iterator
+
+import pytest
+from _pytest.tmpdir import TempPathFactory
+from dagster import DagsterInstance
+
+logging.getLogger("alembic").setLevel(logging.WARNING)
+
+
+@pytest.fixture
+def dagster_instance(tmp_path_factory: TempPathFactory) -> DagsterInstance:
+    return DagsterInstance.ephemeral(tempdir=str(tmp_path_factory.mktemp("dagster_home")))
+
+
+@pytest.fixture
+def local_ray_address() -> Iterator[str]:
+    import ray
+
+    context = ray.init()
+
+    yield "auto"
+
+    context.disconnect()
