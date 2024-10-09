d35c0eda24112359b81409af370fd11ec94d2b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 17:08:48 +0000

    Add package udkm1Dsim/2.0.4/udkm1Dsim-2.0.4-py3-none-any.whl

---
+++ packages/udkm1Dsim/udkm1Dsim-2.0.4-py3-none-any.whl/udkm1Dsim/__init__.py
@@ -0,0 +1,18 @@
+from pint import UnitRegistry
+u = UnitRegistry()
+u.default_format = '~P'
+Q_ = u.Quantity
+from .structures.atoms import Atom, AtomMixed
+from .structures.layers import Layer, AmorphousLayer, UnitCell
+from .structures.structure import Structure
+from .simulations.simulation import Simulation
+from .simulations.heat import Heat
+from .simulations.phonons import Phonon, PhononNum, PhononAna
+from .simulations.magnetization import Magnetization, LLB
+from .simulations.xrays import Xray, XrayKin, XrayDyn, XrayDynMag
+
+__all__ = ['Atom', 'AtomMixed', 'Layer', 'AmorphousLayer', 'UnitCell', 'Structure',
+           'Simulation', 'Heat', 'Phonon', 'PhononNum', 'PhononAna', 'Magnetization', 'LLB',
+           'Xray', 'XrayKin', 'XrayDyn', 'XrayDynMag', 'u', 'Q_']
+
+__version__ = '2.0.4'
