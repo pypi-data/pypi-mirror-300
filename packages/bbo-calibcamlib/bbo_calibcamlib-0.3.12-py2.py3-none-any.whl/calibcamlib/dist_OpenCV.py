a888f673e249883336411d1b20c13d9596390401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:06:14 +0000

    Add package bbo-calibcamlib/0.3.12/bbo_calibcamlib-0.3.12-py2.py3-none-any.whl

---
+++ packages/bbo-calibcamlib/bbo_calibcamlib-0.3.12-py2.py3-none-any.whl/calibcamlib/dist_OpenCV.py
@@ -0,0 +1,70 @@
+# implements full 5 coefficient distortion from OpenCV 4
+
+import numpy as np
+
+def distort(boards_coords_ideal, ks):
+    r2 = np.sum(boards_coords_ideal[..., 0:2] ** 2, axis=-1, keepdims=True)
+    b = boards_coords_ideal
+
+    def distort_dim(b_d, k_rs, k_ps):
+        return (
+                b_d * (1 + k_rs[..., (0,)] * r2 + k_rs[..., (1,)] * r2 ** 2 + k_rs[..., (2,)] * r2 ** 3) +
+                2 * k_ps[0] * b[..., (0,)] * b[..., (1,)] +
+                k_ps[1] * (r2 + 2 * b_d ** 2)
+        )
+
+    boards_coords_dist = np.concatenate((
+        distort_dim(b[..., (0,)], ks[[0, 1, 4]], ks[[2, 3]]),
+        distort_dim(b[..., (1,)], ks[[0, 1, 4]], ks[[3, 2]]),
+    ), -1)
+
+    return boards_coords_dist
+
+
+def distort_inverse(ab_dist, k):
+    n = ab_dist.shape[0]
+
+    s = np.sqrt(np.sum(ab_dist ** 2, axis=1))
+    s_0_mask = s==0
+    if np.all(s_0_mask):
+        return ab_dist
+    r = np.zeros(n)
+
+
+    for u in np.where(s > 0)[0]:
+        rts = np.roots(np.array([k[4], 0, k[1], 0, k[0], 0, 1, -s[u]]))
+        rtsind = np.all([np.imag(rts) == 0, rts >= 0], axis=0)
+        if not np.any(rtsind):
+            r[u] = np.nan
+        else:
+            r[u] = np.min(np.real(rts[rtsind]))
+    ab = ab_dist * (r / s)[:, np.newaxis]
+    ab[s_0_mask] = ab_dist[s_0_mask] 
+
+    if np.all(k[2:4] == 0):
+        return ab
+    else:
+        # Warning: There is no closed-form solution for tangential + radial distortion. Thus, we use a solver from
+        #  scipy.optimize. If this function is used within a time-critical scope, this is probably slow
+        from scipy.optimize import fsolve
+
+        ab_ud = []
+        for p_o, p_d in zip(ab, ab_dist):
+            sol = fsolve(lambda p: dist_opt_func(p, p_d, k), p_o, full_output=True, maxfev=1000, xtol=1e-6)
+            # if not sol[2] == 1:
+            #     print(sol[3])
+            ab_ud.append(sol[0])
+
+        return np.array(ab_ud)
+
+
+def dist_opt_func(ab, ab_d, k):
+    # This function is included in the unit tests and tested correct
+    a = ab[0]
+    b = ab[1]
+    r2 = (a ** 2 + b ** 2)
+    rcoeff = 1 + k[0] * r2 + k[1] * r2 ** 2 + k[4] * r2 ** 3
+    return (
+        2 * k[2] * a * b + k[3] * (3 * a ** 2 + b ** 2) + a * rcoeff - ab_d[0],
+        2 * k[3] * a * b + k[2] * (3 * b ** 2 + a ** 2) + b * rcoeff - ab_d[1]
+    )
---
+++ packages/bbo-calibcamlib/bbo_calibcamlib-0.3.12-py2.py3-none-any.whl/calibcamlib/helper.py
@@ -0,0 +1,70 @@
+import numpy as np
+import scipy
+
+
+def get_line_dist(r1, e1, r2, e2):
+    n = np.cross(e1, e2)
+    return np.abs(np.sum(n * (r1 - r2)))
+
+
+def intersect(bases, vecs):
+    p = np.empty(3)
+    p[:] = np.nan
+
+    ray_ok = ~np.any([
+        np.isnan(bases),
+        np.isnan(vecs)
+    ], axis=(0, 2))
+
+    bases = bases[ray_ok]
+    vecs = vecs[ray_ok]
+
+    n = bases.shape[0]
+    if n < 2:
+        return p
+
+    M = np.empty((n, 3, 3))
+    Mbase = np.empty((n, 3, 1))
+
+    for u in range(n):
+        planebasis = scipy.linalg.null_space(vecs[np.newaxis, u])
+        M[u] = planebasis @ planebasis.T
+        Mbase[u] = M[u] @ bases[u, np.newaxis].T
+
+    if np.linalg.matrix_rank(np.sum(M, axis=0)) < 3:
+        return p
+
+    return np.squeeze(np.linalg.solve(np.sum(M, axis=0), np.sum(Mbase, axis=0)).T)
+
+
+def calc_3derr(X, P, V) -> (float, np.ndarray):
+    """Return the sum of the squared distances of points from lines
+    :param X: N_POINTSx3 array of points
+    :param P: N_LINESx3 array of points on the lines
+    :param V: N_LINESx3 array of line directions
+    :return: float of the sum of the squared distances
+    :return: (N_POINTS, N_LINES,) array of distances
+    """
+    dists = np.zeros(shape=(X.shape[0], P.shape[0]))
+
+    for i, p in enumerate(P):
+        dists[:, i] = calc_min_line_point_dist(X, p, V[i])
+
+    return np.nansum(dists ** 2), dists
+
+
+def calc_min_line_point_dist(x, p, v) -> np.ndarray:
+    """Return the minimum distances of points from a line
+    :param x: Nx3 array of points
+    :param p: (3,) array of a point on the line
+    :param v: (3,) array of the direction of the line
+    :return: (N,) array of distances
+    """
+
+    p = p[np.newaxis, :]
+    v = v[np.newaxis, :]
+    v /= np.linalg.norm(v, axis=1, keepdims=True)
+    d = x - p
+    proj_vecs = d - np.sum(d * v, axis=1, keepdims=True) * v
+    dists = np.linalg.norm(proj_vecs, axis=1)
+    return dists
