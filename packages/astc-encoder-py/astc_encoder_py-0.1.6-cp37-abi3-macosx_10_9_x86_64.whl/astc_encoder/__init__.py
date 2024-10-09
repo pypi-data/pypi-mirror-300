55fadec3cee2ccb761a4444889a1b6a59c65fc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:05:22 +0000

    Add package astc-encoder-py/0.1.6/astc_encoder_py-0.1.6-cp37-abi3-macosx_10_9_x86_64.whl

---
+++ packages/astc-encoder-py/astc_encoder_py-0.1.6-cp37-abi3-macosx_10_9_x86_64.whl/astc_encoder/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.1.6"
+
+from .enum import (
+    ASTCConfigFlags,
+    ASTCProfile,
+    ASTCQualityPreset,
+    ASTCSwizzleComponentSelector,
+    ASTCType,
+)
+
+from .encoder import (
+    ASTCConfig,
+    ASTCContext,
+    ASTCImage,
+    ASTCSwizzle,
+    ASTCError,
+)
