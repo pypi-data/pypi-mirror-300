08625a5e903b65217f5b341a5af14d363a9387b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 07:05:09 +0000

    Add package dlibex/1.12/dlibex-1.12.tar.gz

---
+++ packages/dlibex/dlibex-1.12.tar.gz/dlibex-1.12/setup.py
@@ -0,0 +1,21 @@
+# my_package/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='dlibex',
+    version='1.12',
+    packages=find_packages(),
+    description='This library is used for malware development. For educational purposes only!',
+    author='Anthony',
+    author_email='anthonyadams9821@gmail.com',
+    url='',
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
