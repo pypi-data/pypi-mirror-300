bff1a9479ce319e0d1d90784f878dfef9a076fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 05:05:35 +0000

    Add package vishnuthinks/0.1.1/vishnuthinks-0.1.1.tar.gz

---
+++ packages/vishnuthinks/vishnuthinks-0.1.1.tar.gz/vishnuthinks-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vishnuthinks",  
+    version="0.1.1",  
+    author="Vishnu",  
+    author_email="vishnurajalbandi18@gmail.com",  
+    description="A Python Quiz module for learning Python with random questions.",
+    long_description=open('README.md').read(),
+    
+    
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'vishnuthinks-quiz = vishnuthinks:run_quiz',  
+        ],
+    },
+)
