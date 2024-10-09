a78aebaa863e0d179ea7ba01ea0df67b58e00347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:05:40 +0000

    Add package slixmpp-omemo/1.1.1/slixmpp-omemo-1.1.1.tar.gz

---
+++ packages/slixmpp-omemo/slixmpp-omemo-1.1.1.tar.gz/slixmpp-omemo-1.1.1/slixmpp_omemo/version.py
@@ -0,0 +1,6 @@
+__all__ = [ "__version__" ]  # pylint: disable=unused-variable
+
+__version__ = {}
+__version__["short"] = "1.1.1"
+__version__["tag"] = "beta"
+__version__["full"] = f"{__version__['short']}-{__version__['tag']}"
