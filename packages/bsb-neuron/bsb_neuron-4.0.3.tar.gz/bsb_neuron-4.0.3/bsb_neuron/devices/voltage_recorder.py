24dcb1f83d03098e520088e0b04daca46461e90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 04:05:25 +0000

    Add package bsb-neuron/4.0.3/bsb_neuron-4.0.3.tar.gz

---
+++ packages/bsb-neuron/bsb_neuron-4.0.3.tar.gz/bsb_neuron-4.0.3/bsb_neuron/devices/voltage_recorder.py
@@ -0,0 +1,21 @@
+from bsb import LocationTargetting, config
+
+from ..device import NeuronDevice
+
+
+@config.node
+class VoltageRecorder(NeuronDevice, classmap_entry="voltage_recorder"):
+    locations = config.attr(type=LocationTargetting, default={"strategy": "soma"})
+
+    def implement(self, adapter, simulation, simdata):
+        for model, pop in self.targetting.get_targets(
+            adapter, simulation, simdata
+        ).items():
+            for target in pop:
+                for location in self.locations.get_locations(target):
+                    self._add_voltage_recorder(simdata.result, location)
+
+    def _add_voltage_recorder(self, results, location):
+        section = location.section
+        x = location.arc(0)
+        results.record(section(x)._ref_v)
