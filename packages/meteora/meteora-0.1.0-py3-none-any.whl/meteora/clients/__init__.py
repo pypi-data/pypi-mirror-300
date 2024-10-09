2753fa38847fcb1bb7692aa847266a36c70d9f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 06:05:55 +0000

    Add package meteora/0.1.0/meteora-0.1.0-py3-none-any.whl

---
+++ packages/meteora/meteora-0.1.0-py3-none-any.whl/meteora/clients/__init__.py
@@ -0,0 +1,7 @@
+"""Clients module."""
+
+from meteora.clients.aemet import AemetClient
+from meteora.clients.agrometeo import AgrometeoClient
+from meteora.clients.iem import ASOSOneMinIEMClient, METARASOSIEMClient
+from meteora.clients.meteocat import MeteocatClient
+from meteora.clients.metoffice import MetOfficeClient
