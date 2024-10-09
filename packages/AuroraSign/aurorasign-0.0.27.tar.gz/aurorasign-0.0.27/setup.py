b2b538c9eb9bf4a85561be3e86f3e61bb5486c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 15:05:32 +0000

    Add package AuroraSign/0.0.27/aurorasign-0.0.27.tar.gz

---
+++ packages/AuroraSign/aurorasign-0.0.27.tar.gz/aurorasign-0.0.27/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AuroraSign",
+    version="0.0.27",
+    packages=find_packages(),
+    install_requires=[
+        'asn1crypto',
+        'uritools'
+    ],
+)
