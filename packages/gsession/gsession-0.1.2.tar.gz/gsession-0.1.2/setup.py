26809f416151b34c2d109cc61ce78deff6e3e748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 16:06:22 +0000

    Add package gsession/0.1.2/gsession-0.1.2.tar.gz

---
+++ packages/gsession/gsession-0.1.2.tar.gz/gsession-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gsession',
+    version='0.1.2',
+    description='Un package pour gerer les session de 42',
+    author='guiguito',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'gsession = gsession.main:main',
+        ],
+    },
+)
