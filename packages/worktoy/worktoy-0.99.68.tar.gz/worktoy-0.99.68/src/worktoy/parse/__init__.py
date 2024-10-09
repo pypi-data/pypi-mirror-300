0f05509a2f62bfb21b8e338c8753df5ebe0e3055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-09 07:04:46 +0000

    Add package worktoy/0.99.68/worktoy-0.99.68.tar.gz

---
+++ packages/worktoy/worktoy-0.99.68.tar.gz/worktoy-0.99.68/src/worktoy/parse/__init__.py
@@ -0,0 +1,7 @@
+"""The 'worktoy.parse' package provides tools for parsing data."""
+#  AGPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._maybe import maybe
+from ._maybe_type import maybeType
