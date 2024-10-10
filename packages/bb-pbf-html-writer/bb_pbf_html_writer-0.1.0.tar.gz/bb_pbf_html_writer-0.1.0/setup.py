2ae1c0c176938feeec099405ed9df5d44700719a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 05:05:45 +0000

    Add package bb-pbf-html-writer/0.1.0/bb_pbf_html_writer-0.1.0.tar.gz

---
+++ packages/bb-pbf-html-writer/bb_pbf_html_writer-0.1.0.tar.gz/bb_pbf_html_writer-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+name='bb_pbf_html_writer',
+version='0.1.0',
+author='John Nastala',
+author_email='your.email@example.com',
+description='Writes BB Mega PBF HTML file and adds links to the HTML file.',
+# long_description=open("README.md").read()
+# long_description_content_type='text/plain',
+# url='https://github.com/yourusername/my_package',
+)
