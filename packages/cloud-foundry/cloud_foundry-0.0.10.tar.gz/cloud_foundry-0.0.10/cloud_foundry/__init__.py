0ff2d14587249f4d6d4ad4b884c3d9cd93ef254c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:11:20 +0000

    Add package cloud-foundry/0.0.10/cloud_foundry-0.0.10.tar.gz

---
+++ packages/cloud-foundry/cloud_foundry-0.0.10.tar.gz/cloud_foundry-0.0.10/cloud_foundry/__init__.py
@@ -0,0 +1,5 @@
+
+from cloud_foundry.pulumi.function import Function, import_function
+from cloud_foundry.pulumi.python_function import python_function
+from cloud_foundry.pulumi.rest_api import rest_api
+from cloud_foundry.utils.localstack import is_localstack_deployment
