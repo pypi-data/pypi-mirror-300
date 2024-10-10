d28eca396c5ae0c3f712aef6baf0f5d3f81ea2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 05:05:30 +0000

    Add package ndata/0.1.4/ndata-0.1.4.tar.gz

---
+++ packages/ndata/ndata-0.1.4.tar.gz/ndata-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Get the long description from the README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='ndata',  # Name of the package
+    version='0.1.4',
+    description='Automated Data Preprocessing Library',
+    long_description=long_description,  # Use the README as long description
+    long_description_content_type='text/markdown',  # Tell PyPI the format of the README
+    author='Leandre Nash',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'scipy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
