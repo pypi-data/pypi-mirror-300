a8f5d5c2bfbd8139f000869afc932578e8ff3f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:11:14 +0000

    Add package IQM-Vis/0.2.5.94/iqm_vis-0.2.5.94.tar.gz

---
+++ packages/IQM-Vis/iqm_vis-0.2.5.94.tar.gz/iqm_vis-0.2.5.94/IQM_Vis/examples/kodak.py
@@ -0,0 +1,33 @@
+''' KODAK dataset '''
+# Author: Matt Clifford <matt.clifford@bristol.ac.uk>
+# License: BSD 3-Clause License
+
+import os
+import glob
+import IQM_Vis
+
+def run():
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    dir = os.path.join(file_path,  'KODAK-dataset')
+    image_list = glob.glob(os.path.join(dir, '*'))
+    # remove and folders
+    image_list = [f for f in image_list if os.path.isfile(f)]
+    image_list.sort()
+
+    metrs = IQM_Vis.metrics.get_all_metrics()
+    if '1-MS_SSIM' in metrs:
+      metrs.pop('1-MS_SSIM')
+    data = IQM_Vis.dataset_holder(image_list,
+                                  metrs,
+                                #   IQM_Vis.metrics.get_all_metric_images()
+                                  )
+
+
+    IQM_Vis.make_UI(data,
+                    IQM_Vis.transforms.get_all_transforms(),
+                    restrict_options=3
+                    )
+    IQM_Vis.make_UI()
+
+if __name__ == '__main__':
+    run()
