6b53872c5091ae3e27f3c7ec042ed89f44575f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 06:06:13 +0000

    Add package cu-astr3510/0.0.2/cu_astr3510-0.0.2.tar.gz

---
+++ packages/cu-astr3510/cu_astr3510-0.0.2.tar.gz/cu_astr3510-0.0.2/astr3510/__init__.py
@@ -0,0 +1,6 @@
+from .imports import *
+from .bucket import *
+from .psfs.apertures import *
+from .gaia import *
+from .transit import *
+#from .loupe import *
---
+++ packages/cu-astr3510/cu_astr3510-0.0.2.tar.gz/cu_astr3510-0.0.2/astr3510/tests/test_bucket.py
@@ -0,0 +1,7 @@
+from astr3510 import *
+
+
+def test_bucket():
+    catch_photons_in_bucket()
+
+
