578934de860c1b814fe3885f5db7ca8d6c45f1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 06:05:50 +0000

    Add package bens-plotstyle/1.0/bens_plotstyle-1.0-py3-none-any.whl

---
+++ packages/bens-plotstyle/bens_plotstyle-1.0-py3-none-any.whl/bens_plotstyle/bens_plotstyle.py
@@ -0,0 +1,38 @@
+import matplotlib as mpl    
+import matplotlib.pyplot as plt
+import importlib
+import os
+
+def main():
+    font_dirs = [
+        "~/Library/Fonts/",  # macOS
+        "/usr/share/fonts/",  # Linux
+        "/usr/local/share/fonts/",  # Linux
+        "C:\\Windows\\Fonts\\"  # Windows
+    ]
+    for dir in font_dirs:
+        if os.path.exists(dir):
+            font_dirs = [dir]
+    font_files = mpl.font_manager.findSystemFonts(fontpaths=font_dirs)
+
+    for font_file in font_files:
+        mpl.font_manager.fontManager.addfont(font_file) 
+    try:    
+        mpl.font_manager.findfont('JuliaMono')
+        print("Found font: JuliaMono")
+    except:
+        print("Font 'JuliaMono' not found")
+    package_path = os.path.dirname(os.path.realpath(__file__))
+    # mpl_path = os.path.dirname(package_path)
+    
+    plt.style.use(style=os.path.join(package_path,'mplstyle_templates/pretty_plotting_default.mplstyle'))
+   
+
+def paper():
+    package_path = os.path.dirname(os.path.realpath(__file__))
+    # mpl_path = os.path.dirname(package_path)
+    # print(package_path)
+    plt.style.use(style=os.path.join(package_path,'mplstyle_templates/pretty_plotting_paper.mplstyle'))
+
+if __name__ == '__main__':
+    main()
