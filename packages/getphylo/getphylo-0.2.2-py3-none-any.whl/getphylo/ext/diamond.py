ae0d35160dc53e83c42b5fb0497432c135916f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 04:05:43 +0000

    Add package getphylo/0.2.2/getphylo-0.2.2-py3-none-any.whl

---
+++ packages/getphylo/getphylo-0.2.2-py3-none-any.whl/getphylo/ext/diamond.py
@@ -0,0 +1,56 @@
+'''
+Make DIAMOND databases and run searches using DIAMOND
+
+Functions:
+    make_diamond_database(filename: str, dmnd_database=None) -> None
+    run_diamond_search(filename: str, dmnd_database=None, outname=None) -> None
+'''
+import logging
+from getphylo.utils import io
+
+def make_diamond_database(filename: str, dmnd_database=None, diamond_location='diamond') -> None:
+    '''
+    Create a DIAMOND database from a fasta file.
+        Arguments:
+            filename: path to the input file
+            dmnd_database: path to output directory
+        Returns:
+            None
+    '''
+    if dmnd_database is None:
+        database = " --db " + filename.split('.')[0] + ".dmnd"
+    else:
+        database = " --db " + dmnd_database
+    makedb = " makedb"
+    input_ = " --in " + filename + ""
+    command = diamond_location + makedb + database + input_
+    logging.debug(command)
+    io.run_in_command_line(command)
+
+def run_diamond_search(
+    filename: str, dmnd_database=None, outname=None, diamond_location='diamond'
+    ) -> None:
+    '''
+    Run BLASTP through DIAMOND.
+        Arguments:
+            filename: path to the input fasta file
+            dmnd_database: name of a database file to read
+            outname: name of the output file
+        Returns:
+            None
+    '''
+    if dmnd_database is None:
+        database = " --db " + filename.split('.')[0] + ".dmnd"
+    else:
+        database = " --db " + dmnd_database
+    if outname is None:
+        output = " --out " + dmnd_database.split('.')[0] + ".tsv"
+    else:
+        output = " --out " + outname
+    diamond = diamond_location
+    blastp = " blastp"
+    query = " --query " + filename
+    outfmt = " --outfmt 6 qseqid sseqid pident"
+    command = diamond + blastp + database + query + output + outfmt
+    logging.debug(command)
+    io.run_in_command_line(command)
---
+++ packages/getphylo/getphylo-0.2.2-py3-none-any.whl/getphylo/ext/iqtree.py
@@ -0,0 +1,34 @@
+'''
+Run iqtree.
+
+Functions:
+    run_iqtree(alignment_path: str, partition_path: str, out_path: str) -> None
+
+'''
+from getphylo.utils import io
+
+def run_iqtree(
+    alignment_path: str, out_path: str, partition_path: str=None, iqtree_location: str='iqtree'
+    ) -> None:
+    '''
+    Run fasttree on a protein alignment.
+        Arguments:
+            alignment_path: path to the alignment
+            partition_path: path to the partition file
+            out_path: path to the output file
+        Returns:
+            None
+    '''
+    command = iqtree_location
+    alignment = " ".join(['-s', alignment_path])
+    out_path = " ".join(['-pre', out_path])
+    model = '-m MFP'
+    bootstraps = '-bb 1000'
+    #do a run with or without partition file
+    if partition_path is not None:
+        partition = " ".join(['-spp', partition_path])
+        io.run_in_command_line(
+            " ".join([command, alignment, partition, out_path, model, bootstraps])
+            )
+    else:
+        io.run_in_command_line(" ".join([command, alignment, out_path, model, bootstraps]))
