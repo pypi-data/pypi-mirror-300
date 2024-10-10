0b783c5acafc10d0a62b7589f5120b0eb51ded04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 04:05:43 +0000

    Add package getphylo/0.2.2/getphylo-0.2.2.tar.gz

---
+++ packages/getphylo/getphylo-0.2.2.tar.gz/getphylo-0.2.2/getphylo/ext/fasttree.py
@@ -0,0 +1,25 @@
+'''
+Run fasttree.
+
+Functions:
+    run_fasttree(filename, outfile=None) -> None
+
+'''
+from getphylo.utils import io
+
+def run_fasttree(filename, outfile=None, fasttree_location='fasttree') -> None:
+    '''
+    Run fasttree on a protein alignment.
+        Arguments:
+            filename: path to the alignment
+            outfile: path to the output file
+        Returns:
+            None
+    '''
+
+    if outfile is None:
+        out = " -out " + io.change_extension(filename, "tree") + " "
+    else:
+        out = " -out " + outfile + " "
+    command = fasttree_location
+    io.run_in_command_line(command + out + filename)
