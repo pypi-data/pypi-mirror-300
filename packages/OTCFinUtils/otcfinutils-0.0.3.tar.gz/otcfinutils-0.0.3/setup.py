f84068e1e7ee6653b6771ef5ba382ce85b6296ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 04:05:29 +0000

    Add package OTCFinUtils/0.0.3/otcfinutils-0.0.3.tar.gz

---
+++ packages/OTCFinUtils/otcfinutils-0.0.3.tar.gz/otcfinutils-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='OTCFinUtils',
+      version='0.0.3',
+      description='usefule functions to iteract with dataverse and sharepoint',
+      packages=['OTCFinUtils'],
+      author_email='saldujaily@otcfin.com',
+      zip_safe=False,
+)
