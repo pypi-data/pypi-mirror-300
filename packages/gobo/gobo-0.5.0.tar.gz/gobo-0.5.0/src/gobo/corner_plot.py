e0ccc987f0bedb20ebee4c7586b539d5895050f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 06:06:17 +0000

    Add package gobo/0.5.0/gobo-0.5.0.tar.gz

---
+++ packages/gobo/gobo-0.5.0.tar.gz/gobo-0.5.0/src/gobo/corner_plot.py
@@ -0,0 +1,9 @@
+from gobo.internal.corner_plot import create_multi_distribution_corner_plot, \
+    create_multi_distribution_1d_histogram_credible_interval_figure, \
+    create_multi_distribution_2d_histogram_credible_interval_contour_figure
+
+__all__ = [
+    'create_multi_distribution_corner_plot',
+    'create_multi_distribution_1d_histogram_credible_interval_figure',
+    'create_multi_distribution_2d_histogram_credible_interval_contour_figure',
+]
