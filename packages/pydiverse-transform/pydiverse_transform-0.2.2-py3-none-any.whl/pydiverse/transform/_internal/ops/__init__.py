2b18422373f696392afb705d9c2f22a171c13634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:11:19 +0000

    Add package pydiverse-transform/0.2.2/pydiverse_transform-0.2.2-py3-none-any.whl

---
+++ packages/pydiverse-transform/pydiverse_transform-0.2.2-py3-none-any.whl/pydiverse/transform/_internal/ops/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from .aggregate import *
+from .core import *
+from .datetime import *
+from .generic import *
+from .logical import *
+from .markers import *
+from .numeric import *
+from .string import *
+from .window import *
