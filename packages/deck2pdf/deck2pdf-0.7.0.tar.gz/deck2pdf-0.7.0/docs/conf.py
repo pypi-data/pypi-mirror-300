4ed3b70a20d478e14416db223ef1c63fe8f80648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 06:05:56 +0000

    Add package deck2pdf/0.7.0/deck2pdf-0.7.0.tar.gz

---
+++ packages/deck2pdf/deck2pdf-0.7.0.tar.gz/deck2pdf-0.7.0/docs/conf.py
@@ -0,0 +1,26 @@
+import os
+
+from deck2pdf import __version__ as version
+
+# -- Project information
+project = "deck2pdf"
+copyright = "2024, Kazuya Takei"
+author = "Kazuya Takei"
+release = version
+
+# -- General configuration
+extensions = [
+    "sphinx.ext.todo",
+]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for i18n
+locale_dirs = ["_locales"]
+gettext_compact = False
+gettext_language_team = "Kazuya Takei <myself@attakei.net>"
+gettext_last_translator = os.environ.get("SPHINXINTL_TRANSLATOR", None)
+
+# -- Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
