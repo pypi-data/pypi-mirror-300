f5a789dead494e94ec19ef4a43b1fba332d671ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:10:46 +0000

    Add package numericalpoissongeometry/1.1.2/numericalpoissongeometry-1.1.2.tar.gz

---
+++ packages/numericalpoissongeometry/numericalpoissongeometry-1.1.2.tar.gz/numericalpoissongeometry-1.1.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='numericalpoissongeometry',
+    version='1.1.2',
+    author="Miguel Evangelista-Alvarado, Jose C. Crispín Ruíz, Pablo Suárez-Serrato",
+    author_email="miguel.eva.alv@gmail.com, jcpanta@im.unam.mx, pablo@im.unam.mx",
+    license="MIT",
+    description="A Python Numeric module for (local) calculus on Poisson manifolds",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/appliedgeometry/NumericalPoissonGeometry",
+    packages=setuptools.find_packages(),
+    install_requires=['sympy', 'numpy', 'poissongeometry', 'permutation', 'scipy'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Intended Audience :: Science/Research",
+    ],
+    python_requires='>=3.6, !=2.*',
+)
