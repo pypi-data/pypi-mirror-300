9b94da66c6ac36b1e09663f6c776880c755eeff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:11:11 +0000

    Add package di-service-ksu/0.0.3/di_service_ksu-0.0.3-py3-none-any.whl

---
+++ packages/di-service-ksu/di_service_ksu-0.0.3-py3-none-any.whl/di_service_ksu/Abstractions/meta_inject.py
@@ -0,0 +1,22 @@
+#        _       __        __    __________    _____     __   __     __
+#   	/ \	    |  |	  |__|  |__________|  |  _  \   |__|  \ \   / /
+#      / _ \	|  |	   __       |  |      | |_)  |   __    \ \_/ /   Alitrix - Modern NLP
+#     / /_\ \	|  |	  |  |      |  |      |  _  /   |  |    } _ {    Languages: Python, C#
+#    / _____ \	|  |____  |  |      |  |      | | \ \   |  |   / / \ \   http://github.com/Alitrix
+#   /_/     \_\	|_______| |__|	    |__|      |_|  \_\  |__|  /_/   \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020-2021 The Alitrix Authors <http://github.com/Alitrix>
+
+from ..Utilites.ext_reflection import ExtReflection
+
+class MetaInject(type):
+    def __init__(cls, name, bases, ns):
+        if '__init__' in ns:
+            for attr, value in cls.__dict__.items():
+                if callable(value):
+                    if attr == '__init__':
+                        setattr(cls, attr, ExtReflection.init_inject(value))
+
+        super().__init__(name, bases, ns)
---
+++ packages/di-service-ksu/di_service_ksu-0.0.3-py3-none-any.whl/di_service_ksu/Utilites/__init__.py
@@ -0,0 +1,1 @@
+from .ext_reflection import ExtReflection
