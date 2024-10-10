71867431c63194b1bb87232b486d665a584e7eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 04:05:51 +0000

    Add package mixpeek/0.11.1/mixpeek-0.11.1-py3-none-any.whl

---
+++ packages/mixpeek/mixpeek-0.11.1-py3-none-any.whl/mixpeek/endpoints/embed.py
@@ -0,0 +1,66 @@
+import requests
+#test
+class Embed:
+    def __init__(self, base_url, headers):
+        self.base_url = base_url
+        self.headers = headers
+
+    def video(self, model_id: str, input: str, input_type: str):
+        try:
+            url = f"{self.base_url}embed/"
+            data = {
+                "modality": "video",
+                "model_id": model_id,
+                "input": input,
+                "input_type": input_type
+            }
+            response = requests.post(url, json=data, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            return {"error": str(e)}
+
+    def text(self, model_id: str, input: str, input_type: str):
+        try:
+            url = f"{self.base_url}embed/"
+            data = {
+                "modality": "text",
+                "model_id": model_id,
+                "input": input,
+                "input_type": input_type
+            }
+            response = requests.post(url, json=data, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            return {"error": str(e)}
+
+    def image(self, model_id: str, input: str, input_type: str):
+        try:
+            url = f"{self.base_url}embed/"
+            data = {
+                "modality": "image",
+                "model_id": model_id,
+                "input": input,
+                "input_type": input_type
+            }
+            response = requests.post(url, json=data, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            return {"error": str(e)}
+
+    def audio(self, model_id: str, input: str, input_type: str):
+        try:
+            url = f"{self.base_url}embed/"
+            data = {
+                "modality": "audio",
+                "model_id": model_id,
+                "input": input,
+                "input_type": input_type
+            }
+            response = requests.post(url, json=data, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            return {"error": str(e)}
