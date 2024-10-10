8bb5f70eff67118770d8aadbc669352c8e0ba1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:11:21 +0000

    Add package myCalculatorfr/4.0/mycalculatorfr-4.0.tar.gz

---
+++ packages/myCalculatorfr/mycalculatorfr-4.0.tar.gz/mycalculatorfr-4.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+name='myCalculatorfr',
+    version='4.0',
+    author='vasanth',
+    description='A simple calculator package',
+    packages=find_packages()
+)
+    
