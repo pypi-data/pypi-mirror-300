55f4cbfd58532a43dae6340e79eab0787af56dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 04:05:04 +0000

    Add package Annotations2Sub/2.21.0/Annotations2Sub-2.21.0-py3-none-any.whl

---
+++ packages/Annotations2Sub/Annotations2Sub-2.21.0-py3-none-any.whl/Annotations2Sub/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" 程序入口 """
+
+if not __package__:
+    import os
+    import sys
+
+    path = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])
+    sys.path.append(path)
+    path = os.path.abspath(os.path.join(path, os.pardir))
+    sys.path.append(path)
+
+import sys
+from typing import NoReturn
+
+from Annotations2Sub.cli import Run
+
+
+def main() -> NoReturn:
+    sys.exit(Run())
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/Annotations2Sub/Annotations2Sub-2.21.0-py3-none-any.whl/Annotations2Sub/utils.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import urllib.request
+
+from Annotations2Sub._flags import Flags
+from Annotations2Sub.i18n import _
+
+
+def YellowText(string: str) -> str:
+    """返回黄色文本"""
+    return f"\033[33m{string}\033[0m"
+
+
+def RedText(string: str) -> str:
+    """返回红色文本"""
+    return f"\033[31m{string}\033[0m"
+
+
+def Stderr(string: str):
+    """打印到标准错误"""
+    print(string, file=sys.stderr)
+
+
+def Err(string: str):
+    Stderr(RedText(_("错误: ") + string))
+
+
+def Warn(string: str):
+    Stderr(YellowText(_("警告: ") + string))
+
+
+def Info(string: str):
+    if Flags.verbose:
+        Stderr(string)
+
+
+def GetUrl(url: str) -> str:
+    if not url.startswith("http"):
+        raise ValueError(_('"url" 必须是 http(s)'))
+    with urllib.request.urlopen(url) as r:
+        return r.read().decode("utf-8")
