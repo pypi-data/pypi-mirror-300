81547f5510ff2523fc1fc9db0d7f8afcd88b8a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 04:05:30 +0000

    Add package rabity/0.6.0/rabity-0.6.0.tar.gz

---
+++ packages/rabity/rabity-0.6.0.tar.gz/rabity-0.6.0/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+import setuptools
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_descriptions = fh.read()
+
+setup(
+    name="rabity",  # Name as seen on PyPI
+    version="0.6.0",  # Increment this version for updates
+    description="A library for a server",
+    long_description=long_descriptions,  # Combine README and CHANGELOG
+    author="Parsa",
+    author_email="mehr2.business@example.com",  # Optional
+    packages=find_packages(),  # Automatically find the RunwayLib packag
+    install_requires=["flask"],  # List any dependencies here
+    classifiers=[  # Optional, to specify the package's audience
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Specify minimum Python version
+)
