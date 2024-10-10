3847922022b68ae304c12b59006e0bfe1bf1bd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 04:05:29 +0000

    Add package richqueue/0.7/richqueue-0.7.tar.gz

---
+++ packages/richqueue/richqueue-0.7.tar.gz/richqueue-0.7/richqueue/main.py
@@ -0,0 +1,90 @@
+from typer import Typer
+from .layout import dual_layout
+from rich.live import Live
+import time
+from .console import console
+from .tools import curry
+from .slurm import get_layout_pair, get_node_layout, get_hist_layout
+
+# set up singletons
+app = Typer()
+
+# main CLI command
+
+
+@app.command()
+def show(
+    user: None | str = None,
+    long: bool = False,
+    idle: bool = False,
+    loop: bool = True,
+    hist: int | None = None,
+    hist_unit: str = "weeks",
+    # screen: bool = True,
+    # disappear: bool = True,
+):
+
+    screen = True
+    disappear = not screen
+
+    kwargs = {
+        "user": user,
+        "long": long,
+        "idle": idle,
+        "loop": loop,
+        "hist": hist,
+        "hist_unit": hist_unit,
+        "screen": screen,
+        "disappear": disappear,
+    }
+
+    # console.print(kwargs)
+
+    match (bool(idle), bool(hist)):
+        case (True, False):
+            loop = False
+            layout_func = get_node_layout
+        case (False, True):
+            loop = False
+            layout_func = get_hist_layout
+        case (False, False):
+            layout_func = curry(dual_layout, get_layout_pair)
+        case _:
+            raise Exception("Unsupported CLI options")
+
+    # live updating layout
+    if loop:
+
+        layout = layout_func(**kwargs)
+
+        with Live(
+            layout,
+            refresh_per_second=4,
+            screen=screen,
+            transient=disappear,
+            vertical_overflow="visible",
+        ) as live:
+
+            try:
+                while True:
+                    layout = layout_func(**kwargs)
+                    live.update(layout)
+                    time.sleep(1)
+            except KeyboardInterrupt:
+                live.stop()
+
+    # static layout
+    else:
+
+        layout = layout_func(**kwargs)
+        console.print(layout)
+
+
+# start Typer app
+def main():
+    app()
+
+
+# start Typer app
+if __name__ == "__main__":
+    app()
---
+++ packages/richqueue/richqueue-0.7.tar.gz/richqueue-0.7/richqueue/tools.py
@@ -0,0 +1,65 @@
+import datetime
+
+
+def curry(function, payload):
+    def f(*args, **kwargs):
+        return function(payload, *args, **kwargs)
+
+    return f
+
+
+def human_timedelta(delta):
+
+    bits = []
+
+    days = delta.days
+
+    s = delta.seconds
+
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
+
+    if days:
+        bits.append(f"{days}d")
+
+    if h:
+        bits.append(f"{h}h")
+
+    if m:
+        bits.append(f"{m}m")
+
+    if s:
+        bits.append(f"{s}s")
+
+    bits = (str(b) for b in bits)
+
+    return " ".join(bits)
+
+
+def human_datetime(dt):
+
+    now = datetime.datetime.now()
+
+    bits = []
+
+    bits.append(dt.strftime("%b"))
+
+    day = dt.day
+
+    match day % 10:
+        case 1:
+            suffix = "st"
+        case 2:
+            suffix = "nd"
+        case 3:
+            suffix = "rd"
+        case _:
+            suffix = "th"
+
+    bits.append(f"{day}{suffix}")
+
+    bits.append(dt.strftime("%H:%M"))
+
+    bits = (str(b) for b in bits)
+
+    return " ".join(bits)
